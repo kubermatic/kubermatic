5f2069526c3b0bdf4d4ee4bb4ce43c6616c2505d
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2016-03-06 21:32:45 +0100

    controller: pending services adds hostport of etcd

--- cmd/kubermatic-cluster-controller/main.go
+++ cmd/kubermatic-cluster-controller/main.go
@@ -19,8 +19,9 @@ func main() {
 	homeDir := os.Getenv("HOME")
 	kubeconfig := flag.String("kubeconfig", path.Join(homeDir, ".kube/config"), "The kubeconfig file with a current context.")
 	masterResources := flag.String("master-resources", "", "The master resources path (required).")
-	urlPattern := flag.String("url-pattern", "https://%s.%s.kubermatic.io", "The fmt.Sprintf pattern for the url, interpolated with the cluster name and the dc.")
+	urlPattern := flag.String("url-pattern", "%s.%s.kubermatic.io", "The fmt.Sprintf pattern for the url, interpolated with the cluster name and the dc.")
 	dcFile := flag.String("datacenters", "datacenters.yaml", "The datacenters.yaml file path")
+
 	flag.Parse()
 
 	if *masterResources == "" {
