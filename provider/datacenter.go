dd8ec338914fb8bb153311383e8c26225b91c9bd
realfake <luk.burchard@gmail.com>
2016-11-24 23:17:59 +0100

    Replaced datacenter structs, with structs from github.com/kubermatic/api/types.go

--- provider/datacenter.go
+++ provider/datacenter.go
@@ -5,70 +5,63 @@ import (
 	"hash/fnv"
 	"io/ioutil"
 
-	"github.com/kubermatic/api/provider/drivers/flag"
+	"github.com/kubermatic/api"
+
+	"encoding/base64"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
+// Datacenter holds information of one Datacenter.
+type Datacenter struct {
+	ID        string `yaml:"-"`
+	KubeletID string `yaml:"kubelet-id"`
+
+	Metainfo api.DatacenterSpec `yaml:"metainfo"`
+	Network  api.NetworkSpec    `yaml:"network"`
+	// BUG: Where is this used ?
+	ApiserverSSH struct {
+		Private string `yaml:"private"`
+		Public  string `yaml:"public"`
+	} `yaml:"apiserverSSH"`
+}
+
 // Datacenters describes all running datacenters.
 type Datacenters struct {
 	// Provider are all seed datacenters. Each key is the name of a datacenter provider.
-	Provider map[string]struct {
-		ProviderType string `yaml:"-"` // e.g "aws", "digitalocean"
-
-		// Metainformation
-		Location string `yaml:"location"`
-		Country  string `yaml:"country"`
-		Private  bool   `yaml:"private"`
-
-		// Routing information
-		Region    string `yaml:"region,omitempty"`
-		Zone      string `yaml:"zone,omitempty"`
-		Exactname string `yaml:"exact-name"`
-
-		CustomerPatches []flag.Flags `yaml:"customer-patches,omitempty"` // Patches that are applied to customer clusters.
-		SeedPatches     []flag.Flags `yaml:"seed-patches,omitempty"`     // Patches that are applied to seed clusters.
-
-		Network struct {
-			Flannel struct {
-				CIDR string `yaml:"cidr"`
-			} `yaml:"flannel"`
-		} `yaml:"network"`
-
-		ApiserverSSH struct {
-			Private string `yaml:"private"`
-			Public  string `yaml:"public"`
-		} `yaml:"apiserverSSH"`
-
-		UniqueID string `yaml:"-"`
-	}
+	Drivers map[string][]Datacenter `yaml:"driver"`
 }
 
+// init creates the uid for each DC which is used by the computer.
+// It also sets the Driver type from the group it was placed in.
 func (d *Datacenters) init() error {
-	var buf *bytes.Buffer
+	var bufsum *bytes.Buffer
 	var err error
 	hash := fnv.New32a()
-	for name, provider := range d.Provider {
-		provider.ProviderType = name
-		_, err = buf.WriteString(provider.Exactname)
-		if err != nil {
-			return err
+	for name, providers := range d.Drivers {
+		for _, provider := range providers {
+			provider.DriverType = name
+			_, err = bufsum.WriteString(provider.Metainfo.ExactName)
+			if err != nil {
+				return err
+			}
+			_, err = bufsum.WriteString(provider.Metainfo.Location)
+			if err != nil {
+				return err
+			}
+			_, err = bufsum.WriteString(provider.Metainfo.Country)
+			if err != nil {
+				return err
+			}
+			_, err = bufsum.WriteString(provider.Metainfo.DriverType)
+			if err != nil {
+				return err
+			}
+
+			provider.ID = base64.URLEncoding.EncodeToString(hash.Sum(bufsum.Bytes()))
+			bufsum.Reset()
+			hash.Reset()
 		}
-		_, err = buf.WriteString(provider.Location)
-		if err != nil {
-			return err
-		}
-		_, err = buf.WriteString(provider.Country)
-		if err != nil {
-			return err
-		}
-		_, err = buf.WriteString(provider.ProviderType)
-		if err != nil {
-			return err
-		}
-		provider.UniqueID = string(hash.Sum(buf.Bytes()))
-		buf.Reset()
-		hash.Reset()
 	}
 	return nil
 }
