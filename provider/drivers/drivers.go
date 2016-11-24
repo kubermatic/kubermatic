c1a719f8b8c7b84c2f3b12c2c88d2388313affee
realfake <luk.burchard@gmail.com>
2016-11-24 23:37:16 +0100

    Added creation of empty driver

--- provider/drivers/drivers.go
+++ provider/drivers/drivers.go
@@ -33,13 +33,22 @@ var AvaliableDrivers = Drivers{
 	},
 }
 
-// CreateHost creates a new Driver with a given name.
-func (d Drivers) CreateHost(drivername string, clustername string, flags flag.Flags) (Driver, error) {
+// CreateDriver creates a new Driver with a given name.
+func (d Drivers) CreateDriver(drivername string, clustername string, flags flag.Flags) (Driver, error) {
+	p, err := d.GetEmptyDriver(drivername, clustername)
+	if err != nil {
+		return nil, err
+	}
+	p.SetConfigFromFlags(flags)
+	return p, nil
+}
+
+// GetEmptyDriver return a Driver with the underlying instanciated driver.
+func (d Drivers) GetEmptyDriver(drivername string, clustername string) (Driver, error) {
 	nd, ok := d[drivername]
 	if !ok {
 		return nil, fmt.Errorf("Couldn't find the driver %q", drivername)
 	}
 	p := nd(clustername, clustername)
-	p.SetConfigFromFlags(flags)
 	return p, nil
 }
