d028354ad4c4db4ac70ee0da727bff8b4337e2ac
realfake <luk.burchard@gmail.com>
2016-12-14 14:12:48 +0100

    FIX: Move aws template creation into loop

--- provider/cloud/aws/provider.go
+++ provider/cloud/aws/provider.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/golang/glog"
 	ktemplate "github.com/kubermatic/api/template"
 	"golang.org/x/net/context"
 
@@ -53,22 +52,13 @@ var (
 
 type aws struct {
 	datacenters map[string]provider.DatacenterMeta
-	tpl         *template.Template
 }
 
 // NewCloudProvider returns a new aws provider.
 func NewCloudProvider(datacenters map[string]provider.DatacenterMeta) provider.CloudProvider {
-	p := &aws{
+	return &aws{
 		datacenters: datacenters,
 	}
-
-	parsed, err := template.New("cloud-config-node.yaml").Funcs(ktemplate.FuncMap).ParseFiles(tplPath)
-	if err != nil {
-		glog.Errorln("template not found:", err)
-	}
-	p.tpl = parsed
-
-	return p
 }
 
 func setupVPC(svc *ec2.EC2, cluster *api.Cluster) (string, error) {
@@ -165,10 +155,14 @@ func (*aws) UnmarshalCloudSpec(annotations map[string]string) (*api.CloudSpec, e
 	return spec, nil
 }
 
-func (a *aws) userData(buf *bytes.Buffer, instanceName string, node *api.NodeSpec, clusterState *api.Cluster, dc provider.DatacenterMeta, key *api.KeyCert) error {
-	if a.tpl == nil {
-		return errors.New("No AWS template was found")
-	}
+func (a *aws) userData(
+	buf *bytes.Buffer,
+	instanceName string,
+	node *api.NodeSpec,
+	clusterState *api.Cluster,
+	dc provider.DatacenterMeta,
+	key *api.KeyCert,
+) error {
 	data := ktemplate.Data{
 		DC:                node.DatacenterName,
 		ClusterName:       clusterState.Metadata.Name,
@@ -184,7 +178,15 @@ func (a *aws) userData(buf *bytes.Buffer, instanceName string, node *api.NodeSpe
 		ApiserverToken:    clusterState.Address.Token,
 		FlannelCIDR:       clusterState.Spec.Cloud.Network.Flannel.CIDR,
 	}
-	return a.tpl.Execute(buf, data)
+
+	tpl, err := template.
+		New("cloud-config-node.yaml").
+		Funcs(ktemplate.FuncMap).
+		ParseFiles(tplPath)
+	if err != nil {
+		return err
+	}
+	return tpl.Execute(buf, data)
 }
 
 func (a *aws) CreateNodes(ctx context.Context, cluster *api.Cluster, node *api.NodeSpec, num int) ([]*api.Node, error) {
