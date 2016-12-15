eff0c45a668f80e7af7daa03d6b2244cca6e1bb7
Jason Murray <jason@loodse.com>
2016-12-15 18:38:54 +0100

    Fix instance tag matching for nodes

--- provider/cloud/aws/provider.go
+++ provider/cloud/aws/provider.go
@@ -373,14 +373,14 @@ func (a *aws) Nodes(ctx context.Context, cluster *api.Cluster) ([]*api.Node, err
 
 	nodes := make([]*api.Node, 0, len(resp.Reservations))
 	for _, n := range resp.Reservations {
-		glog.Infof("%d instances in reservation %s\n", len(n.Instances), n.ReservationId)
+		glog.Infof("%d instances in reservation %s\n", len(n.Instances), *n.ReservationId)
 		for _, instance := range n.Instances {
 			var isOwner bool
 			var name string
 			glog.Infof("Instance Tags\n")
 			for _, tag := range instance.Tags {
 				glog.Infof("Tag: %s = %s\n", *tag.Key, *tag.Value)
-				if *tag.Key == defaultKubermaticClusterIDTagKey && *tag.Value == cluster.Metadata.Name {
+				if *tag.Key == defaultKubermaticClusterIDTagKey && *tag.Value == cluster.Metadata.UID {
 					glog.Infof("isOwner of instance %s\n", instance.String())
 					isOwner = true
 				}
