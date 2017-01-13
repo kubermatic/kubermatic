9988c826bfcc5b9c410ef35b1e2a1209fb217aa8
Jason Murray <jason@loodse.com>
2017-01-13 19:23:31 +0100

    Update etcd

--- controller/update/controller.go
+++ controller/update/controller.go
@@ -1,21 +1,27 @@
 package cluster
 
-import "github.com/kubermatic/api"
-import "github.com/kubermatic/api/controller/resources"
-import "k8s.io/client-go/kubernetes"
+import (
+	"fmt"
+
+	"github.com/kubermatic/api"
+	"github.com/kubermatic/api/controller/resources"
+	"github.com/kubermatic/api/provider/kubernetes"
+	k "k8s.io/client-go/kubernetes"
+)
 
 type UpdateController struct {
-	Client              kubernetes.Interface
+	Client              k.Interface
 	MasterResourcesPath string
 	OverwriteHost       string
 	DC                  string
+	Versions            map[string]api.MasterVersion
 }
 
 func (u *UpdateController) doUpdate(c *api.Cluster) (*api.Cluster, error) {
 
 	switch c.Status.MasterUpdatePhase {
 	case api.StartMasterUpdatePhase:
-
+		return u.updateEtcd(c)
 	case api.EtcdMasterUpdatePhase:
 		//wait for etcd, update api server
 	case api.APIServerMasterUpdatePhase:
@@ -31,14 +37,21 @@ func (u *UpdateController) doUpdate(c *api.Cluster) (*api.Cluster, error) {
 
 func (u *UpdateController) updateEtcd(c *api.Cluster) (*api.Cluster, error) {
 
-	etcdDep, err := resources.LoadDeploymentFile(c, u.MasterResourcesPath, u.OverwriteHost, u.DC, s)
+	v := u.Versions[c.Spec.TargetMasterVersion]
+
+	etcdDep, err := resources.LoadDeploymentFile(c, v, u.MasterResourcesPath, u.OverwriteHost, u.DC)
 	if err != nil {
 		return nil, err
 	}
 
-	// push deployment to api
+	ns := kubernetes.NamespaceName(c.Metadata.User, c.Metadata.Name)
 
-	// switch phase
+	_, err = u.Client.ExtensionsV1beta1().Deployments(ns).Create(etcdDep)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create deployment for etcd: %v", err)
+	}
+
+	c.Status.MasterUpdatePhase = api.EtcdMasterUpdatePhase
 
 	return c, nil
 
