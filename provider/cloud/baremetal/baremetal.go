536e3e25c8da5139e70dcd385dc9897b25c0e678
realfake <luk.burchard@gmail.com>
2017-01-18 02:36:38 +0100

    Add Node Cleanup

--- provider/cloud/baremetal/baremetal.go
+++ provider/cloud/baremetal/baremetal.go
@@ -1,12 +1,23 @@
 package baremetal
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"path"
+
+	"strings"
+
 	"github.com/kubermatic/api"
 	"golang.org/x/net/context"
 )
 
-type baremetal struct {
+const serviceURL = "baremetal-provider.api.svc.cluster.local"
+const uuidSize = 10
+const appJSON = "application/json"
 
+type baremetal struct {
 }
 
 func (*baremetal) InitializeCloudSpec(*api.Cluster) error {
@@ -25,18 +36,71 @@ func (*baremetal) CreateNodes(context.Context, *api.Cluster, *api.NodeSpec, int)
 	panic("implement me")
 }
 
-func (*baremetal) Nodes(context.Context, *api.Cluster) ([]*api.Node, error) {
-	panic("implement me")
+func (*baremetal) Nodes(_ context.Context, cl *api.Cluster) ([]*api.Node, error) {
+	resp, err := http.Get(path.Join(serviceURL, fmt.Sprintf("/clusters/%s/nodes", cl.Metadata.Name)))
+	if err != nil {
+		return nil, err
+	}
+
+	var bareNodes []*api.BareMetalNodeSpec
+	err = json.NewDecoder(resp.Body).Decode(bareNodes)
+	if err != nil {
+		return nil, err
+	}
+
+	var nodes []*api.Node
+	for _, b := range bareNodes {
+		uid := strings.Split(b.ID, "-")[2]
+		node := &api.Node{
+			Metadata: api.Metadata{
+				UID:  uid,
+				Name: b.ID,
+			},
+			Status: api.NodeStatus{
+				// TODO(realfake): Probably spec is wrong?
+				// Do we need those ?
+				Addresses: map[string]string{},
+			},
+			Spec: api.NodeSpec{
+				DatacenterName: cl.Spec.Cloud.DatacenterName,
+				BareMetal:      b,
+			},
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes, nil
 }
 
-func (*baremetal) DeleteNodes(ctx context.Context, c *api.Cluster, UIDs []string) error {
-	panic("implement me")
+func (*baremetal) DeleteNodes(ctx context.Context, cl *api.Cluster, UIDs []string) error {
+	for _, uid := range UIDs {
+		client := &http.Client{}
+		req, err := http.NewRequest(http.MethodDelete, path.Join(fmt.Sprintf("/clusters/%s/nodes/%s", cl.Metadata.Name, uid)), nil)
+		if err != nil {
+			return err
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != http.StatusOK {
+			return errors.New(fmt.Sprintf("got status code %d from baremetal service"))
+		}
+	}
+	return nil
 }
 
-func (*baremetal) CleanUp(*api.Cluster) error {
-	panic("implement me")
+func (b *baremetal) CleanUp(cl *api.Cluster) error {
+	nodes, err := b.Nodes(context.Background(), cl)
+	if err != nil {
+		return err
+	}
+
+	var UIDs []string
+	for _, n := range nodes {
+		// Node UID name pattern = "%s"
+		// Not shown in the customer dashboard ?
+		// TODO(realfake): Adopt Node UID naming pattern
+		UIDs = append(UIDs, n.Metadata.UID)
+	}
+	return b.DeleteNodes(context.Background(), cl, UIDs)
 }
-
-
-
-
