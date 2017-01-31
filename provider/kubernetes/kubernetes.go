56aeb55432f076f5a71eca78e4f9f6d0b70137c6
Henrik Schmidt <henrik@henrik-schmidt.de>
2017-01-31 15:27:11 +0100

    Limit node-port generation range

--- provider/kubernetes/kubernetes.go
+++ provider/kubernetes/kubernetes.go
@@ -69,7 +69,7 @@ func NewKubernetesProvider(
 
 func (p *kubernetesProvider) GetFreeNodePort() (int, error) {
 	for {
-		port := rand.IntnRange(30000, 32767)
+		port := rand.IntnRange(30000, 31000)
 		sel := labels.NewSelector()
 		portString := strconv.Itoa(port)
 		req, err := labels.NewRequirement("node-port", selection.Equals, []string{portString})
