17ed0b13450e8b677c47fb43a2e5c6004ecdfc96
realfake <luk.burchard@gmail.com>
2017-02-09 15:26:20 +0100

    Implement new interface into seed provider

--- provider/kubernetes/seed.go
+++ provider/kubernetes/seed.go
@@ -196,5 +196,17 @@ func (p *seedProvider) DeleteCluster(user provider.User, cluster string) error {
 }
 
 func (p *seedProvider) CreateAddon(user provider.User, cluster string, addonName string) (*extensions.ClusterAddon, error) {
-	return nil, nil
+	panic("Not implemented")
+}
+
+func (p *seedProvider) CreateUserSSHKey(publicKey, fingerprint, name string, user provider.User) (*extensions.UserSSHKey, error) {
+	panic("Not implemented")
+}
+
+func (p *seedProvider) DeleteUserSSHKey(user provider.User, fingerprint string) error {
+	panic("Not implemented")
+}
+
+func (p *seedProvider) ListUserSSHKeys(user provider.User) (extensions.UserSSHKeyList, error) {
+	panic("Not implemented")
 }
