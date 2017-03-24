c72509b7edb44ec575953093a42f9b62897c44d0
Jason Murray <jason@loodse.com>
2017-03-24 17:24:20 +0100

    Fix invalid indirection

--- controller/version/path_test.go
+++ controller/version/path_test.go
@@ -22,7 +22,7 @@ func TestPathSearch_Search(t *testing.T) {
 			"1.5.4": {
 				ID: "1.5.4",
 			},
-		}, []*api.MasterUpdate{
+		}, []api.MasterUpdate{
 			{
 				From: "1.5.1",
 				To:   "1.5.2",
@@ -73,7 +73,7 @@ func TestPathSearch_SemverSearch(t *testing.T) {
 			"1.5.4": {
 				ID: "1.5.4",
 			},
-		}, []*api.MasterUpdate{
+		}, []api.MasterUpdate{
 			{
 				From: "1.5.1",
 				To:   "1.5.2",
