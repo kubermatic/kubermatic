2cf8c398985c99d31e42c3dbe39aa9ad2c1275fc
realfake <luk.burchard@gmail.com>
2017-08-10 16:28:40 +0200

    fixes

--- hack/run-api.sh
+++ hack/run-api.sh
@@ -6,13 +6,13 @@ set -o pipefail
 
 
 cd ${GOPATH}/src/github.com/kubermatic/api
-./_build/kubermatic-api \                                                                          
-  --master-kubeconfig=${GOPATH}/src/github.com/kubermatic/config/seed-clusters/dev.kubermatic.io/kubeconfig \
-  --datacenters=${GOPATH}/src/github.com/kubermatic/config/seed-clusters/dev.kubermatic.io/datacenters.yaml \
-  --kubeconfig=${GOPATH}/src/github.com/kubermatic/config/seed-clusters/dev.kubermatic.io/kubeconfig \
-  --worker-name="$(uname -n | tr -cd '[:alnum:]')" \
-  --token-issuer=http://auth.int.kubermatic.io \
-  --address=127.0.0.1:8080 \
-  --client-id=kubermatic \
-  --logtostderr \
-  --v=8
+./_build/kubermatic-api \
+  -master-kubeconfig=../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
+  -datacenters=../secrets/seed-clusters/dev.kubermatic.io/datacenters.yaml \
+  -kubeconfig=../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
+  -worker-name="$(uname -n | tr -cd '[:alnum:]')" \
+  -token-issuer=http://auth.int.kubermatic.io \
+  -address=127.0.0.1:8080 \
+  -client-id=kubermatic \
+  -logtostderr \
+  -v=8
--- hack/run-controller.sh
+++ hack/run-controller.sh
@@ -7,12 +7,12 @@ set -o pipefail
 
 cd ${GOPATH}/src/github.com/kubermatic/api
 ./_build/kubermatic-cluster-controller \
-  --datacenters=${GOPATH}/src/github.com/kubermatic/config/seed-clusters/dev.kubermatic.io/datacenters.yaml \
-  --kubeconfig=${GOPATH}/src/github.com/kubermatic/config/seed-clusters/dev.kubermatic.io/kubeconfig \
-  --versions=${GOPATH}/src/github.com/kubermatic/config/kubermatic/static/master/versions.yaml \
-  --updates=${GOPATH}/src/github.com/kubermatic/config/kubermatic/static/master/updates.yaml \
-  --master-resources=${GOPATH}/src/github.com/kubermatic/config/kubermatic/static/master \
-  --worker-name="$(uname -n | tr -cd '[:alnum:]')" \
-  --external-url=dev.kubermatic.io \
-  --logtostderr=1 \
-  --v=6
+  -datacenters=../secrets/seed-clusters/dev.kubermatic.io/datacenters.yaml \
+  -kubeconfig=../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
+  -versions=../config/kubermatic/static/master/versions.yaml \
+  -updates=../config/kubermatic/static/master/updates.yaml \
+  -master-resources=../secrets/kubermatic/static/master \
+  -worker-name="$(uname -n | tr -cd '[:alnum:]')" \
+  -external-url=dev.kubermatic.io \
+  -logtostderr=1 \
+  -v=6
