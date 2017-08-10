5016d22a40bf3ee8ef6a3e5bafffce6a44ed5f2d
Guus van Weelden <guus@weelden.de>
2017-08-10 17:14:40 +0200

    template skeletons

--- handler/upgrade_test.go
+++ handler/upgrade_test.go
@@ -5,7 +5,6 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/go-kit/kit/endpoint"
 	"github.com/kubermatic/api"
 	"github.com/kubermatic/api/provider"
 )
@@ -14,7 +13,6 @@ func Test_performClusterUpgrade(t *testing.T) {
 	type args struct {
 		kps     map[string]provider.KubernetesProvider
 		updates []api.MasterUpdate
-		endpoint
 	}
 	type endpointArgs struct {
 		name string
@@ -70,21 +68,48 @@ func Test_getClusterUpgrades(t *testing.T) {
 		versions map[string]*api.MasterVersion
 	}
 	type endpointArgs struct {
-		ctx context.Context
-		req interface{}
+		name string
+		ctx  context.Context
+		req  interface{}
+	}
+	type want struct {
+		val interface{}
+		err error
 	}
 	tests := []struct {
 		name         string
 		args         args
+		want         want
 		endpointArgs []endpointArgs
-		want         endpoint.Endpoint
 	}{
-	// TODO: Add test cases.
+		{
+			name: "nop",
+			args: args{
+				kps:      nil,
+				versions: nil,
+			},
+			endpointArgs: []endpointArgs{
+				{
+					name: "nop",
+					ctx:  context.Background(),
+					req:  nil,
+				},
+			},
+			want: want{
+				val: nil,
+				err: nil,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getClusterUpgrades(tt.args.kps, tt.args.versions); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("getClusterUpgrades() = %v, want %v", got, tt.want)
+			fn := getClusterUpgrades(tt.args.kps, tt.args.versions)
+			for _, ttEndpoint := range tt.endpointArgs {
+				t.Run(ttEndpoint.name, func(t *testing.T) {
+					if got, err := fn(ttEndpoint.ctx, ttEndpoint.req); !reflect.DeepEqual(got, tt.want.val) && !reflect.DeepEqual(err, tt.want.err) {
+						t.Errorf("performClusterUpgrade() = %v, want %v", got, tt.want)
+					}
+				})
 			}
 		})
 	}
