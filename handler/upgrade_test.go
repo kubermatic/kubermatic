1d8f1647a9a7c44f18c38728ab442c1f595f5d9b
Guus van Weelden <guus@weelden.de>
2017-08-11 12:20:17 +0200

    add generator funcs

--- handler/upgrade_test.go
+++ handler/upgrade_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/kubermatic/api"
 	"github.com/kubermatic/api/provider"
+	"github.com/kubermatic/api/provider/kubernetes"
 )
 
 func Test_performClusterUpgrade(t *testing.T) {
@@ -79,6 +80,36 @@ func Test_performClusterUpgrade(t *testing.T) {
 	}
 }
 
+func generateFakeDCs() map[string]provider.DatacenterMeta {
+	return nil
+}
+
+func generateBaseKubernetesProvider() map[string]provider.KubernetesProvider {
+	return map[string]provider.KubernetesProvider{
+		"base": kubernetes.NewKubernetesFakeProvider(
+			"base",
+			nil,
+			generateFakeDCs(),
+		),
+	}
+}
+
+func generateClusterUpgradeReq() upgradeReq {
+	return upgradeReq{
+		clusterReq: clusterReq{
+			dcReq: dcReq{
+				dc: "base",
+				userReq: userReq{
+					user: provider.User{
+						Name: "",
+					},
+				},
+			},
+			cluster: "234jkh24234g",
+		},
+	}
+}
+
 func Test_getClusterUpgrades(t *testing.T) {
 	type want struct {
 		val interface{}
@@ -124,6 +155,42 @@ func Test_getClusterUpgrades(t *testing.T) {
 						want: want{
 							val: nil,
 							err: NewWrongRequest("blah", upgradeReq{}),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "base config",
+			args: args{
+				kps:      generateBaseKubernetesProvider(),
+				versions: nil,
+				args: []endpointArgs{
+					{
+						name: "no request",
+						ctx:  context.Background(),
+						req:  nil,
+						want: want{
+							val: nil,
+							err: NewWrongRequest(nil, upgradeReq{}),
+						},
+					},
+					{
+						name: "wrong request",
+						ctx:  context.Background(),
+						req:  "blah",
+						want: want{
+							val: nil,
+							err: NewWrongRequest("blah", upgradeReq{}),
+						},
+					},
+					{
+						name: "base request - empty response",
+						ctx:  context.Background(),
+						req:  generateClusterUpgradeReq(),
+						want: want{
+							val: []*api.MasterVersion{},
+							err: nil,
 						},
 					},
 				},
