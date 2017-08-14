876163a3a72b00266c7a43d9e00cb481eb75367f
Luk <luk.burchard@gmail.com>
2017-08-14 10:29:54 +0200

    Set IsOnCi everytime to true

--- api/test/helper.go
+++ api/test/helper.go
@@ -1,16 +1,13 @@
 package test
 
-import "os"
-
 // IsOnCi returns if we're running on a ci system where no config is mounted
 func IsOnCi(path string) bool {
-	_, err := os.Stat(path)
+	return true
+	/*_, err := os.Stat(path)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return true
 		}
 		panic(err)
-	}
-
-	return false
+	} */
 }
