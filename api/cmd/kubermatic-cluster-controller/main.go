525757f2b800b55b1131ce9dad02cebdf43a5b33
Luk Burchard <realfake@users.noreply.github.com>
2017-08-16 12:03:21 +0200

    Add Prometheus handler (#388)
    * Add metrics package
    
    * Implement metrics
    
    * fix
    
    * update Gopkg.lock
    
    * fixed port and spelling
    
    * Add prom to config

--- api/cmd/kubermatic-cluster-controller/main.go
+++ api/cmd/kubermatic-cluster-controller/main.go
@@ -13,12 +13,15 @@ import (
 	"github.com/kubermatic/kubermatic/api/provider"
 	"github.com/kubermatic/kubermatic/api/provider/cloud"
 
+	"github.com/kubermatic/kubermatic/api/metrics"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
+	prometheusAddr        = flag.String("prometheus-address", "127.0.0.1:8085", "The Address on which the prometheus handler should be exposed")
+	prometheusPath        = flag.String("prometheus-path", "/metrics", "The path on the host, on which the handler is available")
 	kubeConfig            = flag.String("kubeconfig", ".kubeconfig", "The kubeconfig file path with one context per Kubernetes provider")
 	masterResources       = flag.String("master-resources", "", "The master resources path (Required).")
 	externalURL           = flag.String("external-url", "", "The external url for the apiserver host and the the dc.(Required)")
@@ -120,5 +123,6 @@ func main() {
 		go ctrl.Run(wait.NeverStop)
 	}
 
+	go metrics.ServeForever(*prometheusAddr, *prometheusPath)
 	<-wait.NeverStop
 }
