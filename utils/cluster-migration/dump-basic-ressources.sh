48eea577dae3c5076dee05a0048896f1d4301c26
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2017-08-31 17:44:29 +0200

    Add utils script to migrate clusters (#425)
    * Add utils script to migrate clusters
    
    * Use temporary directory & enable bash strict mode
    
    * Check if jq is installed

---
+++ utils/cluster-migration/dump-basic-ressources.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+command -v jq >/dev/null || { echo "'jq' is required to run this"; exit 1; }
+
+dumpdir="$(mktemp -d)"
+
+kubectl get --export -o=json ns | \
+jq '.items[] |
+	select(.metadata.name!="kube-system") |
+	select(.metadata.name!="default") |
+	del(.status,
+        .metadata.uid,
+        .metadata.selfLink,
+        .metadata.resourceVersion,
+        .metadata.creationTimestamp,
+        .metadata.generation
+    )' > $dumpdir/namespaces.json
+
+for ns in $(jq -r '.metadata.name' < $dumpdir/namespaces.json);do
+    echo "Namespace: $ns"
+    kubectl --namespace="${ns}" get --export -o=json deployment,ingress,daemonset,secrets,configmap,service,serviceaccount,statefulsets | \
+    jq '.items[] |
+        select(.type!="kubernetes.io/service-account-token") |
+        del(
+            .spec.clusterIP,
+            .metadata.uid,
+            .metadata.selfLink,
+            .metadata.resourceVersion,
+            .metadata.creationTimestamp,
+            .metadata.generation,
+            .status,
+            .spec.template.spec.securityContext,
+            .spec.template.spec.dnsPolicy,
+            .spec.template.spec.terminationGracePeriodSeconds,
+            .spec.template.spec.restartPolicy,
+            .spec.volumeName
+        )' >> "$dumpdir/basic-resources.json"
+done
+
+printf "backups stored in \n$dumpdir/namespaces.json\n$dumpdir/basic-resources.json\n"
---
+++ utils/cluster-migration/dump-nfs-pvs.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+command -v jq >/dev/null || { echo "'jq' is required to run this"; exit 1; }
+
+dumpdir="$(mktemp -d)"
+
+kubectl get --export -o=json ns | \
+jq '.items[] |
+	select(.metadata.name!="kube-system") |
+	select(.metadata.name!="default") |
+	del(.status,
+        .metadata.uid,
+        .metadata.selfLink,
+        .metadata.resourceVersion,
+        .metadata.creationTimestamp,
+        .metadata.generation
+    )' > $dumpdir/namespaces.json
+
+for ns in $(jq -r '.metadata.name' < $dumpdir/namespaces.json);do
+    echo "Namespace: $ns"
+    kubectl --namespace="${ns}" get --export -o=json pvc | \
+    jq '.items[] |
+        del(
+            .spec.clusterIP,
+            .metadata.uid,
+            .metadata.selfLink,
+            .metadata.resourceVersion,
+            .metadata.creationTimestamp,
+            .metadata.generation,
+            .metadata.annotation,
+            .status
+        )' >> "$dumpdir/pvc.json"
+done
+
+kubectl get --export -o=json pv | \
+jq '.items[] |
+    del(
+        .metadata.uid,
+        .metadata.selfLink,
+        .metadata.resourceVersion,
+        .metadata.creationTimestamp,
+        .metadata.generation,
+        .metadata.annotation,
+        .status,
+        .spec.claimRef
+    )' > "$dumpdir/pv.json"
+
+printf "backups stored in \n$dumpdir/pv.json\n$dumpdir/pvc.json\n"
