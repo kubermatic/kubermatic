02503692f2c6871c258f45adec5eb67fc1757889
Luk Burchard <realfake@users.noreply.github.com>
2017-09-04 18:02:21 +0200

    fix paths (#431)

--- api/hack/run-controller.sh
+++ api/hack/run-controller.sh
@@ -7,11 +7,11 @@ set -o pipefail
 
 cd ${GOPATH}/src/github.com/kubermatic/kubermatic/api
 ./_build/kubermatic-cluster-controller \
-  -datacenters=../secrets/seed-clusters/dev.kubermatic.io/datacenters.yaml \
-  -kubeconfig=../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
+  -datacenters=../../secrets/seed-clusters/dev.kubermatic.io/datacenters.yaml \
+  -kubeconfig=../../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
   -versions=../config/kubermatic/static/master/versions.yaml \
   -updates=../config/kubermatic/static/master/updates.yaml \
-  -master-resources=../secrets/kubermatic/static/master \
+  -master-resources=../config/kubermatic/static/master \
   -worker-name="$(uname -n | tr -cd '[:alnum:]')" \
   -external-url=dev.kubermatic.io \
   -logtostderr=1 \
