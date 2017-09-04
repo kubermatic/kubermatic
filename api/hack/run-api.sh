3c41280dfa381a327b1333299a1f2db47ad9988d
Luk <luk.burchard@gmail.com>
2017-09-04 18:00:22 +0200

    fix paths

--- api/hack/run-api.sh
+++ api/hack/run-api.sh
@@ -3,17 +3,19 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
 
 
 cd ${GOPATH}/src/github.com/kubermatic/kubermatic/api
 ./_build/kubermatic-api \
-  -master-kubeconfig=$GOPATH/src/github.com/kubermatic/secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
-  -datacenters=$GOPATH/src/github.com/kubermatic/secrets/seed-clusters/dev.kubermatic.io/datacenters.yaml \
-  -kubeconfig=$GOPATH/src/github.com/kubermatic/secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
-  -versions=$GOPATH/src/github.com/kubermatic/kubermatic/config/kubermatic/static/master/versions.yaml \
-  -updates=$GOPATH/src/github.com/kubermatic/kubermatic/config/kubermatic/static/master/updates.yaml \
+  -master-kubeconfig=../../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
+  -datacenters=../../secrets/seed-clusters/dev.kubermatic.io/datacenters.yaml \
+  -kubeconfig=../../secrets/seed-clusters/dev.kubermatic.io/kubeconfig \
+  -versions=../config/kubermatic/static/master/versions.yaml \
+  -updates=../config/kubermatic/static/master/updates.yaml \
   -worker-name="$(uname -n | tr -cd '[:alnum:]')" \
-  -token-issuer=http://auth.int.kubermatic.io \
+  -token-issuer=https://dev.kubermatic.io/dex \
+  -prometheus-address=127.0.0.1:18085 \
   -address=127.0.0.1:8080 \
   -client-id=kubermatic \
   -logtostderr \
