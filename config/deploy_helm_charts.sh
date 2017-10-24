a36decda33ef3b0665a77435392919388c75866c
Igor Susman <igor.susman@gmail.com>
2017-10-24 15:28:49 +0200

    initial installer fix

--- config/deploy_helm_charts.sh
+++ config/deploy_helm_charts.sh
@@ -7,8 +7,6 @@ helm upgrade -i k8sniff -f kubermatic-values.yaml -f config/values.yaml k8sniff/
 helm upgrade -i kubermatic -f kubermatic-values.yaml -f config/values.yaml kubermatic/
 helm upgrade -i storage -f kubermatic-values.yaml -f config/values.yaml storage/
 helm upgrade -i nginx-ingress-controller -f kubermatic-values.yaml -f config/values.yaml nginx-ingress-controller/
-helm upgrade -i tpr -f kubermatic-values.yaml -f config/values.yaml thirdpartyresources/
-helm upgrade -i kube-lego -f kubermatic-values.yaml -f config/values.yaml kube-lego/
 
 # Logging
 if grep -q '\bLogging\b' config/values.yaml; then
