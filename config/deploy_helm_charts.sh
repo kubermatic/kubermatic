bfa14408e0b57a8c189ff1f032bc7e8305d3af98
Igor Susman <igor.susman@gmail.com>
2017-10-26 09:23:06 +0200

    remove drone, fix monitoring path

--- config/deploy_helm_charts.sh
+++ config/deploy_helm_charts.sh
@@ -8,7 +8,6 @@ helm upgrade -i kubermatic -f kubermatic-values.yaml -f config/values.yaml kuber
 helm upgrade -i storage -f kubermatic-values.yaml -f config/values.yaml storage/
 helm upgrade -i nginx-ingress-controller -f kubermatic-values.yaml -f config/values.yaml nginx-ingress-controller/
 helm upgrade -i oauth -f kubermatic-values.yaml -f config/values.yaml oauth/
-helm upgrade -i drone -f kubermatic-values.yaml -f config/values.yaml drone/
 helm upgrade -i certs -f kubermatic-values.yaml -f config/values.yaml certs/
 helm upgrade -i cert-manager -f kubermatic-values.yaml -f config/values.yaml cert-manager/
 
@@ -18,7 +17,7 @@ if grep -q '\bLogging\b' config/values.yaml; then
 fi
 #Monitoring
 if grep -q '\bPrometheus\b' config/values.yaml; then
-  helm upgrade -i prometheus -f kubermatic-values.yaml -f config/values.yaml prometheus/
+  helm upgrade -i prometheus -f kubermatic-values.yaml -f config/values.yaml monitoring/prometheus/
 fi
 # Bare metal
 if grep -q '\bIsBareMetal\b' config/values.yaml; then
