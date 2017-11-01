5b7a242429c1a1119f4f15636671169abe0e50aa
Matthias Loibl <mail@matthiasloibl.com>
2017-11-01 16:42:26 +0100

    Fix export to work with grafana-watcher

--- config/monitoring/grafana/export.sh
+++ config/monitoring/grafana/export.sh
@@ -10,10 +10,6 @@ fetch_fields() {
     echo $(curl -sSL -f -k -H "Authorization: Bearer ${1}" "${HOST}/api/${2}" | jq -r "if type==\"array\" then .[] else . end| .${3}")
 }
 
-if [ ! -d "DIR" ] ; then
-    mkdir -p "DIR"
-fi
-
 for key in "${KEYS[@]}" ; do
 
     if [ ! -d "$DIR" ] ; then
@@ -23,13 +19,13 @@ for key in "${KEYS[@]}" ; do
     for dash in $(fetch_fields ${key} 'search?query=&' 'uri'); do
         DB=$(echo ${dash}|sed 's,db/,,g').json
         echo $DB
-        curl -f -k -H "Authorization: Bearer ${key}" "${HOST}/api/dashboards/${dash}" | jq '.dashboard.id = null' | jq '.overwrite = true' > "$DIR/dashboard-$DB"
+        curl -f -k -H "Authorization: Bearer ${key}" "${HOST}/api/dashboards/${dash}" | jq '.dashboard.id = null' | jq '.overwrite = true' > "$DIR/${DB%.json}-dashboard.json"
     done
 
     for id in $(fetch_fields ${key} 'datasources' 'id'); do
         DS=$(echo $(fetch_fields ${key} "datasources/${id}" 'name')|sed 's/ /-/g').json
         echo $DS
-        curl -f -k -H "Authorization: Bearer ${key}" "${HOST}/api/datasources/${id}" | jq '.id = null' | jq '.orgId = null' > "$DIR/datasource-$DS"
+        curl -f -k -H "Authorization: Bearer ${key}" "${HOST}/api/datasources/${id}" | jq '.id = null' | jq '.orgId = null' > "$DIR/${DS%.json}-datasource.json"
     done
 
 done
