c3ff15f021ed9618e6d76b6edab3ca1f0282a877
Henrik Schmidt <henrik@loodse.com>
2017-11-10 14:23:00 +0100

    Only set the annotation if there was one before

--- addon-manager/migrations/fix-node-label.sh
+++ addon-manager/migrations/fix-node-label.sh
@@ -7,5 +7,7 @@ KUBECTL_OPTS=${KUBECTL_OPTS:-}
 NODES=$(${KUBECTL} ${KUBECTL_OPTS} get node -o=custom-columns=:.metadata.name)
 for NODE in ${NODES}; do
     NODECLASS=$(${KUBECTL} ${KUBECTL_OPTS} get node ${NODE} -o json | jq -r '.metadata.annotations."node.k8s.io/node-class"')
-    ${KUBECTL} ${KUBECTL_OPTS} annotate node ${NODE} 'nodeset.k8s.io/node-class'=${NODECLASS}
+    if [ -n "${NODECLASS}" ]; then
+        ${KUBECTL} ${KUBECTL_OPTS} annotate node ${NODE} 'nodeset.k8s.io/node-class'=${NODECLASS}
+    fi
 done
