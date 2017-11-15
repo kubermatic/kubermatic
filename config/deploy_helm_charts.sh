b9f9259cabfa963c1be5a9833021d613045430a6
Henrik Schmidt <henrik@loodse.com>
2017-11-15 11:38:57 +0100

    Fix install order

--- config/deploy_helm_charts.sh
+++ config/deploy_helm_charts.sh
@@ -8,13 +8,13 @@ kubectl create -f role-tiller.yaml
 kubectl create -f rolebinding-tiller.yaml
 helm init --service-account tiller --tiller-namespace kubermatic-installer --upgrade
 sleep 10
-helm upgrade -i k8sniff -f kubermatic-values.yaml -f config/values.yaml k8sniff/
-helm upgrade -i kubermatic -f kubermatic-values.yaml -f config/values.yaml kubermatic/
 helm upgrade -i storage -f kubermatic-values.yaml -f config/values.yaml storage/
+helm upgrade -i k8sniff -f kubermatic-values.yaml -f config/values.yaml k8sniff/
 helm upgrade -i nginx-ingress-controller -f kubermatic-values.yaml -f config/values.yaml nginx-ingress-controller/
 helm upgrade -i oauth -f kubermatic-values.yaml -f config/values.yaml oauth/
+helm upgrade -i kubermatic -f kubermatic-values.yaml -f config/values.yaml kubermatic/
+helm upgrade -i --namespace=cert-manager cert-manager -f kubermatic-values.yaml -f config/values.yaml cert-manager/
 helm upgrade -i certs -f kubermatic-values.yaml -f config/values.yaml certs/
-helm upgrade -i cert-manager -f kubermatic-values.yaml -f config/values.yaml cert-manager/
 
 # Logging
 if grep -q '\bLogging\b' config/values.yaml; then
