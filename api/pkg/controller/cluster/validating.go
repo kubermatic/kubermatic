2b4387c60e9f7fae20753bceabfda46f5b9e1965
Henrik Schmidt <henrik@loodse.com>
2017-11-16 09:39:02 +0100

    Add special validation for byo datacenter

--- api/pkg/controller/cluster/validating.go
+++ api/pkg/controller/cluster/validating.go
@@ -1,6 +1,7 @@
 package cluster
 
 import (
+	"errors"
 	"fmt"
 
 	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
@@ -48,13 +49,20 @@ func (cc *controller) validatingCheckDatacenter(c *kubermaticv1.Cluster) error {
 		return fmt.Errorf("given seed datacenter %q is not configured as a seed datacenter", c.Spec.SeedDatacenterName)
 	}
 
-	//Validate node datacenter
-	dc, found := cc.dcs[c.Spec.Cloud.DatacenterName]
-	if !found {
-		return fmt.Errorf("could not find node datacenter %q", c.Spec.Cloud.DatacenterName)
-	}
-	if dc.IsSeed {
-		return fmt.Errorf("given datacenter %q is not configured as a node datacenter", c.Spec.SeedDatacenterName)
+	// If we have BringYourOwn, than no node provider must be set
+	if c.Spec.Cloud.BringYourOwn != nil {
+		if c.Spec.Cloud.DatacenterName != "" {
+			return errors.New("node dc is not allowed when using bringyourown")
+		}
+	} else {
+		//Validate node datacenter
+		dc, found := cc.dcs[c.Spec.Cloud.DatacenterName]
+		if !found {
+			return fmt.Errorf("could not find node datacenter %q", c.Spec.Cloud.DatacenterName)
+		}
+		if dc.IsSeed {
+			return fmt.Errorf("given datacenter %q is not configured as a node datacenter", c.Spec.SeedDatacenterName)
+		}
 	}
 
 	return nil
