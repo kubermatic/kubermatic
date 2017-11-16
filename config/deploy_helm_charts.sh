1c7c91180a169b901bcd06949c32fb470a1f4410
Henrik Schmidt <henrik@loodse.com>
2017-11-16 12:46:26 +0100

    Use container versions for installer

--- config/deploy_helm_charts.sh
+++ config/deploy_helm_charts.sh
@@ -5,11 +5,14 @@ if [ "$#" -lt 2 ] || [ "${1}" == "--help" ]; then
   cat <<EOF
 Usage: $(basename $0) <path-to-values-file> <path-to-charts>
 
-  <path-to-values-file>   the path to the values.yaml which should be used
-  <path-to-charts>        the path to the directory where the kubermatic charts are located
+  <path-to-values-file>                 the path to the values.yaml which should be used
+  <path-to-charts>                      the path to the directory where the kubermatic charts are located
+
+  <path-to-override-values>  optional   the path to the values.yaml for overrides
 
 Examples:
   $(basename $0) /kubermatic/values/values.yaml /kubermatic
+  $(basename $0) /kubermatic/values/values.yaml /kubermatic /kubermatic/values/override-values.yaml
 EOF
   exit 0
 fi
@@ -17,6 +20,7 @@ fi
 HELM_OPTS="--tiller-namespace=kubermatic-installer"
 VALUESFILE="$1"
 CHARTS_PATH="$2"
+OVERRIDE_VALUESFILE=${3:-}
 
 if [ ! -f ${VALUESFILE} ]; then
     echo "${VALUESFILE} does not exist."
@@ -26,6 +30,15 @@ fi
 if [ ! -d ${CHARTS_PATH} ]; then
     echo "${CHARTS_PATH} does not exist."
     exit 1
+fi
+
+if [ ! -z "${OVERRIDE_VALUESFILE}" ]; then
+    if [ ! -f ${OVERRIDE_VALUESFILE} ]; then
+        echo "${OVERRIDE_VALUESFILE} does not exist."
+        exit 1
+    fi
+
+    VALUESFILE="${VALUESFILE},${OVERRIDE_VALUESFILE}"
 fi
 
 kubectl apply -f ${CHARTS_PATH}/installer/namespace.yaml
