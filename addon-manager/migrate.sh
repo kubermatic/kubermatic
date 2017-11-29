671a70246ed6dfa78b88cfc584b541dfacd72842
Henrik Schmidt <henrik@loodse.com>
2017-11-29 17:41:11 +0100

    Add in-cluster migration step for nodeport

--- addon-manager/migrate.sh
+++ addon-manager/migrate.sh
@@ -4,3 +4,5 @@
 /etc/kubermatic/migrations/replace-flannel-with-canal.sh
 # We changed a annotation key for the node controller
 /etc/kubermatic/migrations/fix-node-label.sh
+# We switched from externalName for NodePort, so we need to delete the service so the apiserver recreates it
+/etc/kubermatic/migrations/delete-kubernetes-service.sh
