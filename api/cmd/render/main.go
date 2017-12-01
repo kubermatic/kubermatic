fbba747734b34b95d891b1383d1f52f0401fbf1f
realfake <luk.burchard@gmail.com>
2017-12-01 15:45:42 +0100

    Update Images

--- api/cmd/render/main.go
+++ api/cmd/render/main.go
@@ -99,16 +99,16 @@ type ImageVersions struct {
 var DefaultImages = ImageVersions{
 	Etcd:            "quay.io/coreos/etcd:v3.1.8",
 	EtcdOperator:    "quay.io/coreos/etcd-operator:v0.5.0",
-	Flannel:         "quay.io/coreos/flannel:v0.8.0-amd64",
+	Flannel:         "quay.io/coreos/flannel:v0.9.1-amd64",
 	FlannelCNI:      "quay.io/coreos/flannel-cni:v0.3.0",
-	Calico:          "quay.io/calico/node:v2.5.1",
-	CalicoCNI:       "quay.io/calico/cni:v1.10.0",
-	Hyperkube:       "quay.io/coreos/hyperkube:v1.7.5_coreos.0",
+	Calico:          "quay.io/calico/node:v2.6.3",
+	CalicoCNI:       "quay.io/calico/cni:v1.11.1",
+	Hyperkube:       "gcr.io/google_containers/hyperkube:v1.8.4",
 	Kenc:            "quay.io/coreos/kenc:0.0.2",
-	KubeDNS:         "gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.4",
-	KubeDNSMasq:     "gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.4",
-	KubeDNSSidecar:  "gcr.io/google_containers/k8s-dns-sidecar-amd64:1.14.4",
-	PodCheckpointer: "quay.io/coreos/pod-checkpointer:0cd390e0bc1dcdcc714b20eda3435c3d00669d0e",
+	KubeDNS:         "gcr.io/google_containers/k8s-dns-kube-dns-amd64:1.14.5",
+	KubeDNSMasq:     "gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:1.14.5",
+	KubeDNSSidecar:  "gcr.io/google_containers/k8s-dns-sidecar-amd64:1.14.5",
+	PodCheckpointer: "quay.io/coreos/pod-checkpointer:e22cc0e3714378de92f45326474874eb602ca0ac",
 }
 
 // AssetConfig holds all configuration needed when generating
