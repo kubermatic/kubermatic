1393cf454e12ff196efb7ba7e6b5cce84f28270a
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-01-04 17:20:17 +0100

    replace hardcoded namespace with helm namespace flag for kubermatic chart (#669)

--- config/deploy_helm_charts.sh
+++ config/deploy_helm_charts.sh
@@ -58,7 +58,7 @@ done
 helm ${HELM_OPTS} upgrade -i storage -f ${VALUESFILE} ${CHARTS_PATH}/storage/
 helm ${HELM_OPTS} upgrade -i nginx -f ${VALUESFILE} ${CHARTS_PATH}/nginx-ingress-controller/
 helm ${HELM_OPTS} upgrade -i oauth -f ${VALUESFILE} ${CHARTS_PATH}/oauth/
-helm ${HELM_OPTS} upgrade -i kubermatic -f ${VALUESFILE} ${CHARTS_PATH}/kubermatic/
+helm ${HELM_OPTS} upgrade -i kubermatic --namespace kubermatic -f ${VALUESFILE} ${CHARTS_PATH}/kubermatic/
 helm ${HELM_OPTS} upgrade -i cert-manager --namespace cert-manager -f ${VALUESFILE} ${CHARTS_PATH}/cert-manager/
 helm ${HELM_OPTS} upgrade -i certs --namespace default -f ${VALUESFILE} ${CHARTS_PATH}/certs/
 helm ${HELM_OPTS} upgrade -i nodeport-exposer -f ${VALUESFILE} ${CHARTS_PATH}/nodeport-exposer/
