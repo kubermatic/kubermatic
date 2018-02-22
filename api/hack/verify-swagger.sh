e330ef686330f867b72c50f4f47bf6f2da61fd57
mrIncompetent <henrik@henrik-schmidt.de>
2018-02-22 14:17:50 +0100

    fix usage of GOPATH

--- api/hack/verify-swagger.sh
+++ api/hack/verify-swagger.sh
@@ -4,12 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-API_DIR="${GOPATH}/src/github.com/kubermatic/kubermatic/api"
+API_DIR="$(go env GOPATH)/src/github.com/kubermatic/kubermatic/api"
 SWAGGER_FILE="swagger.json"
 TMP_SWAGGER="${SWAGGER_FILE}.tmp"
 
 cd ${API_DIR}/vendor/github.com/go-swagger/go-swagger/cmd/swagger
-go install 
+go install
 cd ${API_DIR}/cmd/kubermatic-api/
 swagger generate spec --scan-models -o ${TMP_SWAGGER}
 diff -Naup ${SWAGGER_FILE} ${TMP_SWAGGER}
