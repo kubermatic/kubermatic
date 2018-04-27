2f9da62c653986678127e9167845ef1fb184c0ef
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-04-27 15:52:45 +0200

    Update kube client (#980)
    * remove deprecated nodeclass code
    
    * migrate to new go client
    
    * update go client
    
    * fix machine.yaml for operating system containerLinux (#979)
    
    * codegen
    
    * update vendor
    
    * swagger update

--- api/pkg/provider/cloud/bringyourown/provider.go
+++ api/pkg/provider/cloud/bringyourown/provider.go
@@ -1,7 +1,6 @@
 package bringyourown
 
 import (
-	"github.com/kube-node/nodeset/pkg/nodeset/v1alpha1"
 	apiv1 "github.com/kubermatic/kubermatic/api/pkg/api/v1"
 	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
 	"github.com/kubermatic/kubermatic/api/pkg/provider"
@@ -24,14 +23,6 @@ func (b *bringyourown) InitializeCloudProvider(cloud *kubermaticv1.CloudSpec, na
 
 func (b *bringyourown) CleanUpCloudProvider(*kubermaticv1.CloudSpec) error {
 	return nil
-}
-
-func (b *bringyourown) CreateNodeClass(c *kubermaticv1.Cluster, nSpec *apiv1.NodeSpec, keys []*kubermaticv1.UserSSHKey, version *apiv1.MasterVersion) (*v1alpha1.NodeClass, error) {
-	return nil, nil
-}
-
-func (b *bringyourown) NodeClassName(nSpec *apiv1.NodeSpec) string {
-	return ""
 }
 
 func (b *bringyourown) ValidateNodeSpec(cloudSpec *kubermaticv1.CloudSpec, nodeSpec *apiv1.NodeSpec) error {
