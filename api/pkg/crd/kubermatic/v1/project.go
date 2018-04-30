8347c2920a1c7621a528f6221d93de5e34db4f6f
p0lyn0mial <lukasz.szaszkiewicz@gmail.com>
2018-04-30 13:01:46 +0200

    defines project crd type

---
+++ api/pkg/crd/kubermatic/v1/project.go
@@ -0,0 +1,50 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// These are the valid phases of a project.
+const (
+	// ProjectActive means the project is available for use in the system
+	ProjectActive string = "Active"
+
+	// ProjectInactive means the project is inactive and requires further initialization
+	ProjectInactive string = "Inactive"
+
+	// ProjectTerminating means the project is undergoing graceful termination
+	ProjectTerminating string = "Terminating"
+)
+
+//+genclient
+//+genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Project is the type describing a project.
+type Project struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ProjectSpec   `json:"spec"`
+	Status ProjectStatus `json:"status"`
+}
+
+// ProjectSpec is a specification of a project.
+type ProjectSpec struct {
+	Name string `json:"name"`
+}
+
+// ProjectStatus represents the current status of a project.
+type ProjectStatus struct {
+	Phase string `json:"phase"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ProjectList is a collection of projects.
+type ProjectList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Project `json:"items"`
+}
