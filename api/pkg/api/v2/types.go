c2ad85add20ddb9599a9a9ee2d36aa6775803e3e
mrIncompetent <henrik@henrik-schmidt.de>
2018-05-07 10:08:19 +0200

    add creation timestamp to api node object

--- api/pkg/api/v2/types.go
+++ api/pkg/api/v2/types.go
@@ -5,13 +5,16 @@ import "time"
 // ObjectMeta is an object storing common metadata for persistable objects.
 // swagger:model ObjectMetaV2
 type ObjectMeta struct {
-	//The unique name
+	// The unique name
 	Name string `json:"name"`
-	//The name to display in the frontend
+	// The name to display in the frontend
 	DisplayName string `json:"displayName"`
 
-	//Tells that the resource is being deleted
+	// DeletionTimestamp is a timestamp representing the server time when this object was deleted.
 	DeletionTimestamp *time.Time `json:"deletionTimestamp,omitempty"`
+
+	// CreationTimestamp is a timestamp representing the server time when this object was created.
+	CreationTimestamp time.Time `json:"creationTimestamp,omitempty"`
 
 	Annotations map[string]string `json:"annotations,omitempty"`
 	Labels      map[string]string `json:"labels,omitempty"`
