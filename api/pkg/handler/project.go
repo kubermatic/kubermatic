fdfe52a2ea25df1a6bd5953afa9d924788587b48
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2018-05-07 13:01:33 +0200

    moves Project type to api package (#1005)

--- api/pkg/handler/project.go
+++ api/pkg/handler/project.go
@@ -6,14 +6,8 @@ import (
 	"net/http"
 
 	"github.com/go-kit/kit/endpoint"
+	apiv1 "github.com/kubermatic/kubermatic/api/pkg/api/v1"
 )
-
-// Project is a top-level container for a set of resources
-// swagger:model Project
-type Project struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
 
 func decodeProjectPathReq(c context.Context, r *http.Request) (interface{}, error) {
 	return nil, errors.New("not implemented")
@@ -21,13 +15,12 @@ func decodeProjectPathReq(c context.Context, r *http.Request) (interface{}, erro
 
 func getProjectsEndpoint() endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		return []Project{}, nil
+		return []apiv1.Project{}, nil
 	}
 }
 
 func deleteProjectEndpoint() endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		// Don't return project just success.
 		return nil, errors.New("not implemented")
 	}
 }
@@ -38,7 +31,7 @@ func decodeUpdateProject(c context.Context, r *http.Request) (interface{}, error
 
 func updateProjectEndpoint() endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		return Project{}, nil
+		return apiv1.Project{}, nil
 	}
 }
 
@@ -48,6 +41,6 @@ func decodeCreateProject(c context.Context, r *http.Request) (interface{}, error
 
 func createProjectEndpoint() endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-		return Project{}, nil
+		return apiv1.Project{}, nil
 	}
 }
