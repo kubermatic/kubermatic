f454adb79accec41d63f6d77e831397a8f182e07
mrIncompetent <henrik@henrik-schmidt.de>
2018-05-28 14:52:55 +0200

    add addon type

---
+++ api/pkg/crd/kubermatic/v1/addon.go
@@ -0,0 +1,55 @@
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	AddonPlural = "addons"
+
+	AddonKind = "Addon"
+)
+
+//+genclient
+
+// Addon specifies a add-on
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type Addon struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec AddonSpec `json:"spec"`
+}
+
+type AddonMode string
+
+const (
+	// AddonModeLabelKey is the label key which needs to exist on every manifest and defines how to manage the manifest
+	AddonModeLabelKey = "addonmanager.kubernetes.io/mode"
+
+	//AddonModeReconcile tells that the addon manager should overwrite any change made to the manifest
+	AddonModeReconcile AddonMode = "Reconcile"
+	//AddonModeEnsureExists tells that the addon manager should only make sure the manifest exist. The manifest can later get modified and wont be overwritten
+	AddonModeEnsureExists AddonMode = "EnsureExists"
+)
+
+// AddonSpec specifies details of an addon
+type AddonSpec struct {
+	// Name defines the name of the addon to install
+	Name string
+	// Cluster is the reference to the cluster the addon should be installed in
+	Cluster corev1.ObjectReference
+	// Free form data to use for parsing the manifest templates
+	Variables runtime.RawExtension `json:"variables"`
+}
+
+// AddonList is a list of addons
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type AddonList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Addon `json:"items"`
+}
