dae85b78b2d60733a99f3983e0d6bd6792416ad4
Alvaro Aleman <alv2412@googlemail.com>
2018-05-31 09:48:49 +0200

    Initial boilerplate for backup controller

---
+++ api/pkg/controller/backup/controller.go
@@ -0,0 +1,143 @@
+package backup
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+
+	kubermaticclientset "github.com/kubermatic/kubermatic/api/pkg/crd/client/clientset/versioned"
+	kubermaticv1informers "github.com/kubermatic/kubermatic/api/pkg/crd/client/informers/externalversions/kubermatic/v1"
+	kubermaticv1lister "github.com/kubermatic/kubermatic/api/pkg/crd/client/listers/kubermatic/v1"
+	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
+
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+// Controller stores all components required to create backups
+type Controller struct {
+	backupTemplate string
+	backupSchedule time.Duration
+
+	queue            workqueue.RateLimitingInterface
+	kubermaticClient kubermaticclientset.Interface
+	clusterLister    kubermaticv1lister.ClusterLister
+	clusterSynced    cache.InformerSynced
+}
+
+// New creates a new Backup controller that is responsible for creating backupjobs
+// for all managed user clusters
+func New(
+	backupTemplate string,
+	backupSchedule time.Duration,
+	kubermaticClient kubermaticclientset.Interface,
+	clusterInformer kubermaticv1informers.ClusterInformer) *Controller {
+	c := &Controller{
+		queue:            workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Update"),
+		kubermaticClient: kubermaticClient,
+	}
+
+	clusterInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			c.enqueue(obj.(*kubermaticv1.Cluster))
+		},
+		UpdateFunc: func(old, cur interface{}) {
+			c.enqueue(cur.(*kubermaticv1.Cluster))
+		},
+		DeleteFunc: func(obj interface{}) {
+			cluster, ok := obj.(*kubermaticv1.Cluster)
+			if !ok {
+				tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+				if !ok {
+					runtime.HandleError(fmt.Errorf("couldn't get object from tombstone %#v", obj))
+					return
+				}
+				cluster, ok = tombstone.Obj.(*kubermaticv1.Cluster)
+				if !ok {
+					runtime.HandleError(fmt.Errorf("tombstone contained object that is not a Cluster %#v", obj))
+					return
+				}
+			}
+			c.enqueue(cluster)
+		},
+	})
+	c.clusterLister = clusterInformer.Lister()
+	c.clusterSynced = clusterInformer.Informer().HasSynced
+
+	return c
+}
+
+// Run starts the controller's worker routines. This method is blocking and ends when stopCh gets closed
+func (c *Controller) Run(workerCount int, stopCh <-chan struct{}) {
+	defer runtime.HandleCrash()
+	glog.Infof("Starting Backup controller with %d workers", workerCount)
+	defer glog.Info("Shutting down Backup  controller")
+
+	if !cache.WaitForCacheSync(stopCh, c.clusterSynced) {
+		runtime.HandleError(errors.New("unable to sync caches for Backup controller"))
+		return
+	}
+
+	for i := 0; i < workerCount; i++ {
+		go wait.Until(c.runWorker, time.Second, stopCh)
+	}
+
+	<-stopCh
+}
+
+func (c *Controller) runWorker() {
+	for c.processNextItem() {
+	}
+}
+
+func (c *Controller) processNextItem() bool {
+	key, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	defer c.queue.Done(key)
+
+	err := c.sync(key.(string))
+
+	c.handleErr(err, key)
+	return true
+}
+
+// handleErr checks if an error happened and makes sure we will retry later.
+func (c *Controller) handleErr(err error, key interface{}) {
+	if err == nil {
+		// Forget about the #AddRateLimited history of the key on every successful synchronization.
+		// This ensures that future processing of updates for this key is not delayed because of
+		// an outdated error history.
+		c.queue.Forget(key)
+		return
+	}
+
+	// This controller retries 5 times if something goes wrong. After that, it stops trying.
+	if c.queue.NumRequeues(key) < 5 {
+		glog.V(0).Infof("Error syncing %v: %v", key, err)
+
+		// Re-enqueue the key rate limited. Based on the rate limiter on the
+		// queue and the re-enqueue history, the key will be processed later again.
+		c.queue.AddRateLimited(key)
+		return
+	}
+
+	c.queue.Forget(key)
+	// Report to an external entity that, even after several retries, we could not successfully process this key
+	runtime.HandleError(err)
+	glog.V(0).Infof("Dropping %q out of the backup controller queue after exceeding max retries: %v", key, err)
+}
+
+func (c *Controller) enqueue(cluster *kubermaticv1.Cluster) {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(cluster)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("couldn't get key for object %#v: %v", cluster, err))
+		return
+	}
+
+	c.queue.Add(key)
+}
