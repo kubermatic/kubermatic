65e6a8690eff2ee26e8d010741c076e9ed815f8e
Alvaro Aleman <alv2412@googlemail.com>
2018-06-11 22:16:15 +0200

    Authenticate apiserver when proxying
    This fixes https://github.com/kubevirt/kubevirt/issues/1126 by adding
    the --proxy-client-cert-file and --proxy-client-key-file flags

--- api/hack/run-controller.sh
+++ api/hack/run-controller.sh
@@ -13,9 +13,9 @@ cd $(go env GOPATH)/src/github.com/kubermatic/kubermatic/api
   -versions=../config/kubermatic/static/master/versions.yaml \
   -updates=../config/kubermatic/static/master/updates.yaml \
   -master-resources=../config/kubermatic/static/master \
-  -addons=../addons
+  -addons=../addons \
   -worker-name="$(hostname)" \
   -external-url=dev.kubermatic.io \
-  -backup-container=../config/kubermatic/static/backup-container.yaml
+  -backup-container=../config/kubermatic/static/backup-container.yaml \
   -logtostderr=1 \
   -v=6
