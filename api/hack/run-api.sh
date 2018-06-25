3fd6e1671ab21cbdf6f0cae3bba8b64f3a0a4936
Patrick Kavajin <patrick@loodse.com>
2018-06-25 16:51:59 +0200

    whoops, removed tmp hack

--- api/hack/run-api.sh
+++ api/hack/run-api.sh
@@ -16,7 +16,6 @@ cd $(go env GOPATH)/src/github.com/kubermatic/kubermatic/api
   -master-resources=../config/kubermatic/static/master \
   -worker-name="$(uname -n | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]' )" \
   -token-issuer=https://dev.kubermatic.io/dex \
-  -token-issuer-skip-tls-verify=true \
   -internal-address=127.0.0.1:18085 \
   -prometheus-url=http://localhost:9090 \
   -address=127.0.0.1:8080 \
