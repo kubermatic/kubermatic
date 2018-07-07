0fb5686cdad20f62b088db16b8ea1033c99188da
Alvaro Aleman <alv2412@googlemail.com>
2018-07-08 00:16:03 +0200

    Update verify-addon-version script to work inside docker

--- api/hack/verify-addon-version.sh
+++ api/hack/verify-addon-version.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env sh
 
-set -ex
+set -e
+
+function finish {
+  OLD_EXIT_CODE=$?
+  docker rm -f $CONTAINER_NAME >/dev/null
+  rm -rf ./addons-from-container
+  exit $OLD_EXIT_CODE
+}
+trap finish EXIT
 
 cd $(dirname $0)/../..
 
 IMAGE=$(cat config/kubermatic/values.yaml |grep 'addons:' -A3|grep repository|awk '{ print $2 }'|tr -d '"')
 TAG=$(cat config/kubermatic/values.yaml |grep 'addons:' -A3|grep tag|awk '{ print $2 }'|tr -d '"')
 
-docker run --rm \
-  -v $PWD/addons:/tmp/addons-from-repo \
-    $IMAGE:$TAG \
-      /bin/sh -c "cd /tmp/addons-from-repo && cp .dockerignore Dockerfile README.md /addons/ && diff --brief -r /addons /tmp/addons-from-repo"
+CONTAINER_NAME=$(docker create $IMAGE:$TAG)
+
+docker cp $CONTAINER_NAME:/addons ./addons-from-container
+
+cp addons/.dockerignore ./addons-from-container
+cp addons/Dockerfile ./addons-from-container
+cp addons/README.md ./addons-from-container
+
+diff --brief -r ./addons ./addons-from-container
