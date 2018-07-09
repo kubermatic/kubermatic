kubermatic/kubermatic

commit 139e70fed4d254800b8ef48133eda4838abbc968

cluster-dns (Part 2/2) (#1379)
* Add more OpenVPN sidecars and use user cluster DNS
* K8S Default DNS searches for user-cluster master components
* Add admission-webhook test files.

api/pkg/resources/controllermanager/deployment.go
@@ -74,6 +74,12 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 		return nil, err
 	}
 
+	// Configure user cluster DNS resolver for this pod.
+	dep.Spec.Template.Spec.DNSPolicy, dep.Spec.Template.Spec.DNSConfig, err = resources.UserClusterDNSPolicyAndConfig(data.Cluster)
+	if err != nil {
+		return nil, err
+	}
+
 	dep.Spec.Template.Spec.Volumes = getVolumes()
 	dep.Spec.Template.Spec.InitContainers = []corev1.Container{
 		{
@@ -89,7 +95,13 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 			TerminationMessagePolicy: corev1.TerminationMessageReadFile,
 		},
 	}
+
+	openvpnSidecar, err := resources.OpenVPNSidecarContainer(data, "openvpn-client")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get openvpn sidecar: %v", err)
+	}
 	dep.Spec.Template.Spec.Containers = []corev1.Container{
+		*openvpnSidecar,
 		{
 			Name:            name,
 			Image:           data.ImageRegistry(resources.RegistryKubernetesGCR) + "/google_containers/hyperkube-amd64:v" + data.Cluster.Spec.Version,
@@ -266,6 +278,15 @@ func getVolumes() []corev1.Volume {
 						Name: resources.CloudConfigConfigMapName,
 					},
 					DefaultMode: resources.Int32(420),
+				},
+			},
+		},
+		{
+			Name: resources.OpenVPNClientCertificatesSecretName,
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName:  resources.OpenVPNClientCertificatesSecretName,
+					DefaultMode: resources.Int32(resources.DefaultOwnerReadOnlyMode),
 				},
 			},
 		},

api/pkg/resources/test/fixtures/deployment-vsphere-1.10.0-controller-manager.yaml
@@ -36,6 +36,70 @@ spec:
     spec:
       containers:
       - args:
+        - --client
+        - --proto
+        - tcp
+        - --dev
+        - tun
+        - --auth-nocache
+        - --remote
+        - 192.0.2.13
+        - "1194"
+        - --nobind
+        - --connect-timeout
+        - "5"
+        - --connect-retry
+        - "1"
+        - --ca
+        - /etc/kubernetes/ca-cert/ca.crt
+        - --cert
+        - /etc/openvpn/certs/client.crt
+        - --key
+        - /etc/openvpn/certs/client.key
+        - --remote-cert-tls
+        - server
+        - --link-mtu
+        - "1432"
+        - --cipher
+        - AES-256-GCM
+        - --auth
+        - SHA1
+        - --keysize
+        - "256"
+        - --script-security
+        - "2"
+        - --status
+        - /run/openvpn-status
+        - --log
+        - /dev/stdout
+        command:
+        - /usr/sbin/openvpn
+        image: docker.io/kubermatic/openvpn:v0.4
+        imagePullPolicy: IfNotPresent
+        name: openvpn-client
+        resources:
+          limits:
+            cpu: 40m
+            memory: 64Mi
+          requests:
+            cpu: 10m
+            memory: 30Mi
+        securityContext:
+          privileged: true
+        terminationMessagePath: /dev/termination-log
+        terminationMessagePolicy: File
+        volumeMounts:
+        - mountPath: /etc/openvpn/certs
+          name: openvpn-client-certificates
+          readOnly: true
+        - mountPath: /etc/kubernetes/ca-cert
+          name: ca-cert
+          readOnly: true
+        - mountPath: /sys/class/dmi/id/product_serial
+          name: cloud-config
+          readOnly: true
+          subPath: fakeVmwareUUID
+      - args:
         - --master
         - http://192.0.2.11:8080
         - --service-account-private-key-file
@@ -103,10 +167,17 @@ spec:
         - mountPath: /etc/kubernetes/cloud
           name: cloud-config
           readOnly: true
-        - mountPath: /sys/class/dmi/id/product_serial
-          name: cloud-config
-          readOnly: true
-          subPath: fakeVmwareUUID
+      dnsConfig:
+        nameservers:
+        - 10.10.10.10
+        options:
+        - name: ndots
+          value: "5"
+        searches:
+        - kube-system.svc.cluster.local
+        - svc.cluster.local
+        - cluster.local
+      dnsPolicy: None
       initContainers:
       - command:
         - /bin/sh
@@ -136,4 +207,8 @@ spec:
           defaultMode: 420
           name: cloud-config
         name: cloud-config
+      - name: openvpn-client-certificates
+        secret:
+          defaultMode: 256
+          secretName: openvpn-client-certificates
 status: {}

api/pkg/resources/test/fixtures/deployment-vsphere-1.8.0-controller-manager.yaml
@@ -36,6 +36,70 @@ spec:
     spec:
       containers:
       - args:
+        - --client
+        - --proto
+        - tcp
+        - --dev
+        - tun
+        - --auth-nocache
+        - --remote
+        - 192.0.2.13
+        - "1194"
+        - --nobind
+        - --connect-timeout
+        - "5"
+        - --connect-retry
+        - "1"
+        - --ca
+        - /etc/kubernetes/ca-cert/ca.crt
+        - --cert
+        - /etc/openvpn/certs/client.crt
+        - --key
+        - /etc/openvpn/certs/client.key
+        - --remote-cert-tls
+        - server
+        - --link-mtu
+        - "1432"
+        - --cipher
+        - AES-256-GCM
+        - --auth
+        - SHA1
+        - --keysize
+        - "256"
+        - --script-security
+        - "2"
+        - --status
+        - /run/openvpn-status
+        - --log
+        - /dev/stdout
+        command:
+        - /usr/sbin/openvpn
+        image: docker.io/kubermatic/openvpn:v0.4
+        imagePullPolicy: IfNotPresent
+        name: openvpn-client
+        resources:
+          limits:
+            cpu: 40m
+            memory: 64Mi
+          requests:
+            cpu: 10m
+            memory: 30Mi
+        securityContext:
+          privileged: true
+        terminationMessagePath: /dev/termination-log
+        terminationMessagePolicy: File
+        volumeMounts:
+        - mountPath: /etc/openvpn/certs
+          name: openvpn-client-certificates
+          readOnly: true
+        - mountPath: /etc/kubernetes/ca-cert
+          name: ca-cert
+          readOnly: true
+        - mountPath: /sys/class/dmi/id/product_serial
+          name: cloud-config
+          readOnly: true
+          subPath: fakeVmwareUUID
+      - args:
         - --master
         - http://192.0.2.11:8080
         - --service-account-private-key-file
@@ -103,10 +167,17 @@ spec:
         - mountPath: /etc/kubernetes/cloud
           name: cloud-config
           readOnly: true
-        - mountPath: /sys/class/dmi/id/product_serial
-          name: cloud-config
-          readOnly: true
-          subPath: fakeVmwareUUID
+      dnsConfig:
+        nameservers:
+        - 10.10.10.10
+        options:
+        - name: ndots
+          value: "5"
+        searches:
+        - kube-system.svc.cluster.local
+        - svc.cluster.local
+        - cluster.local
+      dnsPolicy: None
       initContainers:
       - command:
         - /bin/sh
@@ -136,4 +207,8 @@ spec:
           defaultMode: 420
           name: cloud-config
         name: cloud-config
+      - name: openvpn-client-certificates
+        secret:
+          defaultMode: 256
+          secretName: openvpn-client-certificates
 status: {}

api/pkg/resources/test/fixtures/deployment-vsphere-1.9.0-controller-manager.yaml
@@ -36,6 +36,70 @@ spec:
     spec:
       containers:
       - args:
+        - --client
+        - --proto
+        - tcp
+        - --dev
+        - tun
+        - --auth-nocache
+        - --remote
+        - 192.0.2.13
+        - "1194"
+        - --nobind
+        - --connect-timeout
+        - "5"
+        - --connect-retry
+        - "1"
+        - --ca
+        - /etc/kubernetes/ca-cert/ca.crt
+        - --cert
+        - /etc/openvpn/certs/client.crt
+        - --key
+        - /etc/openvpn/certs/client.key
+        - --remote-cert-tls
+        - server
+        - --link-mtu
+        - "1432"
+        - --cipher
+        - AES-256-GCM
+        - --auth
+        - SHA1
+        - --keysize
+        - "256"
+        - --script-security
+        - "2"
+        - --status
+        - /run/openvpn-status
+        - --log
+        - /dev/stdout
+        command:
+        - /usr/sbin/openvpn
+        image: docker.io/kubermatic/openvpn:v0.4
+        imagePullPolicy: IfNotPresent
+        name: openvpn-client
+        resources:
+          limits:
+            cpu: 40m
+            memory: 64Mi
+          requests:
+            cpu: 10m
+            memory: 30Mi
+        securityContext:
+          privileged: true
+        terminationMessagePath: /dev/termination-log
+        terminationMessagePolicy: File
+        volumeMounts:
+        - mountPath: /etc/openvpn/certs
+          name: openvpn-client-certificates
+          readOnly: true
+        - mountPath: /etc/kubernetes/ca-cert
+          name: ca-cert
+          readOnly: true
+        - mountPath: /sys/class/dmi/id/product_serial
+          name: cloud-config
+          readOnly: true
+          subPath: fakeVmwareUUID
+      - args:
         - --master
         - http://192.0.2.11:8080
         - --service-account-private-key-file
@@ -103,10 +167,17 @@ spec:
         - mountPath: /etc/kubernetes/cloud
           name: cloud-config
           readOnly: true
-        - mountPath: /sys/class/dmi/id/product_serial
-          name: cloud-config
-          readOnly: true
-          subPath: fakeVmwareUUID
+      dnsConfig:
+        nameservers:
+        - 10.10.10.10
+        options:
+        - name: ndots
+          value: "5"
+        searches:
+        - kube-system.svc.cluster.local
+        - svc.cluster.local
+        - cluster.local
+      dnsPolicy: None
       initContainers:
       - command:
         - /bin/sh
@@ -136,4 +207,8 @@ spec:
           defaultMode: 420
           name: cloud-config
         name: cloud-config
+      - name: openvpn-client-certificates
+        secret:
+          defaultMode: 256
+          secretName: openvpn-client-certificates
 status: {}

api/pkg/resources/apiserver/deployment.go
@@ -20,11 +20,6 @@ var (
 	defaultApiserverCPURequest    = resource.MustParse("100m")
 	defaultApiserverMemoryLimit   = resource.MustParse("1Gi")
 	defaultApiserverCPULimit      = resource.MustParse("500m")
-
-	defaultOpenVPNMemoryRequest = resource.MustParse("30Mi")
-	defaultOpenVPNCPURequest    = resource.MustParse("10m")
-	defaultOpenVPNMemoryLimit   = resource.MustParse("64Mi")
-	defaultOpenVPNCPULimit      = resource.MustParse("40m")
 )
 
 const (
@@ -90,6 +85,11 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 		return nil, err
 	}
 
+	// Configure user cluster DNS resolver for this pod.
+	dep.Spec.Template.Spec.DNSPolicy, dep.Spec.Template.Spec.DNSConfig, err = resources.UserClusterDNSPolicyAndConfig(data.Cluster)
+	if err != nil {
+		return nil, err
+	}
 	dep.Spec.Template.Spec.Volumes = getVolumes()
 	dep.Spec.Template.Spec.InitContainers = []corev1.Container{
 		{
@@ -106,65 +106,12 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 		},
 	}
 
-	openvpnServiceIP, err := data.ClusterIPByServiceName(resources.OpenVPNServerServiceName)
+	openvpnSidecar, err := resources.OpenVPNSidecarContainer(data, "openvpn-client")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to get openvpn sidecar: %v", err)
 	}
 	dep.Spec.Template.Spec.Containers = []corev1.Container{
-		{
-			Name:            "openvpn-client",
-			Image:           data.ImageRegistry(resources.RegistryDocker) + "/kubermatic/openvpn:v0.4",
-			ImagePullPolicy: corev1.PullIfNotPresent,
-			Command:         []string{"/usr/sbin/openvpn"},
-			Args: []string{
-				"--client",
-				"--proto", "tcp",
-				"--dev", "tun",
-				"--auth-nocache",
-				"--remote", openvpnServiceIP, "1194",
-				"--nobind",
-				"--connect-timeout", "5",
-				"--connect-retry", "1",
-				"--ca", "/etc/kubernetes/ca-cert/ca.crt",
-				"--cert", "/etc/openvpn/certs/client.crt",
-				"--key", "/etc/openvpn/certs/client.key",
-				"--remote-cert-tls", "server",
-				"--link-mtu", "1432",
-				"--cipher", "AES-256-GCM",
-				"--auth", "SHA1",
-				"--keysize", "256",
-				"--script-security", "2",
-				"--status", "/run/openvpn-status",
-				"--log", "/dev/stdout",
-			},
-			SecurityContext: &corev1.SecurityContext{
-				Privileged: resources.Bool(true),
-			},
-			TerminationMessagePath:   corev1.TerminationMessagePathDefault,
-			TerminationMessagePolicy: corev1.TerminationMessageReadFile,
-			Resources: corev1.ResourceRequirements{
-				Requests: corev1.ResourceList{
-					corev1.ResourceMemory: defaultOpenVPNMemoryRequest,
-					corev1.ResourceCPU:    defaultOpenVPNCPURequest,
-				},
-				Limits: corev1.ResourceList{
-					corev1.ResourceMemory: defaultOpenVPNMemoryLimit,
-					corev1.ResourceCPU:    defaultOpenVPNCPULimit,
-				},
-			},
-			VolumeMounts: []corev1.VolumeMount{
-				{
-					MountPath: "/etc/openvpn/certs",
-					Name:      resources.OpenVPNClientCertificatesSecretName,
-					ReadOnly:  true,
-				},
-				{
-					MountPath: "/etc/kubernetes/ca-cert",
-					Name:      resources.CACertSecretName,
-					ReadOnly:  true,
-				},
-			},
-		},
+		*openvpnSidecar,
 		{
 			Name:            name,
 			Image:           data.ImageRegistry(resources.RegistryKubernetesGCR) + "/google_containers/hyperkube-amd64:v" + data.Cluster.Spec.Version,

api/pkg/resources/scheduler/deployment.go
@@ -64,12 +64,23 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 		},
 	}
 
-	// get clusterIP of apiserver
+	// get openvpn sidecar container and apiserverServiceIP
 	apiserverServiceIP, err := data.ClusterIPByServiceName(resources.ApiserverInternalServiceName)
 	if err != nil {
 		return nil, err
 	}
+	openvpnSidecar, err := resources.OpenVPNSidecarContainer(data, "openvpn-client")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get openvpn sidecar: %v", err)
+	}
 
+	// Configure user cluster DNS resolver for this pod.
+	dep.Spec.Template.Spec.DNSPolicy, dep.Spec.Template.Spec.DNSConfig, err = resources.UserClusterDNSPolicyAndConfig(data.Cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	dep.Spec.Template.Spec.Volumes = getVolumes()
 	dep.Spec.Template.Spec.InitContainers = []corev1.Container{
 		{
 			Name:            "apiserver-running",
@@ -85,6 +96,7 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 		},
 	}
 	dep.Spec.Template.Spec.Containers = []corev1.Container{
+		*openvpnSidecar,
 		{
 			Name:            name,
 			Image:           data.ImageRegistry(resources.RegistryKubernetesGCR) + "/google_containers/hyperkube-amd64:v" + data.Cluster.Spec.Version,
@@ -135,4 +147,27 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 	}
 
 	return dep, nil
+}
+
+func getVolumes() []corev1.Volume {
+	return []corev1.Volume{
+		{
+			Name: resources.OpenVPNClientCertificatesSecretName,
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName:  resources.OpenVPNClientCertificatesSecretName,
+					DefaultMode: resources.Int32(resources.DefaultOwnerReadOnlyMode),
+				},
+			},
+		},
+		{
+			Name: resources.CACertSecretName,
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName:  resources.CACertSecretName,
+					DefaultMode: resources.Int32(resources.DefaultOwnerReadOnlyMode),
+				},
+			},
+		},
+	}
 }

commit 54a1993a569641decd1fbb344f10dba0fbad6788

fix VMWare bug introduced by #1379
Commit fa4e3d60ad5a3a804b822ba02850d015f08a0e50 introduced a new container
to the controller-manager pod, however later code added a critical volume mount
to the original container, indexing it by costant int 0. However, adding
the new container in front shifted the old container to index 1.
Thus the volume mount was added to the wrong container.

api/pkg/resources/controllermanager/deployment.go
@@ -100,6 +100,40 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 	if err != nil {
 		return nil, fmt.Errorf("failed to get openvpn sidecar: %v", err)
 	}
+
+	controllerManagerMounts := []corev1.VolumeMount{
+		{
+			Name:      resources.CACertSecretName,
+			MountPath: "/etc/kubernetes/ca-cert",
+			ReadOnly:  true,
+		},
+		{
+			Name:      resources.CAKeySecretName,
+			MountPath: "/etc/kubernetes/ca-key",
+			ReadOnly:  true,
+		},
+		{
+			Name:      resources.ServiceAccountKeySecretName,
+			MountPath: "/etc/kubernetes/service-account-key",
+			ReadOnly:  true,
+		},
+		{
+			Name:      resources.CloudConfigConfigMapName,
+			MountPath: "/etc/kubernetes/cloud",
+			ReadOnly:  true,
+		},
+	}
+	if data.Cluster.Spec.Cloud.VSphere != nil {
+		fakeVMWareUUIDMount := corev1.VolumeMount{
+			Name:      resources.CloudConfigConfigMapName,
+			SubPath:   cloudconfig.FakeVMWareUUIDKeyName,
+			MountPath: "/sys/class/dmi/id/product_serial",
+			ReadOnly:  true,
+		}
+		// Required because of https://github.com/kubernetes/kubernetes/issues/65145
+		controllerManagerMounts = append(controllerManagerMounts, fakeVMWareUUIDMount)
+	}
+
 	dep.Spec.Template.Spec.Containers = []corev1.Container{
 		*openvpnSidecar,
 		{
@@ -146,41 +180,8 @@ func Deployment(data *resources.TemplateData, existing *appsv1.Deployment) (*app
 				SuccessThreshold:    1,
 				TimeoutSeconds:      15,
 			},
-			VolumeMounts: []corev1.VolumeMount{
-				{
-					Name:      resources.CACertSecretName,
-					MountPath: "/etc/kubernetes/ca-cert",
-					ReadOnly:  true,
-				},
-				{
-					Name:      resources.CAKeySecretName,
-					MountPath: "/etc/kubernetes/ca-key",
-					ReadOnly:  true,
-				},
-				{
-					Name:      resources.ServiceAccountKeySecretName,
-					MountPath: "/etc/kubernetes/service-account-key",
-					ReadOnly:  true,
-				},
-				{
-					Name:      resources.CloudConfigConfigMapName,
-					MountPath: "/etc/kubernetes/cloud",
-					ReadOnly:  true,
-				},
-			},
+			VolumeMounts: controllerManagerMounts,
 		},
-	}
-
-	if data.Cluster.Spec.Cloud.VSphere != nil {
-		fakeVMWareUUIDMount := corev1.VolumeMount{
-			Name:      resources.CloudConfigConfigMapName,
-			SubPath:   cloudconfig.FakeVMWareUUIDKeyName,
-			MountPath: "/sys/class/dmi/id/product_serial",
-			ReadOnly:  true,
-		}
-		// Required because of https://github.com/kubernetes/kubernetes/issues/65145
-		dep.Spec.Template.Spec.Containers[0].VolumeMounts = append(dep.Spec.Template.Spec.Containers[0].VolumeMounts,
-			fakeVMWareUUIDMount)
 	}
 
 	return dep, nil

api/pkg/resources/test/fixtures/deployment-vsphere-1.10.0-controller-manager.yaml
@@ -95,10 +95,6 @@ spec:
         - mountPath: /etc/kubernetes/ca-cert
           name: ca-cert
           readOnly: true
-        - mountPath: /sys/class/dmi/id/product_serial
-          name: cloud-config
-          readOnly: true
-          subPath: fakeVmwareUUID
       - args:
         - --master
         - http://192.0.2.11:8080
@@ -167,6 +163,10 @@ spec:
         - mountPath: /etc/kubernetes/cloud
           name: cloud-config
           readOnly: true
+        - mountPath: /sys/class/dmi/id/product_serial
+          name: cloud-config
+          readOnly: true
+          subPath: fakeVmwareUUID
       dnsConfig:
         nameservers:
         - 10.10.10.10

api/pkg/resources/test/fixtures/deployment-vsphere-1.8.0-controller-manager.yaml
@@ -95,10 +95,6 @@ spec:
         - mountPath: /etc/kubernetes/ca-cert
           name: ca-cert
           readOnly: true
-        - mountPath: /sys/class/dmi/id/product_serial
-          name: cloud-config
-          readOnly: true
-          subPath: fakeVmwareUUID
       - args:
         - --master
         - http://192.0.2.11:8080
@@ -167,6 +163,10 @@ spec:
         - mountPath: /etc/kubernetes/cloud
           name: cloud-config
           readOnly: true
+        - mountPath: /sys/class/dmi/id/product_serial
+          name: cloud-config
+          readOnly: true
+          subPath: fakeVmwareUUID
       dnsConfig:
         nameservers:
         - 10.10.10.10

api/pkg/resources/test/fixtures/deployment-vsphere-1.9.0-controller-manager.yaml
@@ -95,10 +95,6 @@ spec:
         - mountPath: /etc/kubernetes/ca-cert
           name: ca-cert
           readOnly: true
-        - mountPath: /sys/class/dmi/id/product_serial
-          name: cloud-config
-          readOnly: true
-          subPath: fakeVmwareUUID
       - args:
         - --master
         - http://192.0.2.11:8080
@@ -167,6 +163,10 @@ spec:
         - mountPath: /etc/kubernetes/cloud
           name: cloud-config
           readOnly: true
+        - mountPath: /sys/class/dmi/id/product_serial
+          name: cloud-config
+          readOnly: true
+          subPath: fakeVmwareUUID
       dnsConfig:
         nameservers:
         - 10.10.10.10