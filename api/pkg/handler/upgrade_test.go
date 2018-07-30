09783abdd3e03e515cd08d15515bf76b46f70e65
Kamil Domański <kamil@domanski.co>
2018-07-30 15:00:14 +0200

    update cluster-upgrade endpoint test to test with project and without

--- api/pkg/handler/upgrade_test.go
+++ api/pkg/handler/upgrade_test.go
@@ -21,6 +21,9 @@ func TestGetClusterUpgrades(t *testing.T) {
 	tests := []struct {
 		name        string
 		cluster     *kubermaticv1.Cluster
+		project     *kubermaticv1.Project
+		user        *kubermaticv1.User
+		apiUser     apiv1.User
 		versions    []*version.MasterVersion
 		updates     []*version.MasterUpdate
 		wantUpdates []*apiv1.MasterVersion
@@ -33,6 +36,39 @@ func TestGetClusterUpgrades(t *testing.T) {
 					Labels: map[string]string{"user": testUsername},
 				},
 				Spec: kubermaticv1.ClusterSpec{Version: "1.6.0"},
+			},
+			project: &kubermaticv1.Project{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "myProjectInternalName",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: "kubermatic.io/v1",
+							Kind:       "User",
+							UID:        "",
+							Name:       "my-first-project",
+						},
+					},
+				},
+				Spec: kubermaticv1.ProjectSpec{Name: "my-first-project"},
+			},
+			user: &kubermaticv1.User{
+				Spec: kubermaticv1.UserSpec{
+					Name:  "George",
+					Email: testEmail,
+					Projects: []kubermaticv1.ProjectGroup{
+						{
+							Group: "owners-myProjectInternalName",
+							Name:  "myProjectInternalName",
+						},
+					},
+				},
+			},
+			apiUser: apiv1.User{
+				ID:    testUsername,
+				Email: testEmail,
+				Roles: map[string]struct{}{
+					"user": struct{}{},
+				},
 			},
 			wantUpdates: []*apiv1.MasterVersion{
 				{
@@ -75,6 +111,39 @@ func TestGetClusterUpgrades(t *testing.T) {
 				},
 				Spec: kubermaticv1.ClusterSpec{Version: "1.6.0"},
 			},
+			project: &kubermaticv1.Project{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "myProjectInternalName",
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: "kubermatic.io/v1",
+							Kind:       "User",
+							UID:        "",
+							Name:       "my-first-project",
+						},
+					},
+				},
+				Spec: kubermaticv1.ProjectSpec{Name: "my-first-project"},
+			},
+			user: &kubermaticv1.User{
+				Spec: kubermaticv1.UserSpec{
+					Name:  "John",
+					Email: testEmail,
+					Projects: []kubermaticv1.ProjectGroup{
+						{
+							Group: "owners-myProjectInternalName",
+							Name:  "myProjectInternalName",
+						},
+					},
+				},
+			},
+			apiUser: apiv1.User{
+				ID:    testUsername,
+				Email: testEmail,
+				Roles: map[string]struct{}{
+					"user": struct{}{},
+				},
+			},
 			wantUpdates: []*apiv1.MasterVersion{},
 			versions: []*version.MasterVersion{
 				{
@@ -86,28 +155,48 @@ func TestGetClusterUpgrades(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			req := httptest.NewRequest("GET", "/api/v3/dc/us-central1/cluster/foo/upgrades", nil)
-			res := httptest.NewRecorder()
-			ep, err := createTestEndpoint(getUser(testUsername, false), []runtime.Object{}, []runtime.Object{test.cluster}, test.versions, test.updates)
-			if err != nil {
-				t.Fatalf("failed to create test endpoint due to %v", err)
-			}
-			ep.ServeHTTP(res, req)
-			if res.Code != http.StatusOK {
-				t.Errorf("Expected status code to be 200, got %d", res.Code)
-				t.Error(res.Body.String())
-				return
+			runTest := func(t *testing.T, req *http.Request) {
+				res := httptest.NewRecorder()
+
+				kubermaticObj := []runtime.Object{test.cluster}
+				if test.project != nil {
+					kubermaticObj = append(kubermaticObj, test.project)
+				}
+				if test.user != nil {
+					kubermaticObj = append(kubermaticObj, test.user)
+				}
+
+				ep, err := createTestEndpoint(test.apiUser, []runtime.Object{}, kubermaticObj, test.versions, test.updates)
+				if err != nil {
+					t.Fatalf("failed to create test endpoint due to %v", err)
+				}
+				ep.ServeHTTP(res, req)
+				if res.Code != http.StatusOK {
+					t.Errorf("Expected status code to be 200, got %d", res.Code)
+					t.Error(res.Body.String())
+					return
+				}
+
+				var gotUpdates []*apiv1.MasterVersion
+				err = json.Unmarshal(res.Body.Bytes(), &gotUpdates)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if diff := deep.Equal(gotUpdates, test.wantUpdates); diff != nil {
+					t.Errorf("got different upgrade response than expected. Diff: %v", diff)
+				}
 			}
 
-			var gotUpdates []*apiv1.MasterVersion
-			err = json.Unmarshal(res.Body.Bytes(), &gotUpdates)
-			if err != nil {
-				t.Fatal(err)
-			}
+			t.Run("without project", func(t *testing.T) {
+				req := httptest.NewRequest("GET", "/api/v3/dc/us-central1/cluster/foo/upgrades", nil)
+				runTest(t, req)
+			})
 
-			if diff := deep.Equal(gotUpdates, test.wantUpdates); diff != nil {
-				t.Errorf("got different upgrade response than expected. Diff: %v", diff)
-			}
+			t.Run("with project", func(t *testing.T) {
+				req := httptest.NewRequest("GET", "/api/v3/projects/myProjectInternalName/dc/us-central1/cluster/foo/upgrades", nil)
+				runTest(t, req)
+			})
 		})
 	}
 }
