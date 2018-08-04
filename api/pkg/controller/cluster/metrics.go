13bbb6d98897d6c6a67d8fa6453658bc631d8fcd
Christoph Mewes <github@xrstf.de>
2018-08-04 20:38:40 +0200

    Count resource updates in seed clusters as a metric (#1671)

--- api/pkg/controller/cluster/metrics.go
+++ api/pkg/controller/cluster/metrics.go
@@ -3,6 +3,7 @@ package cluster
 import (
 	"sync"
 
+	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -18,6 +19,15 @@ var (
 			Help:      "The number of running cluster controller workers.",
 		},
 	)
+
+	updates = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Subsystem: clusterControllerSubsystem,
+			Name:      "updates",
+			Help:      "The number of times a seed cluster resource was updated.",
+		},
+		[]string{"cluster", "type", "resource_name"},
+	)
 )
 
 var registerMetrics sync.Once
@@ -25,7 +35,15 @@ var registerMetrics sync.Once
 // Register the metrics that are to be monitored.
 func init() {
 	registerMetrics.Do(func() {
-		prometheus.MustRegister(workers)
+		prometheus.MustRegister(workers, updates)
 		workers.Set(0)
 	})
+}
+
+func countSeedResourceUpdate(cluster *kubermaticv1.Cluster, typeName, resourceName string) {
+	updates.With(prometheus.Labels{
+		"cluster":       cluster.Name,
+		"type":          typeName,
+		"resource_name": resourceName,
+	}).Inc()
 }
