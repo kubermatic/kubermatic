a76985b4f0b0239fa7dbf831e472d2b9a4a4aafa
Christoph Mewes <github@xrstf.de>
2018-08-16 12:48:03 +0200

    Put region label in user-level Prometheus installations (#1786)
    add the -datacenter-name as a region label to user-cluster prometheus instances

--- api/pkg/resources/resources.go
+++ api/pkg/resources/resources.go
@@ -278,6 +278,7 @@ type PodDisruptionBudgetCreator = func(data *TemplateData, existing *policyv1bet
 type TemplateData struct {
 	Cluster           *kubermaticv1.Cluster
 	DC                *provider.DatacenterMeta
+	SeedDC            string
 	SecretLister      corev1lister.SecretLister
 	ConfigMapLister   corev1lister.ConfigMapLister
 	ServiceLister     corev1lister.ServiceLister
@@ -322,6 +323,7 @@ func String(v string) *string {
 func NewTemplateData(
 	cluster *kubermaticv1.Cluster,
 	dc *provider.DatacenterMeta,
+	seedDatacenter string,
 	secretLister corev1lister.SecretLister,
 	configMapLister corev1lister.ConfigMapLister,
 	serviceLister corev1lister.ServiceLister,
@@ -332,6 +334,7 @@ func NewTemplateData(
 	return &TemplateData{
 		Cluster:           cluster,
 		DC:                dc,
+		SeedDC:            seedDatacenter,
 		ConfigMapLister:   configMapLister,
 		SecretLister:      secretLister,
 		ServiceLister:     serviceLister,
