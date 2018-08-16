fe51b6f0f6e6707a39c7bce933580d6d475c5e89
Christoph Mewes <github@xrstf.de>
2018-08-16 12:48:03 +0200

    Put region label in user-level Prometheus installations (#1786)
    add the -datacenter-name as a region label to user-cluster prometheus instances

--- api/pkg/resources/prometheus/configmap.go
+++ api/pkg/resources/prometheus/configmap.go
@@ -47,6 +47,7 @@ const prometheusConfig = `global:
   scrape_interval: 30s
   external_labels:
     cluster: "{{ .Cluster.Name }}"
+    region: "{{ .SeedDC }}"
 rule_files:
 - "/etc/prometheus/config/rules.yaml"
 scrape_configs:
