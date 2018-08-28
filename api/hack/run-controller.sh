6bd32be8258a2900cbb71cad34f806fa812e20fe
Tobias Hintze <thz@users.noreply.github.com>
2018-08-28 14:21:51 +0200

    unbreak hack/run-controller.sh (#1882)
    Flag -docker-pull-config-json-file was still -image-pull-secret in log and hack-script.

--- api/hack/run-controller.sh
+++ api/hack/run-controller.sh
@@ -21,6 +21,6 @@ cd $(go env GOPATH)/src/github.com/kubermatic/kubermatic/api
   -external-url=dev.kubermatic.io \
   -backup-container=../config/kubermatic/static/backup-container.yaml \
   -cleanup-container=../config/kubermatic/static/cleanup-container.yaml \
-  -image-pull-secret=../../secrets/seed-clusters/dev.kubermatic.io/.dockerconfigjson \
+  -docker-pull-config-json-file=../../secrets/seed-clusters/dev.kubermatic.io/.dockerconfigjson \
   -logtostderr=1 \
   -v=6 $@
