2f74cd93470726fca06ffcd401813e85a716872f
mrIncompetent <henrik@henrik-schmidt.de>
2018-09-14 17:13:57 +0200

    extract ensure cronjob

--- api/pkg/resources/ensure.go
+++ api/pkg/resources/ensure.go
@@ -3,11 +3,14 @@ package resources
 import (
 	"fmt"
 
-	"k8s.io/api/policy/v1beta1"
+	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
+	batchv1beta1client "k8s.io/client-go/kubernetes/typed/batch/v1beta1"
 	policyv1beta1client "k8s.io/client-go/kubernetes/typed/policy/v1beta1"
 	rbacv1client "k8s.io/client-go/kubernetes/typed/rbac/v1"
+	batchv1beta1lister "k8s.io/client-go/listers/batch/v1beta1"
 	policyv1beta1lister "k8s.io/client-go/listers/policy/v1beta1"
 	rbacv1lister "k8s.io/client-go/listers/rbac/v1"
 )
@@ -52,7 +55,7 @@ func EnsureRole(data RoleDataProvider, create RoleCreator, roleLister rbacv1list
 // EnsurePodDisruptionBudget will create the PodDisruptionBudget with the passed create function & create or update it if necessary.
 // To check if it's necessary it will do a lookup of the resource at the lister & compare the existing PodDisruptionBudget with the created one
 func EnsurePodDisruptionBudget(data *TemplateData, create PodDisruptionBudgetCreator, pdbLister policyv1beta1lister.PodDisruptionBudgetNamespaceLister, pdbClient policyv1beta1client.PodDisruptionBudgetInterface) error {
-	var existing *v1beta1.PodDisruptionBudget
+	var existing *policyv1beta1.PodDisruptionBudget
 	pdb, err := create(data, nil)
 	if err != nil {
 		return fmt.Errorf("failed to build PodDisruptionBudget: %v", err)
@@ -81,6 +84,43 @@ func EnsurePodDisruptionBudget(data *TemplateData, create PodDisruptionBudgetCre
 
 	if _, err = pdbClient.Update(pdb); err != nil {
 		return fmt.Errorf("failed to update PodDisruptionBudget %s: %v", pdb.Name, err)
+	}
+
+	return nil
+}
+
+// EnsureCronJob will create the CronJob with the passed create function & create or update it if necessary.
+// To check if it's necessary it will do a lookup of the resource at the lister & compare the existing CronJob with the created one
+func EnsureCronJob(data *TemplateData, create CronJobCreator, cronJobLister batchv1beta1lister.CronJobNamespaceLister, cronJobClient batchv1beta1client.CronJobInterface) error {
+	var existing *batchv1beta1.CronJob
+	cronjob, err := create(data, nil)
+	if err != nil {
+		return fmt.Errorf("failed to build CronJob: %v", err)
+	}
+
+	if existing, err = cronJobLister.Get(cronjob.Name); err != nil {
+		if !kubeerrors.IsNotFound(err) {
+			return err
+		}
+
+		if _, err = cronJobClient.Create(cronjob); err != nil {
+			return fmt.Errorf("failed to create CronJob %s: %v", cronjob.Name, err)
+		}
+		return nil
+	}
+	existing = existing.DeepCopy()
+
+	cronjob, err = create(data, existing.DeepCopy())
+	if err != nil {
+		return fmt.Errorf("failed to build CronJob: %v", err)
+	}
+
+	if DeepEqual(cronjob, existing) {
+		return nil
+	}
+
+	if _, err = cronJobClient.Update(cronjob); err != nil {
+		return fmt.Errorf("failed to update CronJob %s: %v", cronjob.Name, err)
 	}
 
 	return nil
