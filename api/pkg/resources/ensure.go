8cf4ad26625bde1cbc2b57abb31884e6663b74ed
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-09-14 14:05:31 +0200

    extract PodDisruptionBudget handling (#1993)
    * extract PodDisruptionBudget handling
    
    * ensure we always use the copy
    
    * add missing DeepCopy
    
    * fix typos

--- api/pkg/resources/ensure.go
+++ api/pkg/resources/ensure.go
@@ -3,14 +3,17 @@ package resources
 import (
 	"fmt"
 
+	"k8s.io/api/policy/v1beta1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
+	policyv1beta1client "k8s.io/client-go/kubernetes/typed/policy/v1beta1"
 	rbacv1client "k8s.io/client-go/kubernetes/typed/rbac/v1"
+	policyv1beta1lister "k8s.io/client-go/listers/policy/v1beta1"
 	rbacv1lister "k8s.io/client-go/listers/rbac/v1"
 )
 
 // EnsureRole will create the role with the passed create function & create or update it if necessary.
-// To check if its necessary it will do a lookup of the resource at the lister & compare the existing Role with the created one
+// To check if it's necessary it will do a lookup of the resource at the lister & compare the existing Role with the created one
 func EnsureRole(data RoleDataProvider, create RoleCreator, roleLister rbacv1lister.RoleNamespaceLister, roleClient rbacv1client.RoleInterface) error {
 	var existing *rbacv1.Role
 	role, err := create(data, nil)
@@ -28,6 +31,7 @@ func EnsureRole(data RoleDataProvider, create RoleCreator, roleLister rbacv1list
 		}
 		return nil
 	}
+	existing = existing.DeepCopy()
 
 	role, err = create(data, existing.DeepCopy())
 	if err != nil {
@@ -40,6 +44,43 @@ func EnsureRole(data RoleDataProvider, create RoleCreator, roleLister rbacv1list
 
 	if _, err = roleClient.Update(role); err != nil {
 		return fmt.Errorf("failed to update Role %s: %v", role.Name, err)
+	}
+
+	return nil
+}
+
+// EnsurePodDisruptionBudget will create the PodDisruptionBudget with the passed create function & create or update it if necessary.
+// To check if it's necessary it will do a lookup of the resource at the lister & compare the existing PodDisruptionBudget with the created one
+func EnsurePodDisruptionBudget(data *TemplateData, create PodDisruptionBudgetCreator, pdbLister policyv1beta1lister.PodDisruptionBudgetNamespaceLister, pdbClient policyv1beta1client.PodDisruptionBudgetInterface) error {
+	var existing *v1beta1.PodDisruptionBudget
+	pdb, err := create(data, nil)
+	if err != nil {
+		return fmt.Errorf("failed to build PodDisruptionBudget: %v", err)
+	}
+
+	if existing, err = pdbLister.Get(pdb.Name); err != nil {
+		if !kubeerrors.IsNotFound(err) {
+			return err
+		}
+
+		if _, err = pdbClient.Create(pdb); err != nil {
+			return fmt.Errorf("failed to create PodDisruptionBudget %s: %v", pdb.Name, err)
+		}
+		return nil
+	}
+	existing = existing.DeepCopy()
+
+	pdb, err = create(data, existing.DeepCopy())
+	if err != nil {
+		return fmt.Errorf("failed to build PodDisruptionBudget: %v", err)
+	}
+
+	if DeepEqual(pdb, existing) {
+		return nil
+	}
+
+	if _, err = pdbClient.Update(pdb); err != nil {
+		return fmt.Errorf("failed to update PodDisruptionBudget %s: %v", pdb.Name, err)
 	}
 
 	return nil
