c8ba9eb74ece781459a028570e0f5c2e44f60a6f
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-09-16 19:25:43 +0200

    extract ensure StatefulSet (#2003)
    * extract ensure statefulset
    
    * vendor: dep ensure

--- api/pkg/resources/ensure.go
+++ api/pkg/resources/ensure.go
@@ -3,13 +3,18 @@ package resources
 import (
 	"fmt"
 
+	appsv1 "k8s.io/api/apps/v1"
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1client "k8s.io/client-go/kubernetes/typed/apps/v1"
 	batchv1beta1client "k8s.io/client-go/kubernetes/typed/batch/v1beta1"
 	policyv1beta1client "k8s.io/client-go/kubernetes/typed/policy/v1beta1"
 	rbacv1client "k8s.io/client-go/kubernetes/typed/rbac/v1"
+	appsv1lister "k8s.io/client-go/listers/apps/v1"
 	batchv1beta1lister "k8s.io/client-go/listers/batch/v1beta1"
 	policyv1beta1lister "k8s.io/client-go/listers/policy/v1beta1"
 	rbacv1lister "k8s.io/client-go/listers/rbac/v1"
@@ -121,6 +126,49 @@ func EnsureCronJob(data *TemplateData, create CronJobCreator, cronJobLister batc
 
 	if _, err = cronJobClient.Update(cronjob); err != nil {
 		return fmt.Errorf("failed to update CronJob %s: %v", cronjob.Name, err)
+	}
+
+	return nil
+}
+
+// EnsureStatefulSet will create the StatefulSet with the passed create function & create or update it if necessary.
+// To check if it's necessary it will do a lookup of the resource at the lister & compare the existing StatefulSet with the created one
+func EnsureStatefulSet(data StatefulSetDataProvider, create StatefulSetCreator, lister appsv1lister.StatefulSetNamespaceLister, client appsv1client.StatefulSetInterface) error {
+	var existing *appsv1.StatefulSet
+	statefulSet, err := create(data, nil)
+	if err != nil {
+		return fmt.Errorf("failed to build StatefulSet: %v", err)
+	}
+
+	if existing, err = lister.Get(statefulSet.Name); err != nil {
+		if !kubeerrors.IsNotFound(err) {
+			return err
+		}
+
+		if _, err = client.Create(statefulSet); err != nil {
+			return fmt.Errorf("failed to create StatefulSet %s: %v", statefulSet.Name, err)
+		}
+		return nil
+	}
+	existing = existing.DeepCopy()
+
+	statefulSet, err = create(data, existing.DeepCopy())
+	if err != nil {
+		return fmt.Errorf("failed to build StatefulSet: %v", err)
+	}
+
+	if DeepEqual(statefulSet, existing) {
+		return nil
+	}
+
+	// In case we update something immutable we need to delete&recreate. Creation happens on next sync
+	if !equality.Semantic.DeepEqual(statefulSet.Spec.Selector.MatchLabels, existing.Spec.Selector.MatchLabels) {
+		propagation := metav1.DeletePropagationForeground
+		return client.Delete(statefulSet.Name, &metav1.DeleteOptions{PropagationPolicy: &propagation})
+	}
+
+	if _, err = client.Update(statefulSet); err != nil {
+		return fmt.Errorf("failed to update StatefulSet %s: %v", statefulSet.Name, err)
 	}
 
 	return nil
