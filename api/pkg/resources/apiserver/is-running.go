216b5578ad45a0d5b57ca99e8f2439b3159b00d3
Tobias Hintze <thz@users.noreply.github.com>
2018-09-20 22:24:47 +0200

    check for running apiserver with curl (#2052)
    Check apiserver health in initContainer with curl

--- api/pkg/resources/apiserver/is-running.go
+++ api/pkg/resources/apiserver/is-running.go
@@ -20,14 +20,23 @@ func IsRunningInitContainer(data resources.DeploymentDataProvider) (*corev1.Cont
 		Name:            "apiserver-running",
 		Image:           data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/curl:v0.1",
 		ImagePullPolicy: corev1.PullIfNotPresent,
-		Command:         []string{"/usr/bin/curl"},
+		Command:         []string{"/bin/sh"},
 		Args: []string{
-			"--retry", "100",
-			"--retry-delay", "2",
-			"--insecure",
-			"--silent",
-			"--show-error",
-			fmt.Sprintf("%s/healthz", url),
+			"-c",
+			fmt.Sprintf(`
+			name=%s
+			port=%s
+			# manual re-resolving enables use of trailing dot with curl
+			ip=$(getent hosts $name | cut -d" " -f1)
+			while ! curl --resolve $name:$port:$ip --insecure --silent --show-error --max-time 3 %s/healthz; do
+				if [ $(( timeout++ )) -gt 100 ]; then
+					echo "Giving up after 100 tries."
+					exit 1
+				fi
+				sleep 2
+				echo "Retry $timeout/100"
+				ip=$(getent hosts $name | cut -d" " -f1)
+			done`, url.Hostname(), url.Port(), url),
 		},
 		TerminationMessagePath:   corev1.TerminationMessagePathDefault,
 		TerminationMessagePolicy: corev1.TerminationMessageReadFile,
