1ed2122e40a72a1b8462854f16529101372185dc
mrIncompetent <henrik@henrik-schmidt.de>
2018-09-20 16:23:02 +0200

    use wget instead of curl

--- api/pkg/resources/apiserver/is-running.go
+++ api/pkg/resources/apiserver/is-running.go
@@ -8,6 +8,16 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+var cmdTpl = `
+for (( i=1; i<=100; i++ ))
+do
+    echo "[${i}] Probing apiserver..."
+    wget --tries=1 --timeout=1 --no-dns-cache --no-check-certificate %s/healthz && break
+    sleep 2
+done
+exit 1
+`
+
 // IsRunningInitContainer returns a init container which will wait until the apiserver is reachable via its ClusterIP
 func IsRunningInitContainer(data resources.DeploymentDataProvider) (*corev1.Container, error) {
 	// get clusterIP of apiserver
@@ -18,16 +28,12 @@ func IsRunningInitContainer(data resources.DeploymentDataProvider) (*corev1.Cont
 
 	return &corev1.Container{
 		Name:            "apiserver-running",
-		Image:           data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/curl:v0.1",
+		Image:           data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/curl:v0.2-dev-2",
 		ImagePullPolicy: corev1.PullIfNotPresent,
-		Command:         []string{"/usr/bin/curl"},
-		Args: []string{
-			"--retry", "100",
-			"--retry-delay", "2",
-			"--insecure",
-			"--silent",
-			"--show-error",
-			fmt.Sprintf("%s/healthz", url),
+		Command: []string{
+			"/bin/bash",
+			"-ecx",
+			fmt.Sprintf(cmdTpl, url),
 		},
 		TerminationMessagePath:   corev1.TerminationMessagePathDefault,
 		TerminationMessagePolicy: corev1.TerminationMessageReadFile,
