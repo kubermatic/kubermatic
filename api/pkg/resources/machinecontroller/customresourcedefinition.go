441b29e7ab02ce00e20f40bcb4b5c8b1dfc1f541
Alvaro Aleman <alv2412@googlemail.com>
2018-09-21 11:15:59 +0200

    Fix up CRDs

--- api/pkg/resources/machinecontroller/customresourcedefinition.go
+++ api/pkg/resources/machinecontroller/customresourcedefinition.go
@@ -6,6 +6,11 @@ import (
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 )
 
+const (
+	clusterAPIGroup   = "cluster.k8s.io"
+	clusterAPIVersion = "v1alpha1"
+)
+
 // MachineCRD returns the machine CRD definition
 func MachineCRD(existing *apiextensionsv1beta1.CustomResourceDefinition) (*apiextensionsv1beta1.CustomResourceDefinition, error) {
 	if existing == nil {
@@ -13,7 +18,11 @@ func MachineCRD(existing *apiextensionsv1beta1.CustomResourceDefinition) (*apiex
 	}
 
 	existing.Name = resources.MachineCRDName
-	existing.Spec = apiextensionsv1beta1.CustomResourceDefinitionSpec{}
+	existing.Spec.Group = clusterAPIGroup
+	existing.Spec.Version = clusterAPIVersion
+	existing.Spec.Scope = apiextensionsv1beta1.NamespaceScoped
+	existing.Spec.Names.Kind = "Machine"
+	existing.Spec.Names.Plural = "machines"
 
 	return existing, nil
 }
@@ -26,6 +35,11 @@ func MachineSetCRD(existing *apiextensionsv1beta1.CustomResourceDefinition) (*ap
 
 	existing.Name = resources.MachineSetCRDName
 	existing.Spec = apiextensionsv1beta1.CustomResourceDefinitionSpec{}
+	existing.Spec.Group = clusterAPIGroup
+	existing.Spec.Version = clusterAPIVersion
+	existing.Spec.Scope = apiextensionsv1beta1.NamespaceScoped
+	existing.Spec.Names.Kind = "MachineSet"
+	existing.Spec.Names.Plural = "machinesets"
 	existing.Spec.Subresources = &apiextensionsv1beta1.CustomResourceSubresources{Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{}}
 
 	return existing, nil
@@ -39,6 +53,11 @@ func MachineDeploymentCRD(existing *apiextensionsv1beta1.CustomResourceDefinitio
 
 	existing.Name = resources.MachineDeploymentCRDName
 	existing.Spec = apiextensionsv1beta1.CustomResourceDefinitionSpec{}
+	existing.Spec.Group = clusterAPIGroup
+	existing.Spec.Version = clusterAPIVersion
+	existing.Spec.Scope = apiextensionsv1beta1.NamespaceScoped
+	existing.Spec.Names.Kind = "MachineDeployment"
+	existing.Spec.Names.Plural = "machinedeployments"
 	existing.Spec.Subresources = &apiextensionsv1beta1.CustomResourceSubresources{Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{}}
 
 	return existing, nil
@@ -50,8 +69,13 @@ func ClusterCRD(existing *apiextensionsv1beta1.CustomResourceDefinition) (*apiex
 		existing = &apiextensionsv1beta1.CustomResourceDefinition{}
 	}
 
-	existing.Name = resources.MachineSetCRDName
+	existing.Name = resources.ClusterCRDName
 	existing.Spec = apiextensionsv1beta1.CustomResourceDefinitionSpec{}
+	existing.Spec.Group = clusterAPIGroup
+	existing.Spec.Version = clusterAPIVersion
+	existing.Spec.Scope = apiextensionsv1beta1.NamespaceScoped
+	existing.Spec.Names.Kind = "Cluster"
+	existing.Spec.Names.Plural = "clusters"
 	existing.Spec.Subresources = &apiextensionsv1beta1.CustomResourceSubresources{Status: &apiextensionsv1beta1.CustomResourceSubresourceStatus{}}
 
 	return existing, nil
