feef6ee4ca8f2797c92e36d531e28d1d9899e2db
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-09-27 01:34:12 +0200

    Cherry pick user id hashing (#2075)

--- api/pkg/provider/kubernetes/user.go
+++ api/pkg/provider/kubernetes/user.go
@@ -11,7 +11,8 @@ import (
 )
 
 const (
-	userLabelKey = "user"
+	// UserLabelKey defines the label key for the user -> cluster relation
+	UserLabelKey = "user"
 )
 
 // NewUserProvider returns a user provider
