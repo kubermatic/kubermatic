aa53c67822fa0df0c5a0bc5231e8e55aa8887668
mrIncompetent <henrik@henrik-schmidt.de>
2018-09-29 22:24:20 +0200

    fix wrong env var & specify boilerplate code flag

--- api/pkg/controller/ipam/testdata/gen-clusterapi-client.sh
+++ api/pkg/controller/ipam/testdata/gen-clusterapi-client.sh
@@ -6,17 +6,20 @@ cd $(dirname $0)/../../../..
 
 TEMPDIR=$(mktemp -d)
 
-type client-gen &>/dev/null || go build -o $GOPATH/bin/client-gen github.com/kubermatic/kubermatic/api/vendor/k8s.io/code-generator/cmd/client-gen
+type client-gen &>/dev/null || go build -o $(go env GOPATH)/bin/client-gen github.com/kubermatic/kubermatic/api/vendor/k8s.io/code-generator/cmd/client-gen
 
 ls $(go env GOPATH)/src/sigs.k8s.io/cluster-api &>/dev/null || git clone https://github.com/kubernetes-sigs/cluster-api.git $(go env GOPATH)/src/sigs.k8s.io/cluster-api
+
+touch ${TEMPDIR}/header.txt
 
 client-gen --clientset-name clientset \
   --input-base sigs.k8s.io/cluster-api/pkg/apis \
   --input cluster/v1alpha1 \
-  --output-base=$TEMPDIR \
+  --output-base=${TEMPDIR} \
+  --go-header-file=${TEMPDIR}/header.txt \
   --output-package="sigs.k8s.io/cluster-api/pkg/client/clientset_generated"
 
-mv $TEMPDIR/sigs.k8s.io/cluster-api/pkg/client/clientset_generated/clientset/fake/* \
+mv ${TEMPDIR}/sigs.k8s.io/cluster-api/pkg/client/clientset_generated/clientset/fake/* \
   vendor/sigs.k8s.io/cluster-api/pkg/client/clientset_generated/clientset/fake/
 
-rm -rf $TEMPDIR
+rm -rf ${TEMPDIR}
