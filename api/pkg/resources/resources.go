f5d9426709104e5604ad5fdce055f0a2073fd2d5
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-09-29 22:58:26 +0200

    Add apiserver pdb (#2098)
    * add PodDisruptionBudget for the apiserver deployment
    
    * update fixtures
    
    * add missing fixtures

--- api/pkg/resources/resources.go
+++ api/pkg/resources/resources.go
@@ -182,6 +182,8 @@ const (
 
 	// EtcdPodDisruptionBudgetName is the name of the PDB for the etcd statefulset
 	EtcdPodDisruptionBudgetName = "etcd"
+	// ApiserverPodDisruptionBudgetName is the name of the PDB for the apiserver deployment
+	ApiserverPodDisruptionBudgetName = "apiserver"
 
 	// DefaultOwnerReadOnlyMode represents file mode with read permission for owner only
 	DefaultOwnerReadOnlyMode = 0400
