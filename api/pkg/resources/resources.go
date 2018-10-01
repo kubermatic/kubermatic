493cc5638a995e476b9af38cfb5956657deb89a1
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2018-10-01 14:43:02 +0200

    Do not run controller-manager and its controllers with cluster-admin permissions (#2096)

--- api/pkg/resources/resources.go
+++ api/pkg/resources/resources.go
@@ -175,10 +175,6 @@ const (
 	MachineControllerClusterRoleBindingName = "system:kubermatic-machine-controller"
 	//KubeStateMetricsClusterRoleBindingName is the name for the KubeStateMetrics clusterrolebinding
 	KubeStateMetricsClusterRoleBindingName = "system:kubermatic-kube-state-metrics"
-	//ControllerManagerRoleBindingName is the name of the controller-manager's rolebindings
-	ControllerManagerRoleBindingName = "kubermatic:controller-manager"
-	//ControllerManagerClusterRoleBindingName is the name of the controller-manager's clusterrolebindings
-	ControllerManagerClusterRoleBindingName = "kubermatic:controller-manager"
 
 	// EtcdPodDisruptionBudgetName is the name of the PDB for the etcd statefulset
 	EtcdPodDisruptionBudgetName = "etcd"
