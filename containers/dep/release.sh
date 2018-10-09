3df9a08ab37b49448419412b89b65839802e0059
mrIncompetent <henrik@henrik-schmidt.de>
2018-10-09 11:33:49 +0200

    add build number to tag

--- containers/dep/release.sh
+++ containers/dep/release.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-ver=0.5.0
+NUMBER=1
+VERSION=0.5.0
+
 
 set -euox pipefail
 
-docker build --no-cache --pull -t quay.io/kubermatic/dep:$ver .
-docker push quay.io/kubermatic/dep:$ver
+docker build --no-cache --pull -t quay.io/kubermatic/dep:${VERSION}-${NUMBER} .
+docker push quay.io/kubermatic/dep:${VERSION}-${NUMBER}
