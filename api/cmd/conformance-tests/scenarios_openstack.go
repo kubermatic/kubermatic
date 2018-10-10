e20709f9ab22159511986e4dbbaa31d2e1187e14
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-10-10 14:13:24 +0200

    Specify providers to test via flag (#2178)
    * only run tests, for which credentials have been supplied
    
    * generate unique node name during test run to prevent name collisions
    
    * use default key for nodes
    
    * use default key for nodes
    
    * fix path to publickey
    
    * fix comment

--- api/cmd/conformance-tests/scenarios_openstack.go
+++ api/cmd/conformance-tests/scenarios_openstack.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/Masterminds/semver"
 
@@ -88,9 +87,7 @@ func (s *openStackScenario) Nodes(num int) []*kubermaticapiv2.Node {
 	var nodes []*kubermaticapiv2.Node
 	for i := 0; i < num; i++ {
 		node := &kubermaticapiv2.Node{
-			Metadata: kubermaticapiv2.ObjectMeta{
-				Name: "node" + strconv.Itoa(i),
-			},
+			Metadata: kubermaticapiv2.ObjectMeta{},
 			Spec: kubermaticapiv2.NodeSpec{
 				Cloud: kubermaticapiv2.NodeCloudSpec{
 					Openstack: &kubermaticapiv2.OpenstackNodeSpec{
