3254d67484305018c081a87e62930f34539e0871
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2018-10-12 12:23:44 +0200

    interacting with cloud providers APIs without presenting secrets. (#2180)
    allows to consume cloud providers APIs without presenting the credentials. For example getting the list of flavours(sizes).
    Those EPs are needed to maintain backward compatibility where the dashboard was sending request to providers after creating a cluster.
    This didn't work because we stopped sending the credentials from API.

--- api/pkg/handler/digitalocean.go
+++ api/pkg/handler/digitalocean.go
@@ -7,53 +7,82 @@ import (
 
 	"github.com/digitalocean/godo"
 	"github.com/go-kit/kit/endpoint"
-	apiv1 "github.com/kubermatic/kubermatic/api/pkg/api/v1"
 	"golang.org/x/oauth2"
+
+	apiv1 "github.com/kubermatic/kubermatic/api/pkg/api/v1"
+	"github.com/kubermatic/kubermatic/api/pkg/provider"
+	"github.com/kubermatic/kubermatic/api/pkg/util/errors"
 )
 
-func digitaloceanSizeEndpoint() endpoint.Endpoint {
-	reStandard := regexp.MustCompile("(^s|S)")
-	reOptimized := regexp.MustCompile("(^c|C)")
+var reStandard = regexp.MustCompile("(^s|S)")
+var reOptimized = regexp.MustCompile("(^c|C)")
 
+func digitaloceanSizeNoCredentialsEndpoint(projectProvider provider.ProjectProvider) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(DoSizesNoCredentialsReq)
+		clusterProvider := ctx.Value(newClusterProviderContextKey).(provider.NewClusterProvider)
+		userInfo := ctx.Value(userInfoContextKey).(*provider.UserInfo)
+		_, err := projectProvider.Get(userInfo, req.ProjectID, &provider.ProjectGetOptions{})
+		if err != nil {
+			return nil, kubernetesErrorToHTTPError(err)
+		}
+		cluster, err := clusterProvider.Get(userInfo, req.ClusterID, &provider.ClusterGetOptions{})
+		if err != nil {
+			return nil, kubernetesErrorToHTTPError(err)
+		}
+		if cluster.Spec.Cloud.Digitalocean == nil {
+			return nil, errors.NewNotFound("cloud spec for ", req.ClusterID)
+		}
+
+		doToken := cluster.Spec.Cloud.Digitalocean.Token
+		return digitaloceanSize(ctx, doToken)
+	}
+}
+
+func digitaloceanSizeEndpoint() endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
 		req := request.(DoSizesReq)
-		static := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: req.DoToken})
-		client := godo.NewClient(oauth2.NewClient(context.Background(), static))
-
-		listOptions := &godo.ListOptions{
-			Page:    1,
-			PerPage: 1000,
-		}
-
-		sizes, _, err := client.Sizes.List(ctx, listOptions)
-		if err != nil {
-			return nil, fmt.Errorf("failed to list sizes: %v", err)
-		}
-
-		sizeList := apiv1.DigitaloceanSizeList{}
-		// currently there are 3 types of sizes: 1) starting with s, 2) starting with c and 3) the old ones
-		// type 3 isn't listed in the pricing anymore and only will be available for legacy issues until July 1st, 2018
-		// therefor we might not want to log all cases that aren't starting with s or c
-		for k := range sizes {
-			s := apiv1.DigitaloceanSize{
-				Slug:         sizes[k].Slug,
-				Available:    sizes[k].Available,
-				Transfer:     sizes[k].Transfer,
-				PriceMonthly: sizes[k].PriceMonthly,
-				PriceHourly:  sizes[k].PriceHourly,
-				Memory:       sizes[k].Memory,
-				VCPUs:        sizes[k].Vcpus,
-				Disk:         sizes[k].Disk,
-				Regions:      sizes[k].Regions,
-			}
-			switch {
-			case reStandard.MatchString(sizes[k].Slug):
-				sizeList.Standard = append(sizeList.Standard, s)
-			case reOptimized.MatchString(sizes[k].Slug):
-				sizeList.Optimized = append(sizeList.Optimized, s)
-			}
-		}
-
-		return sizeList, nil
+		return digitaloceanSize(ctx, req.DoToken)
 	}
+}
+
+func digitaloceanSize(ctx context.Context, token string) (apiv1.DigitaloceanSizeList, error) {
+	static := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	client := godo.NewClient(oauth2.NewClient(context.Background(), static))
+
+	listOptions := &godo.ListOptions{
+		Page:    1,
+		PerPage: 1000,
+	}
+
+	sizes, _, err := client.Sizes.List(ctx, listOptions)
+	if err != nil {
+		return apiv1.DigitaloceanSizeList{}, fmt.Errorf("failed to list sizes: %v", err)
+	}
+
+	sizeList := apiv1.DigitaloceanSizeList{}
+	// currently there are 3 types of sizes: 1) starting with s, 2) starting with c and 3) the old ones
+	// type 3 isn't listed in the pricing anymore and only will be available for legacy issues until July 1st, 2018
+	// therefor we might not want to log all cases that aren't starting with s or c
+	for k := range sizes {
+		s := apiv1.DigitaloceanSize{
+			Slug:         sizes[k].Slug,
+			Available:    sizes[k].Available,
+			Transfer:     sizes[k].Transfer,
+			PriceMonthly: sizes[k].PriceMonthly,
+			PriceHourly:  sizes[k].PriceHourly,
+			Memory:       sizes[k].Memory,
+			VCPUs:        sizes[k].Vcpus,
+			Disk:         sizes[k].Disk,
+			Regions:      sizes[k].Regions,
+		}
+		switch {
+		case reStandard.MatchString(sizes[k].Slug):
+			sizeList.Standard = append(sizeList.Standard, s)
+		case reOptimized.MatchString(sizes[k].Slug):
+			sizeList.Optimized = append(sizeList.Optimized, s)
+		}
+	}
+
+	return sizeList, nil
 }
