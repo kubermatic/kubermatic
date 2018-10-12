41f97ea679ec35e4eddfe178d7e441218cab100f
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2018-10-12 12:23:44 +0200

    interacting with cloud providers APIs without presenting secrets. (#2180)
    allows to consume cloud providers APIs without presenting the credentials. For example getting the list of flavours(sizes).
    Those EPs are needed to maintain backward compatibility where the dashboard was sending request to providers after creating a cluster.
    This didn't work because we stopped sending the credentials from API.

--- api/pkg/handler/vsphere.go
+++ api/pkg/handler/vsphere.go
@@ -10,44 +10,71 @@ import (
 	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
 	"github.com/kubermatic/kubermatic/api/pkg/provider"
 	"github.com/kubermatic/kubermatic/api/pkg/provider/cloud/vsphere"
+	"github.com/kubermatic/kubermatic/api/pkg/util/errors"
 )
 
 func vsphereNetworksEndpoint(providers provider.CloudRegistry) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-
 		req, ok := request.(VSphereNetworksReq)
 		if !ok {
 			return nil, fmt.Errorf("incorrect type of request, expected = VSphereNetworksReq, got = %T", request)
 		}
 
-		vsProviderInterface, ok := providers[provider.VSphereCloudProvider]
-		if !ok {
-			return nil, fmt.Errorf("unable to get %s provider", provider.VSphereCloudProvider)
-		}
-
-		vsProvider, ok := vsProviderInterface.(*vsphere.Provider)
-		if !ok {
-			return nil, fmt.Errorf("unable to cast vsProviderInterface to *vsphere.Provider")
-		}
-
-		networks, err := vsProvider.GetNetworks(kubermaticv1.CloudSpec{
-			DatacenterName: req.DatacenterName,
-			VSphere: &kubermaticv1.VSphereCloudSpec{
-				InfraManagementUser: kubermaticv1.VSphereCredentials{
-					Username: req.Username,
-					Password: req.Password,
-				},
-			},
-		})
-		if err != nil {
-			return nil, err
-		}
-
-		var apiNetworks []apiv1.VSphereNetwork
-		for _, net := range networks {
-			apiNetworks = append(apiNetworks, apiv1.VSphereNetwork{Name: net.Name})
-		}
-
-		return apiNetworks, nil
+		return getVsphereNetworks(providers, req.Username, req.Password, req.DatacenterName)
 	}
+}
+
+func vsphereNetworksNoCredentialsEndpoint(projectProvider provider.ProjectProvider, providers provider.CloudRegistry) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(VSphereNetworksNoCredentialsReq)
+		userInfo := ctx.Value(userInfoContextKey).(*provider.UserInfo)
+		clusterProvider := ctx.Value(newClusterProviderContextKey).(provider.NewClusterProvider)
+		_, err := projectProvider.Get(userInfo, req.ProjectID, &provider.ProjectGetOptions{})
+		if err != nil {
+			return nil, kubernetesErrorToHTTPError(err)
+		}
+		cluster, err := clusterProvider.Get(userInfo, req.ClusterID, &provider.ClusterGetOptions{})
+		if err != nil {
+			return nil, kubernetesErrorToHTTPError(err)
+		}
+		if cluster.Spec.Cloud.VSphere == nil {
+			return nil, errors.NewNotFound("cloud spec for ", req.ClusterID)
+		}
+
+		datacenterName := cluster.Spec.Cloud.DatacenterName
+		vSpec := cluster.Spec.Cloud.VSphere
+		return getVsphereNetworks(providers, vSpec.Username, vSpec.Password, datacenterName)
+	}
+}
+
+func getVsphereNetworks(providers provider.CloudRegistry, username, password, datacenterName string) ([]apiv1.VSphereNetwork, error) {
+	vsProviderInterface, ok := providers[provider.VSphereCloudProvider]
+	if !ok {
+		return nil, fmt.Errorf("unable to get %s provider", provider.VSphereCloudProvider)
+	}
+
+	vsProvider, ok := vsProviderInterface.(*vsphere.Provider)
+	if !ok {
+		return nil, fmt.Errorf("unable to cast vsProviderInterface to *vsphere.Provider")
+	}
+
+	networks, err := vsProvider.GetNetworks(kubermaticv1.CloudSpec{
+		DatacenterName: datacenterName,
+		VSphere: &kubermaticv1.VSphereCloudSpec{
+			InfraManagementUser: kubermaticv1.VSphereCredentials{
+				Username: username,
+				Password: password,
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var apiNetworks []apiv1.VSphereNetwork
+	for _, net := range networks {
+		apiNetworks = append(apiNetworks, apiv1.VSphereNetwork{Name: net.Name})
+	}
+
+	return apiNetworks, nil
 }
