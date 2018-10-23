1438fff09a00398b8cd009360e179b49435797b8
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2018-10-23 15:35:57 +0200

    Update machine controller (#2269)
    * update machine-controller to v0.9.7
    
    * update fixtures
    
    * vendor: update machine-controller

--- api/vendor/github.com/kubermatic/machine-controller/pkg/cloudprovider/provider/vsphere/cloudconfig.go
+++ api/vendor/github.com/kubermatic/machine-controller/pkg/cloudprovider/provider/vsphere/cloudconfig.go
@@ -3,16 +3,38 @@ package vsphere
 import (
 	"bytes"
 	"fmt"
-	"strconv"
-	"strings"
+	"text/template"
 
-	"gopkg.in/ini.v1"
+	"github.com/kubermatic/machine-controller/pkg/ini"
+
+	"github.com/Masterminds/sprig"
 )
 
-// Allowed escaping characters by gopkg.in/gcfg.v1 - the lib kubernetes uses
-var escaper = strings.NewReplacer(
-	`\`, `\\`,
-	`"`, `\"`,
+const (
+	cloudConfigTpl = `[Global]
+user          = {{ .Global.User | iniEscape }}
+password      = {{ .Global.Password | iniEscape }}
+port          = {{ .Global.VCenterPort | iniEscape }}
+insecure-flag = {{ .Global.InsecureFlag }}
+
+[Disk]
+scsicontrollertype = {{ .Disk.SCSIControllerType | iniEscape }}
+
+[Workspace]
+server            = {{ .Workspace.VCenterIP | iniEscape }}
+datacenter        = {{ .Workspace.Datacenter | iniEscape }}
+folder            = {{ .Workspace.Folder | iniEscape }}
+default-datastore = {{ .Workspace.DefaultDatastore | iniEscape }}
+resourcepool-path = {{ .Workspace.ResourcePoolPath | iniEscape }}
+
+{{ range $name, $vc := .VirtualCenter }}
+[VirtualCenter {{ $name | iniEscape }}]
+user = {{ $vc.User | iniEscape }}
+password = {{ $vc.Password | iniEscape }}
+port = {{ $vc.VCenterPort }}
+datacenters = {{ $vc.Datacenters | iniEscape }}
+{{ end }}
+`
 )
 
 type WorkspaceOpts struct {
@@ -51,80 +73,18 @@ type CloudConfig struct {
 }
 
 func CloudConfigToString(c *CloudConfig) (string, error) {
-	cfg := ini.Empty()
+	funcMap := sprig.TxtFuncMap()
+	funcMap["iniEscape"] = ini.Escape
 
-	// Global
-	gsec, err := cfg.NewSection("Global")
+	tpl, err := template.New("cloud-config").Funcs(funcMap).Parse(cloudConfigTpl)
 	if err != nil {
-		return "", fmt.Errorf("failed to create the global section in ini: %v", err)
-	}
-	if _, err := gsec.NewKey("user", escaper.Replace(c.Global.User)); err != nil {
-		return "", fmt.Errorf("failed to write global.user: %v", err)
-	}
-	if _, err := gsec.NewKey("password", escaper.Replace(c.Global.Password)); err != nil {
-		return "", fmt.Errorf("failed to write global.password: %v", err)
-	}
-	if _, err := gsec.NewKey("port", c.Global.VCenterPort); err != nil {
-		return "", fmt.Errorf("failed to write global.port: %v", err)
-	}
-	if _, err := gsec.NewKey("insecure-flag", strconv.FormatBool(c.Global.InsecureFlag)); err != nil {
-		return "", fmt.Errorf("failed to write global.insecure-flag: %v", err)
+		return "", fmt.Errorf("failed to parse the cloud config template: %v", err)
 	}
 
-	// Disk
-	dsec, err := cfg.NewSection("Disk")
-	if err != nil {
-		return "", fmt.Errorf("failed to create the Disk section in ini: %v", err)
-	}
-	if _, err := dsec.NewKey("scsicontrollertype", escaper.Replace(c.Disk.SCSIControllerType)); err != nil {
-		return "", fmt.Errorf("failed to write Disk.scsicontrollertype: %v", err)
+	buf := &bytes.Buffer{}
+	if err := tpl.Execute(buf, c); err != nil {
+		return "", fmt.Errorf("failed to execute cloud config template: %v", err)
 	}
 
-	// Workspace
-	wsec, err := cfg.NewSection("Workspace")
-	if err != nil {
-		return "", fmt.Errorf("failed to create the Workspace section in ini: %v", err)
-	}
-	if _, err := wsec.NewKey("server", escaper.Replace(c.Workspace.VCenterIP)); err != nil {
-		return "", fmt.Errorf("failed to write Workspace.server: %v", err)
-	}
-	if _, err := wsec.NewKey("datacenter", escaper.Replace(c.Workspace.Datacenter)); err != nil {
-		return "", fmt.Errorf("failed to write Workspace.datacenter: %v", err)
-	}
-	if _, err := wsec.NewKey("folder", escaper.Replace(c.Workspace.Folder)); err != nil {
-		return "", fmt.Errorf("failed to write Workspace.folder: %v", err)
-	}
-	if _, err := wsec.NewKey("default-datastore", escaper.Replace(c.Workspace.DefaultDatastore)); err != nil {
-		return "", fmt.Errorf("failed to write Workspace.default-datastore: %v", err)
-	}
-	if _, err := wsec.NewKey("resourcepool-path", escaper.Replace(c.Workspace.ResourcePoolPath)); err != nil {
-		return "", fmt.Errorf("failed to write Workspace.resourcepool-path: %v", err)
-	}
-
-	for ip, vc := range c.VirtualCenter {
-		sectionName := fmt.Sprintf("VirtualCenter \"%s\"", ip)
-		vcsec, err := cfg.NewSection(sectionName)
-		if err != nil {
-			return "", fmt.Errorf("failed to create the %s section in ini: %v", sectionName, err)
-		}
-
-		if _, err := vcsec.NewKey("user", escaper.Replace(vc.User)); err != nil {
-			return "", fmt.Errorf("failed to write %s.user: %v", sectionName, err)
-		}
-		if _, err := vcsec.NewKey("password", escaper.Replace(vc.Password)); err != nil {
-			return "", fmt.Errorf("failed to write %s.password: %v", sectionName, err)
-		}
-		if _, err := vcsec.NewKey("port", escaper.Replace(vc.VCenterPort)); err != nil {
-			return "", fmt.Errorf("failed to write %s.port: %v", sectionName, err)
-		}
-		if _, err := vcsec.NewKey("datacenters", escaper.Replace(vc.Datacenters)); err != nil {
-			return "", fmt.Errorf("failed to write %s.datacenters: %v", sectionName, err)
-		}
-	}
-
-	b := &bytes.Buffer{}
-	if _, err := cfg.WriteTo(b); err != nil {
-		return "", fmt.Errorf("failed to write ini to buffer: %v", err)
-	}
-	return b.String(), nil
+	return buf.String(), nil
 }
