c4e69c99e58aa79947e05e2bbd2a8351ccf67b97
Alvaro Aleman <alv2412@googlemail.com>
2018-12-07 13:33:45 +0100

    Update conformance-test image

--- containers/conformance-tests/install.sh
+++ containers/conformance-tests/install.sh
@@ -4,10 +4,11 @@ ROOT_DIR="/opt/kube-test"
 
 set -euox pipefail
 
+apt update && apt install -y git-crypt curl
+
 TMP_ROOT="./.install-tmp"
 
-#for VERSION in 1.9 1.10 1.11 1.12; do
-for VERSION in 1.12; do
+for VERSION in 1.10 1.11 1.12; do
     DIRECTORY="${ROOT_DIR}/${VERSION}"
     if [ ! -d "${DIRECTORY}" ]; then
 
@@ -28,3 +29,11 @@ for VERSION in 1.12; do
 done
 
 rm -rf ${TMP_ROOT}
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+mv kubectl /usr/local/bin
+
+mkdir $HOME/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo3amVmCkIZo4cgj2kjU2arZKlzzOhaOuveH9aJbL4mlVHVsEcVk+RSty4AMK1GQL3+Ii7iGicKWwge4yefc75aOtUncfF01rnBsNvi3lOqJR/6POHy4OnPXJElvEn7jii/pAUeyr8halBezQTUkvRiUtlJo6oEb2dRN5ujyFm5TuIxgM0UFVGBRoD0agGr87GaQsUahf+PE1zHEid+qQPz7EdMo8/eRNtgikhBG1/ae6xRstAi0QU8EgjKvK1ROXOYTlpTBFElApOXZacH91WvG0xgPnyxIXoKtiCCNGeu/0EqDAgiXfmD2HK/WAXwJNwcmRvBaedQUS4H0lNmvj5' > $HOME/.ssh/id_rsa.pub
+chmod 0700 $HOME/.ssh
