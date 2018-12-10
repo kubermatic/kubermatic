d895c767003fcb803b115b0f7ca7846c9512dd24
mrIncompetent <henrik@henrik-schmidt.de>
2018-12-10 15:40:51 +0100

    add startup container

---
+++ containers/startup-script/release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+TAG=v0.1.0-alpha1
+IMAGE=quay.io/kubermatic/startup-script
+
+set -euox pipefail
+
+docker build --no-cache --pull -t ${IMAGE}:${TAG} .
+docker push ${IMAGE}:${TAG}
