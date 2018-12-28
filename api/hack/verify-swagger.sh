7f146917abac95a86639f805eb553958146257d2
mrIncompetent <henrik@henrik-schmidt.de>
2018-12-28 10:48:03 +0100

    update swagger veriy

--- api/hack/verify-swagger.sh
+++ api/hack/verify-swagger.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
+set -euxo pipefail
 
 API_DIR="$(go env GOPATH)/src/github.com/kubermatic/kubermatic/api"
 SWAGGER_FILE="swagger.json"
 TMP_SWAGGER="${SWAGGER_FILE}.tmp"
 trap "rm -f $TMP_SWAGGER" EXIT SIGINT SIGTERM
 
-cd ${API_DIR}/vendor/github.com/go-swagger/go-swagger/cmd/swagger
-go install
+BINPATH=$(go env GOPATH)/bin/swagger
+go build -o ${BINPATH} github.com/kubermatic/kubermatic/api/vendor/github.com/go-swagger/go-swagger/cmd/swagger
+
 cd ${API_DIR}/cmd/kubermatic-api/
-swagger generate spec --scan-models -o ${TMP_SWAGGER}
+${BINPATH} generate spec --scan-models -o ${TMP_SWAGGER}
 diff -Naup ${SWAGGER_FILE} ${TMP_SWAGGER}
 
-swagger validate ${SWAGGER_FILE}
+${BINPATH} validate ${SWAGGER_FILE}
