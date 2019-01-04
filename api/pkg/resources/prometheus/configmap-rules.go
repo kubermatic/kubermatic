281311dee7c53f67cdf7a1142680dbc99b2dc94a
Christoph Mewes <christoph@loodse.com>
2019-01-04 10:48:44 +0100

    improve readability / make fd alert useful for all processes instead of just etcd

--- api/pkg/resources/prometheus/configmap-rules.go
+++ api/pkg/resources/prometheus/configmap-rules.go
@@ -142,8 +142,7 @@ groups:
   rules:
   - alert: MachineControllerTooManyErrors
     annotations:
-      message: Machine Controller in {{ $labels.namespace }} has too many errors in
-        its loop.
+      message: Machine Controller in {{ $labels.namespace }} has too many errors in its loop.
     expr: |
       sum(rate(machine_controller_errors_total[5m])) by (namespace) > 0.01
     for: 10m
@@ -152,8 +151,7 @@ groups:
 
   - alert: MachineControllerMachineDeletionTakesTooLong
     annotations:
-      message: Machine {{ $labels.machine }} of cluster {{ $labels.cluster }} is stuck
-        in deletion for more than 30min.
+      message: Machine {{ $labels.machine }} of cluster {{ $labels.cluster }} is stuck in deletion for more than 30min.
     expr: (time() - machine_controller_machine_deleted) > 30*60
     for: 0m
     labels:
@@ -163,97 +161,99 @@ groups:
   rules:
   - alert: EtcdInsufficientMembers
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": insufficient members ({{ $value
-        }}).'
+      message: 'Etcd cluster "{{ $labels.job }}": insufficient members ({{ $value }}).'
     expr: |
-      sum(up{job=~".*etcd.*"} == bool 1) by (job) < ((count(up{job=~".*etcd.*"}) by (job) + 1) / 2)
+      sum(up{job="etcd"} == bool 1) by (job) < ((count(up{job="etcd"}) by (job) + 1) / 2)
     for: 3m
     labels:
       severity: critical
+
   - alert: EtcdNoLeader
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": member {{ $labels.instance }} has
-        no leader.'
+      message: 'Etcd cluster "{{ $labels.job }}": member {{ $labels.instance }} has no leader.'
     expr: |
-      etcd_server_has_leader{job=~".*etcd.*"} == 0
+      etcd_server_has_leader{job="etcd"} == 0
     for: 1m
     labels:
       severity: critical
+
   - alert: EtcdHighNumberOfLeaderChanges
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": instance {{ $labels.instance }}
-        has seen {{ $value }} leader changes within the last hour.'
+      message: 'Etcd cluster "{{ $labels.job }}": instance {{ $labels.instance }} has seen {{ $value }} leader changes within the last hour.'
     expr: |
-      rate(etcd_server_leader_changes_seen_total{job=~".*etcd.*"}[15m]) > 3
+      rate(etcd_server_leader_changes_seen_total{job="etcd"}[15m]) > 3
     for: 15m
     labels:
       severity: warning
+
   - alert: EtcdGRPCRequestsSlow
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": gRPC requests to {{ $labels.grpc_method
-        }} are taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": gRPC requests to {{ $labels.grpc_method }} are taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, sum(rate(grpc_server_handling_seconds_bucket{job=~".*etcd.*", grpc_type="unary"}[5m])) by (job, instance, grpc_service, grpc_method, le))
+      histogram_quantile(0.99, sum(rate(grpc_server_handling_seconds_bucket{job="etcd", grpc_type="unary"}[5m])) by (job, instance, grpc_service, grpc_method, le))
       > 0.15
     for: 10m
     labels:
       severity: critical
+
   - alert: EtcdMemberCommunicationSlow
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": member communication with {{ $labels.To
-        }} is taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": member communication with {{ $labels.To }} is taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, rate(etcd_network_peer_round_trip_time_seconds_bucket{job=~".*etcd.*"}[5m]))
+      histogram_quantile(0.99, rate(etcd_network_peer_round_trip_time_seconds_bucket{job="etcd"}[5m]))
       > 0.15
     for: 10m
     labels:
       severity: warning
+
   - alert: EtcdHighNumberOfFailedProposals
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": {{ $value }} proposal failures within
-        the last hour on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": {{ $value }} proposal failures within the last hour on etcd instance {{ $labels.instance }}.'
     expr: |
-      rate(etcd_server_proposals_failed_total{job=~".*etcd.*"}[15m]) > 5
+      rate(etcd_server_proposals_failed_total{job="etcd"}[15m]) > 5
     for: 15m
     labels:
       severity: warning
+
   - alert: EtcdHighFsyncDurations
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile fync durations are
-        {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile fync durations are {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, rate(etcd_disk_wal_fsync_duration_seconds_bucket{job=~".*etcd.*"}[5m]))
+      histogram_quantile(0.99, rate(etcd_disk_wal_fsync_duration_seconds_bucket{job="etcd"}[5m]))
       > 0.5
     for: 10m
     labels:
       severity: warning
+
   - alert: EtcdHighCommitDurations
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile commit durations
-        {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile commit durations {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, rate(etcd_disk_backend_commit_duration_seconds_bucket{job=~".*etcd.*"}[5m]))
+      histogram_quantile(0.99, rate(etcd_disk_backend_commit_duration_seconds_bucket{job="etcd"}[5m]))
       > 0.25
     for: 10m
     labels:
       severity: warning
+
+- name: process.filedescriptors
+  rules:
   - expr: process_open_fds / process_max_fds
     record: instance:fd_utilization
+
   - alert: FdExhaustionClose
     annotations:
-      message: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust its
-        file descriptors soon'
+      message: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust its file descriptors soon'
     expr: |
-      predict_linear(instance:fd_utilization{job=~".*etcd.*"}[1h], 3600 * 4) > 1
+      predict_linear(instance:fd_utilization[1h], 3600 * 4) > 1
     for: 10m
     labels:
       severity: warning
+
   - alert: FdExhaustionClose
     annotations:
-      description: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust
-        its file descriptors soon'
+      description: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust its file descriptors soon'
     expr: |
-      predict_linear(instance:fd_utilization{job=~".*etcd.*"}[10m], 3600) > 1
+      predict_linear(instance:fd_utilization[10m], 3600) > 1
     for: 10m
     labels:
       severity: critical
