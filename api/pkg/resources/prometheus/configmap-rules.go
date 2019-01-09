d5231495d9fbd1d31600108ac0903a9abaded436
Christoph Mewes <github@xrstf.de>
2019-01-09 14:34:46 +0100

    Cleanup Alerts (#2583)
    * remove EtcdHighNumberOfFailedGRPCRequests alert because it's much more stable to alert on *other* systems not being able to talk to etcd (i.e. up{apiserver}==0)
    
    * improve readability / make fd alert useful for all processes instead of just etcd
    
    * move "up" checks down into user clusters and alert much sooner (3m instead of 15m)
    
    * move machine-controller alerts into cluster ns, pre-aggregate metrics for dashboards
    
    * rebuild rules.yaml
    
    * remove jsonnet for building our alerting rules, re-format rules
    
    * update test fixtures
    
    * disable CPUThrottlingHigh alert
    
    * do not scrape prow e2e test clusters anymore
    
    * alert on unready nodes in user clusters
    
    * update test fixtures
    
    * lower threshold for alerting on unready nodes to 30 minutes
    
    * group kubermatic rules into one file, remove duplicate apiserver-absent rule
    
    * alert sooner on unready pods
    
    * move ksm scraping alert to user cluster, as we don't scrape ksm metrics on seed level

--- api/pkg/resources/prometheus/configmap-rules.go
+++ api/pkg/resources/prometheus/configmap-rules.go
@@ -142,141 +142,198 @@ groups:
   rules:
   - alert: MachineControllerTooManyErrors
     annotations:
-      message: Machine Controller in {{ $labels.namespace }} has too many errors in
-        its loop.
+      message: Machine Controller in {{ $labels.namespace }} has too many errors in its loop.
     expr: |
       sum(rate(machine_controller_errors_total[5m])) by (namespace) > 0.01
     for: 10m
     labels:
       severity: warning
+
   - alert: MachineControllerMachineDeletionTakesTooLong
     annotations:
-      message: Machine {{ $labels.machine }} of cluster {{ $labels.cluster }} is stuck
-        in deletion for more than 30min.
+      message: Machine {{ $labels.machine }} of cluster {{ $labels.cluster }} is stuck in deletion for more than 30min.
     expr: (time() - machine_controller_machine_deleted) > 30*60
     for: 0m
     labels:
       severity: warning
+
+  - record: job:machine_controller_errors_total:rate5m
+    expr: rate(machine_controller_errors_total[5m])
+    labels:
+      kubermatic: federate
+
+  - record: job:machine_controller_workers:sum
+    expr: sum(machine_controller_workers)
+    labels:
+      kubermatic: federate
+
+  - record: job:machine_controller_machines:sum
+    expr: sum(machine_controller_machines)
+    labels:
+      kubermatic: federate
+
 - name: etcd
   rules:
   - alert: EtcdInsufficientMembers
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": insufficient members ({{ $value
-        }}).'
+      message: 'Etcd cluster "{{ $labels.job }}": insufficient members ({{ $value }}).'
     expr: |
-      sum(up{job=~".*etcd.*"} == bool 1) by (job) < ((count(up{job=~".*etcd.*"}) by (job) + 1) / 2)
+      sum(up{job="etcd"} == bool 1) by (job) < ((count(up{job="etcd"}) by (job) + 1) / 2)
     for: 3m
     labels:
       severity: critical
+
   - alert: EtcdNoLeader
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": member {{ $labels.instance }} has
-        no leader.'
+      message: 'Etcd cluster "{{ $labels.job }}": member {{ $labels.instance }} has no leader.'
     expr: |
-      etcd_server_has_leader{job=~".*etcd.*"} == 0
+      etcd_server_has_leader{job="etcd"} == 0
     for: 1m
     labels:
       severity: critical
+
   - alert: EtcdHighNumberOfLeaderChanges
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": instance {{ $labels.instance }}
-        has seen {{ $value }} leader changes within the last hour.'
+      message: 'Etcd cluster "{{ $labels.job }}": instance {{ $labels.instance }} has seen {{ $value }} leader changes within the last hour.'
     expr: |
-      rate(etcd_server_leader_changes_seen_total{job=~".*etcd.*"}[15m]) > 3
+      rate(etcd_server_leader_changes_seen_total{job="etcd"}[15m]) > 3
     for: 15m
     labels:
       severity: warning
-  - alert: EtcdHighNumberOfFailedGRPCRequests
-    annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": {{ $value }}% of requests for {{
-        $labels.grpc_method }} failed on etcd instance {{ $labels.instance }}.'
-    expr: |
-      100 * sum(rate(grpc_server_handled_total{job=~".*etcd.*", grpc_code!="OK"}[5m])) BY (job, instance, grpc_service, grpc_method)
-        /
-      sum(rate(grpc_server_handled_total{job=~".*etcd.*"}[5m])) BY (job, instance, grpc_service, grpc_method)
-        > 1
-    for: 10m
-    labels:
-      severity: warning
-  - alert: EtcdHighNumberOfFailedGRPCRequests
-    annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": {{ $value }}% of requests for {{
-        $labels.grpc_method }} failed on etcd instance {{ $labels.instance }}.'
-    expr: |
-      100 * sum(rate(grpc_server_handled_total{job=~".*etcd.*", grpc_code!="OK"}[5m])) BY (job, instance, grpc_service, grpc_method)
-        /
-      sum(rate(grpc_server_handled_total{job=~".*etcd.*"}[5m])) BY (job, instance, grpc_service, grpc_method)
-        > 5
-    for: 5m
-    labels:
-      severity: critical
+
   - alert: EtcdGRPCRequestsSlow
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": gRPC requests to {{ $labels.grpc_method
-        }} are taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": gRPC requests to {{ $labels.grpc_method }} are taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, sum(rate(grpc_server_handling_seconds_bucket{job=~".*etcd.*", grpc_type="unary"}[5m])) by (job, instance, grpc_service, grpc_method, le))
+      histogram_quantile(0.99, sum(rate(grpc_server_handling_seconds_bucket{job="etcd", grpc_type="unary"}[5m])) by (job, instance, grpc_service, grpc_method, le))
       > 0.15
     for: 10m
     labels:
       severity: critical
+
   - alert: EtcdMemberCommunicationSlow
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": member communication with {{ $labels.To
-        }} is taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": member communication with {{ $labels.To }} is taking {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, rate(etcd_network_peer_round_trip_time_seconds_bucket{job=~".*etcd.*"}[5m]))
+      histogram_quantile(0.99, rate(etcd_network_peer_round_trip_time_seconds_bucket{job="etcd"}[5m]))
       > 0.15
     for: 10m
     labels:
       severity: warning
+
   - alert: EtcdHighNumberOfFailedProposals
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": {{ $value }} proposal failures within
-        the last hour on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": {{ $value }} proposal failures within the last hour on etcd instance {{ $labels.instance }}.'
     expr: |
-      rate(etcd_server_proposals_failed_total{job=~".*etcd.*"}[15m]) > 5
+      rate(etcd_server_proposals_failed_total{job="etcd"}[15m]) > 5
     for: 15m
     labels:
       severity: warning
+
   - alert: EtcdHighFsyncDurations
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile fync durations are
-        {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile fync durations are {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, rate(etcd_disk_wal_fsync_duration_seconds_bucket{job=~".*etcd.*"}[5m]))
+      histogram_quantile(0.99, rate(etcd_disk_wal_fsync_duration_seconds_bucket{job="etcd"}[5m]))
       > 0.5
     for: 10m
     labels:
       severity: warning
+
   - alert: EtcdHighCommitDurations
     annotations:
-      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile commit durations
-        {{ $value }}s on etcd instance {{ $labels.instance }}.'
+      message: 'Etcd cluster "{{ $labels.job }}": 99th percentile commit durations {{ $value }}s on etcd instance {{ $labels.instance }}.'
     expr: |
-      histogram_quantile(0.99, rate(etcd_disk_backend_commit_duration_seconds_bucket{job=~".*etcd.*"}[5m]))
+      histogram_quantile(0.99, rate(etcd_disk_backend_commit_duration_seconds_bucket{job="etcd"}[5m]))
       > 0.25
     for: 10m
     labels:
       severity: warning
+
+- name: process.filedescriptors
+  rules:
   - expr: process_open_fds / process_max_fds
     record: instance:fd_utilization
+
   - alert: FdExhaustionClose
     annotations:
-      message: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust its
-        file descriptors soon'
+      message: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust its file descriptors soon'
     expr: |
-      predict_linear(instance:fd_utilization{job=~".*etcd.*"}[1h], 3600 * 4) > 1
+      predict_linear(instance:fd_utilization[1h], 3600 * 4) > 1
     for: 10m
     labels:
       severity: warning
+
   - alert: FdExhaustionClose
     annotations:
-      description: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust
-        its file descriptors soon'
+      description: '{{ $labels.job }} instance {{ $labels.instance }} will exhaust its file descriptors soon'
     expr: |
-      predict_linear(instance:fd_utilization{job=~".*etcd.*"}[10m], 3600) > 1
+      predict_linear(instance:fd_utilization[10m], 3600) > 1
     for: 10m
     labels:
       severity: critical
+
+- name: kubernetes-absent
+  rules:
+  - alert: KubernetesApiserverDown
+    annotations:
+      message: Kubernetes apiserver has disappeared from Prometheus target discovery.
+    expr: absent(up{job="apiserver"} == 1)
+    for: 3m
+    labels:
+      severity: critical
+
+  - alert: KubernetesControllerManagerDown
+    annotations:
+      message: Kubernetes controller-manager has disappeared from Prometheus target discovery.
+    expr: absent(up{job="controller-manager"} == 1)
+    for: 3m
+    labels:
+      severity: critical
+
+  - alert: KubernetesSchedulerDown
+    annotations:
+      message: Kubernetes scheduler has disappeared from Prometheus target discovery.
+    expr: absent(up{job="scheduler"} == 1)
+    for: 3m
+    labels:
+      severity: critical
+
+  - alert: MachineControllerDown
+    annotations:
+      message: Machine controller has disappeared from Prometheus target discovery.
+    expr: absent(up{job="machine-controller"} == 1)
+    for: 3m
+    labels:
+      severity: critical
+
+  - alert: EtcdDown
+    annotations:
+      message: Etcd has disappeared from Prometheus target discovery.
+    expr: absent(up{job="etcd"} == 1)
+    for: 3m
+    labels:
+      severity: critical
+
+- name: kubernetes-nodes
+  rules:
+  - alert: KubernetesNodeNotReady
+    annotations:
+      message: '{{ $labels.node }} has been unready for more than an hour.'
+    expr: kube_node_status_condition{condition="Ready",status="true"} == 0
+    for: 30m
+    labels:
+      severity: warning
+
+- name: kube-state-metrics
+  rules:
+  - alert: KubeStateMetricsHighErrorRate
+    annotations:
+      message: >
+        kube-state-metrics is experiencing {{ printf "%0.0f" $value }} errors / 5min
+        when scraping {{ $labels.resource }} resources on {{ $labels.instance }}.
+    expr: sum(rate(ksm_scrape_error_total[5m])) by (instance, resource) > 0.1
+    for: 15m
+    labels:
+      severity: warning
 `
