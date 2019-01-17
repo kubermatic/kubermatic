060113b7fe5526021176d5632ecb8785a0c35aa6
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-01-17 15:52:08 +0100

    deprecates POST v1/nodes endpoint (#2644)
    * depreciates POST v1/nodes endpoint
    
    creating new nodes is depreciated, the users should create a Node Deployment instead.
    using POST /api/v1/projects/{project_id}/dc/{dc}/clusters/{cluster_id}/nodedeployments
    
    * fix typo

--- api/pkg/handler/node_v1_test.go
+++ api/pkg/handler/node_v1_test.go
@@ -376,7 +376,7 @@ func TestGetNodeForCluster(t *testing.T) {
 	}
 }
 
-func TestCreateNodeForCluster(t *testing.T) {
+func TestCreateNodeForClusterIsDeprecated(t *testing.T) {
 	t.Parallel()
 	testcases := []struct {
 		Name                               string
@@ -396,8 +396,8 @@ func TestCreateNodeForCluster(t *testing.T) {
 		{
 			Name:                               "scenario 1: create a node that match the given spec",
 			Body:                               `{"spec":{"cloud":{"digitalocean":{"size":"s-1vcpu-1gb","backups":false,"ipv6":false,"monitoring":false,"tags":[]}},"operatingSystem":{"ubuntu":{"distUpgradeOnBoot":false}}}}`,
-			ExpectedResponse:                   `{"id":"%s","name":"%s","creationTimestamp":"0001-01-01T00:00:00Z","spec":{"cloud":{"digitalocean":{"size":"s-1vcpu-1gb","backups":false,"ipv6":false,"monitoring":false,"tags":["kubermatic","kubermatic-cluster-defClusterID"]}},"operatingSystem":{"ubuntu":{"distUpgradeOnBoot":false}},"versions":{"kubelet":""}},"status":{"machineName":"%s","capacity":{"cpu":"","memory":""},"allocatable":{"cpu":"","memory":""},"nodeInfo":{"kernelVersion":"","containerRuntime":"","containerRuntimeVersion":"","kubeletVersion":"","operatingSystem":"","architecture":""}}}`,
-			HTTPStatus:                         http.StatusCreated,
+			ExpectedResponse:                   `{"error":{"code":400,"message":"Creating Nodes is deprecated. Please create a Node Deployment instead","details":"If you are calling this API endpoint directrly then use POST \"v1/projects/{project_id}/dc/{dc}/clusters/{cluster_id}/nodedeployments\" instead"}}`,
+			HTTPStatus:                         http.StatusBadRequest,
 			RewriteClusterNameAndNamespaceName: true,
 			ProjectIDToSync:                    test.GenDefaultProject().Name,
 			ClusterIDToSync:                    test.GenDefaultCluster().Name,
@@ -409,8 +409,8 @@ func TestCreateNodeForCluster(t *testing.T) {
 		{
 			Name:                               "scenario 2: cluster components are not ready",
 			Body:                               `{"spec":{"cloud":{"digitalocean":{"size":"s-1vcpu-1gb","backups":false,"ipv6":false,"monitoring":false,"tags":[]}},"operatingSystem":{"ubuntu":{"distUpgradeOnBoot":false}}}}`,
-			ExpectedResponse:                   `{"error":{"code":503,"message":"Cluster components are not ready yet"}}`,
-			HTTPStatus:                         http.StatusServiceUnavailable,
+			ExpectedResponse:                   `{"error":{"code":400,"message":"Creating Nodes is deprecated. Please create a Node Deployment instead","details":"If you are calling this API endpoint directrly then use POST \"v1/projects/{project_id}/dc/{dc}/clusters/{cluster_id}/nodedeployments\" instead"}}`,
+			HTTPStatus:                         http.StatusBadRequest,
 			RewriteClusterNameAndNamespaceName: true,
 			ProjectIDToSync:                    test.GenDefaultProject().Name,
 			ClusterIDToSync:                    test.GenDefaultCluster().Name,
--- api/pkg/util/errors/errors.go
+++ api/pkg/util/errors/errors.go
@@ -7,8 +7,9 @@ import (
 
 // HTTPError represents an HTTP server error.
 type HTTPError struct {
-	code int
-	msg  string
+	code    int
+	msg     string
+	details string
 }
 
 // New creates a brand new HTTPError object
@@ -16,6 +17,15 @@ func New(code int, msg string) HTTPError {
 	return HTTPError{
 		code: code,
 		msg:  msg,
+	}
+}
+
+// NewWithDetails creates a brand new HTTPError object
+func NewWithDetails(code int, msg string, details string) HTTPError {
+	return HTTPError{
+		code:    code,
+		msg:     msg,
+		details: details,
 	}
 }
 
@@ -29,47 +39,42 @@ func (err HTTPError) StatusCode() int {
 	return err.code
 }
 
+// Details returns additional message about the error
+func (err HTTPError) Details() string {
+	return err.details
+}
+
 // NewNotFound creates a HTTP 404 error for a kind.
 func NewNotFound(kind, name string) error {
-	return HTTPError{http.StatusNotFound, fmt.Sprintf("%s %q not found", kind, name)}
+	return HTTPError{http.StatusNotFound, fmt.Sprintf("%s %q not found", kind, name), ""}
 }
 
 // NewWrongRequest creates a HTTP 400 error, if we got a wrong request type.
 func NewWrongRequest(got, want interface{}) error {
-	return HTTPError{http.StatusBadRequest, fmt.Sprintf("Got a '%T' request - expected a '%T' request", got, want)}
-}
-
-// NewUnknownVersion creates a HTTP 404 error for a kind in a datacenter.
-func NewUnknownVersion(version string) error {
-	return HTTPError{http.StatusMethodNotAllowed, fmt.Sprintf("Unknown version '%s'", version)}
-}
-
-// NewUnknownUpgradePath creates a HTTP 404 error for a kind in a datacenter.
-func NewUnknownUpgradePath(from, to string) error {
-	return HTTPError{http.StatusNotFound, fmt.Sprintf("There is no upgrade path from version %q to %q", from, to)}
+	return HTTPError{http.StatusBadRequest, fmt.Sprintf("Got a '%T' request - expected a '%T' request", got, want), ""}
 }
 
 // NewBadRequest creates a HTTP 400 error.
 func NewBadRequest(msg string, options ...interface{}) error {
-	return HTTPError{http.StatusBadRequest, fmt.Sprintf(msg, options...)}
+	return HTTPError{http.StatusBadRequest, fmt.Sprintf(msg, options...), ""}
 }
 
 // NewConflict creates a HTTP 409 error for a kind in a datacenter.
 func NewConflict(kind, dc, name string) error {
-	return HTTPError{http.StatusConflict, fmt.Sprintf("%s %q in dc %q already exists", kind, name, dc)}
+	return HTTPError{http.StatusConflict, fmt.Sprintf("%s %q in dc %q already exists", kind, name, dc), ""}
 }
 
 // NewNotAuthorized creates a HTTP 401 error.
 func NewNotAuthorized() error {
-	return HTTPError{http.StatusUnauthorized, "not authorized"}
+	return HTTPError{http.StatusUnauthorized, "not authorized", ""}
 }
 
 // NewNotImplemented creates a HTTP 501 'not implemented' error.
 func NewNotImplemented() error {
-	return HTTPError{http.StatusNotImplemented, "not implemented"}
+	return HTTPError{http.StatusNotImplemented, "not implemented", ""}
 }
 
 // NewAlreadyExists creates a HTTP 409 already exists error
 func NewAlreadyExists(kind, name string) error {
-	return HTTPError{http.StatusConflict, fmt.Sprintf("%s %q already exists", kind, name)}
+	return HTTPError{http.StatusConflict, fmt.Sprintf("%s %q already exists", kind, name), ""}
 }
