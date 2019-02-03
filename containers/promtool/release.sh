24406f4b023fdcc5e99af00bbc6a4f9d4df53713
Alvaro Aleman <alv2412@googlemail.com>
2019-02-03 15:05:22 +0100

    Remove promtool image, we manage it in the infra repo

--- containers/promtool/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-NUMBER=2
-VERSION=2.7.0
-
-set -euox pipefail
-
-docker build --no-cache --pull -t quay.io/kubermatic/promtool:${VERSION}-${NUMBER} .
-docker push quay.io/kubermatic/promtool:${VERSION}-${NUMBER}
