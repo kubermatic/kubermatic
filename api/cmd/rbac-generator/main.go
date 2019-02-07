794495532ee2702d6b8da436d2179e6a61b35169
Lukasz Zajaczkowski <zreigz@gmail.com>
2019-02-07 13:10:17 +0100

    enable prometheus endpoint for metrics (#2754)
    * enable prometheus endpoint for metrics
    
    * review changes (#2763)

--- api/cmd/rbac-generator/main.go
+++ api/cmd/rbac-generator/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"time"
 
-	"github.com/kubermatic/kubermatic/api/pkg/util/informer"
-
 	"github.com/golang/glog"
+	"github.com/oklog/run"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	rbaccontroller "github.com/kubermatic/kubermatic/api/pkg/controller/rbac"
 	kubermaticclientset "github.com/kubermatic/kubermatic/api/pkg/crd/client/clientset/versioned"
 	"github.com/kubermatic/kubermatic/api/pkg/crd/client/informers/externalversions"
+	"github.com/kubermatic/kubermatic/api/pkg/metrics"
 	"github.com/kubermatic/kubermatic/api/pkg/signals"
+	"github.com/kubermatic/kubermatic/api/pkg/util/informer"
 	"github.com/kubermatic/kubermatic/api/pkg/util/workerlabel"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -21,8 +27,9 @@ import (
 )
 
 type controllerRunOptions struct {
-	kubeconfig string
-	masterURL  string
+	kubeconfig   string
+	masterURL    string
+	internalAddr string
 
 	workerName  string
 	workerCount int
@@ -39,11 +46,13 @@ type controllerContext struct {
 }
 
 func main() {
+	var g run.Group
 	ctrlCtx := controllerContext{}
 	flag.StringVar(&ctrlCtx.runOptions.kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&ctrlCtx.runOptions.masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&ctrlCtx.runOptions.workerName, "worker-name", "", "The name of the worker that will only processes resources with label=worker-name.")
 	flag.IntVar(&ctrlCtx.runOptions.workerCount, "worker-count", 4, "Number of workers which process the clusters in parallel.")
+	flag.StringVar(&ctrlCtx.runOptions.internalAddr, "internal-address", "127.0.0.1:8085", "The address on which the /metrics endpoint will be served")
 	flag.Parse()
 
 	config, err := clientcmd.BuildConfigFromFlags(ctrlCtx.runOptions.masterURL, ctrlCtx.runOptions.kubeconfig)
@@ -56,7 +65,12 @@ func main() {
 		glog.Fatal(err)
 	}
 
+	// register the global error metric. Ensures that runtime.HandleError() increases the error metric
+	metrics.RegisterRuntimErrorMetricCounter("kubermatic_rbac_generator", prometheus.DefaultRegisterer)
+
+	// register an operating system signals on which we will gracefully close the app
 	ctrlCtx.stopCh = signals.SetupSignalHandler()
+
 	ctrlCtx.kubeMasterClient = kubernetes.NewForConfigOrDie(config)
 	ctrlCtx.kubermaticMasterClient = kubermaticclientset.NewForConfigOrDie(config)
 	ctrlCtx.kubermaticMasterInformerFactory = externalversions.NewFilteredSharedInformerFactory(ctrlCtx.kubermaticMasterClient, informer.DefaultInformerResyncPeriod, metav1.NamespaceAll, selector)
@@ -125,11 +139,59 @@ func main() {
 	for _, seedClusterProvider := range ctrlCtx.allClusterProviders {
 		seedClusterProvider.StartInformers(ctrlCtx.stopCh)
 		if err := seedClusterProvider.WaitForCachesToSync(ctrlCtx.stopCh); err != nil {
-			glog.Fatalf("failed to sync cache: %v", err)
+			glog.Fatalf("Closing the controller, failed to sync cache: %v", err)
 		}
 	}
 
-	go ctrl.Run(ctrlCtx.runOptions.workerCount, ctrlCtx.stopCh)
+	// This group is forever waiting in a goroutine for signals to stop
+	{
+		g.Add(func() error {
+			<-ctrlCtx.stopCh
+			glog.Info("A user has requested to stop the controller")
+			return nil
+		}, func(err error) {
+			/*an empty body*/
+		})
+	}
 
-	<-ctrlCtx.stopCh
+	// This group is running an internal http metrics server with metrics
+	{
+		m := http.NewServeMux()
+		m.Handle("/metrics", promhttp.Handler())
+
+		s := http.Server{
+			Addr:         ctrlCtx.runOptions.internalAddr,
+			Handler:      m,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
+
+		g.Add(func() error {
+			glog.Infof("Starting the internal HTTP metrics server at %s/metrics\n", ctrlCtx.runOptions.internalAddr)
+			return s.ListenAndServe()
+		}, func(err error) {
+			glog.Infof("Stopping internal HTTP metrics server, err = %v", err)
+			timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			if err := s.Shutdown(timeoutCtx); err != nil {
+				glog.Errorf("Failed to shutdown the internal HTTP server gracefully, err = %v", err)
+			}
+		})
+	}
+
+	// This group is running the actual controller logic
+	{
+		g.Add(func() error {
+			// controller will return iff ctrlCtx is stopped
+			ctrl.Run(ctrlCtx.runOptions.workerCount, ctrlCtx.stopCh)
+			return nil
+		}, func(err error) {
+			glog.Infof("Stopping RBACGenerator controller, err = %v", err)
+		})
+	}
+
+	if err := g.Run(); err != nil {
+		glog.Fatal(err)
+	}
 }
