ad8e1f4760945c2ef172a3293196595b55af1151
Christoph Mewes <christoph@loodse.com>
2019-02-09 01:03:18 +0100

    handle missing yq

--- config/monitoring/prometheus/rules/src/compile.sh
+++ config/monitoring/prometheus/rules/src/compile.sh
@@ -2,6 +2,14 @@
 
 cd $(dirname $0)
 
+# Install yq if not installed
+if ! [ -x "$(command -v yq)" ]; then
+	echo "yq not installed / vailable in PATH!"
+	echo "Executing go get on github.com/mikefarah/yq ..."
+	go get github.com/mikefarah/yq
+	echo "Done!"
+fi
+
 comment="# This file has been generated, do not edit."
 
 for file in */*.yaml; do
