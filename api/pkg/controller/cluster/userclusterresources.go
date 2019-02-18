41413a68e82fa104d740b82816184134fbe7966f
Nikhita Raghunath <nikitaraghunath@gmail.com>
2019-02-18 13:51:07 +0530

    remove the resources from the cluster controller

--- api/pkg/controller/cluster/userclusterresources.go
+++ api/pkg/controller/cluster/userclusterresources.go
@@ -8,17 +8,13 @@ import (
 
 	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
 	"github.com/kubermatic/kubermatic/api/pkg/resources"
-	"github.com/kubermatic/kubermatic/api/pkg/resources/controllermanager"
 	"github.com/kubermatic/kubermatic/api/pkg/resources/ipamcontroller"
 	"github.com/kubermatic/kubermatic/api/pkg/resources/machinecontroller"
 	"github.com/kubermatic/kubermatic/api/pkg/resources/metrics-server"
 	"github.com/kubermatic/kubermatic/api/pkg/resources/openvpn"
-	"github.com/kubermatic/kubermatic/api/pkg/resources/scheduler"
-	"github.com/kubermatic/kubermatic/api/pkg/resources/vpnsidecar"
 
 	admissionv1alpha1 "k8s.io/api/admissionregistration/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -38,23 +34,7 @@ func (cc *Controller) reconcileUserClusterResources(cluster *kubermaticv1.Cluste
 		}
 	}
 
-	if err = cc.userClusterEnsureRoles(cluster, client); err != nil {
-		return nil, err
-	}
-
 	if err = cc.userClusterEnsureConfigMaps(cluster, client); err != nil {
-		return nil, err
-	}
-
-	if err = cc.userClusterEnsureRoleBindings(cluster, client); err != nil {
-		return nil, err
-	}
-
-	if err = cc.userClusterEnsureClusterRoles(cluster, client); err != nil {
-		return nil, err
-	}
-
-	if err = cc.userClusterEnsureClusterRoleBindings(cluster, client); err != nil {
 		return nil, err
 	}
 
@@ -120,229 +100,6 @@ func (cc *Controller) userClusterEnsureInitializerConfiguration(c *kubermaticv1.
 			return fmt.Errorf("failed to update InitializerConfiguration %s: %v", initializerConfiguration.Name, err)
 		}
 		glog.V(4).Infof("Updated InitializerConfiguration %s inside user-cluster %s", initializerConfiguration.Name, c.Name)
-	}
-
-	return nil
-}
-
-func (cc *Controller) userClusterEnsureRoles(c *kubermaticv1.Cluster, client kubernetes.Interface) error {
-	creators := []resources.RoleCreator{
-		machinecontroller.EndpointReaderRole,
-		machinecontroller.KubeSystemRole,
-		machinecontroller.KubePublicRole,
-		machinecontroller.ClusterInfoReaderRole,
-	}
-
-	data, err := cc.getClusterTemplateData(c)
-	if err != nil {
-		return err
-	}
-
-	for _, create := range creators {
-		var existing *rbacv1.Role
-		role, err := create(data, nil)
-		if err != nil {
-			return fmt.Errorf("failed to build Role: %v", err)
-		}
-
-		if existing, err = client.RbacV1().Roles(role.Namespace).Get(role.Name, metav1.GetOptions{}); err != nil {
-			if !errors.IsNotFound(err) {
-				return err
-			}
-
-			if _, err = client.RbacV1().Roles(role.Namespace).Create(role); err != nil {
-				return fmt.Errorf("failed to create Role %s in namespace %s: %v", role.Name, role.Namespace, err)
-			}
-			glog.V(4).Infof("Created Role %s inside user-cluster %s", role.Name, c.Name)
-			continue
-		}
-
-		role, err = create(data, existing.DeepCopy())
-		if err != nil {
-			return fmt.Errorf("failed to build Role: %v", err)
-		}
-
-		if resources.DeepEqual(role, existing) {
-			continue
-		}
-
-		if _, err = client.RbacV1().Roles(role.Namespace).Update(role); err != nil {
-			return fmt.Errorf("failed to update Role %s in namespace %s: %v", role.Name, role.Namespace, err)
-		}
-		glog.V(4).Infof("Updated Role %s inside user-cluster %s", role.Name, c.Name)
-	}
-
-	return nil
-}
-
-func (cc *Controller) userClusterEnsureRoleBindings(c *kubermaticv1.Cluster, client kubernetes.Interface) error {
-	creators := []resources.RoleBindingCreator{
-		machinecontroller.DefaultRoleBinding,
-		machinecontroller.KubeSystemRoleBinding,
-		machinecontroller.KubePublicRoleBinding,
-		machinecontroller.ClusterInfoAnonymousRoleBinding,
-		metricsserver.RolebindingAuthReader,
-		scheduler.RoleBindingAuthDelegator,
-		controllermanager.RoleBindingAuthDelegator,
-	}
-
-	for _, create := range creators {
-		var existing *rbacv1.RoleBinding
-		rb, err := create(nil, nil)
-		if err != nil {
-			return fmt.Errorf("failed to build RoleBinding: %v", err)
-		}
-
-		if existing, err = client.RbacV1().RoleBindings(rb.Namespace).Get(rb.Name, metav1.GetOptions{}); err != nil {
-			if !errors.IsNotFound(err) {
-				return err
-			}
-
-			if _, err = client.RbacV1().RoleBindings(rb.Namespace).Create(rb); err != nil {
-				return fmt.Errorf("failed to create RoleBinding %s in namespace %s: %v", rb.Name, rb.Namespace, err)
-			}
-			glog.V(4).Infof("Created RoleBinding %s inside user-cluster %s", rb.Name, c.Name)
-			continue
-		}
-
-		rb, err = create(nil, existing.DeepCopy())
-		if err != nil {
-			return fmt.Errorf("failed to build RoleBinding: %v", err)
-		}
-
-		if equality.Semantic.DeepEqual(rb, existing) {
-			continue
-		}
-
-		if _, err = client.RbacV1().RoleBindings(rb.Namespace).Update(rb); err != nil {
-			return fmt.Errorf("failed to update RoleBinding %s in namespace %s: %v", rb.Name, rb.Namespace, err)
-		}
-		glog.V(4).Infof("Updated RoleBinding %s inside user-cluster %s", rb.Name, c.Name)
-	}
-
-	return nil
-}
-
-// GetUserClusterRoleCreators returns a list of GetUserClusterRoleCreators
-func GetUserClusterRoleCreators(c *kubermaticv1.Cluster) []resources.ClusterRoleCreator {
-	creators := []resources.ClusterRoleCreator{
-		machinecontroller.ClusterRole,
-		vpnsidecar.DnatControllerClusterRole,
-		metricsserver.ClusterRole,
-	}
-
-	if len(c.Spec.MachineNetworks) > 0 {
-		creators = append(creators, ipamcontroller.ClusterRole)
-	}
-
-	return creators
-}
-
-func (cc *Controller) userClusterEnsureClusterRoles(c *kubermaticv1.Cluster, client kubernetes.Interface) error {
-	creators := GetUserClusterRoleCreators(c)
-
-	data, err := cc.getClusterTemplateData(c)
-	if err != nil {
-		return err
-	}
-
-	for _, create := range creators {
-		var existing *rbacv1.ClusterRole
-		cRole, err := create(data, nil)
-		if err != nil {
-			return fmt.Errorf("failed to build ClusterRole: %v", err)
-		}
-
-		if existing, err = client.RbacV1().ClusterRoles().Get(cRole.Name, metav1.GetOptions{}); err != nil {
-			if !errors.IsNotFound(err) {
-				return err
-			}
-
-			if _, err = client.RbacV1().ClusterRoles().Create(cRole); err != nil {
-				return fmt.Errorf("failed to create ClusterRole %s: %v", cRole.Name, err)
-			}
-			glog.V(4).Infof("Created ClusterRole %s inside user-cluster %s", cRole.Name, c.Name)
-			continue
-		}
-
-		cRole, err = create(data, existing.DeepCopy())
-		if err != nil {
-			return fmt.Errorf("failed to build ClusterRole: %v", err)
-		}
-
-		if equality.Semantic.DeepEqual(cRole, existing) {
-			continue
-		}
-
-		if _, err = client.RbacV1().ClusterRoles().Update(cRole); err != nil {
-			return fmt.Errorf("failed to update ClusterRole %s: %v", cRole.Name, err)
-		}
-		glog.V(4).Infof("Updated ClusterRole %s inside user-cluster %s", cRole.Name, c.Name)
-	}
-
-	return nil
-}
-
-// GetUserClusterRoleBindingCreators returns a list of ClusterRoleBindingCreators which should be used to - guess what - create user cluster role bindings.
-func GetUserClusterRoleBindingCreators(c *kubermaticv1.Cluster) []resources.ClusterRoleBindingCreator {
-	creators := []resources.ClusterRoleBindingCreator{
-		machinecontroller.ClusterRoleBinding,
-		machinecontroller.NodeBootstrapperClusterRoleBinding,
-		machinecontroller.NodeSignerClusterRoleBinding,
-		vpnsidecar.DnatControllerClusterRoleBinding,
-		metricsserver.ClusterRoleBindingResourceReader,
-		metricsserver.ClusterRoleBindingAuthDelegator,
-		scheduler.ClusterRoleBindingAuthDelegator,
-		controllermanager.ClusterRoleBindingAuthDelegator,
-	}
-
-	if len(c.Spec.MachineNetworks) > 0 {
-		creators = append(creators, ipamcontroller.ClusterRoleBinding)
-	}
-
-	return creators
-}
-
-func (cc *Controller) userClusterEnsureClusterRoleBindings(c *kubermaticv1.Cluster, client kubernetes.Interface) error {
-	creators := GetUserClusterRoleBindingCreators(c)
-
-	data, err := cc.getClusterTemplateData(c)
-	if err != nil {
-		return err
-	}
-
-	for _, create := range creators {
-		var existing *rbacv1.ClusterRoleBinding
-		crb, err := create(data, nil)
-		if err != nil {
-			return fmt.Errorf("failed to build ClusterRoleBinding: %v", err)
-		}
-
-		if existing, err = client.RbacV1().ClusterRoleBindings().Get(crb.Name, metav1.GetOptions{}); err != nil {
-			if !errors.IsNotFound(err) {
-				return err
-			}
-
-			if _, err = client.RbacV1().ClusterRoleBindings().Create(crb); err != nil {
-				return fmt.Errorf("failed to create ClusterRoleBinding %s: %v", crb.Name, err)
-			}
-			glog.V(4).Infof("Created ClusterRoleBinding %s inside user-cluster %s", crb.Name, c.Name)
-			continue
-		}
-
-		crb, err = create(data, existing.DeepCopy())
-		if err != nil {
-			return fmt.Errorf("failed to build ClusterRoleBinding: %v", err)
-		}
-
-		if equality.Semantic.DeepEqual(crb, existing) {
-			continue
-		}
-
-		if _, err = client.RbacV1().ClusterRoleBindings().Update(crb); err != nil {
-			return fmt.Errorf("failed to update ClusterRoleBinding %s: %v", crb.Name, err)
-		}
-		glog.V(4).Infof("Updated ClusterRoleBinding %s inside user-cluster %s", crb.Name, c.Name)
 	}
 
 	return nil
