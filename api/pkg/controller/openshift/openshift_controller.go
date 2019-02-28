4d6c67404b9a4e30f8c901605b05e5dd1c57b647
Alvaro Aleman <alv2412@googlemail.com>
2019-02-28 11:37:09 +0100

    Scaffolding for openshift sync health

--- api/pkg/controller/openshift/openshift_controller.go
+++ api/pkg/controller/openshift/openshift_controller.go
@@ -151,6 +151,10 @@ func (r *Reconciler) reconcile(ctx context.Context, cluster *kubermaticv1.Cluste
 	return r.createClusterAccessToken(ctx, osData)
 }
 
+func (r *Reconciler) syncHeath(ctx context.Context, osData *openshiftData) error {
+	currentHealth := osData.Cluster().Status.Health.DeepCopy()
+}
+
 // Openshift doesn't seem to support a token-file-based authentication at all
 // It can be passed down onto the kube-apiserver but does still not work, presumably because OS puts another authentication
 // layer on top
