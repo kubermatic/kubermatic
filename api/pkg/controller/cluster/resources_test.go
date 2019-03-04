183a4e39382e52fa810a152267746141e8d1bc8e
Alvaro Aleman <alv2412@googlemail.com>
2019-03-04 10:06:02 +0100

    Remove test occurence of the upstream triple package

--- api/pkg/controller/cluster/resources_test.go
+++ api/pkg/controller/cluster/resources_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/kubermatic/kubermatic/api/pkg/provider"
 	"github.com/kubermatic/kubermatic/api/pkg/resources"
 	"github.com/kubermatic/kubermatic/api/pkg/resources/certificates"
+	"github.com/kubermatic/kubermatic/api/pkg/resources/certificates/triple"
 	"github.com/kubermatic/kubermatic/api/pkg/semver"
 
 	corev1 "k8s.io/api/core/v1"
@@ -22,7 +23,6 @@ import (
 	listerscorev1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	certutil "k8s.io/client-go/util/cert"
-	"k8s.io/client-go/util/cert/triple"
 )
 
 func TestLaunchingCreateNamespace(t *testing.T) {
