5cd0d79fbd59d17276ffd8286dd2d04608e02010
Alvaro Aleman <alv2412@googlemail.com>
2019-03-05 14:35:14 +0100

    Print reconciling info on level four

--- api/pkg/controller/rbac-user-cluster/reconciler.go
+++ api/pkg/controller/rbac-user-cluster/reconciler.go
@@ -25,7 +25,7 @@ type reconciler struct {
 
 // Reconcile creates and updates ClusterRoles and ClusterRoleBinding to achieve the desired state
 func (r *reconciler) Reconcile(resourceName string) error {
-	glog.Infof("Reconciling RBAC for %s", resourceName)
+	glog.V(4).Infof("Reconciling RBAC for %s", resourceName)
 
 	err := r.ensureRBACClusterRole(resourceName)
 	if err != nil {
