c8efbbfc46d8b29e0a5c60aa4192c75a1e9e73be
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2019-03-07 17:21:12 +0100

    reuse the existing deploy script (#2953)
    * reuse the existing deploy script
    
    * rename helper.sh to lib.sh
    
    * update run script
    
    * use lib.sh everywhere
    
    * fix tiller-namespace for run

---
+++ api/hack/lib.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function retry {
+  local retries=$1
+  shift
+
+  local count=0
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** $count))
+    count=$(($count + 1))
+    if [ $count -lt $retries ]; then
+      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep $wait
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      return $exit
+    fi
+  done
+  return 0
+}
+
+echodate() { echo "$(date) $@"; }
