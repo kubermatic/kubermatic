c23b1dbc97de777fbe36a6d950c89a1a484da106
mrIncompetent <henrik@henrik-schmidt.de>
2019-03-07 14:00:02 +0100

    reuse the existing deploy script

--- api/hack/ci/ci-deploy-dev-cloud.sh
+++ api/hack/ci/ci-deploy-dev-cloud.sh
@@ -2,101 +2,13 @@
 
 set -euo pipefail
 export GIT_HEAD_HASH="$(git rev-parse HEAD|tr -d '\n')"
-echodate() { echo "$(date) $@"; }
 cd $(dirname $0)/../../..
 
-function retry {
-  local retries=$1
-  shift
-
-  local count=0
-  until "$@"; do
-    exit=$?
-    wait=$((2 ** $count))
-    count=$(($count + 1))
-    if [ $count -lt $retries ]; then
-      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
-      sleep $wait
-    else
-      echo "Retry $count/$retries exited $exit, no more retries left."
-      return $exit
-    fi
-  done
-  return 0
-}
-
-declare -A chartNamespaces
-chartNamespaces=(
-["nginx-ingress-controller"]="nginx-ingress-controller"
-["cert-manager"]="cert-manager"
-["certs"]="default"
-["minio"]="minio"
-["nodeport-proxy"]="nodeport-proxy"
-["iap"]="iap"
-["kubermatic"]="kubermatic"
-["oauth"]="oauth"
-["prometheus"]="monitoring"
-["node-exporter"]="monitoring"
-["kube-state-metrics"]="monitoring"
-["grafana"]="monitoring"
-["alertmanager"]="monitoring"
-["elasticsearch"]="logging"
-["fluentbit"]="logging"
-["kibana"]="logging"
-)
-
-function deployMaster {
-  retry 5 kubectl apply -f ./config/kubermatic/crd/
-  for chart in "${!chartNamespaces[@]}"; do
-    if [[ "${chartNamespaces[$chart]}" == "monitoring" ]] || [[ "${chartNamespaces[$chart]}" == "logging" ]] ; then
-      directory_name="${chartNamespaces[$chart]}/$chart"
-    else
-      directory_name=$chart
-    fi
-
-    echodate "Deploying chart $chart"
-    retry 5 helm upgrade $1 --install --force --wait --timeout 300 \
-      --namespace=${chartNamespaces[$chart]} \
-      --values $VALUES_FILE \
-      --set=kubermatic.isMaster=true \
-      --set=kubermatic.controller.image.tag=$GIT_HEAD_HASH \
-      --set=kubermatic.api.image.tag=$GIT_HEAD_HASH \
-      --set=kubermatic.rbac.image.tag=$GIT_HEAD_HASH \
-      $chart ./config/$directory_name/
-    echodate "Finished deploying chart $chart"
-  done
-}
-
-function deploySeed {
-  retry 5 kubectl apply -f ./config/kubermatic/crd/
-  for chart in "${!chartNamespaces[@]}"; do
-      # Don't deploy ingress-related charts on non-master seeds
-      if echo $chart| grep -qE 'certs|cert-manager|oauth|iap|nginx-ingress-controller'; then
-        continue
-      fi
-
-    if [[ "${chartNamespaces[$chart]}" == "monitoring" ]] || [[ "${chartNamespaces[$chart]}" == "logging" ]] ; then
-      directory_name="${chartNamespaces[$chart]}/$chart"
-    else
-      directory_name=$chart
-    fi
-
-    echodate "Deploying chart $chart"
-    retry 5 helm upgrade $1 --install --force --wait --timeout 300 \
-      --namespace=${chartNamespaces[$chart]} \
-      --values $VALUES_FILE \
-      --set=kubermatic.isMaster=false \
-      --set=kubermatic.controller.image.tag=$GIT_HEAD_HASH \
-      --set=kubermatic.api.image.tag=$GIT_HEAD_HASH \
-      --set=kubermatic.rbac.image.tag=$GIT_HEAD_HASH \
-      $chart ./config/$directory_name/
-    echodate "Finished deploying chart $chart"
-  done
-}
+source ./api/hack/helper.sh
 
 echodate "Logging into Quay"
 docker ps &>/dev/null || start-docker.sh
-retry 5 docker login -u $QUAY_IO_USERNAME -p $QUAY_IO_PASSWORD quay.io
+retry 5 docker login -u ${QUAY_IO_USERNAME} -p ${QUAY_IO_PASSWORD} quay.io
 echodate "Successfully logged into Quay"
 
 echodate "Building binaries"
@@ -111,51 +23,46 @@ echodate "Getting secrets from Vault"
 export VAULT_ADDR=https://vault.loodse.com/
 retry 5 vault write \
   --format=json auth/approle/login \
-  role_id=$VAULT_ROLE_ID secret_id=$VAULT_SECRET_ID > /tmp/vault-token-response.json
+  role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID} > /tmp/vault-token-response.json
 export VAULT_TOKEN="$(cat /tmp/vault-token-response.json| jq .auth.client_token -r)"
 export KUBECONFIG=/tmp/kubeconfig
 export VALUES_FILE=/tmp/values.yaml
+export HELM_EXTRA_ARGS="--tiller-namespace=kubermatic-installer \
+    --set=kubermatic.controller.image.tag=${GIT_HEAD_HASH} \
+    --set=kubermatic.api.image.tag=${GIT_HEAD_HASH} \
+    --set=kubermatic.rbac.image.tag=${GIT_HEAD_HASH}"
 
 # deploy to dev
-vault kv get -field=kubeconfig \
-  dev/seed-clusters/dev.kubermatic.io > $KUBECONFIG
-vault kv get -field=values.yaml \
-  dev/seed-clusters/dev.kubermatic.io > $VALUES_FILE
+vault kv get -field=kubeconfig dev/seed-clusters/dev.kubermatic.io > ${KUBECONFIG}
+vault kv get -field=values.yaml dev/seed-clusters/dev.kubermatic.io > ${VALUES_FILE}
 echodate "Successfully got secrets for dev from Vault"
 
 echodate "Deploying Kubermatic to dev"
-deployMaster "--tiller-namespace=kubermatic-installer"
-echodate "Successfully deployed Kubermatic to dev"
+./api/hack/deploy.sh master ${VALUES_FILE} ${HELM_EXTRA_ARGS}
 
 # deploy to cloud-asia
-vault kv get -field=kubeconfig \
-  dev/seed-clusters/cloud.kubermatic.io > $KUBECONFIG
-vault kv get -field=asia-east1-a-1-values.yaml \
-  dev/seed-clusters/cloud.kubermatic.io > $VALUES_FILE
+vault kv get -field=kubeconfig dev/seed-clusters/cloud.kubermatic.io > ${KUBECONFIG}
+vault kv get -field=asia-east1-a-1-values.yaml dev/seed-clusters/cloud.kubermatic.io > ${VALUES_FILE}
+kubectl config use-context asia-east1-a-1
 echodate "Successfully got secrets for cloud-asia from Vault"
 
 echodate "Deploying Kubermatic to cloud-asia"
-deploySeed "--kube-context=asia-east1-a-1 --tiller-namespace=kubermatic-installer"
-echodate "Successfully deployed Kubermatic to cloud-asia"
+./api/hack/deploy.sh seed ${VALUES_FILE} ${HELM_EXTRA_ARGS}
 
-# deploy to cloud-europe
-vault kv get -field=kubeconfig \
-  dev/seed-clusters/cloud.kubermatic.io > $KUBECONFIG
-vault kv get -field=europe-west3-c-1-values.yaml \
-  dev/seed-clusters/cloud.kubermatic.io > $VALUES_FILE
-echodate "Successfully got secrets for cloud-europe from Vault"
+# deploy to cloud-eu
+vault kv get -field=kubeconfig dev/seed-clusters/cloud.kubermatic.io > ${KUBECONFIG}
+vault kv get -field=europe-west3-c-1-values.yaml dev/seed-clusters/cloud.kubermatic.io > ${VALUES_FILE}
+kubectl config use-context europe-west3-c-1
+echodate "Successfully got secrets for cloud-eu from Vault"
 
-echodate "Deploying Kubermatic to cloud-europe"
-deployMaster "--kube-context=europe-west3-c-1 --tiller-namespace=kubermatic-installer"
-echodate "Successfully deployed Kubermatic to cloud-europe"
+echodate "Deploying Kubermatic to cloud-eu"
+./api/hack/deploy.sh master ${VALUES_FILE} ${HELM_EXTRA_ARGS}
 
 # deploy to cloud-us
-vault kv get -field=kubeconfig \
-  dev/seed-clusters/cloud.kubermatic.io > $KUBECONFIG
-vault kv get -field=us-central1-c-1-values.yaml \
-  dev/seed-clusters/cloud.kubermatic.io > $VALUES_FILE
+vault kv get -field=kubeconfig dev/seed-clusters/cloud.kubermatic.io > ${KUBECONFIG}
+vault kv get -field=us-central1-c-1-values.yaml dev/seed-clusters/cloud.kubermatic.io > ${VALUES_FILE}
+kubectl config use-context us-central1-c-1
 echodate "Successfully got secrets for cloud-us from Vault"
 
 echodate "Deploying Kubermatic to cloud-us"
-deploySeed "--kube-context=us-central1-c-1 --tiller-namespace=kubermatic-installer"
-echodate "Successfully deployed Kubermatic to cloud-us"
+./api/hack/deploy.sh seed ${VALUES_FILE} ${HELM_EXTRA_ARGS}
--- api/hack/deploy.sh
+++ api/hack/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -xeuo pipefail
+set -euo pipefail
 
 if [ "$#" -lt 1 ] || [ "${1}" == "--help" ]; then
   cat <<EOF
@@ -13,31 +13,51 @@ if [[ ! -f ${2} ]]; then
     exit 1
 fi
 
-HELM_EXTRA_ARGS=${3:-""}
+HELM_EXTRA_ARGS=${@:3}
 VALUES_FILE=$(realpath ${2})
+if [[ "${1}" = "master" ]]; then
+  MASTER_FLAG="--set=kubermatic.isMaster=true"
+else
+  MASTER_FLAG="--set=kubermatic.isMaster=false"
+fi
+
 cd "$(dirname "$0")/../../"
 
-kubectl apply -f ./config/kubermatic/crd/
+source ./api/hack/helper.sh
 
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace nginx-ingress-controller nginx-ingress-controller ./config/nginx-ingress-controller/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace cert-manager cert-manager ./config/cert-manager/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace default certs ./config/certs/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace minio minio ./config/minio/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace nodeport-proxy nodeport-proxy ./config/nodeport-proxy/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace iap iap ./config/iap/
+function deploy {
+  local name=$1
+  local namespace=$2
+  local path=$3
 
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace monitoring prometheus ./config/monitoring/prometheus/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace monitoring node-exporter ./config/monitoring/node-exporter/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace monitoring kube-state-metrics ./config/monitoring/kube-state-metrics/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace monitoring grafana ./config/monitoring/grafana/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace monitoring alertmanager ./config/monitoring/alertmanager/
+  retry 5 helm upgrade --install --force --wait --timeout 300 ${MASTER_FLAG} ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace ${namespace} ${name} ${path}
+}
 
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace logging elasticsearch ./config/logging/elasticsearch/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace logging fluentbit ./config/logging/fluentbit/
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace logging kibana ./config/logging/kibana/
-
-helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace kubermatic kubermatic ./config/kubermatic/
+echo "Deploying the CRD's..."
+retry 5 kubectl apply -f ./config/kubermatic/crd/
 
 if [[ "${1}" = "master" ]]; then
-    helm upgrade --install --wait --timeout 300 ${HELM_EXTRA_ARGS} --values ${VALUES_FILE} --namespace oauth oauth ./config/oauth/
+    deploy "nginx-ingress-controller" "nginx-ingress-controller" ./config/nginx-ingress-controller/
+    deploy "cert-manager" "cert-manager" ./config/cert-manager/
+    deploy "certs" "default" ./config/certs/
+    deploy "oauth" "oauth" ./config/oauth/
+    deploy "iap" "iap" ./config/iap/
 fi
+
+deploy "minio" "minio" ./config/minio/
+deploy "nodeport-proxy" "nodeport-proxy" ./config/nodeport-proxy/
+
+#Monitoring
+deploy "prometheus" "monitoring" ./config/monitoring/prometheus/
+deploy "node-exporter" "monitoring" ./config/monitoring/node-exporter/
+deploy "kube-state-metrics" "monitoring" ./config/monitoring/kube-state-metrics/
+deploy "grafana" "monitoring" ./config/monitoring/grafana/
+deploy "alertmanager" "monitoring" ./config/monitoring/alertmanager/
+
+#Logging
+deploy "elasticsearch" "logging" ./config/logging/elasticsearch/
+deploy "fluentbit" "logging" ./config/logging/fluentbit/
+deploy "kibana" "logging" ./config/logging/kibana/
+
+#Kubermatic
+deploy "kubermatic" "kubermatic" ./config/kubermatic/
