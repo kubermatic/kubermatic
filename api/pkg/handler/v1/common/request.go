4dca3bfa88d77ba2ea879876120e3ef120079014
zreigz <zreigz@gmail.com>
2019-03-26 10:30:57 +0100

    new endpoint to get a list of service accounts

--- api/pkg/handler/v1/common/request.go
+++ api/pkg/handler/v1/common/request.go
@@ -31,7 +31,7 @@ type ProjectIDGetter interface {
 }
 
 // GetProjectRq defines HTTP request for getProject endpoint
-// swagger:parameters getProject getUsersForProject listClustersForProject
+// swagger:parameters getProject getUsersForProject listClustersForProject listServiceAccounts
 type GetProjectRq struct {
 	ProjectReq
 }
