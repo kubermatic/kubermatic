6d4aa29cf944a7dcfd9184e63529369a5b624f9c
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2019-04-02 18:39:44 +0200

    Fix mutatingWebhookConfiguration idempotency (#3101)

--- api/pkg/controller/usercluster/resources/machine-controller/webhook.go
+++ api/pkg/controller/usercluster/resources/machine-controller/webhook.go
@@ -20,49 +20,41 @@ func MutatingwebhookConfigurationCreator(caCert *x509.Certificate, namespace str
 			failurePolicy := admissionregistrationv1beta1.Fail
 			mdURL := fmt.Sprintf("https://%s.%s.svc.cluster.local./machinedeployments", resources.MachineControllerWebhookServiceName, namespace)
 			mURL := fmt.Sprintf("https://%s.%s.svc.cluster.local./machines", resources.MachineControllerWebhookServiceName, namespace)
-			sideEffectClass := admissionregistrationv1beta1.SideEffectClassNone
 
-			mutatingWebhookConfiguration.Webhooks = []admissionregistrationv1beta1.Webhook{
-				{
-					Name:              fmt.Sprintf("%s-machinedeployments", resources.MachineControllerMutatingWebhookConfigurationName),
-					NamespaceSelector: &metav1.LabelSelector{},
-					FailurePolicy:     &failurePolicy,
-					SideEffects:       &sideEffectClass,
-					Rules: []admissionregistrationv1beta1.RuleWithOperations{
-						{
-							Operations: []admissionregistrationv1beta1.OperationType{admissionregistrationv1beta1.Create, admissionregistrationv1beta1.Update},
-							Rule: admissionregistrationv1beta1.Rule{
-								APIGroups:   []string{clusterAPIGroup},
-								APIVersions: []string{clusterAPIVersion},
-								Resources:   []string{"machinedeployments"},
-							},
-						},
-					},
-					ClientConfig: admissionregistrationv1beta1.WebhookClientConfig{
-						URL:      &mdURL,
-						CABundle: certutil.EncodeCertPEM(caCert),
-					},
+			if len(mutatingWebhookConfiguration.Webhooks) != 2 {
+				mutatingWebhookConfiguration.Webhooks = []admissionregistrationv1beta1.Webhook{{}, {}}
+			}
+
+			mutatingWebhookConfiguration.Webhooks[0].Name = fmt.Sprintf("%s-machinedeployments", resources.MachineControllerMutatingWebhookConfigurationName)
+			mutatingWebhookConfiguration.Webhooks[0].NamespaceSelector = &metav1.LabelSelector{}
+			mutatingWebhookConfiguration.Webhooks[0].FailurePolicy = &failurePolicy
+			mutatingWebhookConfiguration.Webhooks[0].Rules = []admissionregistrationv1beta1.RuleWithOperations{{
+				Operations: []admissionregistrationv1beta1.OperationType{admissionregistrationv1beta1.Create, admissionregistrationv1beta1.Update},
+				Rule: admissionregistrationv1beta1.Rule{
+					APIGroups:   []string{clusterAPIGroup},
+					APIVersions: []string{clusterAPIVersion},
+					Resources:   []string{"machinedeployments"},
 				},
-				{
-					Name:              fmt.Sprintf("%s-machines", resources.MachineControllerMutatingWebhookConfigurationName),
-					NamespaceSelector: &metav1.LabelSelector{},
-					FailurePolicy:     &failurePolicy,
-					SideEffects:       &sideEffectClass,
-					Rules: []admissionregistrationv1beta1.RuleWithOperations{
-						{
-							Operations: []admissionregistrationv1beta1.OperationType{admissionregistrationv1beta1.Create, admissionregistrationv1beta1.Update},
-							Rule: admissionregistrationv1beta1.Rule{
-								APIGroups:   []string{clusterAPIGroup},
-								APIVersions: []string{clusterAPIVersion},
-								Resources:   []string{"machines"},
-							},
-						},
-					},
-					ClientConfig: admissionregistrationv1beta1.WebhookClientConfig{
-						URL:      &mURL,
-						CABundle: certutil.EncodeCertPEM(caCert),
-					},
+			}}
+			mutatingWebhookConfiguration.Webhooks[0].ClientConfig = admissionregistrationv1beta1.WebhookClientConfig{
+				URL:      &mdURL,
+				CABundle: certutil.EncodeCertPEM(caCert),
+			}
+
+			mutatingWebhookConfiguration.Webhooks[1].Name = fmt.Sprintf("%s-machines", resources.MachineControllerMutatingWebhookConfigurationName)
+			mutatingWebhookConfiguration.Webhooks[1].NamespaceSelector = &metav1.LabelSelector{}
+			mutatingWebhookConfiguration.Webhooks[1].FailurePolicy = &failurePolicy
+			mutatingWebhookConfiguration.Webhooks[1].Rules = []admissionregistrationv1beta1.RuleWithOperations{{
+				Operations: []admissionregistrationv1beta1.OperationType{admissionregistrationv1beta1.Create, admissionregistrationv1beta1.Update},
+				Rule: admissionregistrationv1beta1.Rule{
+					APIGroups:   []string{clusterAPIGroup},
+					APIVersions: []string{clusterAPIVersion},
+					Resources:   []string{"machines"},
 				},
+			}}
+			mutatingWebhookConfiguration.Webhooks[1].ClientConfig = admissionregistrationv1beta1.WebhookClientConfig{
+				URL:      &mURL,
+				CABundle: certutil.EncodeCertPEM(caCert),
 			}
 
 			return mutatingWebhookConfiguration, nil
