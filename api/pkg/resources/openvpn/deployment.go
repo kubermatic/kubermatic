e1b078bf646e1551c683303d1bdf11ee51ac6a69
mrIncompetent <henrik@henrik-schmidt.de>
2019-04-09 17:49:05 +0200

    update the openvpn container

--- api/pkg/resources/openvpn/deployment.go
+++ api/pkg/resources/openvpn/deployment.go
@@ -112,7 +112,7 @@ func DeploymentCreator(data *resources.TemplateData) reconciling.NamedDeployment
 			dep.Spec.Template.Spec.InitContainers = []corev1.Container{
 				{
 					Name:            "iptables-init",
-					Image:           data.ImageRegistry(resources.RegistryDocker) + "/kubermatic/openvpn:v0.4",
+					Image:           data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/openvpn:v0.5",
 					ImagePullPolicy: corev1.PullIfNotPresent,
 					Command:         []string{"/bin/bash"},
 					Args: []string{
@@ -171,7 +171,7 @@ iptables -A INPUT -i tun0 -j DROP
 			dep.Spec.Template.Spec.Containers = []corev1.Container{
 				{
 					Name:                     name,
-					Image:                    data.ImageRegistry(resources.RegistryDocker) + "/kubermatic/openvpn:v0.4",
+					Image:                    data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/openvpn:v0.5",
 					ImagePullPolicy:          corev1.PullIfNotPresent,
 					Command:                  []string{"/usr/sbin/openvpn"},
 					Args:                     vpnArgs,
@@ -224,7 +224,7 @@ iptables -A INPUT -i tun0 -j DROP
 				},
 				{
 					Name:            "ip-forward",
-					Image:           data.ImageRegistry(resources.RegistryDocker) + "/kubermatic/openvpn:v0.4",
+					Image:           data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/openvpn:v0.5",
 					ImagePullPolicy: corev1.PullIfNotPresent,
 					Command:         []string{"/bin/bash"},
 					Args: []string{
--- containers/openvpn/release.sh
+++ containers/openvpn/release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ver=v0.4
+ver=v0.5
 
 set -euox pipefail
 
-docker build --no-cache --pull -t kubermatic/openvpn:$ver .
-docker push kubermatic/openvpn:$ver
+docker build --no-cache --pull -t quay.io/kubermatic/openvpn:$ver .
+#docker push quay.io/kubermatic/openvpn:$ver
