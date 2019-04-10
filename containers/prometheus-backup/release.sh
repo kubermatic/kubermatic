af296ec3d7de8cb2166c1848e3f97cb3f25b98d3
Christoph Mewes <christoph@loodse.com>
2019-04-10 13:43:19 +0200

    use a single 'util' Docker image with commonly used tools

--- containers/prometheus-backup/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-NUMBER=1
-VERSION=1.0.0
-
-set -euox pipefail
-
-docker build --no-cache --pull -t quay.io/kubermatic/prometheus-backup:${VERSION}-${NUMBER} .
-docker push quay.io/kubermatic/prometheus-backup:${VERSION}-${NUMBER}
