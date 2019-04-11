01b04075eb5d9313d092ad86040013b10b0b6cc2
mrIncompetent <henrik@henrik-schmidt.de>
2019-04-11 12:07:25 +0200

    fix openvpn container release script

--- containers/openvpn/release.sh
+++ containers/openvpn/release.sh
@@ -5,4 +5,4 @@ ver=v0.5
 set -euox pipefail
 
 docker build --no-cache --pull -t quay.io/kubermatic/openvpn:$ver .
-#docker push quay.io/kubermatic/openvpn:$ver
+docker push quay.io/kubermatic/openvpn:$ver
