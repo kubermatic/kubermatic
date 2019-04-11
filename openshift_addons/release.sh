f322f03e05cdb8a512ddab1c3441008fc72fe9f2
mrIncompetent <henrik@henrik-schmidt.de>
2019-04-11 15:40:46 +0200

    update images

--- openshift_addons/release.sh
+++ openshift_addons/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -ex
-export TAG=v0.7-dev1
+export TAG=v0.7
 
 docker build -t quay.io/kubermatic/openshift-addons:${TAG} .
 docker push quay.io/kubermatic/openshift-addons:${TAG}
