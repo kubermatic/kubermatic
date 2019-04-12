32767e8aa9872a0fc1ffca624e3d74351c8da4ca
mrIncompetent <henrik@henrik-schmidt.de>
2019-04-12 10:33:08 +0200

    migrate the monitoring-controller to controller-runtime

--- api/pkg/controller/monitoring/monitoring_controller.go
+++ api/pkg/controller/monitoring/monitoring_controller.go
@@ -8,33 +8,31 @@ import (
 	"github.com/golang/glog"
 
 	k8cuserclusterclient "github.com/kubermatic/kubermatic/api/pkg/cluster/client"
-	kubermaticv1informers "github.com/kubermatic/kubermatic/api/pkg/crd/client/informers/externalversions/kubermatic/v1"
-	kubermaticv1lister "github.com/kubermatic/kubermatic/api/pkg/crd/client/listers/kubermatic/v1"
 	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
 	"github.com/kubermatic/kubermatic/api/pkg/provider"
 
-	kerrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/util/wait"
-	appsv1informer "k8s.io/client-go/informers/apps/v1"
-	corev1informers "k8s.io/client-go/informers/core/v1"
-	rbacv1informer "k8s.io/client-go/informers/rbac/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	kubeapierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	autoscalingv1beta2 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1beta2"
 	"k8s.io/client-go/kubernetes"
-	appsv1lister "k8s.io/client-go/listers/apps/v1"
-	corev1lister "k8s.io/client-go/listers/core/v1"
-	rbacb1lister "k8s.io/client-go/listers/rbac/v1"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
-
+	"k8s.io/client-go/tools/record"
 	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 const (
 	// The monitoring controller waits for the cluster to become healthy,
 	// before adding the monitoring components to the clusters
 	healthCheckPeriod = 5 * time.Second
+
+	ControllerName = "kubermatic_monitoring_controller"
 )
 
 // userClusterConnectionProvider offers functions to retrieve clients for the given user clusters
@@ -47,11 +45,13 @@ type Features struct {
 	VPA bool
 }
 
-// Controller stores all components required for monitoring
-type Controller struct {
-	kubeClient              kubernetes.Interface
-	dynamicClient           ctrlruntimeclient.Client
+// Reconciler stores all components required for monitoring
+type Reconciler struct {
+	ctrlruntimeclient.Client
 	userClusterConnProvider userClusterConnectionProvider
+	workerName              string
+
+	recorder record.EventRecorder
 
 	dcs                                              map[string]provider.DatacenterMeta
 	dc                                               string
@@ -67,29 +67,17 @@ type Controller struct {
 	// example: kubermatic.io -> kubermatic.io/path,kubermatic.io/port
 	monitoringScrapeAnnotationPrefix string
 
-	queue workqueue.RateLimitingInterface
-
-	clusterLister            kubermaticv1lister.ClusterLister
-	serviceAccountLister     corev1lister.ServiceAccountLister
-	configMapLister          corev1lister.ConfigMapLister
-	roleLister               rbacb1lister.RoleLister
-	roleBindingLister        rbacb1lister.RoleBindingLister
-	serviceLister            corev1lister.ServiceLister
-	statefulSetLister        appsv1lister.StatefulSetLister
-	clusterRoleBindingLister rbacb1lister.ClusterRoleBindingLister
-	deploymentLister         appsv1lister.DeploymentLister
-	secretLister             corev1lister.SecretLister
-
 	features Features
 }
 
-// New creates a new Monitoring controller that is responsible for
+// Add creates a new Monitoring controller that is responsible for
 // operating the monitoring components for all managed user clusters
-func New(
-	kubeClient kubernetes.Interface,
-	dynamicClient ctrlruntimeclient.Client,
-	userClusterConnProvider userClusterConnectionProvider,
+func Add(
+	mgr manager.Manager,
+	numWorkers int,
+	workerName string,
 
+	userClusterConnProvider userClusterConnectionProvider,
 	dc string,
 	dcs map[string]provider.DatacenterMeta,
 	overwriteRegistry string,
@@ -102,28 +90,14 @@ func New(
 	inClusterPrometheusScrapingConfigsFile string,
 	dockerPullConfigJSON []byte,
 
-	clusterInformer kubermaticv1informers.ClusterInformer,
-	serviceAccountInformer corev1informers.ServiceAccountInformer,
-	configMapInformer corev1informers.ConfigMapInformer,
-	roleInformer rbacv1informer.RoleInformer,
-	roleBindingInformer rbacv1informer.RoleBindingInformer,
-	serviceInformer corev1informers.ServiceInformer,
-	statefulSetInformer appsv1informer.StatefulSetInformer,
-	clusterRoleBindingInformer rbacv1informer.ClusterRoleBindingInformer,
-	deploymentInformer appsv1informer.DeploymentInformer,
-	secretInformer corev1informers.SecretInformer,
-
 	features Features,
-) (*Controller, error) {
-	c := &Controller{
-		kubeClient:              kubeClient,
-		dynamicClient:           dynamicClient,
+) error {
+	reconciler := &Reconciler{
+		Client:                  mgr.GetClient(),
 		userClusterConnProvider: userClusterConnProvider,
+		workerName:              workerName,
 
-		queue: workqueue.NewNamedRateLimitingQueue(
-			workqueue.NewItemExponentialFailureRateLimiter(1*time.Second, 5*time.Minute),
-			"monitoring_cluster",
-		),
+		recorder: mgr.GetRecorder(ControllerName),
 
 		overwriteRegistry:                                overwriteRegistry,
 		nodePortRange:                                    nodePortRange,
@@ -141,238 +115,139 @@ func New(
 		features: features,
 	}
 
-	clusterInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			c.enqueue(obj.(*kubermaticv1.Cluster))
-		},
-		UpdateFunc: func(_, new interface{}) {
-			c.enqueue(new.(*kubermaticv1.Cluster))
-		},
-		DeleteFunc: func(obj interface{}) {
-			cluster, ok := obj.(*kubermaticv1.Cluster)
-			if !ok {
-				tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
-				if !ok {
-					runtime.HandleError(fmt.Errorf("couldn't get object from tombstone %#v", obj))
-					return
-				}
-				cluster, ok = tombstone.Obj.(*kubermaticv1.Cluster)
-				if !ok {
-					runtime.HandleError(fmt.Errorf("tombstone contained object that is not a Cluster %#v", obj))
-					return
-				}
-			}
-			c.enqueue(cluster)
-		},
-	})
-	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) { c.handleChildObject(obj) },
-		UpdateFunc: func(old, cur interface{}) { c.handleChildObject(cur) },
-		DeleteFunc: func(obj interface{}) { c.handleChildObject(obj) },
-	})
-	serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) { c.handleChildObject(obj) },
-		UpdateFunc: func(old, cur interface{}) { c.handleChildObject(cur) },
-		DeleteFunc: func(obj interface{}) { c.handleChildObject(obj) },
-	})
-	configMapInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) { c.handleChildObject(obj) },
-		UpdateFunc: func(old, cur interface{}) { c.handleChildObject(cur) },
-		DeleteFunc: func(obj interface{}) { c.handleChildObject(obj) },
-	})
-	serviceAccountInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) { c.handleChildObject(obj) },
-		UpdateFunc: func(old, cur interface{}) { c.handleChildObject(cur) },
-		DeleteFunc: func(obj interface{}) { c.handleChildObject(obj) },
-	})
-	roleInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) { c.handleChildObject(obj) },
-		UpdateFunc: func(old, cur interface{}) { c.handleChildObject(cur) },
-		DeleteFunc: func(obj interface{}) { c.handleChildObject(obj) },
-	})
-	roleBindingInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) { c.handleChildObject(obj) },
-		UpdateFunc: func(old, cur interface{}) { c.handleChildObject(cur) },
-		DeleteFunc: func(obj interface{}) { c.handleChildObject(obj) },
-	})
-	clusterRoleBindingInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    func(obj interface{}) { c.handleChildObject(obj) },
-		UpdateFunc: func(old, cur interface{}) { c.handleChildObject(cur) },
-		DeleteFunc: func(obj interface{}) { c.handleChildObject(obj) },
-	})
-
-	c.clusterLister = clusterInformer.Lister()
-	c.serviceLister = serviceInformer.Lister()
-	c.configMapLister = configMapInformer.Lister()
-	c.serviceAccountLister = serviceAccountInformer.Lister()
-	c.deploymentLister = deploymentInformer.Lister()
-	c.statefulSetLister = statefulSetInformer.Lister()
-	c.roleLister = roleInformer.Lister()
-	c.roleBindingLister = roleBindingInformer.Lister()
-	c.clusterRoleBindingLister = clusterRoleBindingInformer.Lister()
-	c.secretLister = secretInformer.Lister()
-
-	return c, nil
-}
-
-// Run starts the controller's worker routines. This method is blocking and ends when stopCh gets closed
-func (c *Controller) Run(workerCount int, stopCh <-chan struct{}) {
-	defer runtime.HandleCrash()
-
-	for i := 0; i < workerCount; i++ {
-		go wait.Until(c.runWorker, time.Second, stopCh)
-	}
-
-	<-stopCh
-}
-
-func (c *Controller) runWorker() {
-	for c.processNextItem() {
-	}
-}
-
-func (c *Controller) processNextItem() bool {
-	key, quit := c.queue.Get()
-	if quit {
-		return false
-	}
-	defer c.queue.Done(key)
-
-	if err := c.sync(key.(string)); err != nil {
-		glog.V(0).Infof("Error syncing %v: %v", key, err)
-
-		// Re-enqueue the key rate limited. Based on the rate limiter on the
-		// queue and the re-enqueue history, the key will be processed later again.
-		c.queue.AddRateLimited(key)
-		return true
-	}
-
-	// Forget about the #AddRateLimited history of the key on every successful synchronization.
-	// This ensures that future processing of updates for this key is not delayed because of
-	// an outdated error history.
-	c.queue.Forget(key)
-	return true
-}
-
-func (c *Controller) enqueueAfter(cluster *kubermaticv1.Cluster, duration time.Duration) {
-	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(cluster)
+	ctrlOptions := controller.Options{Reconciler: reconciler, MaxConcurrentReconciles: numWorkers}
+	c, err := controller.New(ControllerName, mgr, ctrlOptions)
 	if err != nil {
-		runtime.HandleError(fmt.Errorf("couldn't get key for object %#v: %v", cluster, err))
-		return
+		return err
 	}
 
-	c.queue.AddAfter(key, duration)
-}
+	ownerHandler := &handler.EnqueueRequestForOwner{IsController: true, OwnerType: &kubermaticv1.Cluster{}}
 
-func (c *Controller) enqueue(cluster *kubermaticv1.Cluster) {
-	c.enqueueAfter(cluster, 0)
-}
-
-func (c *Controller) handleChildObject(obj interface{}) {
-	object, ok := obj.(metav1.Object)
-	if !ok {
-		utilruntime.HandleError(fmt.Errorf("error decoding object, invalid type"))
-		return
+	typesToWatch := []runtime.Object{
+		&corev1.ServiceAccount{},
+		&rbacv1.Role{},
+		&rbacv1.RoleBinding{},
+		&corev1.Secret{},
+		&corev1.ConfigMap{},
+		&appsv1.Deployment{},
+		&appsv1.StatefulSet{},
+		&autoscalingv1beta2.VerticalPodAutoscaler{},
+		&corev1.Service{},
 	}
 
-	if ownerRef := metav1.GetControllerOf(object); ownerRef != nil && ownerRef.Kind == kubermaticv1.ClusterKindName {
-		cluster, err := c.clusterLister.Get(ownerRef.Name)
-		if err != nil {
-			glog.V(4).Infof("Ignoring orphaned object '%s' from cluster '%s'", object.GetSelfLink(), ownerRef.Name)
-			return
+	for _, t := range typesToWatch {
+		if err := c.Watch(&source.Kind{Type: t}, ownerHandler); err != nil {
+			return fmt.Errorf("failed to create watcher for %T: %v", t, err)
 		}
-		c.enqueue(cluster)
-		return
 	}
+
+	return c.Watch(&source.Kind{Type: &kubermaticv1.Cluster{}}, &handler.EnqueueRequestForObject{})
 }
 
-func (c *Controller) sync(key string) error {
+func (r *Reconciler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	clusterFromCache, err := c.clusterLister.Get(key)
-	if err != nil {
-		if kerrors.IsNotFound(err) {
-			glog.V(2).Infof("cluster '%s' in work queue no longer exists", key)
-			return nil
+	cluster := &kubermaticv1.Cluster{}
+	if err := r.Get(ctx, request.NamespacedName, cluster); err != nil {
+		if kubeapierrors.IsNotFound(err) {
+			return reconcile.Result{}, nil
 		}
-		return err
+		return reconcile.Result{}, err
 	}
-	cluster := clusterFromCache.DeepCopy()
 
 	if cluster.Spec.Pause {
-		glog.V(6).Infof("skipping cluster %s due to it was set to paused", key)
-		return nil
+		glog.V(4).Infof("skipping cluster %s due to it was set to paused", cluster.Name)
+		return reconcile.Result{}, nil
+	}
+
+	if cluster.Labels[kubermaticv1.WorkerNameLabelKey] != r.workerName {
+		return reconcile.Result{}, nil
 	}
 
 	if cluster.DeletionTimestamp != nil {
-		// Cluster got deleted - all monitoring components will die soon
-		return nil
+		// Cluster got deleted - all monitoring components will be automatically garbage collected (Due to the ownerRef)
+		return reconcile.Result{}, nil
 	}
 
 	if cluster.Status.NamespaceName == "" {
-		glog.V(6).Infof("skipping cluster %s because it has no namespace yet", key)
-		c.enqueueAfter(clusterFromCache, healthCheckPeriod)
-		return nil
+		glog.V(4).Infof("skipping cluster %s because it has no namespace yet", cluster.Name)
+		return reconcile.Result{RequeueAfter: healthCheckPeriod}, nil
 	}
 
 	// Wait until the UCCM is ready - otherwise we deploy with missing RBAC resources
 	if !cluster.Status.Health.UserClusterControllerManager {
-		glog.V(6).Infof("skipping cluster %s because the UserClusterControllerManager is not ready yet", key)
-		c.enqueueAfter(clusterFromCache, healthCheckPeriod)
-		return nil
+		glog.V(6).Infof("skipping cluster %s because the UserClusterControllerManager is not ready yet", cluster.Name)
+		return reconcile.Result{RequeueAfter: healthCheckPeriod}, nil
 	}
-	glog.V(4).Infof("syncing cluster %s", key)
 
-	data, err := c.getClusterTemplateData(context.Background(), c.dynamicClient, cluster)
+	// Add a wrapping here so we can emit an event on error
+	result, err := r.reconcile(ctx, cluster)
 	if err != nil {
-		return err
+		glog.Errorf("Failed to reconcile cluster %s: %v", cluster.Name, err)
+		r.recorder.Eventf(cluster, corev1.EventTypeWarning, "ReconcilingError", "%v", err)
+	}
+	if result == nil {
+		result = &reconcile.Result{}
+	}
+	return *result, err
+}
+
+func (r *Reconciler) reconcile(ctx context.Context, cluster *kubermaticv1.Cluster) (*reconcile.Result, error) {
+	glog.V(4).Infof("Reconciling cluster %s", cluster.Name)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	data, err := r.getClusterTemplateData(context.Background(), r.Client, cluster)
+	if err != nil {
+		return nil, err
 	}
 
 	// check that all service accounts are created
-	if err := c.ensureServiceAccounts(ctx, cluster, data); err != nil {
-		return err
+	if err := r.ensureServiceAccounts(ctx, cluster, data); err != nil {
+		return nil, err
 	}
 
 	// check that all roles are created
-	if err := c.ensureRoles(ctx, cluster); err != nil {
-		return err
+	if err := r.ensureRoles(ctx, cluster); err != nil {
+		return nil, err
 	}
 
 	// check that all role bindings are created
-	if err := c.ensureRoleBindings(ctx, cluster); err != nil {
-		return err
+	if err := r.ensureRoleBindings(ctx, cluster); err != nil {
+		return nil, err
 	}
 
 	// check that all secrets are created
-	if err := c.ensureSecrets(ctx, cluster, data); err != nil {
-		return err
+	if err := r.ensureSecrets(ctx, cluster, data); err != nil {
+		return nil, err
 	}
 
 	// check that all ConfigMaps are available
-	if err := c.ensureConfigMaps(ctx, cluster, data); err != nil {
-		return err
+	if err := r.ensureConfigMaps(ctx, cluster, data); err != nil {
+		return nil, err
 	}
 
 	// check that all Deployments are available
-	if err := c.ensureDeployments(ctx, cluster, data); err != nil {
-		return err
+	if err := r.ensureDeployments(ctx, cluster, data); err != nil {
+		return nil, err
 	}
 
 	// check that all StatefulSets are created
-	if err := c.ensureStatefulSets(ctx, cluster, data); err != nil {
-		return err
+	if err := r.ensureStatefulSets(ctx, cluster, data); err != nil {
+		return nil, err
 	}
 
 	// check that all VerticalPodAutoscaler's are created
-	if err := c.ensureVerticalPodAutoscalers(ctx, cluster); err != nil {
-		return err
+	if err := r.ensureVerticalPodAutoscalers(ctx, cluster); err != nil {
+		return nil, err
 	}
 
 	// check that all Services's are created
-	if err := c.ensureServices(ctx, cluster, data); err != nil {
-		return err
+	if err := r.ensureServices(ctx, cluster, data); err != nil {
+		return nil, err
 	}
 
-	return nil
+	return &reconcile.Result{}, nil
 }
