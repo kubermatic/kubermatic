06db665da84d478152f02dc2b17f8717b2c7a9a5
mrIncompetent <henrik@henrik-schmidt.de>
2019-04-25 10:55:46 +0200

    use template functions in all addons

--- openshift_addons/release.sh
+++ openshift_addons/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -ex
-export TAG=v0.8
+export TAG=v0.9-dev1
 
 docker build -t quay.io/kubermatic/openshift-addons:${TAG} .
 docker push quay.io/kubermatic/openshift-addons:${TAG}
