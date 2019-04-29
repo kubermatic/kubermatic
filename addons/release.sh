376b1928bae4b44dccb861c55c1eaa4fdc578b90
Alvaro Aleman <alv2412@googlemail.com>
2019-04-29 12:41:59 +0200

    Use gcr.io/google_containers instead of k8s.gcr.io

--- addons/release.sh
+++ addons/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -ex
-export TAG=v0.2.10-dev-3
+export TAG=v0.2.10-dev-4
 
 docker build -t quay.io/kubermatic/addons:${TAG} .
 docker push quay.io/kubermatic/addons:${TAG}
