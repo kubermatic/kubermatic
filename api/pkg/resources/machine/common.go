ca93da6e48de85e0185165885353b9037d3d38e7
Nikhita Raghunath <nikitaraghunath@gmail.com>
2019-05-08 12:51:52 +0530

    Use kubernetes instead of kubermatic in tags

--- api/pkg/resources/machine/common.go
+++ api/pkg/resources/machine/common.go
@@ -168,7 +168,7 @@ func getOpenstackProviderSpec(c *kubermaticv1.Cluster, nodeSpec apiv1.NodeSpec, 
 	for key, value := range nodeSpec.Cloud.Openstack.Tags {
 		config.Tags[key] = value
 	}
-	config.Tags["kubermatic-cluster"] = c.Name
+	config.Tags["kubernetes-cluster"] = c.Name
 
 	ext := &runtime.RawExtension{}
 	b, err := json.Marshal(config)
@@ -208,7 +208,7 @@ func getDigitaloceanProviderSpec(c *kubermaticv1.Cluster, nodeSpec apiv1.NodeSpe
 	}
 
 	tags := sets.NewString(nodeSpec.Cloud.Digitalocean.Tags...)
-	tags.Insert("kubermatic", "kubermatic-cluster-"+c.Name)
+	tags.Insert("kubernetes", "kubernetes-cluster-"+c.Name)
 	config.Tags = make([]providerconfig.ConfigVarString, len(tags.List()))
 	for i, tag := range tags.List() {
 		config.Tags[i].Value = tag
