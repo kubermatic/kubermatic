459e4d010175d3403528ea4266f99b8e9a6f27f9
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2019-05-14 16:01:25 +0200

    Avoid enqueuing the same cluster twice (#3467)
    `handler.EnqueueRequestForOwner` enqueues the owning object with the
    triggering objects namespace, which doesn't work if the owning object
    is non-namespaced.
    
    We worked around this by removing the namespace from clusters in the
    `Reconcile`. The problem is that we still have two possible queue keys
    for the cluster, allowing two workers to concurrently work on the same
    cluster, causing a lot of errors.
    
    This PR changes the whole logic to not use the ownerreference but the
    namespace of the objects to find the corresponding cluster.

---
+++ api/pkg/controller/util/util.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"context"
+	"fmt"
+
+	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
+
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// EnqueueClusterForNamespacedObject enqueues the cluster that owns a namespaced object, if any
+// It is used by various controllers to react to changes in the resources in the cluster namespace
+func EnqueueClusterForNamespacedObject(client ctrlruntimeclient.Client) *handler.EnqueueRequestsFromMapFunc {
+	return &handler.EnqueueRequestsFromMapFunc{ToRequests: handler.ToRequestsFunc(func(a handler.MapObject) []reconcile.Request {
+		clusterList := &kubermaticv1.ClusterList{}
+		if err := client.List(context.Background(), &ctrlruntimeclient.ListOptions{}, clusterList); err != nil {
+			utilruntime.HandleError(fmt.Errorf("failed to list Clusters: %v", err))
+			return []reconcile.Request{}
+		}
+		for _, cluster := range clusterList.Items {
+			if cluster.Status.NamespaceName == a.Meta.GetNamespace() {
+				return []reconcile.Request{{NamespacedName: types.NamespacedName{Name: cluster.Name}}}
+			}
+		}
+		return []reconcile.Request{}
+	})}
+}
