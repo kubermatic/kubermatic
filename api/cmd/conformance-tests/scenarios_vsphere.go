89df698e78c2a28578f698c061e01262b768d510
Alvaro Aleman <alv2412@googlemail.com>
2019-05-27 16:55:06 +0200

    Fix datacenter name for vsphere

--- api/cmd/conformance-tests/scenarios_vsphere.go
+++ api/cmd/conformance-tests/scenarios_vsphere.go
@@ -69,7 +69,7 @@ func (s *vSphereScenario) Cluster(secrets secrets) *kubermaticv1.Cluster {
 				DNSDomain: "cluster.local",
 			},
 			Cloud: kubermaticv1.CloudSpec{
-				DatacenterName: "prow-build-cluster",
+				DatacenterName: "vsphere-ger",
 				VSphere: &kubermaticv1.VSphereCloudSpec{
 					Username: secrets.VSphere.Username,
 					Password: secrets.VSphere.Password,
