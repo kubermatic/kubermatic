0345bf4f4c241c101865f45e4045b3384741e0ef
zreigz <zreigz@gmail.com>
2019-05-28 18:10:05 +0200

    update swagger:meta

--- api/pkg/handler/routes_v1.go
+++ api/pkg/handler/routes_v1.go
@@ -1,9 +1,3 @@
-// Kubermatic API.
-// Kubermatic API. This describes possible operations which can be made against the Kubermatic API.
-//
-//     Schemes: http, https
-//     Host: localhost
-//
 //     Security:
 //     - api_key:
 //
