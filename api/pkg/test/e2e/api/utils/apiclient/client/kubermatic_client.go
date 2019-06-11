fa1976b25b8479122e775684ee4a6b72fa920422
Lukasz Zajaczkowski <zreigz@gmail.com>
2019-06-11 09:02:21 +0200

    update swagger client for custom credentials (#3600)

--- api/pkg/test/e2e/api/utils/apiclient/client/kubermatic_client.go
+++ api/pkg/test/e2e/api/utils/apiclient/client/kubermatic_client.go
@@ -11,11 +11,15 @@ import (
 
 	strfmt "github.com/go-openapi/strfmt"
 
+	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/aws"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/azure"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/datacenter"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/digitalocean"
+	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/gcp"
+	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/hetzner"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/openstack"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/operations"
+	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/packet"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/project"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/serviceaccounts"
 	"github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient/client/tokens"
@@ -67,15 +71,23 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *Kubermatic
 	cli := new(Kubermatic)
 	cli.Transport = transport
 
+	cli.Aws = aws.New(transport, formats)
+
 	cli.Azure = azure.New(transport, formats)
 
 	cli.Datacenter = datacenter.New(transport, formats)
 
 	cli.Digitalocean = digitalocean.New(transport, formats)
 
+	cli.Gcp = gcp.New(transport, formats)
+
+	cli.Hetzner = hetzner.New(transport, formats)
+
 	cli.Openstack = openstack.New(transport, formats)
 
 	cli.Operations = operations.New(transport, formats)
+
+	cli.Packet = packet.New(transport, formats)
 
 	cli.Project = project.New(transport, formats)
 
@@ -133,15 +145,23 @@ func (cfg *TransportConfig) WithSchemes(schemes []string) *TransportConfig {
 
 // Kubermatic is a client for kubermatic
 type Kubermatic struct {
+	Aws *aws.Client
+
 	Azure *azure.Client
 
 	Datacenter *datacenter.Client
 
 	Digitalocean *digitalocean.Client
 
+	Gcp *gcp.Client
+
+	Hetzner *hetzner.Client
+
 	Openstack *openstack.Client
 
 	Operations *operations.Client
+
+	Packet *packet.Client
 
 	Project *project.Client
 
@@ -162,15 +182,23 @@ type Kubermatic struct {
 func (c *Kubermatic) SetTransport(transport runtime.ClientTransport) {
 	c.Transport = transport
 
+	c.Aws.SetTransport(transport)
+
 	c.Azure.SetTransport(transport)
 
 	c.Datacenter.SetTransport(transport)
 
 	c.Digitalocean.SetTransport(transport)
 
+	c.Gcp.SetTransport(transport)
+
+	c.Hetzner.SetTransport(transport)
+
 	c.Openstack.SetTransport(transport)
 
 	c.Operations.SetTransport(transport)
+
+	c.Packet.SetTransport(transport)
 
 	c.Project.SetTransport(transport)
 
