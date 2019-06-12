d5a364dd12e3603df61f4b44cb2be88e5226febb
Kamil Domański <kamil@domanski.co>
2019-06-12 17:32:25 +0200

    GCP: add a firewall rule to allow ICMP per cluster (#3594)
    * GCP: add a firewall rule to allow ICMP per cluster
    
    No migration is required, since we haven't shipped GCP support yet.
    
    * do not store firewal rule names in the cluster object
    
    * proceed if a firewa rule already exits
    
    * update Gopkg

--- api/pkg/crd/kubermatic/v1/cluster.go
+++ api/pkg/crd/kubermatic/v1/cluster.go
@@ -354,10 +354,9 @@ type PacketCloudSpec struct {
 
 // GCPCloudSpec specifies access data to GCP.
 type GCPCloudSpec struct {
-	ServiceAccount   string `json:"serviceAccount"`
-	Network          string `json:"network"`
-	Subnetwork       string `json:"subnetwork"`
-	FirewallRuleName string `json:"firewallRuleName"`
+	ServiceAccount string `json:"serviceAccount"`
+	Network        string `json:"network"`
+	Subnetwork     string `json:"subnetwork"`
 }
 
 // ClusterHealthStatus stores health information of the components of a cluster.
