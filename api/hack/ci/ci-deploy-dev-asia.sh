ba38af5f8f172d73fe602dfa9d622095fcd4f76c
Nikhita Raghunath <nikitaraghunath@gmail.com>
2019-06-21 17:55:45 +0530

    Setup CI to deploy to dev-asia (#3688)

---
+++ api/hack/ci/ci-deploy-dev-asia.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+export DEPLOY_STACK=${DEPLOY_STACK:-kubermatic}
+export GIT_HEAD_HASH="$(git rev-parse HEAD|tr -d '\n')"
+cd $(dirname $0)/../../..
+
+source ./api/hack/lib.sh
+
+if [[ "${DEPLOY_STACK}" == "kubermatic" ]]; then
+  ./api/hack/ci/ci-push-images.sh
+fi
+
+echodate "Getting secrets from Vault"
+export VAULT_ADDR=https://vault.loodse.com/
+retry 5 vault write \
+  --format=json auth/approle/login \
+  role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID} > /tmp/vault-token-response.json
+export VAULT_TOKEN="$(cat /tmp/vault-token-response.json| jq .auth.client_token -r)"
+export KUBECONFIG=/tmp/kubeconfig
+export VALUES_FILE=/tmp/values.yaml
+export HELM_EXTRA_ARGS="--set=kubermatic.controller.image.tag=${GIT_HEAD_HASH} \
+    --set=kubermatic.api.image.tag=${GIT_HEAD_HASH} \
+    --set=kubermatic.masterController.image.tag=${GIT_HEAD_HASH}"
+
+# deploy to dev-asia
+vault kv get -field=kubeconfig dev/seed-clusters/dev.kubermatic.io > ${KUBECONFIG}
+vault kv get -field=values.yaml dev/seed-clusters/dev.kubermatic.io > ${VALUES_FILE}
+kubectl config use-context asia-south1-c
+echodate "Successfully got secrets for dev-asia from Vault"
+
+echodate "Deploying ${DEPLOY_STACK} stack to dev-asia"
+TILLER_NAMESPACE=kubermatic-installer ./api/hack/deploy.sh seed ${VALUES_FILE} ${HELM_EXTRA_ARGS}
+echodate "Successfully deployed ${DEPLOY_STACK} stack to dev-asia"
