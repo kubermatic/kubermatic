dec85a8921686c9f946cfe77e24e04f752f6f390
Christoph Mewes <christoph@loodse.com>
2019-07-01 11:52:12 +0200

    fix Kibana index setup code (#3706)

--- config/logging/kibana/config/ensure-index-pattern.sh
+++ config/logging/kibana/config/ensure-index-pattern.sh
@@ -11,8 +11,14 @@ kibana() {
 }
 
 ensure_index_pattern() {
-   if [[ "$(kibana --show-error --connect-timeout 1 "${KIBANA_URL}/api/status" | jq -r '.status.overall.state')" != "green" ]]; then
-      echo "Kibana is not yet available or unhealthy."
+   output=$(kibana --show-error --connect-timeout 1 "${KIBANA_URL}/api/status")
+   if [ $? -ne 0 ] || [ "${output:0:1}" != "{" ]; then
+      echo "Kibana is not yet available."
+      return 1
+   fi
+
+   if [[ "$(echo "$output" | jq -r '.status.overall.state')" != "green" ]]; then
+      echo "Kibana is still unhealthy."
       return 1
    fi
 
@@ -40,11 +46,11 @@ ensure_index_pattern() {
    fi
 
    echo "Setting pattern as default..."
-   config=$(curl --silent "${ELASTICSEARCH_URL}/.kibana/doc/config:${KIBANA_VERSION}" | jq '._source // {"type": "config","config":{}}')
+   config=$(curl --silent "${ELASTICSEARCH_HOSTS}/.kibana/doc/config:${KIBANA_VERSION}" | jq '._source // {"type": "config","config":{}}')
 
    echo "$config" | \
       jq ".config.defaultIndex = \"${id}\"" | \
-      curl --silent -XPUT -H 'Content-Type:application/json' "${ELASTICSEARCH_URL}/.kibana/doc/config:$KIBANA_VERSION" -d @- > /dev/null
+      curl --silent -XPUT -H 'Content-Type:application/json' "${ELASTICSEARCH_HOSTS}/.kibana/doc/config:$KIBANA_VERSION" -d @- > /dev/null
 
    echo "Kibana successfully configured."
 }
