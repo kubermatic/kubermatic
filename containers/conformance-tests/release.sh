0881654d308e38e53a2fd5b928b876d7bea420ed
mrIncompetent <henrik@henrik-schmidt.de>
2019-07-08 18:00:27 +0200

    use buildah for building images during e2e tests

--- containers/conformance-tests/release.sh
+++ containers/conformance-tests/release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-TAG=v0.9.2
+TAG=v0.9.4
 
 set -euox pipefail
 
-docker build --no-cache --pull -t kubermatic/kubernetes-test-binaries:${TAG} .
-docker push kubermatic/kubernetes-test-binaries:${TAG}
+docker build --pull -t kubermatic/kubernetes-test-binaries:${TAG} .
+#docker push kubermatic/kubernetes-test-binaries:${TAG}
