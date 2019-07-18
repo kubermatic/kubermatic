a02d1a9d4a632d9084ffa7de2ea3b4c056085244
Alvaro Aleman <alv2412@googlemail.com>
2019-07-18 14:33:37 +0200

    Fix json tag

--- api/pkg/crd/kubermatic/v1/datacenter.go
+++ api/pkg/crd/kubermatic/v1/datacenter.go
@@ -26,7 +26,7 @@ type SeedSpec struct {
 	// A reference to the Kubeconfig of this cluster
 	Kubeconfig corev1.ObjectReference `json:"kubeconfig"`
 	// The possible datacenters for the nodes
-	Datacenters map[string]Datacenter `json:"node_location,omitempty"`
+	Datacenters map[string]Datacenter `json:"datacenters,omitempty"`
 	// Optional: Overwrite the DNS domain for this seed
 	SeedDNSOverwrite *string `json:"seed_dns_overwrite,omitempty"`
 }
