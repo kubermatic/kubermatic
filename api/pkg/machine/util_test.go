4b08c4bf64aa219b2ba9bff1621a7eb8620976d7
zreigz <zreigz@gmail.com>
2019-07-18 13:05:25 +0200

    Show SSH Username

---
+++ api/pkg/machine/util_test.go
@@ -0,0 +1,66 @@
+package machine_test
+
+import (
+	"testing"
+
+	"github.com/kubermatic/kubermatic/api/pkg/machine"
+
+	apiv1 "github.com/kubermatic/kubermatic/api/pkg/api/v1"
+)
+
+func TestCredentialEndpoint(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		name           string
+		distribution   *apiv1.OperatingSystemSpec
+		cloudProvider  *apiv1.NodeCloudSpec
+		expectedResult string
+	}{
+		{
+			name: "test SSH login name for AWS:Ubuntu",
+			distribution: &apiv1.OperatingSystemSpec{
+				Ubuntu: &apiv1.UbuntuSpec{DistUpgradeOnBoot: false},
+			},
+
+			cloudProvider: &apiv1.NodeCloudSpec{
+				AWS: &apiv1.AWSNodeSpec{},
+			},
+			expectedResult: "",
+		},
+		{
+			name: "test SSH login name for VSphere:ContainerLinux",
+			distribution: &apiv1.OperatingSystemSpec{
+				ContainerLinux: &apiv1.ContainerLinuxSpec{},
+			},
+
+			cloudProvider: &apiv1.NodeCloudSpec{
+				VSphere: &apiv1.VSphereNodeSpec{},
+			},
+			expectedResult: "",
+		},
+		{
+			name: "test SSH login name for Openstack:CentOS",
+			distribution: &apiv1.OperatingSystemSpec{
+				CentOS: &apiv1.CentOSSpec{},
+			},
+
+			cloudProvider: &apiv1.NodeCloudSpec{
+				Openstack: &apiv1.OpenstackNodeSpec{},
+			},
+			expectedResult: "",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			resultLoginName, err := machine.GetSSHUserName(tc.distribution, tc.cloudProvider)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if tc.expectedResult != resultLoginName {
+				t.Fatalf("expected %s got %s", tc.expectedResult, resultLoginName)
+			}
+
+		})
+	}
+}
