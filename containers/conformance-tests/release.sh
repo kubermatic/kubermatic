cae9e98f8f344d75dbe7cfc3582d1a5f3aca4411
mrIncompetent <henrik@henrik-schmidt.de>
2019-07-26 13:40:32 +0200

    use docker instead of buildah

--- containers/conformance-tests/release.sh
+++ containers/conformance-tests/release.sh
@@ -4,5 +4,5 @@ TAG=v0.10.0
 
 set -euox pipefail
 
-buildah build-using-dockerfile --squash --tag docker.io/kubermatic/kubernetes-test-binaries:${TAG} .
-buildah push kubermatic/kubernetes-test-binaries:${TAG}
+docker build --no-cache --pull -t kubermatic/kubernetes-test-binaries:${TAG} .
+docker push kubermatic/kubernetes-test-binaries:${TAG}
