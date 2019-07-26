2eb4c55fd2e091b33741bccd518ecc05dfbc735c
Christoph Mewes <christoph@loodse.com>
2019-07-26 22:48:13 +0200

    cleanup

--- api/pkg/controller/operator-master/controller.go
+++ api/pkg/controller/operator-master/controller.go
@@ -7,15 +7,10 @@ import (
 
 	operatorv1alpha1 "github.com/kubermatic/kubermatic/api/pkg/crd/operator/v1alpha1"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
@@ -50,58 +45,10 @@ func Add(
 		return err
 	}
 
-	// reconcile for every KubermaticConfiguration labelled with the current worker name
-	predicate := newPredicateFuncs(func(meta metav1.Object) bool {
-		return meta.GetLabels()[WorkerNameLabel] == workerName
-	})
-
-	// put the config's identifier on the queue
-	eventHandler := newEventHandler(func(a handler.MapObject) []reconcile.Request {
-		return []reconcile.Request{
-			{
-				NamespacedName: types.NamespacedName{
-					Namespace: a.Meta.GetNamespace(),
-					Name:      a.Meta.GetName(),
-				},
-			},
-		}
-	})
-
 	obj := &operatorv1alpha1.KubermaticConfiguration{}
-	if err := c.Watch(&source.Kind{Type: obj}, eventHandler, predicate); err != nil {
+	if err := c.Watch(&source.Kind{Type: obj}, &handler.EnqueueRequestForObject{}); err != nil {
 		return fmt.Errorf("failed to create watcher for %T: %v", obj, err)
 	}
 
 	return nil
-}
-
-// newEventHandler takes a obj->request mapper function and wraps it into an
-// handler.EnqueueRequestsFromMapFunc.
-func newEventHandler(rf handler.ToRequestsFunc) *handler.EnqueueRequestsFromMapFunc {
-	return &handler.EnqueueRequestsFromMapFunc{
-		ToRequests: rf,
-	}
-}
-
-// newPredicateFuncs takes a predicate and returns a struct appliying the
-// function for Create, Update, Delete and Generic events. For Update events,
-// only the new version is considered.
-func newPredicateFuncs(pred func(metav1.Object) bool) predicate.Funcs {
-	return predicate.Funcs{
-		CreateFunc: func(e event.CreateEvent) bool {
-			return pred(e.Meta)
-		},
-
-		UpdateFunc: func(e event.UpdateEvent) bool {
-			return pred(e.MetaNew)
-		},
-
-		DeleteFunc: func(e event.DeleteEvent) bool {
-			return pred(e.Meta)
-		},
-
-		GenericFunc: func(e event.GenericEvent) bool {
-			return pred(e.Meta)
-		},
-	}
 }
