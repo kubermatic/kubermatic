8d5cfeed4295a3db0dd27a7edfae8e6a16af08cd
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-07-30 13:10:30 +0200

    Move the sleep

--- api/pkg/docker/docker.go
+++ api/pkg/docker/docker.go
@@ -63,12 +63,12 @@ func DownloadImage(ctx context.Context, log *zap.Logger, dryRun bool, image stri
 		case <-timer.C:
 			return fmt.Errorf("failed to pull image %s during %v", image, timeout)
 		default:
-			time.Sleep(interval)
 			if err := execCommand(log, dryRun, cmd); err == nil {
 				return nil
 			} else {
 				log.Debug(fmt.Sprintf("%v", err))
 			}
+			time.Sleep(interval)
 		}
 	}
 }
