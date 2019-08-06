8426b389018738aad5e5c052f42f7f8b3c0e4aa7
Alvaro Aleman <alv2412@googlemail.com>
2019-08-06 09:15:08 +0200

    Fix datacenter name for dev asia

--- api/hack/ci/ci-deploy-dev-asia.sh
+++ api/hack/ci/ci-deploy-dev-asia.sh
@@ -23,7 +23,7 @@ export HELM_EXTRA_ARGS="--set=kubermatic.controller.image.tag=${GIT_HEAD_HASH} \
     --set=kubermatic.api.image.tag=${GIT_HEAD_HASH} \
     --set=kubermatic.masterController.image.tag=${GIT_HEAD_HASH} \
     --set=kubermatic.controller.addons.kubernetes.image.tag=${GIT_HEAD_HASH} \
-    --set=kubermatic.contoller.datacenterName=asia-south1-c"
+    --set=kubermatic.controller.datacenterName=asia-south1-c"
 
 # deploy to dev-asia
 vault kv get -field=kubeconfig dev/seed-clusters/dev.kubermatic.io > ${KUBECONFIG}
