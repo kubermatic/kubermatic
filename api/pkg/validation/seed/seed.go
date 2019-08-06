82f9dfa85b871ea485759edd36e17ab49117d562
Alvaro Aleman <alv2412@googlemail.com>
2019-08-06 16:43:08 +0200

    Add basic business logic for seed webhook

---
+++ api/pkg/validation/seed/seed.go
@@ -0,0 +1,107 @@
+package seed
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
+	"github.com/kubermatic/kubermatic/api/pkg/provider"
+	"k8s.io/apimachinery/pkg/util/sets"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func New(
+	ctx context.Context,
+	seedsGetter provider.SeedsGetter,
+	seedKubeconfigGetter provider.SeedKubeconfigGetter,
+	listOpts *ctrlruntimeclient.ListOptions) *SeedValidator {
+
+	if listOpts == nil {
+		listOpts = &ctrlruntimeclient.ListOptions{}
+	}
+	return &SeedValidator{
+		ctx:                  ctx,
+		seedsGetter:          seedsGetter,
+		seedKubeconfigGetter: seedKubeconfigGetter,
+		lock:                 &sync.Mutex{},
+		listOpts:             listOpts,
+	}
+}
+
+type SeedValidator struct {
+	ctx                  context.Context
+	seedsGetter          provider.SeedsGetter
+	seedKubeconfigGetter provider.SeedKubeconfigGetter
+	lock                 *sync.Mutex
+	// Can be used to insert a labelSelector
+	listOpts *ctrlruntimeclient.ListOptions
+}
+
+func (sv *SeedValidator) Validate(seed *kubermaticv1.Seed, isDelete bool) error {
+	// We need locking to make the validation concurrency-safe
+	sv.lock.Lock()
+	defer sv.lock.Unlock()
+
+	client, err := sv.clientForSeed(seed.Name)
+	if err != nil {
+		return fmt.Errorf("failed to get client for seed %q: %v", seed.Name, err)
+	}
+
+	seeds, err := sv.seedsGetter()
+	if err != nil {
+		return fmt.Errorf("failed to get seeds: %v", err)
+	}
+
+	return sv.validate(seed, client, seeds, isDelete)
+}
+
+func (sv *SeedValidator) validate(seed *kubermaticv1.Seed, seedClient ctrlruntimeclient.Client, existingSeeds map[string]*kubermaticv1.Seed, isDelete bool) error {
+
+	newDatacenters := sets.NewString()
+	for datacenter := range seed.Spec.Datacenters {
+		newDatacenters.Insert(datacenter)
+	}
+
+	for _, existingSeed := range existingSeeds {
+		for existingDatacenter := range existingSeed.Spec.Datacenters {
+			if newDatacenters.Has(existingDatacenter) {
+				return fmt.Errorf("datacenter %q already exists in seed %q, can only have one datacenter with a given name", existingDatacenter, existingSeed.Name)
+			}
+		}
+	}
+
+	clusters := &kubermaticv1.ClusterList{}
+	if err := seedClient.List(sv.ctx, sv.listOpts, clusters); err != nil {
+		return fmt.Errorf("failed to list clusters: %v", err)
+	}
+
+	for _, cluster := range clusters.Items {
+		if !newDatacenters.Has(cluster.Spec.Cloud.DatacenterName) {
+			return fmt.Errorf("datacenter %q is still in use, can not delete it",
+				cluster.Spec.Cloud.DatacenterName)
+		}
+	}
+
+	if isDelete && len(clusters.Items) > 0 {
+		return fmt.Errorf("can not delete seed, there are still %d clusters in it", len(clusters.Items))
+	}
+
+	return nil
+}
+
+func (sv *SeedValidator) clientForSeed(seedName string) (ctrlruntimeclient.Client, error) {
+	cfg, err := sv.seedKubeconfigGetter(seedName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get kubeconfig for seed %q: %v", seedName, err)
+	}
+
+	// TODO: Restmapper cache? User will use master to create Seeds, our EU master -> asia seed
+	// takes 10-20s for the discovery
+	client, err := ctrlruntimeclient.New(cfg, ctrlruntimeclient.Options{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to construct client for seed %q: %v", seedName, err)
+	}
+
+	return client, nil
+}
