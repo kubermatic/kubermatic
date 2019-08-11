1876048ef20eb933f20e0d524913a4a8262a398e
Alvaro Aleman <alv2412@googlemail.com>
2019-08-11 14:56:05 +0200

    Fix bash junit writer

--- api/hack/lib.sh
+++ api/hack/lib.sh
@@ -1,4 +1,15 @@
 retry() {
+  # Works only with bash but doesn't fail on other shells
+  start_time=${SECONDS:-0}
+  actual_retry $@
+  elapsed_time=$((${SECONDS:-0} - $start_time))
+  rc=$?
+  write_junit "$rc" "$elapsed_time"
+  return $rc
+}
+
+# We use an extra wrapping to write junit and have a timer
+actual_retry() {
   retries=$1 ; shift
 
   count=0
@@ -11,12 +22,10 @@ retry() {
       sleep $delay
     else
       echo "Retry $count/$retries exited $rc, no more retries left."
-      write_junit $rc
       return $rc
     fi
     delay=$(( delay * 2 ))
   done
-  write_junit 0
   return 0
 }
 
@@ -25,16 +34,20 @@ echodate() {
 }
 
 write_junit() {
-  rc=$1
   # Doesn't make any sense if we don't know a testname
   [ -z "$TEST_NAME" ] && return
   # Only run in CI
   [ -z "${ARTIFACTS}" ] && return
-  cat <<EOF > ${ARTIFACTS}/$(echo $TEST_NAME|sed 's/ /_/g').xml
+
+  rc=$1
+  duration=${2:-0}
+  errors=0; [ "$rc" -ne 0 ] && errors=1
+  TEST_NAME="[KUBERMATIC] $TEST_NAME"
+  cat <<EOF > ${ARTIFACTS}/junit.$(echo $TEST_NAME|sed 's/ /_/g').xml
 <?xml version="1.0" ?>
 <testsuites>
-    <testsuite errors="$rc" failures="$rc" name="$TEST_NAME" tests="1">
-        <testcase classname="$TEST_NAME" name="$TEST_NAME" time="0">
+    <testsuite errors="$errors" failures="$errors" name="$TEST_NAME" tests="1">
+        <testcase classname="$TEST_NAME" name="$TEST_NAME" time="$duration">
         </testcase>
     </testsuite>
 </testsuites>
