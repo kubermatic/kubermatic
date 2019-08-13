abfa2cc556d2b4c33fe344bd562120608f768505
zreigz <zreigz@gmail.com>
2019-08-13 09:38:18 +0200

    add script to verify generated api client

---
+++ api/hack/verify-api-client.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+function cleanup() {
+    if [[ -n "${TMP_DIFFROOT:-}" ]]; then
+        rm -rf "${TMP_DIFFROOT}"
+    fi
+}
+trap cleanup EXIT SIGINT SIGTERM
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")/..
+API_DIR="$(go env GOPATH)/src/github.com/kubermatic/kubermatic/api"
+DIFFROOT="${API_DIR}/pkg/test/e2e/api/utils/apiclient"
+
+TMP_DIFFROOT=$(mktemp -d)
+
+cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}"
+
+"${SCRIPT_ROOT}/hack/gen-api-client.sh"
+
+echo "diffing ${DIFFROOT} against freshly generated api client"
+ret=0
+diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
+cp -a "${TMP_DIFFROOT}"/* "${DIFFROOT}"
+
+if [[ $ret -eq 0 ]]
+then
+    echo "${DIFFROOT} up to date."
+else
+    echo "${DIFFROOT} is out of date. Please run hack/gen-api-client.sh"
+    exit 1
+fi
