56f1b0f52765b2f11cfdd4826ab235285eb4e5c1
Alvaro Aleman <alv2412@googlemail.com>
2019-08-20 11:30:55 +0200

    Fix openshift crd manifest

---
+++ openshift_addons/get_crds.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+# This script can be used to get all crd manifests from a running
+# openshift cluster
+
+
+targetFile="$(mktemp)"
+
+for crd in $(kubectl get crd -o name); do
+	echo "Getting crd $crd"
+	echo -e '\n---\n' >> $targetFile
+	# We cant use --export because the status has mandatory fields that are not preserved
+	kubectl get $crd -o json|jq '{metadata: {name: .metadata.name}, apiVersion: .apiVersion, kind: .kind, spec: .spec}' >> $targetFile
+done
+
+mv $targetFile $(dirname $0)/crd/crds.yaml
