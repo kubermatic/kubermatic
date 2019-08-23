de5367abb9eaffb7b93ecbbd5c103c87523a9f91
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2019-08-23 10:21:26 +0200

    Openshift: Deploy kube-controller-manager (#4128)
    * Create image pull secret for openshift
    
    * Update apiserver
    
    * Add openshift apiserver
    
    * Make little world
    
    * Specificy correct imagePullSecret type
    
    * Deploy config for openshift apiserver
    
    * Fix path for aggregator ca
    
    * Add serving cert for openshift apiserver
    
    * Openshift kube-apiserver starts
    
    * Add kubeconfig to openshift-apiserver
    
    * Add code to add openshift apiservices
    
    * Plug the version argument through the usercluster controller
    
    * Create openshift-apiserver ns
    
    * Create openshift-apiserver service
    
    * Create reconciliation codegen for endpoints
    
    * Create endpoints for openshift apiserver service
    
    * Add openshift crds
    
    * WIP
    
    * Remove endpoint reconciling
    
    * Add missing allowed name
    
    * Try to run everything in one pod
    
    * Revert "Try to run everything in one pod"
    
    This reverts commit 5f773a2abb1ce040d496a8522acf07d452a184c2.
    
    * Use kubectl for openshift addons
    
    * Remove openshift networking addon
    
    * Revert "Create reconciliation codegen for endpoints"
    
    This reverts commit 37d59942d693aa137bc119bd748346ccd0fcdde4.
    
    * Properly return nerror when spec.Openshift is nil
    
    * Add comment
    
    * Add test for openshift controlplane config
    
    * Add comment
    
    * Dont deploy defunct metrics apiservice on openshift
    
    * Update fixtures
    
    * Fix linting
    
    * Kube controller manager builds
    
    * Fixup merge failures
    
    * Update kube-controller-manager deployment
    
    * Get rid of single-use kubeControllerConfigVariables struct
    
    * Add tests for kube-controller-manager configmap
    
    * Move constant paths to const

--- api/pkg/controller/openshift/resources/controlplane_configmap.go
+++ api/pkg/controller/openshift/resources/controlplane_configmap.go
@@ -295,7 +295,7 @@ apiServerArguments:
   enable-aggregator-routing:
   # Thist _must_ stay false, if its true, the kube-apiserver will try to resolve endpoints for
   # the services that service the extension apis and error out because they are of type
- 	# ExternalName
+  # ExternalName
   - 'false'
   feature-gates:
   - ExperimentalCriticalPodAnnotation=true
