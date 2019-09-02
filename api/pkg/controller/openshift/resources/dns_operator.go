14be9116d9176baab122302a91c11130954f2f64
Alvaro Aleman <alv2412@googlemail.com>
2019-09-02 14:23:53 +0200

    Openshift: Deploy dns operator

---
+++ api/pkg/controller/openshift/resources/dns_operator.go
@@ -0,0 +1,124 @@
+package resources
+
+import (
+	"fmt"
+
+	"github.com/kubermatic/kubermatic/api/pkg/resources"
+	"github.com/kubermatic/kubermatic/api/pkg/resources/apiserver"
+	"github.com/kubermatic/kubermatic/api/pkg/resources/reconciling"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	utilpointer "k8s.io/utils/pointer"
+)
+
+const openshiftDNSOperatorDeploymentName = "openshift-dns-operator"
+
+var (
+	openshiftDNSOperatorResourceRequirements = corev1.ResourceRequirements{
+		Requests: corev1.ResourceList{
+			corev1.ResourceMemory: resource.MustParse("50Mi"),
+			corev1.ResourceCPU:    resource.MustParse("10m"),
+		},
+		Limits: corev1.ResourceList{
+			corev1.ResourceMemory: resource.MustParse("200Mi"),
+			corev1.ResourceCPU:    resource.MustParse("100m"),
+		},
+	}
+)
+
+func OpenshiftDNSOperatorFactory(data openshiftData) reconciling.NamedDeploymentCreatorGetter {
+	return func() (string, reconciling.DeploymentCreator) {
+		return openshiftDNSOperatorDeploymentName, func(d *appsv1.Deployment) (*appsv1.Deployment, error) {
+
+			image, err := openshiftDNSOperatorImage(data.Cluster().Spec.Version.String())
+			if err != nil {
+				return nil, err
+			}
+
+			env, err := openshiftDNSOperatorEnv(data.Cluster().Spec.Version.String())
+			if err != nil {
+				return nil, err
+			}
+			env = append(env, corev1.EnvVar{
+				Name:  "KUBECONFIG",
+				Value: "/etc/kubernetes/kubeconfig/kubeconfig",
+			})
+
+			d.Spec.Selector = &metav1.LabelSelector{
+				MatchLabels: resources.BaseAppLabel(openshiftDNSOperatorDeploymentName, nil),
+			}
+			d.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{
+				{Name: openshiftImagePullSecretName},
+			}
+			d.Spec.Template.Spec.AutomountServiceAccountToken = utilpointer.BoolPtr(false)
+			// Configure user cluster DNS resolver for this pod.
+			d.Spec.Template.Spec.DNSPolicy, d.Spec.Template.Spec.DNSConfig, err = resources.UserClusterDNSPolicyAndConfig(data)
+			if err != nil {
+				return nil, err
+			}
+
+			d.Spec.Template.Spec.Containers = []corev1.Container{{
+				Name:    "dns-operator",
+				Image:   image,
+				Env:     env,
+				Command: []string{"dns-operator"},
+				VolumeMounts: []corev1.VolumeMount{{
+					Name:      resources.InternalUserClusterAdminKubeconfigSecretName,
+					MountPath: "/etc/kubernetes/kubeconfig",
+				}},
+				Resources: *openshiftDNSOperatorResourceRequirements.DeepCopy(),
+			}}
+			d.Spec.Template.Spec.Volumes = []corev1.Volume{
+				{
+					// TODO: Properly limit this instead of just using cluster-admin
+					Name: resources.InternalUserClusterAdminKubeconfigSecretName,
+					VolumeSource: corev1.VolumeSource{
+						Secret: &corev1.SecretVolumeSource{
+							SecretName: resources.InternalUserClusterAdminKubeconfigSecretName,
+						},
+					},
+				},
+			}
+
+			d.Spec.Template.Labels, err = data.GetPodTemplateLabels(openshiftDNSOperatorDeploymentName, d.Spec.Template.Spec.Volumes, nil)
+			if err != nil {
+				return nil, err
+			}
+
+			d.Spec.Template.Spec.InitContainers = nil
+			wrappedPodSpec, err := apiserver.IsRunningWrapper(data, d.Spec.Template.Spec, sets.NewString("dns-operator"))
+			if err != nil {
+				return nil, fmt.Errorf("failed to add apiserver.IsRunningWrapper: %v", err)
+			}
+			d.Spec.Template.Spec = *wrappedPodSpec
+
+			return d, nil
+		}
+	}
+}
+
+func openshiftDNSOperatorEnv(openshiftVersion string) ([]corev1.EnvVar, error) {
+	switch openshiftVersion {
+	case "4.1.9":
+		return []corev1.EnvVar{
+			{Name: "RELEASE_VERSION", Value: "4.1.9"},
+			{Name: "IMAGE", Value: "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:390cc1784aba986fad6315142d1d2524b2707a91eea3705d448367b51a112438"},
+			{Name: "OPENSHIFT_CLI_IMAGE", Value: "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:52ef9f5ade93e32f85e13bb9f588b2e126717256789023f8eb455b1147761562"},
+		}, nil
+	default:
+		return nil, fmt.Errorf("can not find env vars for openshift version %q", openshiftVersion)
+	}
+}
+
+func openshiftDNSOperatorImage(openshiftVersion string) (string, error) {
+	switch openshiftVersion {
+	case "4.1.9":
+		return "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:2aca09bcf2d705c8fe457e21507319550d4363fd07012db6403f59c314ecc7e0", nil
+	default:
+		return "", fmt.Errorf("no image available for openshift version %q", openshiftVersion)
+	}
+}
