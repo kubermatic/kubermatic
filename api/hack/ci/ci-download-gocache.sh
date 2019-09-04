8e955901014941d995e61ea0dc137381d7a8cf16
Alvaro Aleman <alv2412@googlemail.com>
2019-09-04 11:28:33 +0200

    Add script to download gocache

---
+++ api/hack/ci/ci-download-gocache.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Required for signal propagation to work so
+# the cleanup trap gets executed when the script
+# receives a SIGINT
+set -o monitor
+
+# Make sure we never error, this is always best-effort only
+exit_gracefully { exit 0 }
+trap exit_gracefully EXIT
+
+source ./api/hack/lib.sh
+
+if [ -z $GOCACHE_MINIO_ADDRESS ]; then
+  echodate "env var GOCACHE_MINIO_ADDRESS unset, can not download gocache"
+  exit 0
+fi
+
+GOCACHE=$(go env GOCACHE)
+# Make sure it actually exists
+mkdir -p $GOCACHE
+
+if ls -1qA ./somedir/ | grep -q .; then
+  echodate "gocache at $GOCACHE is not empty, omitting download of gocache"
+  exit 0
+fi
+
+CACHE_VERSION="${PULL_BASE_SHA}"
+TEST_NAME="Download and extract gocache"
+retry 5 curl --fail
+    --progress-bar\
+    -H "Content-Type: binary/octet-stream" \
+    ${GOCACHE_MINIO_ADDRESS}/${CACHE_VERSION}.tar \
+    |tar -C $GOCACHE -xvf -
