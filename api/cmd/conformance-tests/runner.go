db9852019e38c5595afe654dabf91dc43a0b8dc6
Alvaro Aleman <alv2412@googlemail.com>
2019-09-10 11:04:43 +0200

    E2e tests: Increase Azure delete timeout to 30 minutes

--- api/cmd/conformance-tests/runner.go
+++ api/cmd/conformance-tests/runner.go
@@ -380,6 +380,11 @@ func (r *testRunner) executeScenario(log *zap.SugaredLogger, scenario testScenar
 		ProjectID: r.kubermatcProjectID,
 		Dc:        r.seed.Name,
 	}
+	deleteTimeout := 15 * time.Minute
+	if cluster.Spec.Cloud.Azure != nil {
+		// 15 Minutes are not enough for Azure
+		deleteTimeout = 30 * time.Minute
+	}
 	deleteParms.SetTimeout(15 * time.Second)
 	if err := junitReporterWrapper(
 		"[Kubermatic] Delete cluster",
@@ -389,7 +394,7 @@ func (r *testRunner) executeScenario(log *zap.SugaredLogger, scenario testScenar
 			if err != nil {
 				return fmt.Errorf("failed to parse selector: %v", err)
 			}
-			return wait.PollImmediate(5*time.Second, 15*time.Minute, func() (bool, error) {
+			return wait.PollImmediate(5*time.Second, deleteTimeout, func() (bool, error) {
 				clusterList := &kubermaticv1.ClusterList{}
 				listOpts := &ctrlruntimeclient.ListOptions{LabelSelector: selector}
 				if err := r.seedClusterClient.List(r.ctx, listOpts, clusterList); err != nil {
