b87a8b7216580eadde92588d4b551cedd31c4559
Alvaro Aleman <alv2412@googlemail.com>
2019-09-12 15:56:56 +0200

    Fix variable name

--- api/hack/ci/ci-run-kind-conformance-tests.sh
+++ api/hack/ci/ci-run-kind-conformance-tests.sh
@@ -153,7 +153,7 @@ kubectl apply -f ${KUBERMATIC_CRD_PATH}
 
 TEST_NAME="Get kind cluster Kubeconfig"
 echodate "Get kind cluster Kubeconfig"
-KUBECONFIG_PRASED=$(mktemp)
+KUBECONFIG_PARSED=$(mktemp)
 cp ${KUBECONFIG_PATH} ${KUBECONFIG_PARSED}
 KUBECONFIG_PATH=${KUBECONFIG_PARSED}
 yq w -i ${KUBECONFIG_PATH} clusters[0].name ${SEED_NAME}
