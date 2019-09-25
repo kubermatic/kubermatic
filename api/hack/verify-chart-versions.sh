7e86dc6ea867e3a9d9e10ffc98770a37cd952c27
Christoph Mewes <christoph@loodse.com>
2019-09-25 15:09:22 +0200

    add script to verify that chart versions are properly updated

---
+++ api/hack/verify-chart-versions.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This script is used as a presubmit to check that Helm chart versions
+# have been updated if charts have been modified. Without the Prow env
+# vars, this script won't run properly.
+
+set -euo pipefail
+
+cd $(dirname $0)/../..
+
+branch=alertmanager-0.19
+charts=$(find config/ -name Chart.yaml | cut -d/ -f2- | xargs -l1 dirname | sort)
+exitCode=0
+
+while read -r chartdir; do
+  chartYAML="config/$chartdir/Chart.yaml"
+
+  # if this chart was touched in this PR
+  if ! git diff --exit-code --no-patch "master...$PULL_PULL_SHA" "config/$chartdir/"; then
+    oldVersion=""
+
+    # as we scan for charts by looking through the current files, we can always
+    # determine the new version (unless the Chart.yaml is missing entirely)
+    newVersion="$(yq read "$chartYAML" version)"
+
+    # if the chart already existed, retrieve its old version
+    if git show "$PULL_BASE_SHA:$chartYAML" > /dev/null 2>&1; then
+      oldVersion=$(git show "$PULL_BASE_SHA:$chartYAML" | yq read - version)
+    fi
+
+    if [ "$oldVersion" = "$newVersion" ]; then
+      echo "Chart $chartdir was modified but its version ($oldVersion) was not changed. Please adjust $chartYAML."
+      exitCode=1
+    fi
+  fi
+done <<< "$charts"
+
+exit $exitCode
