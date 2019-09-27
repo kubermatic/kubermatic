67e61dc3889c47d9caba1eb2be3a3e53c75703a3
Alvaro Aleman <alv2412@googlemail.com>
2019-09-27 11:09:04 +0200

    Add the cloud credential operator

---
+++ api/pkg/controller/openshift/resources/cloud_credential_operator.go
@@ -0,0 +1,91 @@
+package resources
+
+import (
+	"fmt"
+
+	"github.com/kubermatic/kubermatic/api/pkg/resources"
+	"github.com/kubermatic/kubermatic/api/pkg/resources/apiserver"
+	"github.com/kubermatic/kubermatic/api/pkg/resources/reconciling"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	utilpointer "k8s.io/utils/pointer"
+)
+
+const (
+	cloudCredentialOperatorDeploymentName = "cloud-credential-operator"
+)
+
+func CloudCredentialOperator(data openshiftData) reconciling.NamedDeploymentCreatorGetter {
+	return func() (string, reconciling.DeploymentCreator) {
+		return cloudCredentialOperatorDeploymentName, func(d *appsv1.Deployment) (*appsv1.Deployment, error) {
+
+			image, err := cloudCredentialOperatorImage(data.Cluster().Spec.Version.String())
+			if err != nil {
+				return nil, err
+			}
+
+			d.Spec.Selector = &metav1.LabelSelector{
+				MatchLabels: resources.BaseAppLabel(cloudCredentialOperatorDeploymentName, nil),
+			}
+			d.Spec.Template.Spec.ImagePullSecrets = []corev1.LocalObjectReference{
+				{Name: openshiftImagePullSecretName},
+			}
+			d.Spec.Template.Spec.AutomountServiceAccountToken = utilpointer.BoolPtr(false)
+			d.Spec.Template.Spec.Containers = []corev1.Container{{
+				Name:    cloudCredentialOperatorDeploymentName,
+				Command: []string{"/root/manager", "--log-level", "debug"},
+				Image:   image,
+				Env: []corev1.EnvVar{
+					{
+						Name:  "RELEASE_VERSION",
+						Value: data.Cluster().Spec.Version.String(),
+					},
+					{
+						Name:  "KUBECONFIG",
+						Value: "/etc/kubernetes/kubeconfig/kubeconfig",
+					},
+				},
+				VolumeMounts: []corev1.VolumeMount{{
+					Name:      resources.InternalUserClusterAdminKubeconfigSecretName,
+					MountPath: "/etc/kubernetes/kubeconfig",
+				}},
+			}}
+			d.Spec.Template.Spec.Volumes = []corev1.Volume{
+				{
+					// TODO: Properly limit this instead of just using cluster-admin
+					Name: resources.InternalUserClusterAdminKubeconfigSecretName,
+					VolumeSource: corev1.VolumeSource{
+						Secret: &corev1.SecretVolumeSource{
+							SecretName: resources.InternalUserClusterAdminKubeconfigSecretName,
+						},
+					},
+				},
+			}
+
+			d.Spec.Template.Labels, err = data.GetPodTemplateLabels(cloudCredentialOperatorDeploymentName, d.Spec.Template.Spec.Volumes, nil)
+			if err != nil {
+				return nil, err
+			}
+			wrappedPodSpec, err := apiserver.IsRunningWrapper(data, d.Spec.Template.Spec, sets.NewString(cloudCredentialOperatorDeploymentName), "CredentialsRequest,cloudcredential.openshift.io/v1")
+			if err != nil {
+				return nil, fmt.Errorf("failed to add apiserver.IsRunningWrapper: %v", err)
+			}
+			d.Spec.Template.Spec = *wrappedPodSpec
+
+			return d, nil
+		}
+
+	}
+}
+
+func cloudCredentialOperatorImage(openshiftVersion string) (string, error) {
+	switch openshiftVersion {
+	case openshiftVersion419:
+		return "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:1071e05a8fd4e13630be9097f4e412356af481af4568bf241f208e337665864f", nil
+	default:
+		return "", fmt.Errorf("no cloud credential operator image available for openshift version %q", openshiftVersion)
+	}
+}
