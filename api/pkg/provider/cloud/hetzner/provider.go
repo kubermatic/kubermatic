148d00e01f004b082118e10c32f58381ab5a05f7
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-09-30 14:23:50 +0200

    Fix the endpoint to work with presets too

--- api/pkg/provider/cloud/hetzner/provider.go
+++ api/pkg/provider/cloud/hetzner/provider.go
@@ -2,6 +2,8 @@ package hetzner
 
 import (
 	"context"
+	"errors"
+	"github.com/kubermatic/kubermatic/api/pkg/resources"
 
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
@@ -41,4 +43,21 @@ func (h *hetzner) CleanUpCloudProvider(cluster *kubermaticv1.Cluster, _ provider
 // ValidateCloudSpecUpdate verifies whether an update of cloud spec is valid and permitted
 func (h *hetzner) ValidateCloudSpecUpdate(oldSpec kubermaticv1.CloudSpec, newSpec kubermaticv1.CloudSpec) error {
 	return nil
+}
+
+// GetCredentialsForCluster returns the credentials for the passed in cloud spec or an error
+func GetCredentialsForCluster(cloud kubermaticv1.CloudSpec, secretKeySelector provider.SecretKeySelectorValueFunc) (hetznerToken string, err error) {
+	hetznerToken = cloud.Hetzner.Token
+
+	if hetznerToken == "" {
+		if cloud.Hetzner.CredentialsReference == nil {
+			return "", errors.New("no credentials provided")
+		}
+		hetznerToken, err = secretKeySelector(cloud.Hetzner.CredentialsReference, resources.HetznerToken)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return hetznerToken, nil
 }
