1a4dd1c82ba01ed5f5499e2f6528bcea655c9733
Alvaro Aleman <alv2412@googlemail.com>
2019-10-01 15:33:37 +0200

    Fix platform name in openshift infrastructure CRD

--- api/pkg/controller/usercluster/resources/openshift/unstructured.go
+++ api/pkg/controller/usercluster/resources/openshift/unstructured.go
@@ -24,10 +24,28 @@ func InfrastructureCreatorGetter(platform string) reconciling.NamedUnstructuredC
 			if _, ok := originalContent["spec"]; !ok {
 				originalContent["spec"] = struct{}{}
 			}
-			originalContent["status"] = infrastructureStatus{Platform: platform}
+			originalContent["status"] = infrastructureStatus{Platform: translateKubernetesCloudProviderName(platform)}
 			u.SetUnstructuredContent(originalContent)
 
 			return u, nil
 		}
+	}
+}
+
+// From https://github.com/openshift/cloud-credential-operator/blob/ec6f38d73a7921e79d0ca7555da3a864e808e681/vendor/github.com/openshift/api/config/v1/types_infrastructure.go#L64-L87
+func translateKubernetesCloudProviderName(kubernetesCloudProviderName string) string {
+	switch kubernetesCloudProviderName {
+	case "aws":
+		return "AWS"
+	case "azure":
+		return "Azure"
+	case "gcp":
+		return "GCP"
+	case "openstack":
+		return "OpenStack"
+	case "vsphere":
+		return "VSphere"
+	default:
+		return "None"
 	}
 }
