7cd6ca7a3dd00a2ee4f2817eb35062b2a5cbfd08
Sebastian Florek <sebastian.florek@loodse.com>
2019-10-04 10:18:20 +0200

    Fix tests

--- api/pkg/resources/kubernetes-dashboard/deployment.go
+++ api/pkg/resources/kubernetes-dashboard/deployment.go
@@ -71,7 +71,7 @@ func DeploymentCreator(data kubernetesDashboardData) reconciling.NamedDeployment
 			dep.Spec.Template.Spec.Containers = getContainers(data)
 			dep.Spec.Template.Spec.Affinity = resources.HostnameAntiAffinity(name, data.Cluster().Name)
 
-			wrappedPodSpec, err := apiserver.IsRunningWrapper(data, dep.Spec.Template.Spec, sets.NewString(name), "", "")
+			wrappedPodSpec, err := apiserver.IsRunningWrapper(data, dep.Spec.Template.Spec, sets.NewString(name))
 			if err != nil {
 				return nil, fmt.Errorf("failed to add apiserver.IsRunningWrapper: %v", err)
 			}
