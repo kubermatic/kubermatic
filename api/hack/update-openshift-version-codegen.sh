040bac52bc30a9b18ef95a95ffb1b57b3c6f46da
Alvaro Aleman <alv2412@googlemail.com>
2019-10-10 17:41:34 +0200

    Add comment

--- api/hack/update-openshift-version-codegen.sh
+++ api/hack/update-openshift-version-codegen.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This file can be used to update the generated image names for Openshift.
+# The desired versions msut be configured first in
+# codegen/openshift_versions/main.go and a const for each version must be
+# added to pkg/controller/openshift/resources/const.go
+#
+# Also, executing this script requires access to the ocp quay repo.
+
 set -o errexit
 set -o nounset
 set -o pipefail
