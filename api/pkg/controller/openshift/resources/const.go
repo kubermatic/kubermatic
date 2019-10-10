3b7bc6900f9759a5d29d35a8d2f03f3819c5406e
Alvaro Aleman <alv2412@googlemail.com>
2019-10-10 15:47:24 +0200

    Add generated openshift versions

--- api/pkg/controller/openshift/resources/const.go
+++ api/pkg/controller/openshift/resources/const.go
@@ -1,7 +1,8 @@
 package resources
 
 const (
-	openshiftVersion419 = "4.1.9"
+	openshiftVersion419  = "4.1.9"
+	openshiftVersion4118 = "4.1.18"
 )
 
 //go:generate go run ../../../../codegen/openshift_versions/main.go
