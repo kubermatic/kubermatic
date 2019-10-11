cd08dbf2dd4684d3585436b30f780ed264f00fd7
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-10-11 17:20:40 +0200

    Expose audit logging in cluster API object (#4478)

--- api/pkg/handler/v1/cluster/cluster.go
+++ api/pkg/handler/v1/cluster/cluster.go
@@ -647,6 +647,7 @@ func convertInternalClusterToExternal(internalCluster *kubermaticv1.Cluster) *ap
 			Version:                             internalCluster.Spec.Version,
 			MachineNetworks:                     internalCluster.Spec.MachineNetworks,
 			OIDC:                                internalCluster.Spec.OIDC,
+			AuditLogging:                        internalCluster.Spec.AuditLogging,
 			UsePodSecurityPolicyAdmissionPlugin: internalCluster.Spec.UsePodSecurityPolicyAdmissionPlugin,
 		},
 		Status: apiv1.ClusterStatus{
