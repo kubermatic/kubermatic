f4b8192f02182944f2d66047948cc24bd20eb1e9
Christoph Mewes <christoph@loodse.com>
2019-10-11 19:40:19 +0200

    Grafana Plugins & Cluster Dashboard (#4480)
    * add grafana-plugins Docker image, add usercluster dashboard
    
    * move nginx dashboard into its own folder
    
    * re-use the existing dashboard formatting for the verification

--- api/hack/lib.sh
+++ api/hack/lib.sh
@@ -93,3 +93,35 @@ get_latest_dashboard_hash() {
   echodate "The latest dashboard hash for $FOR_BRANCH is $HASH" >/dev/stderr
   echo "$HASH"
 }
+
+format_dashboard() {
+  local filename="$1"
+  local tmpfile="$filename.tmp"
+
+  cat "$filename" | \
+    jq '(.templating.list[] | select(.type=="query") | .options) = []' | \
+    jq '(.templating.list[] | select(.type=="query") | .refresh) = 2' | \
+    jq '(.templating.list[] | select(.type=="query") | .current) = {}' | \
+    jq '(.templating.list[] | select(.type=="datasource") | .current) = {}' | \
+    jq '(.templating.list[] | select(.type=="interval") | .current) = {}' | \
+    jq '(.panels[] | select(.scopedVars!=null) | .scopedVars) = {}' | \
+    jq '(.templating.list[] | select(.type=="datasource") | .hide) = 2' | \
+    jq '(.annotations.list) = []' | \
+    jq '(.links) = []' | \
+    jq '(.refresh) = "30s"' | \
+    jq '(.time.from) = "now-6h"' | \
+    jq '(.editable) = true' | \
+    jq '(.panels[] | select(.type!="row") | .editable) = true' | \
+    jq '(.panels[] | select(.type!="row") | .transparent) = true' | \
+    jq '(.panels[] | select(.type!="row") | .timeRegions) = []' | \
+    jq '(.hideControls) = false' | \
+    jq '(.time.to) = "now"' | \
+    jq '(.timezone) = ""' | \
+    jq '(.graphTooltip) = 1' | \
+    jq 'del(.panels[] | select(.repeatPanelId!=null))' | \
+    jq 'del(.id)' | \
+    jq 'del(.iteration)' | \
+    jq --sort-keys '.' > "$tmpfile"
+
+  mv "$tmpfile" "$filename"
+}
