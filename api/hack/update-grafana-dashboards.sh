f79e9f5c3b110afd8cab5ca20cf1a17d65b2b533
Christoph Mewes <christoph@loodse.com>
2019-10-11 14:17:03 +0200

    re-use the existing dashboard formatting for the verification

--- api/hack/update-grafana-dashboards.sh
+++ api/hack/update-grafana-dashboards.sh
@@ -2,37 +2,10 @@
 
 set -euo pipefail
 
-cd $(dirname $0)/../../config/monitoring/grafana/dashboards
+cd $(dirname $0)/../..
+source api/hack/lib.sh
 
-for dashboard in */*.json; do
-  echo "$dashboard"
-
-  tmpfile="$dashboard.tmp"
-
-  cat "$dashboard" | \
-    jq '(.templating.list[] | select(.type=="query") | .options) = []' | \
-    jq '(.templating.list[] | select(.type=="query") | .refresh) = 2' | \
-    jq '(.templating.list[] | select(.type=="query") | .current) = {}' | \
-    jq '(.templating.list[] | select(.type=="datasource") | .current) = {}' | \
-    jq '(.templating.list[] | select(.type=="interval") | .current) = {}' | \
-    jq '(.panels[] | select(.scopedVars!=null) | .scopedVars) = {}' | \
-    jq '(.templating.list[] | select(.type=="datasource") | .hide) = 2' | \
-    jq '(.annotations.list) = []' | \
-    jq '(.links) = []' | \
-    jq '(.refresh) = "30s"' | \
-    jq '(.time.from) = "now-6h"' | \
-    jq '(.editable) = true' | \
-    jq '(.panels[] | select(.type!="row") | .editable) = true' | \
-    jq '(.panels[] | select(.type!="row") | .transparent) = true' | \
-    jq '(.panels[] | select(.type!="row") | .timeRegions) = []' | \
-    jq '(.hideControls) = false' | \
-    jq '(.time.to) = "now"' | \
-    jq '(.timezone) = ""' | \
-    jq '(.graphTooltip) = 1' | \
-    jq 'del(.panels[] | select(.repeatPanelId!=null))' | \
-    jq 'del(.id)' | \
-    jq 'del(.iteration)' | \
-    jq --sort-keys '.' > "$tmpfile"
-
-  mv "$tmpfile" "$dashboard"
+for dashboard in config/monitoring/grafana/dashboards/*/*.json; do
+  echodate "$dashboard"
+  format_dashboard "$dashboard"
 done
