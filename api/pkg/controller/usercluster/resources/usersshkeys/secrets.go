6a3f8ea6c1208e2fe2296207d675f5b89a151642
Moath Qasim <moad.qassem@gmail.com>
2019-10-16 12:15:25 +0200

    debugging
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

--- api/pkg/controller/usercluster/resources/usersshkeys/secrets.go
+++ api/pkg/controller/usercluster/resources/usersshkeys/secrets.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 
 	"github.com/kubermatic/kubermatic/api/pkg/resources"
 	"github.com/kubermatic/kubermatic/api/pkg/resources/reconciling"
 
 	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	secretsDirLink = "..data"
 )
 
 // CreateUserSSHKeysSecrets creates a secret in the usercluster from the user ssh keys.
@@ -21,27 +24,18 @@ func CreateUserSSHKeysSecrets(path string) reconciling.NamedSecretCreatorGetter 
 
 func createSecretsFromUserSSHDirPath(path string) reconciling.SecretCreator {
 	return func(existing *corev1.Secret) (secret *corev1.Secret, e error) {
-		files, err := ioutil.ReadDir(path)
+		secretsDir, err := os.Readlink(fmt.Sprintf("%v/%v", path, secretsDirLink))
+		if err != nil {
+			return nil, err
+		}
+
+		files, err := ioutil.ReadDir(fmt.Sprintf("%v/%v", path, secretsDir))
 		if err != nil {
 			return nil, err
 		}
 
 		if existing.Data == nil {
 			existing.Data = map[string][]byte{}
-		}
-
-		if err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-
-			if info.IsDir() {
-
-			}
-			fmt.Printf("Path: %v, File name: %v, IsDir: %v", path, info.Name(), info.IsDir())
-			return nil
-		}); err != nil {
-			return nil, fmt.Errorf("failed to walk directory: %v", err)
 		}
 
 		for _, file := range files {
