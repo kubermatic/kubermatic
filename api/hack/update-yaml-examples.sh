f2a4e0df85310531fece580c7d085d82f0fe261a
Christoph Mewes <christoph@loodse.com>
2019-10-17 16:32:24 +0200

    allow to rebuild YAML without destroying uncommitted changes

--- api/hack/update-yaml-examples.sh
+++ api/hack/update-yaml-examples.sh
@@ -4,9 +4,11 @@ set -euo pipefail
 
 cd $(dirname $0)/..
 
+dummy=kubermaticNoOmitPlease
+
 # remove omitempty tags from structs so that genyaml will not skip fields
-sed -i 's/,omitempty//g' pkg/crd/kubermatic/v1/*.go
-sed -i 's/,omitempty//g' vendor/k8s.io/api/core/v1/*.go
+sed -i "s/,omitempty/,$dummy/g" pkg/crd/kubermatic/v1/*.go
+sed -i "s/,omitempty/,$dummy/g" vendor/k8s.io/api/core/v1/*.go
 
 # there are some fields that we do actually want to ignore
 sed -i 's/omitgenyaml/omitempty/g' pkg/crd/kubermatic/v1/*.go
@@ -14,5 +16,6 @@ sed -i 's/omitgenyaml/omitempty/g' pkg/crd/kubermatic/v1/*.go
 go run cmd/example-yaml-generator/main.go . ../docs
 
 # revert our changes
-git checkout pkg/crd/kubermatic/v1
-git checkout vendor/k8s.io/api/core/v1
+sed -i 's/omitempty/omitgenyaml/g' pkg/crd/kubermatic/v1/*.go
+sed -i "s/,$dummy/,omitempty/g" pkg/crd/kubermatic/v1/*.go
+sed -i "s/,$dummy/,omitempty/g" vendor/k8s.io/api/core/v1/*.go
