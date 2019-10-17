e3067306024b8a5bb43c88997c8f8e05c80a5462
Tobias Hintze <th-git@thzn.de>
2019-10-17 15:17:11 +0200

    Cleanup tests.

--- api/pkg/controller/clustercomponentdefaulter/clustercomponentdefaulter_test.go
+++ api/pkg/controller/clustercomponentdefaulter/clustercomponentdefaulter_test.go
@@ -1,7 +1,6 @@
 package clustercomponentdefaulter
 
 import (
-	"fmt"
 	"testing"
 
 	kubermaticv1 "github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
@@ -18,124 +17,61 @@ import (
 
 const clusterName = "test-cluster"
 
-func exampleOverrides() []kubermaticv1.ComponentSettings {
-	return []kubermaticv1.ComponentSettings{
-		{
-			Apiserver: kubermaticv1.APIServerSettings{
-				DeploymentSettings:          kubermaticv1.DeploymentSettings{Replicas: utilpointer.Int32Ptr(1)},
-				EndpointReconcilingDisabled: utilpointer.BoolPtr(true),
-			},
-			Scheduler:         kubermaticv1.DeploymentSettings{Replicas: utilpointer.Int32Ptr(2)},
-			ControllerManager: kubermaticv1.DeploymentSettings{Replicas: utilpointer.Int32Ptr(3)},
-		},
+func exampleCluster(settings *kubermaticv1.ComponentSettings) *kubermaticv1.Cluster {
+	cluster := &kubermaticv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{Name: clusterName},
+		Spec:       kubermaticv1.ClusterSpec{ComponentsOverride: *settings},
 	}
+	return cluster
+}
+func exampleSettings(reconciling *bool, replicas bool) *kubermaticv1.ComponentSettings {
+	settings := &kubermaticv1.ComponentSettings{
+		Apiserver: kubermaticv1.APIServerSettings{EndpointReconcilingDisabled: reconciling},
+	}
+	if replicas {
+		settings.Apiserver.Replicas = utilpointer.Int32Ptr(1)
+		settings.Scheduler.Replicas = utilpointer.Int32Ptr(2)
+	}
+	return settings
 }
 
 func TestReconciliation(t *testing.T) {
 	testCases := []struct {
 		name     string
-		cluster  kubermaticv1.Cluster
-		override kubermaticv1.ComponentSettings
-		verify   func(error, *kubermaticv1.Cluster) error
+		cluster  *kubermaticv1.Cluster
+		override *kubermaticv1.ComponentSettings
+		verify   func(input, override, reconciled *kubermaticv1.ComponentSettings) []string
 	}{
 		{
-			name:    "Defaulting without EndpointReconcilingDisabled with Replicas",
-			cluster: kubermaticv1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: clusterName}},
-			override: kubermaticv1.ComponentSettings{
-				Apiserver: kubermaticv1.APIServerSettings{
-					DeploymentSettings: kubermaticv1.DeploymentSettings{Replicas: utilpointer.Int32Ptr(1)},
-				},
-				Scheduler:         kubermaticv1.DeploymentSettings{Replicas: utilpointer.Int32Ptr(2)},
-				ControllerManager: kubermaticv1.DeploymentSettings{Replicas: utilpointer.Int32Ptr(3)},
-			},
-			verify: func(err error, cluster *kubermaticv1.Cluster) error {
-				if err != nil {
-					return fmt.Errorf("expected err to be nil, was: %v", err)
-				}
-				if val := cluster.Spec.ComponentsOverride.Apiserver.EndpointReconcilingDisabled; val != nil {
-					return fmt.Errorf("expected EndpointReconcilingDisabled to be nil, was %v", val)
-				}
-				if cluster.Spec.ComponentsOverride.Apiserver.Replicas == nil ||
-					cluster.Spec.ComponentsOverride.Scheduler.Replicas == nil ||
-					cluster.Spec.ComponentsOverride.ControllerManager.Replicas == nil ||
-					*cluster.Spec.ComponentsOverride.Apiserver.Replicas != 1 ||
-					*cluster.Spec.ComponentsOverride.Scheduler.Replicas != 2 ||
-					*cluster.Spec.ComponentsOverride.ControllerManager.Replicas != 3 {
-					return fmt.Errorf("expected unmodified Replica counts, at least one was modified")
-				}
-				return nil
+			name:     "Defaulting without EndpointReconcilingDisabled",
+			cluster:  exampleCluster(exampleSettings(nil, true)),
+			override: exampleSettings(nil, false),
+			verify: func(input, override, reconciled *kubermaticv1.ComponentSettings) []string {
+				return deep.Equal(reconciled, input)
 			},
 		},
 		{
-			name:     "Defaulting with EndpointReconcilingDisabled with Replicas",
-			cluster:  kubermaticv1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: clusterName}},
-			override: exampleOverrides()[0],
-			verify: func(err error, cluster *kubermaticv1.Cluster) error {
-				if err != nil {
-					return fmt.Errorf("expected err to be nil, was: %v", err)
-				}
-				if diff := deep.Equal(cluster.Spec.ComponentsOverride, exampleOverrides()[0]); diff != nil {
-					return fmt.Errorf("unexpected difference in components override: %v", diff)
-				}
-				return nil
+			name:     "Defaulting with EndpointReconcilingDisabled: true",
+			cluster:  exampleCluster(exampleSettings(nil, true)),
+			override: exampleSettings(utilpointer.BoolPtr(true), true),
+			verify: func(input, override, reconciled *kubermaticv1.ComponentSettings) []string {
+				return deep.Equal(reconciled, override)
 			},
 		},
 		{
-			name:    "Defaulting with EndpointReconcilingDisabled: true",
-			cluster: kubermaticv1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: clusterName}},
-			override: kubermaticv1.ComponentSettings{
-				Apiserver: kubermaticv1.APIServerSettings{EndpointReconcilingDisabled: utilpointer.BoolPtr(true)},
-			},
-			verify: func(err error, cluster *kubermaticv1.Cluster) error {
-				if err != nil {
-					return fmt.Errorf("expected err to be nil, was: %v", err)
-				}
-				if val := cluster.Spec.ComponentsOverride.Apiserver.EndpointReconcilingDisabled; val == nil || *val != true {
-					return fmt.Errorf("expected EndpointReconcilingDisabled to be true, was %v", val)
-				}
-				return nil
+			name:     "Defaulting with EndpointReconcilingDisabled: false",
+			cluster:  exampleCluster(exampleSettings(nil, true)),
+			override: exampleSettings(utilpointer.BoolPtr(false), true),
+			verify: func(input, override, reconciled *kubermaticv1.ComponentSettings) []string {
+				return deep.Equal(reconciled, override)
 			},
 		},
 		{
-			name:    "Defaulting with EndpointReconcilingDisabled: false",
-			cluster: kubermaticv1.Cluster{ObjectMeta: metav1.ObjectMeta{Name: clusterName}},
-			override: kubermaticv1.ComponentSettings{
-				Apiserver: kubermaticv1.APIServerSettings{EndpointReconcilingDisabled: utilpointer.BoolPtr(false)},
-			},
-			verify: func(err error, cluster *kubermaticv1.Cluster) error {
-				if err != nil {
-					return fmt.Errorf("expected err to be nil, was: %v", err)
-				}
-				if val := cluster.Spec.ComponentsOverride.Apiserver.EndpointReconcilingDisabled; val == nil || *val != false {
-					return fmt.Errorf("expected EndpointReconcilingDisabled to be false, was %v", val)
-				}
-				return nil
-			},
-		},
-		{
-			name: "No override when EndpointReconcilingDisabled is specified in cluster",
-			cluster: kubermaticv1.Cluster{
-				ObjectMeta: metav1.ObjectMeta{Name: clusterName},
-				Spec: kubermaticv1.ClusterSpec{
-					ComponentsOverride: kubermaticv1.ComponentSettings{
-						Apiserver: kubermaticv1.APIServerSettings{
-							EndpointReconcilingDisabled: utilpointer.BoolPtr(false),
-						},
-					},
-				},
-			},
-			override: kubermaticv1.ComponentSettings{
-				Apiserver: kubermaticv1.APIServerSettings{EndpointReconcilingDisabled: utilpointer.BoolPtr(true)},
-			},
-			verify: func(err error, cluster *kubermaticv1.Cluster) error {
-				if err != nil {
-					return fmt.Errorf("expected err to be nil, was: %v", err)
-				}
-				if diff := deep.Equal(&cluster.Spec.ComponentsOverride.Apiserver,
-					&kubermaticv1.APIServerSettings{EndpointReconcilingDisabled: utilpointer.BoolPtr(false)}); diff != nil {
-					return fmt.Errorf("unexpected difference in Apiserver override: %v", diff)
-				}
-				return nil
+			name:     "No override when EndpointReconcilingDisabled is specified in cluster",
+			cluster:  exampleCluster(exampleSettings(utilpointer.BoolPtr(false), true)),
+			override: exampleSettings(utilpointer.BoolPtr(true), false),
+			verify: func(input, override, reconciled *kubermaticv1.ComponentSettings) []string {
+				return deep.Equal(reconciled, input)
 			},
 		},
 	}
@@ -143,15 +79,18 @@ func TestReconciliation(t *testing.T) {
 	logger := zap.NewExample().Sugar()
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			client := fake.NewFakeClient([]runtime.Object{&tc.cluster}...)
-			r := &Reconciler{client: client, log: logger, defaults: tc.override}
-			reconcileErr := r.reconcile(logger, &tc.cluster)
+			cluster := tc.cluster.DeepCopy()
+			client := fake.NewFakeClient([]runtime.Object{cluster}...)
+			r := &Reconciler{client: client, log: logger, defaults: *tc.override}
+			if err := r.reconcile(logger, cluster); err != nil {
+				t.Fatalf("failed to reconcile cluster: %v", err)
+			}
 			reconciledCluster := &kubermaticv1.Cluster{}
 			if err := r.client.Get(r.ctx, types.NamespacedName{Name: clusterName}, reconciledCluster); err != nil {
-				t.Fatalf("failed to get reconciledCluster %s: %v", clusterName, err)
+				t.Fatalf("failed to get reconciledCluster: %v", err)
 			}
-			if err := tc.verify(reconcileErr, reconciledCluster); err != nil {
-				t.Fatalf("verification failed: %v", err)
+			if diff := tc.verify(&tc.cluster.Spec.ComponentsOverride, tc.override, &reconciledCluster.Spec.ComponentsOverride); diff != nil {
+				t.Fatalf("unexpected difference in cluster after reconcilation: %v", diff)
 			}
 		})
 	}
