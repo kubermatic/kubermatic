c113646003f1346add64b52bebbae27e42f542ec
Christoph Mewes <christoph@loodse.com>
2019-10-21 22:03:46 +0200

    Add script for syncing data to the docs repository (#4514)
    * add postsubmit for syncing data to the docs repository
    
    * safety run first
    
    * need make and SSH
    
    * push for real
    
    * improve commit message so github doesn't get confused hopefully

---
+++ api/hack/ci/ci-update-docs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd $(dirname $0)/../../..
+
+TARGET_DIR=docs_sync
+REVISION=$(git rev-parse --short HEAD)
+
+# configure Git
+git config --global user.email "dev@loodse.com"
+git config --global user.name "Prow CI Robot"
+git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /ssh/id_rsa'
+
+# create a fresh clone
+git clone git@github.com:kubermatic/docs.git $TARGET_DIR
+cd $TARGET_DIR
+
+# copy interesting files over
+cp ../docs/zz_generated.seed.yaml data/seed.yaml
+
+# re-create Prometheus runbook
+make runbook
+
+# update repo
+git add .
+git diff --cached --stat
+git commit -m "Syncing with kubermatic/kubermatic@$REVISION"
+git push
