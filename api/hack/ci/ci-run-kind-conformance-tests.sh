dbd737d3e42b204a67aaf04a8667b739e1f60be5
Alvaro Aleman <alv2412@googlemail.com>
2019-10-27 13:31:28 +0100

    Fix ssh key deployment

--- api/hack/ci/ci-run-kind-conformance-tests.sh
+++ api/hack/ci/ci-run-kind-conformance-tests.sh
@@ -272,9 +272,10 @@ helm install --wait --timeout 180 \
   --name kubermatic-oauth-e2e ${DEX_PATH}
 
 # Install the machine-controller pubkey
-RUN mkdir $HOME/.ssh && \
-    echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo3amVmCkIZo4cgj2kjU2arZKlzzOhaOuveH9aJbL4mlVHVsEcVk+RSty4AMK1GQL3+Ii7iGicKWwge4yefc75aOtUncfF01rnBsNvi3lOqJR/6POHy4OnPXJElvEn7jii/pAUeyr8halBezQTUkvRiUtlJo6oEb2dRN5ujyFm5TuIxgM0UFVGBRoD0agGr87GaQsUahf+PE1zHEid+qQPz7EdMo8/eRNtgikhBG1/ae6xRstAi0QU8EgjKvK1ROXOYTlpTBFElApOXZacH91WvG0xgPnyxIXoKtiCCNGeu/0EqDAgiXfmD2HK/WAXwJNwcmRvBaedQUS4H0lNmvj5' > $HOME/.ssh/id_rsa.pub && \
-    chmod 0700 $HOME/.ssh
+mkdir $HOME/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo3amVmCkIZo4cgj2kjU2arZKlzzOhaOuveH9aJbL4mlVHVsEcVk+RSty4AMK1GQL3+Ii7iGicKWwge4yefc75aOtUncfF01rnBsNvi3lOqJR/6POHy4OnPXJElvEn7jii/pAUeyr8halBezQTUkvRiUtlJo6oEb2dRN5ujyFm5TuIxgM0UFVGBRoD0agGr87GaQsUahf+PE1zHEid+qQPz7EdMo8/eRNtgikhBG1/ae6xRstAi0QU8EgjKvK1ROXOYTlpTBFElApOXZacH91WvG0xgPnyxIXoKtiCCNGeu/0EqDAgiXfmD2HK/WAXwJNwcmRvBaedQUS4H0lNmvj5' \
+	> $HOME/.ssh/id_rsa.pub
+chmod 0700 $HOME/.ssh
 
 echodate "Installing seed"
 SEED_MANIFEST="$(mktemp)"
