aff22a3ad722357bca4ec76311cf610c9ad498b7
Christopher Adigun <christopher@loodse.com>
2019-11-11 10:29:13 +0100

    add ci scripts for new run clusters
    Signed-off-by: Christopher Adigun <christopher@loodse.com>

---
+++ api/hack/ci/ci-deploy-run-lab-stable.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+export DEPLOY_STACK=${DEPLOY_STACK:-kubermatic}
+export GIT_HEAD_HASH="$(git rev-parse HEAD|tr -d '\n')"
+cd $(dirname $0)/../../..
+
+source ./api/hack/lib.sh
+
+if [[ "${DEPLOY_STACK}" == "kubermatic" ]]; then
+  ./api/hack/ci/ci-push-images.sh
+fi
+
+echodate "Getting secrets from Vault"
+export VAULT_ADDR=https://vault.loodse.com/
+retry 5 vault write \
+  --format=json auth/approle/login \
+  role_id=${VAULT_ROLE_ID} secret_id=${VAULT_SECRET_ID} > /tmp/vault-token-response.json
+export VAULT_TOKEN="$(cat /tmp/vault-token-response.json| jq .auth.client_token -r)"
+export KUBECONFIG=/tmp/kubeconfig
+export VALUES_FILE=/tmp/values.yaml
+
+# deploy to run-lab cluster
+vault kv get -field=kubeconfig dev/seed-clusters/run.lab.kubermatic.io > ${KUBECONFIG}
+vault kv get -field=values.yaml dev/seed-clusters/run.lab.kubermatic.io > ${VALUES_FILE}
+echodate "Successfully got secrets for run from Vault"
+
+echodate "Deploying ${DEPLOY_STACK} stack to run.lab.kubermatic.io"
+TILLER_NAMESPACE=kubermatic ./api/hack/deploy.sh master ${VALUES_FILE}
+echodate "Successfully deployed ${DEPLOY_STACK} stack to run.lab.kubermatic.io"
