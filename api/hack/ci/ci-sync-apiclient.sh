811c43b06c057de563283d870a2cce3bef91954f
Kamil Domański <kamil@domanski.co>
2019-11-12 13:06:15 +0100

    added script for synchronizing the api client library (#4664)

---
+++ api/hack/ci/ci-sync-apiclient.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -eu
+
+URL="git@github.com:kubermatic/go-kubermatic.git"
+
+commit_and_push() {
+    local repodir branch source_sha
+    repodir="$1"
+    branch="$2"
+    source_sha="$3"
+
+    (
+        cd "$repodir"
+
+        git config --local user.email "dev@loodse.com"
+        git config --local user.name "Prow CI Robot"
+        git config --local core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /ssh/id_rsa'
+
+        git add .
+        git commit -m "Syncing client from Kubermatic $source_sha"
+        git push origin "$branch"
+    )
+}
+
+if [ -z "${PULL_BASE_REF=}" ]; then
+    echo "\$PULL_BASE_REF undefined, I don't know which branch to sync."
+    exit 1
+fi
+
+if [ -n "$(git rev-parse --show-prefix)" ]; then
+    echo "You must run this script from repo root"
+    exit 1
+fi
+
+# clone the target and pick the right branch
+tempdir="$(mktemp -d --suffix "-apiclient-sync")"
+trap "rm -rf '$tempdir'" EXIT
+git clone "$URL" "$tempdir"
+(
+    cd "$tempdir"
+    git checkout "$PULL_BASE_REF" || git checkout -b "$PULL_BASE_REF"
+)
+
+# rewrite all the import paths
+echo "Rewriting import paths"
+sed_expression="s#github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient#github.com/kubermatic/go-kubermatic#g"
+time find api/pkg/test/e2e/api/utils/apiclient/ -type f -execdir sed "$sed_expression" -i {} \;
+
+# sync the files
+echo "Synchronizing the files"
+rsync --archive --verbose --delete "./api/pkg/test/e2e/api/utils/apiclient/client/" "$tempdir/client/"
+rsync --archive --verbose --delete "./api/pkg/test/e2e/api/utils/apiclient/models/" "$tempdir/models/"
+
+# commit and push
+commit_and_push "$tempdir" "$PULL_BASE_REF" "$(git rev-parse HEAD)"
