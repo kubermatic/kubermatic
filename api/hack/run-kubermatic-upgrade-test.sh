84ce458c8df6aeb45365cf066d37cfeb7b8388f0
Kamil Domański <kamil@domanski.co>
2019-11-13 11:57:58 +0100

    CI scripts: use hardcoded Github hostkey (#4670)

--- api/hack/run-kubermatic-upgrade-test.sh
+++ api/hack/run-kubermatic-upgrade-test.sh
@@ -11,7 +11,8 @@ cd $(go env GOPATH)/src/github.com/kubermatic/kubermatic/api/hack/ci
 export UPGRADE_TEST_BASE_HASH=${UPGRADE_TEST_BASE_HASH:-"master"}
 
 # We need to fetch UPGRADE_TEST_BASE_HASH in case its not in either the PRs base or the Prs HEAD
-git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /ssh/id_rsa'
+ensure_github_host_pubkey
+git config --global core.sshCommand 'ssh -o CheckHostIP=no -i /ssh/id_rsa'
 git remote add origin git@github.com:kubermatic/kubermatic.git
 git fetch origin ${UPGRADE_TEST_BASE_HASH}
 
