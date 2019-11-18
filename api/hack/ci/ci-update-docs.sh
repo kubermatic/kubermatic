b1122b1ec576b0d74b57f91cb428451500050cf1
Kamil Domański <kamil@domanski.co>
2019-11-18 16:16:31 +0100

    [release/v2.12] CI scripts: use hardcoded Github hostkey (#4670) (#4700)

--- api/hack/ci/ci-update-docs.sh
+++ api/hack/ci/ci-update-docs.sh
@@ -10,7 +10,8 @@ REVISION=$(git rev-parse --short HEAD)
 # configure Git
 git config --global user.email "dev@loodse.com"
 git config --global user.name "Prow CI Robot"
-git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /ssh/id_rsa'
+git config --global core.sshCommand 'ssh -o CheckHostIP=no -i /ssh/id_rsa'
+ensure_github_host_pubkey
 
 # create a fresh clone
 git clone git@github.com:kubermatic/docs.git $TARGET_DIR
