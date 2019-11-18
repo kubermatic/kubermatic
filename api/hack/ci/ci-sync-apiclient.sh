7adbc0545905154fcb588c3445640b86fe2a82ec
Kamil Domański <kamil@domanski.co>
2019-11-18 17:12:30 +0100

    [release/v2.12] added script for synchronizing the api client library (#4664) (#4701)
    * added script for synchronizing the api client library (#4664)
    
    * client-sync: remove not-posix parameter from the script (#4668)
    
    Apparently the `--suffix` param is a GNU thing and doesn't exist in busybox mktemp.
    
    * CI scripts: use hardcoded Github hostkey (#4670)
    
    * sync-api client: exit with success if there's nothing to sync (#4681)
    
    * sync-apiclient: use -exec instead of -execdir which is non-POSIX (#4676)

---
+++ api/hack/ci/ci-sync-apiclient.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -eu
+
+. $(dirname $0)/../lib.sh
+
+URL="git@github.com:kubermatic/go-kubermatic.git"
+
+commit_and_push() {
+    local repodir branch source_sha
+    repodir="$1"
+    branch="$2"
+    source_sha="$3"
+
+    (
+        cd "$repodir"
+
+        git config --local user.email "dev@loodse.com"
+        git config --local user.name "Prow CI Robot"
+        git config --local core.sshCommand 'ssh -o CheckHostIP=no -i /ssh/id_rsa'
+
+        git add .
+        if ! git status | grep -q 'nothing to commit'; then
+            git commit -m "Syncing client from Kubermatic $source_sha"
+            git push origin "$branch"
+        fi
+    )
+}
+
+if [ -z "${PULL_BASE_REF=}" ]; then
+    echo "\$PULL_BASE_REF undefined, I don't know which branch to sync."
+    exit 1
+fi
+
+if [ -n "$(git rev-parse --show-prefix)" ]; then
+    echo "You must run this script from repo root"
+    exit 1
+fi
+
+# Ensure Github's host key is available and disable IP checking.
+ensure_github_host_pubkey
+
+# clone the target and pick the right branch
+tempdir="$(mktemp -d)"
+trap "rm -rf '$tempdir'" EXIT
+GIT_SSH_COMMAND="ssh -o CheckHostIP=no -i /ssh/id_rsa" git clone "$URL" "$tempdir"
+(
+    cd "$tempdir"
+    git checkout "$PULL_BASE_REF" || git checkout -b "$PULL_BASE_REF"
+)
+
+# rewrite all the import paths
+echo "Rewriting import paths"
+sed_expression="s#github.com/kubermatic/kubermatic/api/pkg/test/e2e/api/utils/apiclient#github.com/kubermatic/go-kubermatic#g"
+time find api/pkg/test/e2e/api/utils/apiclient/ -type f -exec sed "$sed_expression" -i {} \;
+
+# sync the files
+echo "Synchronizing the files"
+rsync --archive --verbose --delete "./api/pkg/test/e2e/api/utils/apiclient/client/" "$tempdir/client/"
+rsync --archive --verbose --delete "./api/pkg/test/e2e/api/utils/apiclient/models/" "$tempdir/models/"
+
+# commit and push
+commit_and_push "$tempdir" "$PULL_BASE_REF" "$(git rev-parse HEAD)"
