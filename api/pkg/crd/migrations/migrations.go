caa28382144520dd8f17a4ece9d6122085ada29f
Lukasz Zajaczkowski <zreigz@gmail.com>
2019-11-26 21:37:29 +0100

    add migration for cluster user labels (#4744)

--- api/pkg/crd/migrations/migrations.go
+++ api/pkg/crd/migrations/migrations.go
@@ -12,6 +12,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog"
@@ -99,6 +100,7 @@ func cleanupCluster(cluster *kubermaticv1.Cluster, ctx *cleanupContext) error {
 		setExposeStrategyIfEmpty,
 		setProxyModeIfEmpty,
 		cleanupDashboardAddon,
+		migrateClusterUserLabel,
 	}
 
 	w := sync.WaitGroup{}
@@ -165,5 +167,30 @@ func cleanupDashboardAddon(cluster *kubermaticv1.Cluster, ctx *cleanupContext) e
 			return err
 		}
 	}
+	return nil
+}
+
+func migrateClusterUserLabel(cluster *kubermaticv1.Cluster, ctx *cleanupContext) error {
+	// If there is not label - nothing to migrate
+	if cluster.Labels == nil {
+		return nil
+	}
+	newLabels := map[string]string{}
+	userLabelSet := sets.NewString("user", "user_RAW")
+	for key, value := range cluster.Labels {
+		if userLabelSet.Has(key) {
+			continue
+		}
+		newLabels[key] = value
+	}
+	if len(newLabels) != len(cluster.Labels) {
+		cluster.Labels = newLabels
+		updatedCluster, err := ctx.kubermaticClient.KubermaticV1().Clusters().Update(cluster)
+		if err != nil {
+			return err
+		}
+		*cluster = *updatedCluster
+	}
+
 	return nil
 }
