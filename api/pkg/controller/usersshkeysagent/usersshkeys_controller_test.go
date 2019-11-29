380f0048b8c96ec321626f0d893c237334b73749
MqueMazz <moad.qassem@gmail.com>
2019-11-29 10:05:57 +0100

    Sync SSH keys to authorized_keys files on nodes (#4531)
    * Connecting the new operator in the user cluster controller
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Adding tests and the authorized_keys file watcher
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Vendor the fsnotify lib
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Fix go lint issues
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Fix the authorized_keys paths
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Fix the file watching bug on update
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Add more testing scenarios and organize imports
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Fix the mounting paths bug
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Remove the default registry
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * debug
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Refactor user-ssh-key-agent daemonset creator
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Adding servicename for the user-ssh-key-agent daemonset
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Addressing PR review
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Addressing PR review, building and pushing user-ssh-agent image
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Debug user ssh keys image push
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>D
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * debug
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>D
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Fix the image name
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>D
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Addressing PR reviews
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Update authorized_keys file permissions on reconcile
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Update authorized_keys file permissions on reconcile
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Update authorized_keys file permissions on reconcile
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Refactoring and adding success messages
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * Refactoring and adding tests for the file modes checks
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

---
+++ api/pkg/controller/usersshkeysagent/usersshkeys_controller_test.go
@@ -0,0 +1,165 @@
+package usersshkeysagent
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	kubermaticlog "github.com/kubermatic/kubermatic/api/pkg/log"
+	"github.com/kubermatic/kubermatic/api/pkg/resources"
+
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestReconcileUserSSHKeys(t *testing.T) {
+	sshPath := fmt.Sprintf("%v%v", os.TempDir(), ".ssh")
+	if err := os.Mkdir(sshPath, 0700); err != nil {
+		t.Fatalf("error while creating .ssh dir: %v", err)
+	}
+
+	defer func() {
+		if err := cleanupFiles([]string{sshPath}); err != nil {
+			t.Fatalf("failed to cleanup test files: %v", err)
+		}
+	}()
+
+	authorizedKeysPath := fmt.Sprintf("%v/%v", sshPath, "authorized_keys")
+	_, err := os.Create(authorizedKeysPath)
+	if err != nil {
+		t.Fatalf("error while creating authorized_keys file: %v", err)
+	}
+
+	if err := os.Chmod(authorizedKeysPath, os.FileMode(0600)); err != nil {
+		t.Fatalf("error while changing file mode: %v", err)
+	}
+
+	testCases := []struct {
+		name             string
+		reconciler       Reconciler
+		modifier         func(string, string) error
+		sshDirPath       string
+		expectedSSHKey   string
+		expectedFileMode int16
+		expectedDirMode  int16
+	}{
+		{
+			name: "Test updating authorized_keys file from reconcile",
+			reconciler: Reconciler{
+				log: kubermaticlog.New(true, kubermaticlog.FormatConsole).Sugar(),
+				authorizedKeysPath: []string{
+					authorizedKeysPath,
+				},
+			},
+			expectedSSHKey: "ssh-rsa test_user_ssh_key\nssh-rsa test_user_ssh_key_2",
+		},
+		{
+			name: "Test ssh dir file mode",
+			reconciler: Reconciler{
+				log: kubermaticlog.New(true, kubermaticlog.FormatConsole).Sugar(),
+				authorizedKeysPath: []string{
+					authorizedKeysPath,
+				},
+			},
+			modifier:         changeFileModes,
+			expectedSSHKey:   "ssh-rsa test_user_ssh_key\nssh-rsa test_user_ssh_key_2",
+			sshDirPath:       sshPath,
+			expectedFileMode: 384,
+			expectedDirMode:  448,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.reconciler.Client = fake.NewFakeClient(
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						ResourceVersion: "123456",
+						Name:            resources.UserSSHKeys,
+						Namespace:       metav1.NamespaceSystem,
+					},
+					Data: map[string][]byte{
+						"key-test":   []byte("ssh-rsa test_user_ssh_key"),
+						"key-test-2": []byte("ssh-rsa test_user_ssh_key_2"),
+					},
+				})
+
+			if tc.modifier != nil {
+				if err := tc.modifier(sshPath, authorizedKeysPath); err != nil {
+					t.Fatalf("error while executing test modifier: %v", err)
+				}
+			}
+
+			if _, err := tc.reconciler.Reconcile(reconcile.Request{
+				NamespacedName: types.NamespacedName{Name: resources.UserSSHKeys, Namespace: metav1.NamespaceSystem}}); err != nil {
+				t.Fatalf("failed to run reconcile: %v", err)
+			}
+
+			for _, path := range tc.reconciler.authorizedKeysPath {
+				key, err := readAuthorizedKeysFile(path)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if key != tc.expectedSSHKey {
+					t.Fatal("usersshkey secret and authorized_keys file don't match")
+				}
+
+				if tc.modifier != nil {
+					authorizedKeysInfo, err := os.Stat(path)
+					if err != nil {
+						t.Fatalf("failed describing file %s: %v", path, err)
+					}
+
+					if int16(authorizedKeysInfo.Mode()) != tc.expectedFileMode {
+						t.Fatal("authorized_keys file mode and its expected file mode don't match")
+					}
+
+					sshDirInfo, err := os.Stat(tc.sshDirPath)
+					if err != nil {
+						t.Fatalf("failed describing file %s: %v", path, err)
+					}
+
+					if int16(sshDirInfo.Mode()) != tc.expectedDirMode {
+						t.Fatal(".ssh dir mode and its expected file mode don't match")
+					}
+
+				}
+			}
+		})
+	}
+}
+
+func readAuthorizedKeysFile(path string) (string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSuffix(string(data), "\n"), nil
+}
+
+func cleanupFiles(tmpFiles []string) error {
+	for _, tmpFile := range tmpFiles {
+		if err := os.RemoveAll(tmpFile); err == nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func changeFileModes(sshDir, authorizedKeysFile string) error {
+	if err := os.Chmod(authorizedKeysFile, 0700); err != nil {
+		return fmt.Errorf("error while changing file mode: %v", err)
+	}
+
+	if err := os.Chmod(sshDir, 0600); err != nil {
+		return fmt.Errorf("error while changing file mode: %v", err)
+	}
+	return nil
+}
