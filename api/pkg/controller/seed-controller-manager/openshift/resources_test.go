97b42e53bcf7cd7475f411cc7abff71362b396b9
Alvaro Aleman <alv2412@googlemail.com>
2019-12-03 12:26:18 +0100

    Add missing openshift settings to test

--- api/pkg/controller/seed-controller-manager/openshift/resources_test.go
+++ api/pkg/controller/seed-controller-manager/openshift/resources_test.go
@@ -2,6 +2,8 @@ package openshift
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/kubermatic/kubermatic/api/pkg/semver"
@@ -102,6 +104,22 @@ func TestEnsureResourcesAreDeployedIdempotency(t *testing.T) {
 		}
 	}()
 
+	oidcCATempfile, err := ioutil.TempFile("", "kubermatic-openshift-controller-test")
+	if err != nil {
+		t.Fatalf("failed to get tempfile for oidc ca: %v", err)
+	}
+	if err := oidcCATempfile.Close(); err != nil {
+		t.Errorf("failed to close OIDC ca tempfile: %v", err)
+	}
+	defer func() {
+		if err := os.Remove(oidcCATempfile.Name()); err != nil {
+			t.Errorf("failed to remove OIDC ca tempfile: %v", err)
+		}
+	}()
+	if err := ioutil.WriteFile(oidcCATempfile.Name(), oidcCA, 0644); err != nil {
+		t.Fatalf("failed to write oidcCA to tempfile: %v", err)
+	}
+
 	testCluster := &kubermaticv1.Cluster{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        "test-cluster",
@@ -112,7 +130,8 @@ func TestEnsureResourcesAreDeployedIdempotency(t *testing.T) {
 			Cloud: kubermaticv1.CloudSpec{
 				DatacenterName: "my-dc",
 			},
-			Version: *semver.NewSemverOrDie("4.1.18"),
+			Openshift: &kubermaticv1.Openshift{ImagePullSecret: "{}"},
+			Version:   *semver.NewSemverOrDie("4.1.18"),
 		},
 		Status: kubermaticv1.ClusterStatus{
 			NamespaceName: "cluster-test-cluster",
@@ -168,6 +187,7 @@ func TestEnsureResourcesAreDeployedIdempotency(t *testing.T) {
 				},
 			}, nil
 		},
+		oidc: OIDCConfig{CAFile: oidcCATempfile.Name()},
 	}
 
 	if err := r.networkDefaults(ctx, testCluster); err != nil {
@@ -191,3 +211,37 @@ func TestEnsureResourcesAreDeployedIdempotency(t *testing.T) {
 		t.Error("expected to find at least one deployment, got zero")
 	}
 }
+
+var oidcCA = []byte(`
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+`)
