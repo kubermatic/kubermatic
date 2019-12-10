35bc0134b6861d35df7902ab3538418ac0efc972
Christoph Mewes <christoph@loodse.com>
2019-12-10 13:39:29 +0100

    remove ci-run-minimal-conformance-tests.sh symlink

--- api/hack/ci/ci-run-kubermatic-upgrade-test.sh
+++ api/hack/ci/ci-run-kubermatic-upgrade-test.sh
@@ -6,9 +6,9 @@ set -euo pipefail
 # receives a SIGINT
 set -o monitor
 
-. $(dirname $0)/lib.sh
+cd $(dirname $0)
 
-cd $(go env GOPATH)/src/github.com/kubermatic/kubermatic/api/hack/ci
+. ../lib.sh
 
 export UPGRADE_TEST_BASE_HASH=${UPGRADE_TEST_BASE_HASH:-"master"}
 
