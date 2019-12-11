717537d12113902dea1165c7eca87a92694131ae
Alvaro Aleman <alv2412@googlemail.com>
2019-12-11 15:55:29 +0100

    Remove conformance tester as its unused

--- containers/conformance-tests/release.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-TAG=v0.10.8
-
-set -euox pipefail
-
-docker build --no-cache --pull -t kubermatic/kubernetes-test-binaries:${TAG} .
-docker push kubermatic/kubernetes-test-binaries:${TAG}
