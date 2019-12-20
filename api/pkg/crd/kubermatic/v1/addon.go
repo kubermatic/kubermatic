6553aacb7fedd0b5b3c9b91f2bfd006a16237f01
Alvaro Aleman <alv2412@googlemail.com>
2019-12-20 09:51:11 +0100

    Revert "Revert "Safe LastSuccessfulDeploymentTimestamp on addons""
    This reverts commit 64e1f24460f969e79fa9d0ea513519ee4ba8c648.

--- api/pkg/crd/kubermatic/v1/addon.go
+++ api/pkg/crd/kubermatic/v1/addon.go
@@ -16,6 +16,7 @@ const (
 )
 
 //+genclient
+//+genclient:noStatus
 
 // Addon specifies a add-on
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -23,7 +24,8 @@ type Addon struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec AddonSpec `json:"spec"`
+	Spec   AddonSpec   `json:"spec"`
+	Status AddonStatus `json:"status"`
 }
 
 // AddonSpec specifies details of an addon
@@ -41,6 +43,14 @@ type AddonSpec struct {
 	RequiredResourceTypes []schema.GroupVersionKind `json:"requiredResourceTypes,omitempty"`
 	// IsDefault indicates whether the addon is default
 	IsDefault bool `json:"isDefault,omitempty"`
+}
+
+// AddonStatus contains details about the current status of an addon
+type AddonStatus struct {
+	// LastSuccessfulDeploymentTimestamp contains the time at which the addon
+	// was successfully deployed last. It is nil if the addon was never deployed
+	// successfully.
+	LastSuccessfulDeploymentTimestamp *metav1.Time `json:"lastSuccessfulDeploymentTimestamp,omitempty"`
 }
 
 // AddonList is a list of addons
