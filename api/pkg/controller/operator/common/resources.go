6e9a54d7fb33d55199744a4b9495a903bf5f043a
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2019-12-23 19:42:56 +0100

    Operator: Fix addon defaulting (#4919)
    * Operator: Fix addon defaulting
    
    * Fix log

--- api/pkg/controller/operator/common/resources.go
+++ api/pkg/controller/operator/common/resources.go
@@ -30,6 +30,13 @@ const (
 	// VersionLabel is the label containing the application's version.
 	VersionLabel = "app.kubernetes.io/version"
 
+	// OpenshiftAddonsFileName is the name of the openshift addons manifest file
+	// in the master files.
+	OpenshiftAddonsFileName = "openshift-addons.yaml"
+	// KubernetesAddonsFileName is the name of the kubernetes addons manifest file
+	// in the master files.
+	KubernetesAddonsFileName = "kubernetes-addons.yaml"
+
 	DockercfgSecretName                   = "dockercfg"
 	DexCASecretName                       = "dex-ca"
 	MasterFilesSecretName                 = "extra-files"
@@ -81,9 +88,15 @@ func DexCASecretCreator(cfg *operatorv1alpha1.KubermaticConfiguration) reconcili
 }
 
 func MasterFilesSecretCreator(cfg *operatorv1alpha1.KubermaticConfiguration) reconciling.NamedSecretCreatorGetter {
+	data := map[string]string{}
+	for key, val := range cfg.Spec.MasterFiles {
+		data[key] = val
+	}
+	data[OpenshiftAddonsFileName] = cfg.Spec.UserCluster.Addons.Openshift.DefaultManifests
+	data[KubernetesAddonsFileName] = cfg.Spec.UserCluster.Addons.Kubernetes.DefaultManifests
 	return func() (string, reconciling.SecretCreator) {
 		return MasterFilesSecretName, func(s *corev1.Secret) (*corev1.Secret, error) {
-			return createSecretData(s, cfg.Spec.MasterFiles), nil
+			return createSecretData(s, data), nil
 		}
 	}
 }
