a1467836c0aefc4c699599893c9c4cc9aa6980b6
Alvaro Aleman <alv2412@googlemail.com>
2019-12-23 17:42:44 +0100

    Add doc.go

---
+++ api/pkg/controller/master-controller-manager/doc.go
@@ -0,0 +1,5 @@
+/*
+Package master-controller-manager contains all controllers that run within the master-controller-manager
+in ./cmd/master-controller-manager
+*/
+package mastercontrollermanager
---
+++ api/pkg/controller/shared/doc.go
@@ -0,0 +1,4 @@
+/*
+Package shared contains all controllers that are imported by more than one controller-manager
+*/
+package shared
