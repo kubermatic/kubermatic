e9c55ca2642d0d2435791989f9a86d8f58a902fc
zreigz <zreigz@gmail.com>
2019-12-30 11:05:20 +0100

    check default cluster role binding for the cluster creator

--- api/pkg/test/e2e/api/cluster_rbac_test.go
+++ api/pkg/test/e2e/api/cluster_rbac_test.go
@@ -44,35 +44,9 @@ func TestCreateClusterRoleBinding(t *testing.T) {
 			}
 
 			apiRunner := CreateAPIRunner(masterToken, t)
-			project, err := apiRunner.CreateProject(rand.String(10))
-			if err != nil {
-				t.Fatalf("can not create project %v", err)
-			}
+			project, cluster := createProjectWithCluster(t, apiRunner, tc.dc, tc.credential, tc.version, tc.location, tc.replicas)
 			teardown := cleanUpProject(project.ID, getMaxAttempts)
 			defer teardown(t)
-
-			cluster, err := apiRunner.CreateDOCluster(project.ID, tc.dc, rand.String(10), tc.credential, tc.version, tc.location, tc.replicas)
-			if err != nil {
-				t.Fatalf("can not create cluster due to error: %v", GetErrorResponse(err))
-			}
-
-			var clusterReady bool
-			for attempt := 1; attempt <= getMaxAttempts; attempt++ {
-				healthStatus, err := apiRunner.GetClusterHealthStatus(project.ID, tc.dc, cluster.ID)
-				if err != nil {
-					t.Fatalf("can not get health status %v", GetErrorResponse(err))
-				}
-
-				if IsHealthyCluster(healthStatus) {
-					clusterReady = true
-					break
-				}
-				time.Sleep(30 * time.Second)
-			}
-
-			if !clusterReady {
-				t.Fatalf("cluster not ready after %d attempts", getMaxAttempts)
-			}
 
 			roleNameList := []v1.RoleName{}
 			// wait for controller
@@ -99,6 +73,22 @@ func TestCreateClusterRoleBinding(t *testing.T) {
 			namesSet := sets.NewString(tc.expectedRoleNames...)
 			if !namesSet.HasAll(roleNames...) {
 				t.Fatalf("expects roles %v, got %v", tc.expectedRoleNames, roleNames)
+			}
+
+			// test if default cluster role bindings were created
+			clusterBindings, err := apiRunner.GetClusterBindings(project.ID, tc.dc, cluster.ID)
+			if err != nil {
+				t.Fatalf("can not get cluster bindings due to error: %v", err)
+			}
+			// every role should have exactly one binding
+			if len(clusterBindings) != len(roleNameList) {
+				t.Fatalf("expected number of bindings %d, got %d", len(roleNameList), len(clusterBindings))
+			}
+			namesSet = sets.NewString(tc.expectedClusterRoleNames...)
+			for _, clusterBinding := range clusterBindings {
+				if !namesSet.Has(clusterBinding.RoleRefName) {
+					t.Fatalf("expected role reference name %s in the binding", clusterBinding.RoleRefName)
+				}
 			}
 
 			for _, roleName := range roleNameList {
@@ -151,4 +141,36 @@ func TestCreateClusterRoleBinding(t *testing.T) {
 			cleanUpCluster(t, apiRunner, project.ID, tc.dc, cluster.ID)
 		})
 	}
+}
+
+func createProjectWithCluster(t *testing.T, apiRunner *APIRunner, dc, credential, version, location string, replicas int32) (*v1.Project, *v1.Cluster) {
+	project, err := apiRunner.CreateProject(rand.String(10))
+	if err != nil {
+		t.Fatalf("can not create project %v", err)
+	}
+
+	cluster, err := apiRunner.CreateDOCluster(project.ID, dc, rand.String(10), credential, version, location, replicas)
+	if err != nil {
+		t.Fatalf("can not create cluster due to error: %v", GetErrorResponse(err))
+	}
+
+	var clusterReady bool
+	for attempt := 1; attempt <= getMaxAttempts; attempt++ {
+		healthStatus, err := apiRunner.GetClusterHealthStatus(project.ID, dc, cluster.ID)
+		if err != nil {
+			t.Fatalf("can not get health status %v", GetErrorResponse(err))
+		}
+
+		if IsHealthyCluster(healthStatus) {
+			clusterReady = true
+			break
+		}
+		time.Sleep(30 * time.Second)
+	}
+
+	if !clusterReady {
+		t.Fatalf("cluster not ready after %d attempts", getMaxAttempts)
+	}
+
+	return project, cluster
 }
