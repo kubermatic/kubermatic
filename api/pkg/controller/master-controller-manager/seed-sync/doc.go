83d8cfa4f553ba75c68944bc2e1a051a4e853938
Alvaro Aleman <alv2412@googlemail.com>
2020-01-02 15:46:14 +0100

    More docs

---
+++ api/pkg/controller/master-controller-manager/seed-sync/doc.go
@@ -0,0 +1,5 @@
+/*
+Package seedsync contains a controller that is responsible for synchronizing the `Seed` custom
+resources onto the corresponding seed clusters, so that the seed-controller-manager can use them.
+*/
+package seedsync
---
+++ api/pkg/controller/seed-controller-manager/backup/doc.go
@@ -0,0 +1,4 @@
+/*
+Package backup contains a controller that is reponsible for creating backup-related resources.
+*/
+package backup
