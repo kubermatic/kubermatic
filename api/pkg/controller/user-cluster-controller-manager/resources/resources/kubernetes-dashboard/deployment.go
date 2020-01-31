f4ea0578e167947bdd53729a88386cf69e0c0858
Sebastian Florek <sebastian.florek@loodse.com>
2020-01-31 11:10:08 +0100

    Bump Kubernetes Dashboard to v2.0.0-rc3

--- api/pkg/controller/user-cluster-controller-manager/resources/resources/kubernetes-dashboard/deployment.go
+++ api/pkg/controller/user-cluster-controller-manager/resources/resources/kubernetes-dashboard/deployment.go
@@ -31,7 +31,7 @@ var (
 const (
 	scraperName      = resources.MetricsScraperDeploymentName
 	scraperImageName = "kubernetesui/metrics-scraper"
-	scraperTag       = "v1.0.1"
+	scraperTag       = "v1.0.3"
 )
 
 // DeploymentCreator returns the function to create and update the dashboard-metrics-scraper deployment
