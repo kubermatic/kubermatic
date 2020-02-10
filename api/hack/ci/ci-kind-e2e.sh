ce8c20a870cca94422185779f3865135528020a6
Kamil Domański <kamil@domanski.co>
2020-02-10 16:30:44 +0100

    refactor ci-kind-e2e.sh for upgrade testing

--- api/hack/ci/ci-kind-e2e.sh
+++ api/hack/ci/ci-kind-e2e.sh
@@ -8,7 +8,15 @@ source ./api/hack/lib.sh
 export KUBERMATIC_NO_WORKER_NAME=true
 export GIT_HEAD_HASH="$(git rev-parse HEAD)"
 
-export KUBERMATIC_VERSION="${UPGRADE_TEST_BASE_HASH:-$GIT_HEAD_HASH}"
+if [ -n "${UPGRADE_TEST_BASE_HASH:-}" ]; then
+  KUBERMATIC_VERSION="${UPGRADE_TEST_BASE_HASH:-}"
+  UPGRADE_TO_VERSION="$GIT_HEAD_HASH"
+else
+  KUBERMATIC_VERSION="$GIT_HEAD_HASH"
+fi
+
+git checkout "$KUBERMATIC_VERSION"
+
 echodate "Setting up kubermatic in kind on revision ${KUBERMATIC_VERSION}"
 . ./api/hack/ci/ci-setup-kubermatic-in-kind.sh --kubermatic-version "$KUBERMATIC_VERSION"
 echodate "Done setting up kubermatic in kind"
@@ -21,31 +29,31 @@ fi
 echodate "Running conformance tests"
 ./api/hack/ci/ci-run-conformance-tester.sh
 
-# No upgradetest, just exit
-if [[ -z ${UPGRADE_TEST_BASE_HASH:-} ]]; then
-  echodate "Success!"
-  exit 0
+# upgradetest
+if [[ -n ${UPGRADE_TO_VERSION:-} ]]; then
+  KUBERMATIC_VERSION="${UPGRADE_TO_VERSION}"
+  git checkout "$KUBERMATIC_VERSION"
+
+  # Kill OIDC proxy if it still runs to avoid "socket already in use" errors
+  if [[ -n "$(pidof oidc-proxy-client)" ]]; then
+    echodate "oidc-proxy-client still running, killing"
+    kill "$(pidof oidc-proxy-client)"
+    echodate "Done killing oidc-proxy-client"
+  fi
+
+  export ONLY_TEST_CREATION=false
+  export KUBERMATIC_USE_EXISTING_CLUSTER=true
+  echodate "Setting up kubermatic in kind on revision ${KUBERMATIC_VERSION} for upgradetest"
+  . ./api/hack/ci/ci-setup-kubermatic-in-kind.sh --kubermatic-version "${KUBERMATIC_VERSION}"
+  echodate "Done setting up kubermatic in kind"
+
+  echodate "Getting existing project id"
+  # Redirect stdout only, so errors are still visible in the log
+  retry 3 kubectl get project -o name > /tmp/project_id
+  export KUBERMATIC_PROJECT_ID="$(cat /tmp/project_id | cut -d '/' -f2)"
+  echodate "Using existing project with id \"${KUBERMATIC_PROJECT_ID}\""
+
+  echodate "Running conformance tests a second time"
+  ./api/hack/ci/ci-run-conformance-tester.sh
 fi
 
-# Kill OIDC proxy if it still runs to avoid "socket already in use" errors
-if [[ -n "$(pidof oidc-proxy-client)" ]]; then
-  echodate "oidc-proxy-client still running, killing"
-  kill "$(pidof oidc-proxy-client)"
-  echodate "Done killing oidc-proxy-client"
-fi
-
-export ONLY_TEST_CREATION=false
-export KUBERMATIC_VERSION="${GIT_HEAD_HASH}"
-export KUBERMATIC_USE_EXISTING_CLUSTER=true
-echodate "Setting up kubermatic in kind on revision ${KUBERMATIC_VERSION} for upgradetest"
-. ./api/hack/ci/ci-setup-kubermatic-in-kind.sh --kubermatic-version "${KUBERMATIC_VERSION}"
-echodate "Done setting up kubermatic in kind"
-
-echodate "Getting existing project id"
-# Redirect stdout only, so errors are still visible in the log
-retry 3 kubectl get project -o name > /tmp/project_id
-export KUBERMATIC_PROJECT_ID="$(cat /tmp/project_id | cut -d '/' -f2)"
-echodate "Using existing project with id \"${KUBERMATIC_PROJECT_ID}\""
-
-echodate "Running conformance tests a second time"
-./api/hack/ci/ci-run-conformance-tester.sh
