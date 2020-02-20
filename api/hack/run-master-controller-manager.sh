fb06468c6e29a4abc22adb898564fa89571c4c37
Mohamed El Sayed <m.elsayed@gmail.com>
2020-02-20 15:16:28 +0200

    Configurable pprof port for run scripts (#5120)

--- api/hack/run-master-controller-manager.sh
+++ api/hack/run-master-controller-manager.sh
@@ -8,6 +8,7 @@ make -C $(dirname $0)/.. master-controller-manager
 
 KUBERMATIC_WORKERNAME=${KUBERMATIC_WORKERNAME:-$(uname -n)}
 KUBERMATIC_DEBUG=${KUBERMATIC_DEBUG:-true}
+PPROF_PORT=${PPROF_PORT:-6600}
 
 cd $(go env GOPATH)/src/github.com/kubermatic/kubermatic/api
 ./_build/master-controller-manager \
@@ -16,6 +17,7 @@ cd $(go env GOPATH)/src/github.com/kubermatic/kubermatic/api
   -internal-address=127.0.0.1:8086 \
   -worker-name="$(tr -cd '[:alnum:]' <<< $KUBERMATIC_WORKERNAME | tr '[:upper:]' '[:lower:]')" \
   -log-debug=$KUBERMATIC_DEBUG \
+  -pprof-listen-address=":${PPROF_PORT}" \
   -log-format=Console \
 	-logtostderr \
 	-v=4 # Log-level for the Kube dependencies. Increase up to 9 to get request-level logs.
