e53a9c7e64291371190f8d2ea56ebedf48134c9c
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-02-20 15:33:27 +0100

    Use external settings object

--- api/pkg/handler/websocket/settings.go
+++ api/pkg/handler/websocket/settings.go
@@ -2,7 +2,8 @@ package websocket
 
 import (
 	"encoding/json"
-
+	api "github.com/kubermatic/kubermatic/api/pkg/api/v1"
+	"github.com/kubermatic/kubermatic/api/pkg/crd/kubermatic/v1"
 	"github.com/kubermatic/kubermatic/api/pkg/log"
 	"github.com/kubermatic/kubermatic/api/pkg/watcher"
 
@@ -16,7 +17,7 @@ func WriteSettings(providers watcher.Providers, ws *websocket.Conn) {
 		return
 	}
 
-	initialResponse, err := json.Marshal(initialSettings)
+	initialResponse, err := json.Marshal(api.GlobalSettings(initialSettings.Spec))
 	if err != nil {
 		log.Logger.Debug(err)
 		return
@@ -28,10 +29,29 @@ func WriteSettings(providers watcher.Providers, ws *websocket.Conn) {
 	}
 
 	providers.SettingsWatcher.Subscribe(func(settings interface{}) {
-		response, err := json.Marshal(settings)
-		if err != nil {
-			log.Logger.Debug(err)
-			return
+		var response []byte
+		if settings != nil {
+			var externalSettings api.GlobalSettings
+			internalSettings, ok := settings.(*v1.KubermaticSetting)
+			if ok {
+				externalSettings = api.GlobalSettings(internalSettings.Spec)
+			} else {
+				log.Logger.Debug("cannot convert settings: %v", settings)
+			}
+
+			response, err = json.Marshal(externalSettings)
+			if err != nil {
+				log.Logger.Debug(err)
+				return
+			}
+		} else {
+			// Explicitly set null response instead returning defaulted global settings structure.
+			// It allows clients to distinct null response and default or empty global settings structure.
+			response, err = json.Marshal(nil)
+			if err != nil {
+				log.Logger.Debug(err)
+				return
+			}
 		}
 
 		if err := ws.WriteMessage(websocket.TextMessage, response); err != nil {
