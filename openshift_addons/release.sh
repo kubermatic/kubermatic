ceab0d0a469a72ae91d72950cd910dcfc5a1ddb5
Youssef Azrak <youssef@loodse.com>
2020-02-25 16:20:43 +0100

    update openvpn for openshift - removing new tag

--- openshift_addons/release.sh
+++ openshift_addons/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -ex
-export TAG=v0.10-dev
+export TAG=v0.9
 
 docker build -t quay.io/kubermatic/openshift-addons:${TAG} .
 docker push quay.io/kubermatic/openshift-addons:${TAG}
