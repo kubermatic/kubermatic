f531089647f1ca46ce72d85b0c3d974066090a6e
Youssef Azrak <youssef@loodse.com>
2020-02-25 16:13:45 +0100

    update openvpn for openshift

--- openshift_addons/release.sh
+++ openshift_addons/release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -ex
-export TAG=v0.9
+export TAG=v0.10-dev
 
 docker build -t quay.io/kubermatic/openshift-addons:${TAG} .
 docker push quay.io/kubermatic/openshift-addons:${TAG}
