cc77056e1a681b5f1d29f7dae84e5df246ec4dfa
Youssef Azrak <youssef@loodse.com>
2020-02-25 17:47:46 +0100

    Update coredns for user-cluster

--- api/pkg/resources/dns/dns.go
+++ api/pkg/resources/dns/dns.go
@@ -93,7 +93,7 @@ func DeploymentCreator(data deploymentCreatorData) reconciling.NamedDeploymentCr
 				*openvpnSidecar,
 				{
 					Name:  resources.DNSResolverDeploymentName,
-					Image: data.ImageRegistry(resources.RegistryGCR) + "/google_containers/coredns:1.1.3",
+					Image: data.ImageRegistry(resources.RegistryGCR) + "/google_containers/coredns:1.6.7",
 					Args:  []string{"-conf", "/etc/coredns/Corefile"},
 					VolumeMounts: []corev1.VolumeMount{
 						{
