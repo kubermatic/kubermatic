865c50953ef4b412e527cdda92f9e1a86e7b4d44
Youssef Azrak <youssef@loodse.com>
2020-02-25 16:02:22 +0100

    update openvpn in user-cluster

--- api/pkg/resources/vpnsidecar/openvpnClient.go
+++ api/pkg/resources/vpnsidecar/openvpnClient.go
@@ -34,7 +34,7 @@ type openvpnData interface {
 func OpenVPNSidecarContainer(data openvpnData, name string) (*corev1.Container, error) {
 	return &corev1.Container{
 		Name:    name,
-		Image:   data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/openvpn:v0.5",
+		Image:   data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/openvpn:v0.6-dev",
 		Command: []string{"/usr/sbin/openvpn"},
 		Args: []string{
 			"--client",
