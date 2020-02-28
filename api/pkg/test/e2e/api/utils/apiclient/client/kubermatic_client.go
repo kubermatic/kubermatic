f4a6c48bfc0f8280f66b5730b806c17911531c02
Christoph Mewes <christoph@loodse.com>
2020-02-28 13:42:51 +0100

    remove cloud.kubermatic.io remnants

--- api/pkg/test/e2e/api/utils/apiclient/client/kubermatic_client.go
+++ api/pkg/test/e2e/api/utils/apiclient/client/kubermatic_client.go
@@ -40,7 +40,7 @@ var Default = NewHTTPClient(nil)
 const (
 	// DefaultHost is the default Host
 	// found in Meta (info) section of spec file
-	DefaultHost string = "cloud.kubermatic.io"
+	DefaultHost string = "dev.kubermatic.io"
 	// DefaultBasePath is the default BasePath
 	// found in Meta (info) section of spec file
 	DefaultBasePath string = "/"
