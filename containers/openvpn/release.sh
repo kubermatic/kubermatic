3fb467ed93bc3904d31d8961c9c038d898c0b90f
Youssef Azrak <30932864+youssefazrak@users.noreply.github.com>
2020-03-03 15:44:12 +0100

    Update openvpn container to v2.4.8 (#5144)
    * Update openvpn container to 2.4.8
    
    * Added dev tag to openvpn container
    
    * update openvpn in user-cluster
    
    * update openvpn for openshift
    
    * update openvpn for addons
    
    * update openvpn for openshift - removing new tag
    
    * openvpn package version specified in Dockerfile
    
    * openvpn versioning updated
    
    * openvpn versioning updated - Dockerfile improved

--- containers/openvpn/release.sh
+++ containers/openvpn/release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ver=v0.5
+ver=2.4.8-r1
 
 set -euox pipefail
 
-docker build --no-cache --pull -t quay.io/kubermatic/openvpn:$ver .
-docker push quay.io/kubermatic/openvpn:$ver
+docker build --build-arg OPENVPN_VERSION=${ver} --no-cache --pull -t quay.io/kubermatic/openvpn:v${ver} .
+docker push quay.io/kubermatic/openvpn:v${ver}
