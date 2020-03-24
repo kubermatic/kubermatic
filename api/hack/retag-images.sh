3b7957a4b0ddf1eb35cb04ffcd46eb1b2142dda2
Christoph Mewes <christoph@loodse.com>
2020-03-24 20:44:11 +0100

    fix retagging images for offline env (#5249)

--- api/hack/retag-images.sh
+++ api/hack/retag-images.sh
@@ -1,32 +1,39 @@
 #!/usr/bin/env bash
+
+set -euo pipefail
+
 TARGET_REGISTRY=${TARGET_REGISTRY:-127.0.0.1:5000}
 
 function retag {
-  local IMAGE=$1
+  local IMAGE="$1"
 
   ORG="$(echo ${IMAGE} | cut -d / -f2)"
   NAME="$(echo ${IMAGE} | cut -d / -f3 | cut -d : -f1)"
   TAG="$(echo ${IMAGE} | cut -d / -f3 | cut -d : -f2)"
   TARGET_IMAGE="${TARGET_REGISTRY}/${ORG}/${NAME}:${TAG}"
-  echo "Retagging ${IMAGE} as ${TARGET_IMAGE}"
 
-  if curl -Ss --fail "http://${TARGET_REGISTRY}/v2/${ORG}/${NAME}/tags/list" | jq -e ".tags | index(\"${TAG}\")" >/dev/null; then
-    echo "Skipping image ${TARGET_IMAGE} because it already exists in the target registry"
+  echo -n "Retagging ${IMAGE} => ${TARGET_IMAGE}"
+
+  if curl -s --fail "http://${TARGET_REGISTRY}/v2/${ORG}/${NAME}/tags/list" | jq -e ".tags | index(\"${TAG}\")" >/dev/null; then
+    echo " skipping, exists already"
     return
   fi
 
-  docker pull ${IMAGE}
-  docker tag ${IMAGE} ${TARGET_IMAGE}
-  docker push ${TARGET_IMAGE}
+  echo " ..."
+
+  docker pull "${IMAGE}"
+  docker tag "${IMAGE}" "${TARGET_IMAGE}"
+  docker push "${TARGET_IMAGE}"
+
+  echo "Done retagging ${IMAGE}"
 }
 
 IMAGES=$(cat /dev/stdin | grep "image: " | cut -d : -f 2,3)
-for IMAGE in ${IMAGES}
-do
+for IMAGE in ${IMAGES}; do
   # Make sure we strip all quotes
   IMAGE="${IMAGE%\'}"
   IMAGE="${IMAGE#\'}"
   IMAGE="${IMAGE%\"}"
   IMAGE="${IMAGE#\"}"
-  retag ${IMAGE}
+  retag "${IMAGE}"
 done
