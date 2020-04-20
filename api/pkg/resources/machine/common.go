1373125111ef69b42d90cc31149619cb0f52bda9
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-04-20 16:22:24 +0200

    Add properties required for RHEL (#5315)
    * Add properties required for RHEL
    
    * Make properties machine-deployment-specific
    
    * Generate code & swagger spec

--- api/pkg/resources/machine/common.go
+++ api/pkg/resources/machine/common.go
@@ -115,6 +115,7 @@ func getAzureProviderSpec(c *kubermaticv1.Cluster, nodeSpec apiv1.NodeSpec, dc *
 		AvailabilitySet:   providerconfig.ConfigVarString{Value: c.Spec.Cloud.Azure.AvailabilitySet},
 		SecurityGroupName: providerconfig.ConfigVarString{Value: c.Spec.Cloud.Azure.SecurityGroup},
 		Zones:             nodeSpec.Cloud.Azure.Zones,
+		ImageID:           providerconfig.ConfigVarString{Value: nodeSpec.Cloud.Azure.ImageID},
 
 		// https://github.com/kubermatic/kubermatic/issues/5013#issuecomment-580357280
 		AssignPublicIP: providerconfig.ConfigVarBool{Value: nodeSpec.Cloud.Azure.AssignPublicIP},
@@ -306,6 +307,7 @@ func getGCPProviderSpec(c *kubermaticv1.Cluster, nodeSpec apiv1.NodeSpec, dc *ku
 		Network:               providerconfig.ConfigVarString{Value: c.Spec.Cloud.GCP.Network},
 		Subnetwork:            providerconfig.ConfigVarString{Value: c.Spec.Cloud.GCP.Subnetwork},
 		AssignPublicIPAddress: &providerconfig.ConfigVarBool{Value: true},
+		CustomImage:           providerconfig.ConfigVarString{Value: nodeSpec.Cloud.GCP.CustomImage},
 	}
 
 	tags := sets.NewString(nodeSpec.Cloud.GCP.Tags...)
