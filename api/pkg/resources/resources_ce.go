a5b1a049799f4cb1658d2ede9020bb8afb8b46bb
Christoph Mewes <christoph@loodse.com>
2020-05-29 16:25:08 +0200

    use kubermatic/api-ce repository for Community Edition (#5419)

--- api/pkg/resources/resources_ce.go
+++ api/pkg/resources/resources_ce.go
@@ -4,4 +4,19 @@ package resources
 
 const (
 	KubermaticEdition = "Community Edition"
+
+	// DefaultKubermaticImage defines the default Docker repository containing the Kubermatic API image.
+	DefaultKubermaticImage = "quay.io/kubermatic/api-ce"
+
+	// DefaultDNATControllerImage defines the default Docker repository containing the DNAT controller image.
+	DefaultDNATControllerImage = "quay.io/kubermatic/kubeletdnat-controller"
+
+	// DefaultDashboardAddonImage defines the default Docker repository containing the dashboard image.
+	DefaultDashboardImage = "quay.io/kubermatic/dashboard-ce"
+
+	// DefaultKubernetesAddonImage defines the default Docker repository containing the Kubernetes addons.
+	DefaultKubernetesAddonImage = "quay.io/kubermatic/addons"
+
+	// DefaultOpenshiftAddonImage defines the default Docker repository containing the Openshift addons.
+	DefaultOpenshiftAddonImage = "quay.io/kubermatic/openshift-addons"
 )
