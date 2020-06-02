560817ac72493969f7154416402b2e736bb57818
Christoph Mewes <christoph@loodse.com>
2020-06-02 19:00:44 +0200

    remove unused oidc-proxy-client and other deprecated stuff

--- api/hack/ci/ci-kind-e2e.sh
+++ api/hack/ci/ci-kind-e2e.sh
@@ -9,13 +9,13 @@ export KUBERMATIC_NO_WORKER_NAME=true
 export GIT_HEAD_HASH="$(git rev-parse HEAD)"
 
 export KUBERMATIC_VERSION="${UPGRADE_TEST_BASE_HASH:-$GIT_HEAD_HASH}"
-echodate "Setting up kubermatic in kind on revision ${KUBERMATIC_VERSION}"
+echodate "Setting up Kubermatic in kind on revision ${KUBERMATIC_VERSION}"
 
 beforeKubermaticSetup=$(nowms)
 source ./api/hack/ci/ci-setup-kubermatic-in-kind.sh
 pushElapsed kind_kubermatic_setup_duration_milliseconds $beforeKubermaticSetup
 
-echodate "Done setting up kubermatic in kind"
+echodate "Done setting up Kubermatic in kind"
 
 # We can safely assume that the base version got tested already
 if [[ -n ${UPGRADE_TEST_BASE_HASH:-} ]]; then
@@ -31,19 +31,12 @@ if [[ -z ${UPGRADE_TEST_BASE_HASH:-} ]]; then
   exit 0
 fi
 
-# Kill OIDC proxy if it still runs to avoid "socket already in use" errors
-if [[ -n "$(pidof oidc-proxy-client)" ]]; then
-  echodate "oidc-proxy-client still running, killing"
-  kill "$(pidof oidc-proxy-client)"
-  echodate "Done killing oidc-proxy-client"
-fi
-
 export ONLY_TEST_CREATION=false
 export KUBERMATIC_VERSION="${GIT_HEAD_HASH}"
 export KUBERMATIC_USE_EXISTING_CLUSTER=true
-echodate "Setting up kubermatic in kind on revision ${KUBERMATIC_VERSION} for upgradetest"
+echodate "Setting up Kubermatic in kind on revision ${KUBERMATIC_VERSION} for upgradetest"
 source ./api/hack/ci/ci-setup-kubermatic-in-kind.sh
-echodate "Done setting up kubermatic in kind"
+echodate "Done setting up Kubermatic in kind"
 
 echodate "Getting existing project id"
 # Redirect stdout only, so errors are still visible in the log
