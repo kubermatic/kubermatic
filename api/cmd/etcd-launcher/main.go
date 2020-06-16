b40178cdd7a38ec0b7da065fc87ae2c92574234d
moelsayed <m.elsayed@gmail.com>
2020-06-16 13:30:52 +0200

    add etcd-launcher

---
+++ api/cmd/etcd-launcher/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+const (
+	initialStateEnvName      = "INITIAL_STATE"
+	initialClusterEnvName    = "INITIAL_CLUSTER"
+	defaultClusterSize       = 3
+	defaultEtcdctlAPIVersion = "3"
+)
+
+type envConfig struct {
+	namespace             string
+	clusterSize           int
+	podName               string
+	podIP                 string
+	etcdctlAPIVersion     string
+	dataDir               string
+	token                 string
+	enableCorruptionCheck bool
+}
+
+func main() {
+	// normal workflow, we don't do migrations anymore
+	config, err := getConfigFromEnv()
+	if err != nil {
+		log.Fatalf("failed to get launcher configuration: %v", err)
+	}
+
+	// not required, will leave it for now.
+	os.Setenv(initialStateEnvName, "new")
+	os.Setenv(initialClusterEnvName, initialMemberList(config.clusterSize, config.namespace))
+
+	log.Print("initializing etcd..")
+	log.Printf("initial-state: %s", os.Getenv(initialStateEnvName))
+	log.Printf("initial-cluster: %s", os.Getenv(initialClusterEnvName))
+
+	err = syscall.Exec(
+		"/usr/local/bin/etcd",
+		etcdCmd(config),
+		os.Environ())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func initialMemberList(n int, namespace string) string {
+	members := []string{}
+	for i := 0; i < n; i++ {
+		members = append(members, fmt.Sprintf("etcd-%d=http://etcd-%d.etcd.%s.svc.cluster.local:2380", i, i, namespace))
+	}
+	return strings.Join(members, ",")
+}
+
+func getConfigFromEnv() (*envConfig, error) {
+	config := &envConfig{}
+	if n := os.Getenv("NAMESPACE"); n != "" {
+		config.namespace = n
+	} else {
+		return nil, errors.New("NAMESPACE is not set")
+	}
+
+	var err error
+	config.clusterSize = defaultClusterSize
+	if s := os.Getenv("ECTD_CLUSTER_SIZE"); s != "" {
+		if config.clusterSize, err = strconv.Atoi(s); err != nil {
+			return nil, fmt.Errorf("failed to read ECTD_CLUSTER_SIZE: %v", err)
+		}
+		if config.clusterSize > defaultClusterSize {
+			return nil, fmt.Errorf("ECTD_CLUSTER_SIZE is smaller then %d", defaultClusterSize)
+		}
+	}
+
+	if p := os.Getenv("POD_NAME"); p != "" {
+		config.podName = p
+	} else {
+		return nil, errors.New("POD_NAME is not set")
+	}
+
+	if i := os.Getenv("POD_IP"); i != "" {
+		config.podIP = i
+	} else {
+		return nil, errors.New("POD_IP is not set")
+	}
+	config.etcdctlAPIVersion = defaultEtcdctlAPIVersion
+	if v := os.Getenv("ETCDCTL_API"); v == "2" || v == "3" {
+		config.etcdctlAPIVersion = v
+	}
+	if t := os.Getenv("TOKEN"); t != "" {
+		config.token = t
+	} else {
+		return nil, errors.New("TOEKN is not set")
+	}
+
+	if c := os.Getenv("ENABLE_CORRUPTION_CHECK"); strings.ToLower(c) == "true" {
+		config.enableCorruptionCheck = true
+	}
+	config.dataDir = fmt.Sprintf("/var/run/etcd/pod_%s/", config.podName)
+
+	return config, nil
+}
+
+func etcdCmd(config *envConfig) []string {
+	cmd := []string{
+		"etcd",
+		fmt.Sprintf("--name=%s", config.podName),
+		fmt.Sprintf("--data-dir=%s", config.dataDir),
+		fmt.Sprintf("--initial-cluster=%s", initialMemberList(config.clusterSize, config.namespace)),
+		fmt.Sprintf("--initial-cluster-token=%s", config.token),
+		"--initial-cluster-state=new",
+		fmt.Sprintf("--advertise-client-urls=https://%s.etcd.%s.svc.cluster.local:2379,https://%s:2379", config.podName, config.namespace, config.podIP),
+		fmt.Sprintf("--listen-client-urls=https://%s:2379,https://127.0.0.1:2379", config.podIP),
+		fmt.Sprintf("--listen-peer-urls=http://%s:2380", config.podIP),
+		fmt.Sprintf("--initial-advertise-peer-urls=http://%s.etcd.%s.svc.cluster.local:2380", config.podName, config.namespace),
+		"--trusted-ca-file=/etc/etcd/pki/ca/ca.crt",
+		"--client-cert-auth",
+		"--cert-file=/etc/etcd/pki/tls/etcd-tls.crt",
+		"--key-file=/etc/etcd/pki/tls/etcd-tls.key",
+		"--auto-compaction-retention=8",
+	}
+
+	if config.enableCorruptionCheck {
+		cmd = append(cmd, []string{
+			"--experimental-initial-corrupt-check=true",
+			"--experimental-corrupt-check-time=10m",
+		}...)
+	}
+
+	return cmd
+}
