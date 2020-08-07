cddc4983e10ea8c871cdf37fd0bb5e2b124f7837
Olaf Klischat <olaf.klischat@gmail.com>
2020-08-07 05:01:54 +0200

    fix codegen
    Signed-off-by: Olaf Klischat <olaf.klischat@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -47,7 +47,7 @@ echodate "Generating operator:v1alpha1"
   --go-header-file /tmp/headerfile
 
 # Temporary fixes due to: https://github.com/kubernetes/kubernetes/issues/71655
-GENERIC_FILE="v2/pkg/crd/client/informers/externalversions/generic.go"
+GENERIC_FILE="pkg/crd/client/informers/externalversions/generic.go"
 sed -i s/usersshkeys/usersshkeies/g ${GENERIC_FILE}
 
 echodate "Generating deepcopy funcs for other packages"
@@ -55,11 +55,6 @@ go run k8s.io/code-generator/cmd/deepcopy-gen \
   --input-dirs k8c.io/kubermatic/v2/pkg/semver \
   -O zz_generated.deepcopy \
   --go-header-file /tmp/headerfile
-
-# move files into their correct location, generate-groups.sh does not handle
-# non-v1 module names very well
-cp -r v2/* .
-rm -rf v2/
 
 rm /tmp/headerfile
 
