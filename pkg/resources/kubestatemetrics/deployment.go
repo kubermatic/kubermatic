3346b84b1e5ee642eeff3386a2dcb74a19b48caa
Christoph Mewes <christoph@loodse.com>
2020-08-27 01:36:59 +0200

    update kube-state-metrics to 1.9.7 (#5790)

--- pkg/resources/kubestatemetrics/deployment.go
+++ pkg/resources/kubestatemetrics/deployment.go
@@ -48,7 +48,7 @@ var (
 
 const (
 	name    = "kube-state-metrics"
-	version = "v1.9.4"
+	version = "v1.9.7"
 )
 
 // DeploymentCreator returns the function to create and update the kube-state-metrics deployment
