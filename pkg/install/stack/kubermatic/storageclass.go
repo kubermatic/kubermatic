07f38b634de7a3c682e3ceba35eab049f54a1dda
Christoph Mewes <christoph@loodse.com>
2020-09-10 15:16:39 +0200

    finish basic StorageClass creation support

--- pkg/install/stack/kubermatic/storageclass.go
+++ pkg/install/stack/kubermatic/storageclass.go
@@ -17,55 +17,114 @@ limitations under the License.
 package kubermatic
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
 	storagev1 "k8s.io/api/storage/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/utils/pointer"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func newEKSStorageClass() storagev1.StorageClass {
-	s := storagev1.StorageClass{
-		Parameters: map[string]string{},
-	}
-	s.Provisioner = "kubernetes.io/aws-ebs"
-	s.Parameters["type"] = "gp2"
+type storageClassFactory func(context.Context, *logrus.Entry, ctrlruntimeclient.Client, string) (storagev1.StorageClass, error)
 
-	return s
+var (
+	storageClassFactories = map[string]storageClassFactory{
+		"copy-default": func(ctx context.Context, logger *logrus.Entry, kubeClient ctrlruntimeclient.Client, name string) (storagev1.StorageClass, error) {
+			s := &storagev1.StorageClass{
+				Parameters: map[string]string{},
+			}
+			s.Name = name
+
+			classes := storagev1.StorageClassList{}
+			if err := kubeClient.List(ctx, &classes); err != nil {
+				return *s, fmt.Errorf("cannot list existing StorageClasses: %v", err)
+			}
+
+			for _, class := range classes.Items {
+				if isDefaultStorageClass(class) {
+					logger.Infof("Duplicating existing default class %q…", class.Name)
+
+					s = class.DeepCopy()
+					s.Annotations = map[string]string{}
+					s.Labels = map[string]string{}
+					s.ResourceVersion = ""
+					s.Name = name
+
+					return *s, nil
+				}
+			}
+
+			return *s, errors.New("cannot duplicate existing default StorageClass, because none of the classes are marked as the default")
+		},
+		"gce": func(_ context.Context, _ *logrus.Entry, _ ctrlruntimeclient.Client, name string) (storagev1.StorageClass, error) {
+			s := storagev1.StorageClass{
+				Parameters: map[string]string{},
+			}
+			s.Name = name
+			s.Provisioner = "kubernetes.io/gce-pd"
+			s.Parameters["type"] = "pd-ssd"
+
+			return s, nil
+		},
+		"aws": func(_ context.Context, _ *logrus.Entry, _ ctrlruntimeclient.Client, name string) (storagev1.StorageClass, error) {
+			s := storagev1.StorageClass{
+				Parameters: map[string]string{},
+			}
+			s.Name = name
+			s.Provisioner = "kubernetes.io/aws-ebs"
+			s.Parameters["type"] = "gp2"
+
+			return s, nil
+		},
+		"azure": func(_ context.Context, _ *logrus.Entry, _ ctrlruntimeclient.Client, name string) (storagev1.StorageClass, error) {
+			s := storagev1.StorageClass{
+				Parameters: map[string]string{},
+			}
+			s.Name = name
+			s.Provisioner = "kubernetes.io/azure-disk"
+			s.Parameters["storageaccounttype"] = "Standard_LRS"
+			s.Parameters["kind"] = "managed"
+
+			return s, nil
+		},
+		"hetzner": func(_ context.Context, _ *logrus.Entry, _ ctrlruntimeclient.Client, name string) (storagev1.StorageClass, error) {
+			s := storagev1.StorageClass{
+				Parameters: map[string]string{},
+			}
+			s.Name = name
+			s.Provisioner = "csi.hetzner.cloud"
+
+			return s, nil
+		},
+		"digitalocean": func(_ context.Context, _ *logrus.Entry, _ ctrlruntimeclient.Client, name string) (storagev1.StorageClass, error) {
+			s := storagev1.StorageClass{
+				Parameters: map[string]string{},
+			}
+			s.Name = name
+
+			// see https://github.com/digitalocean/csi-digitalocean/blob/master/deploy/kubernetes/releases/csi-digitalocean-v1.3.0.yaml
+			s.Provisioner = "dobs.csi.digitalocean.com"
+			s.AllowVolumeExpansion = pointer.BoolPtr(true)
+
+			return s, nil
+		},
+	}
+)
+
+func SupportedStorageClassProviders() sets.String {
+	return sets.StringKeySet(storageClassFactories)
 }
 
-func newGKEStorageClass() storagev1.StorageClass {
-	s := storagev1.StorageClass{
-		Parameters: map[string]string{},
+func isDefaultStorageClass(sc storagev1.StorageClass) bool {
+	if sc.Annotations["storageclass.kubernetes.io/is-default-class"] == "true" {
+		return true
 	}
-	s.Provisioner = "kubernetes.io/gce-pd"
-	s.Parameters["type"] = "pd-ssd"
-
-	return s
-}
-
-func newAKSStorageClass() storagev1.StorageClass {
-	s := storagev1.StorageClass{
-		Parameters: map[string]string{},
-	}
-	s.Provisioner = "kubernetes.io/azure-disk"
-	s.Parameters["storageaccounttype"] = "Standard_LRS"
-	s.Parameters["kind"] = "managed"
-
-	return s
-}
-
-func storageClassForProvider(name string, p string) *storagev1.StorageClass {
-	var sc storagev1.StorageClass
-
-	switch p {
-	case "aks":
-		sc = newAKSStorageClass()
-	case "eks":
-		sc = newEKSStorageClass()
-	case "gke":
-		sc = newGKEStorageClass()
-	default:
-		return nil
+	if sc.Annotations["storageclass.beta.kubernetes.io/is-default-class"] == "true" {
+		return true
 	}
 
-	sc.Name = name
-
-	return &sc
+	return false
 }
