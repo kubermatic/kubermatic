f8f65fad262be57061d4e04a0e8e9fd469caf5df
Christoph Mewes <christoph@loodse.com>
2020-09-14 10:53:12 +0200

    Update Minio to RELEASE.2020-09-10T22-02-45 / bump utilities (#5854)
    * Update Minio to 2020-09-10T22-02-45 / bump utilities
    
    * bump chart versions
    
    * rebuild Prometheus rules
    
    * 3.3.2 had a bug with indenting arrays

--- pkg/controller/master-controller-manager/seed-proxy/resources.go
+++ pkg/controller/master-controller-manager/seed-proxy/resources.go
@@ -228,7 +228,7 @@ func masterDeploymentCreator(seed *kubermaticv1.Seed, secret *corev1.Secret) rec
 			d.Spec.Template.Spec.Containers = []corev1.Container{
 				{
 					Name:    "proxy",
-					Image:   "quay.io/kubermatic/util:1.3.5",
+					Image:   "quay.io/kubermatic/util:1.4.1",
 					Command: []string{"/bin/bash"},
 					Args:    []string{"-c", strings.TrimSpace(proxyScript)},
 					Env: []corev1.EnvVar{
