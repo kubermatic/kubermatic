e88e0d774bb1af87fa9980f00b41c3428a324361
Lovro Sviben <46844730+lsviben@users.noreply.github.com>
2020-09-28 10:56:59 +0200

    Constraint template create endpoint (#5917)
    * Constraint Template create endpoint
    
    * generated swagger and api client
    
    * small fix
    
    * lint fix
    
    * added validation
    
    * generated swagger and api clients
    
    * changed validation message
    
    * validation test fix

--- pkg/handler/v2/constraint_template/constraint_template_test.go
+++ pkg/handler/v2/constraint_template/constraint_template_test.go
@@ -17,6 +17,8 @@ limitations under the License.
 package constrainttemplate_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -123,6 +125,69 @@ func TestGetConstraintTemplates(t *testing.T) {
 			req := httptest.NewRequest("GET", fmt.Sprintf("/api/v2/constrainttemplates/%s", tc.CTName), strings.NewReader(""))
 			res := httptest.NewRecorder()
 			ep, err := test.CreateTestEndpoint(*tc.ExistingAPIUser, nil, tc.ExistingObjects, nil, nil, hack.NewTestRouting)
+			if err != nil {
+				t.Fatalf("failed to create test endpoint due to %v", err)
+			}
+
+			ep.ServeHTTP(res, req)
+
+			if res.Code != tc.HTTPStatus {
+				t.Fatalf("Expected HTTP status code %d, got %d: %s", tc.HTTPStatus, res.Code, res.Body.String())
+			}
+
+			test.CompareWithResult(t, res, tc.ExpectedResponse)
+		})
+	}
+}
+
+func TestCreateConstraintTemplates(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		Name             string
+		CTtoCreate       apiv2.ConstraintTemplate
+		ExpectedResponse string
+		HTTPStatus       int
+		ExistingAPIUser  *apiv1.User
+	}{
+		{
+			Name:             "scenario 1: admin can create constraint template",
+			CTtoCreate:       test.GenDefaultConstraintTemplate("labelconstraint"),
+			ExpectedResponse: `{"name":"labelconstraint","spec":{"crd":{"spec":{"names":{"kind":"labelconstraint","shortNames":["lc"]}}},"targets":[{"target":"admission.k8s.gatekeeper.sh","rego":"\n\t\tpackage k8srequiredlabels\n\n        deny[{\"msg\": msg, \"details\": {\"missing_labels\": missing}}] {\n          provided := {label | input.review.object.metadata.labels[label]}\n          required := {label | label := input.parameters.labels[_]}\n          missing := required - provided\n          count(missing) \u003e 0\n          msg := sprintf(\"you must provide labels: %v\", [missing])\n        }"}]},"status":{}}`,
+			HTTPStatus:       http.StatusOK,
+			ExistingAPIUser:  test.GenDefaultAdminAPIUser(),
+		},
+		{
+			Name:             "scenario 2: non-admin can not create constraint template",
+			CTtoCreate:       test.GenDefaultConstraintTemplate("labelconstraint"),
+			ExpectedResponse: `{"error":{"code":403,"message":"forbidden: \"bob@acme.com\" doesn't have admin rights"}}`,
+			HTTPStatus:       http.StatusForbidden,
+			ExistingAPIUser:  test.GenDefaultAPIUser(),
+		},
+		{
+			Name:             "scenario 3: admin cannot create invalid constraint template",
+			CTtoCreate:       test.GenDefaultConstraintTemplate("invalid"),
+			ExpectedResponse: `{"error":{"code":400,"message":"template's name invalid is not equal to the lowercase of CRD's Kind: labelconstraint"}}`,
+			HTTPStatus:       http.StatusBadRequest,
+			ExistingAPIUser:  test.GenDefaultAPIUser(),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.Name, func(t *testing.T) {
+			var reqBody struct {
+				Name string                         `json:"name"`
+				Spec v1beta1.ConstraintTemplateSpec `json:"spec"`
+			}
+			reqBody.Spec = tc.CTtoCreate.Spec
+			reqBody.Name = tc.CTtoCreate.Name
+
+			body, err := json.Marshal(reqBody)
+			if err != nil {
+				t.Fatalf("error marshalling body into json: %v", err)
+			}
+			req := httptest.NewRequest("POST", "/api/v2/constrainttemplates", bytes.NewBuffer(body))
+			res := httptest.NewRecorder()
+			ep, err := test.CreateTestEndpoint(*tc.ExistingAPIUser, nil, []runtime.Object{test.APIUserToKubermaticUser(*tc.ExistingAPIUser)}, nil, nil, hack.NewTestRouting)
 			if err != nil {
 				t.Fatalf("failed to create test endpoint due to %v", err)
 			}
