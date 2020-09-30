c102912f58f11f6f9a52d579ee1a68ad8d7ca02a
moelsayed <m.elsayed@gmail.com>
2020-09-30 19:25:59 +0200

    update fixtures

--- pkg/resources/etcd/testdata/etcd-command-with-corruption-flags.golden.sh
+++ pkg/resources/etcd/testdata/etcd-command-with-corruption-flags.golden.sh
@@ -14,10 +14,8 @@
 
 export MASTER_ENDPOINT="https://etcd-0.etcd.cluster-lg69pmx8wf.svc.cluster.local:2379"
 
-
 export INITIAL_STATE="new"
 export INITIAL_CLUSTER="etcd-0=http://etcd-0.etcd.cluster-lg69pmx8wf.svc.cluster.local:2380,etcd-1=http://etcd-1.etcd.cluster-lg69pmx8wf.svc.cluster.local:2380,etcd-2=http://etcd-2.etcd.cluster-lg69pmx8wf.svc.cluster.local:2380"
-
 
 echo "initial-state: ${INITIAL_STATE}"
 echo "initial-cluster: ${INITIAL_CLUSTER}"
@@ -31,6 +29,7 @@ exec /usr/local/bin/etcd \
     --advertise-client-urls "https://${POD_NAME}.etcd.cluster-lg69pmx8wf.svc.cluster.local:2379,https://${POD_IP}:2379" \
     --listen-client-urls "https://${POD_IP}:2379,https://127.0.0.1:2379" \
     --listen-peer-urls "http://${POD_IP}:2380" \
+    --listen-metrics-urls "http://${POD_IP}:2378,http://127.0.0.1:2378" \
     --initial-advertise-peer-urls "http://${POD_NAME}.etcd.cluster-lg69pmx8wf.svc.cluster.local:2380" \
     --trusted-ca-file /etc/etcd/pki/ca/ca.crt \
     --client-cert-auth \
