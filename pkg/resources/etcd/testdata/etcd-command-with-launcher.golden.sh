1aba388345070d45cf385ad3df703255e28a5421
moelsayed <m.elsayed@gmail.com>
2020-10-01 10:57:52 +0200

    fix tests

--- pkg/resources/etcd/testdata/etcd-command-with-launcher.golden.sh
+++ pkg/resources/etcd/testdata/etcd-command-with-launcher.golden.sh
@@ -1,1 +1,1 @@
-$(NAMESPACE)
+/opt/bin/etcd-launcher -namespace $(NAMESPACE) -etcd-cluster-size $(ETCD_CLUSTER_SIZE) -pod-name $(POD_NAME) -pod-ip $(POD_IP) -api-version $(ETCDCTL_API) -token $(TOKEN)
