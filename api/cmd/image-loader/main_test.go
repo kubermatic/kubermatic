b5b855d008134177e185bd08dca2ff6468e39339
Christoph Mewes <christoph@loodse.com>
2020-10-23 15:20:45 +0200

    [release/v2.14] Support KubermaticConfiguration in image-loader (#6071)

--- api/cmd/image-loader/main_test.go
+++ api/cmd/image-loader/main_test.go
@@ -20,36 +20,38 @@ import (
 	"context"
 	"testing"
 
+	"github.com/kubermatic/kubermatic/api/pkg/controller/operator/common"
+	operatorv1alpha1 "github.com/kubermatic/kubermatic/api/pkg/crd/operator/v1alpha1"
 	kubermaticlog "github.com/kubermatic/kubermatic/api/pkg/log"
 	"github.com/kubermatic/kubermatic/api/pkg/resources"
-	"github.com/kubermatic/kubermatic/api/pkg/version"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func TestRetagImageForAllVersions(t *testing.T) {
 	log := kubermaticlog.New(true, kubermaticlog.FormatConsole).Sugar()
-	masterResources := "../../../config/kubermatic/static/master/versions.yaml"
-	addonPath := "../../../addons"
 
-	versions, err := version.LoadVersions(masterResources)
+	config, err := common.DefaultConfiguration(&operatorv1alpha1.KubermaticConfiguration{}, log)
 	if err != nil {
-		t.Errorf("Error loading versions: %v", err)
+		t.Errorf("failed to determine versions: %v", err)
 	}
+
+	versions := getVersionsFromKubermaticConfiguration(config)
+	addonPath := "../../../addons"
 
 	// Cannot be set during go-test
 	resources.KUBERMATICCOMMIT = "latest"
 
 	imageSet := sets.NewString()
 	for _, v := range versions {
-		images, err := getImagesForVersion(log.Desugar(), v, addonPath)
+		images, err := getImagesForVersion(log, v, addonPath)
 		if err != nil {
 			t.Errorf("Error calling getImagesForVersion: %v", err)
 		}
 		imageSet.Insert(images...)
 	}
 
-	if err := processImages(context.Background(), log.Desugar(), true, imageSet.List(), "test-registry:5000"); err != nil {
+	if err := processImages(context.Background(), log, true, imageSet.List(), "test-registry:5000"); err != nil {
 		t.Errorf("Error calling processImages: %v", err)
 	}
 }
