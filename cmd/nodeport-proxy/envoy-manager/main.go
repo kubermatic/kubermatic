f9a16eea3a0acd3c2e17f6dfecb152caba9d5206
Iacopo Rozzo <iacopo@loodse.com>
2020-11-14 21:03:09 +0100

    Refactor nodeport proxy (#6154)
    * Update dependencies
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * No need of explicitly keeping the latest applied snapshot
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Move controller to pkg
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Refactor controller
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Use endpoints instead of pods to create lb endpoints
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Avoid process unnecessary events
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Use IDHash to match xds configuration
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Fix role for nodeport proxy sa
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Fix typo
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Fix lint violation
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Add some unit tests coverage
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Increase nodeport-proxy chart version
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    * Fix lint violation
    
    Signed-off-by: Iacopo Rozzo <iacopo@kubermatic.com>
    
    Co-authored-by: Iacopo Rozzo <iacopo@kubermatic.com>

--- cmd/nodeport-proxy/envoy-manager/main.go
+++ cmd/nodeport-proxy/envoy-manager/main.go
@@ -21,7 +21,6 @@ import (
 	"flag"
 	"fmt"
 	"net"
-	"time"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -31,47 +30,29 @@ import (
 	endpointservice "github.com/envoyproxy/go-control-plane/envoy/service/endpoint/v3"
 	listenerservice "github.com/envoyproxy/go-control-plane/envoy/service/listener/v3"
 	routeservice "github.com/envoyproxy/go-control-plane/envoy/service/route/v3"
-	envoycachev3 "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
 	xdsv3 "github.com/envoyproxy/go-control-plane/pkg/server/v3"
 
-	controllerutil "k8c.io/kubermatic/v2/pkg/controller/util"
+	"k8c.io/kubermatic/v2/pkg/controller/nodeport-proxy/envoymanager"
 	kubermaticlog "k8c.io/kubermatic/v2/pkg/log"
 	"k8c.io/kubermatic/v2/pkg/util/cli"
 
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	ctrlruntimeconfig "sigs.k8s.io/controller-runtime/pkg/client/config"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
-	"sigs.k8s.io/controller-runtime/pkg/source"
-)
-
-var (
-	namespace           string
-	listenAddress       string
-	envoyNodeName       string
-	exposeAnnotationKey string
-
-	envoyStatsPort int
-	envoyAdminPort int
-)
-
-const (
-	defaultExposeAnnotationKey = "nodeport-proxy.k8s.io/expose"
-	clusterConnectTimeout      = 1 * time.Second
 )
 
 func main() {
 	logOpts := kubermaticlog.NewDefaultOptions()
 	logOpts.AddFlags(flag.CommandLine)
 
+	var listenAddress string
+	ctrlOpts := envoymanager.Options{}
 	flag.StringVar(&listenAddress, "listen-address", ":8001", "Address to serve on")
-	flag.StringVar(&envoyNodeName, "envoy-node-name", "kube", "Name of the envoy nodes to apply the config to via xds")
-	flag.IntVar(&envoyAdminPort, "envoy-admin-port", 9001, "Envoys admin port")
-	flag.IntVar(&envoyStatsPort, "envoy-stats-port", 8002, "Limited port which should be opened on envoy to expose metrics and the health check. Endpoints are: /healthz & /stats")
-	flag.StringVar(&namespace, "namespace", "", "The namespace we should use for pods and services. Leave empty for all namespaces.")
-	flag.StringVar(&exposeAnnotationKey, "expose-annotation-key", defaultExposeAnnotationKey, "The annotation key used to determine if a service should be exposed")
+	flag.StringVar(&ctrlOpts.EnvoyNodeName, "envoy-node-name", "kube", "Name of the envoy nodes to apply the config to via xds.")
+	flag.IntVar(&ctrlOpts.EnvoyAdminPort, "envoy-admin-port", 9001, "Envoys admin port")
+	flag.IntVar(&ctrlOpts.EnvoyStatsPort, "envoy-stats-port", 8002, "Limited port which should be opened on envoy to expose metrics and the health check. Endpoints are: /healthz & /stats")
+	flag.StringVar(&ctrlOpts.Namespace, "namespace", "", "The namespace we should use for pods and services. Leave empty for all namespaces.")
+	flag.StringVar(&ctrlOpts.ExposeAnnotationKey, "expose-annotation-key", envoymanager.DefaultExposeAnnotationKey, "The annotation key used to determine if a service should be exposed")
 	flag.Parse()
 
 	// setup signal handler
@@ -94,7 +75,7 @@ func main() {
 	cli.Hello(log, "Envoy-Manager", logOpts.Debug)
 	log.Infow("Starting the server...", "address", listenAddress)
 
-	snapshotCache := envoycachev3.NewSnapshotCache(true, hasher{}, log.With("component", "envoycache"))
+	snapshotCache := envoymanager.NewSnapshotCache(log.With("component", "envoycache"))
 	srv := xdsv3.NewServer(ctx, snapshotCache, nil)
 	grpcServer := grpc.NewServer()
 
@@ -120,29 +101,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	mgr, err := manager.New(config, manager.Options{Namespace: namespace})
+	mgr, err := manager.New(config, manager.Options{Namespace: ctrlOpts.Namespace})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r := &reconciler{
-		ctx:                 ctx,
-		Client:              mgr.GetClient(),
-		namespace:           namespace,
-		envoySnapshotCache:  snapshotCache,
-		log:                 log,
-		lastAppliedSnapshot: envoycachev3.NewSnapshot("v0.0.0", nil, nil, nil, nil, nil, nil),
+	r := &envoymanager.Reconciler{
+		Ctx:                ctx,
+		Client:             mgr.GetClient(),
+		Options:            ctrlOpts,
+		Log:                log,
+		EnvoySnapshotCache: snapshotCache,
 	}
-	ctrl, err := controller.New("envoy-manager", mgr,
-		controller.Options{Reconciler: r, MaxConcurrentReconciles: 1})
-	if err != nil {
-		log.Fatalw("failed to construct mgr", zap.Error(err))
-	}
-
-	for _, t := range []runtime.Object{&corev1.Pod{}, &corev1.Service{}} {
-		if err := ctrl.Watch(&source.Kind{Type: t}, controllerutil.EnqueueConst("")); err != nil {
-			log.Fatalw("failed to watch", "kind", t, zap.Error(err))
-		}
+	if err := r.SetupWithManager(mgr); err != nil {
+		log.Fatal(err)
 	}
 
 	if err := mgr.Start(stopCh); err != nil {
---
+++ pkg/controller/nodeport-proxy/envoymanager/controller.go
@@ -0,0 +1,246 @@
+/*
+Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package envoymanager
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/Masterminds/semver"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+
+	envoycachev3 "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	envoyresourcev3 "github.com/envoyproxy/go-control-plane/pkg/resource/v3"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	DefaultExposeAnnotationKey = "nodeport-proxy.k8s.io/expose"
+	clusterConnectTimeout      = 1 * time.Second
+)
+
+type Options struct {
+	// Namespace where Services and Endpoints are watched.
+	Namespace string
+	// NodeName is the name used to retrieve the xds configuration.
+	// It is supposed to match the id (AKA service-node) of the Envoy instance
+	// being controlled.
+	EnvoyNodeName string
+	// ExposeAnnotationKey is the annotation used to expose services.
+	ExposeAnnotationKey string
+
+	// EnvoyAdminPort port used to exposed Envoy admin interface.
+	EnvoyAdminPort int
+	// EnvoyStatsPort is the port used to expose Envoy stats.
+	EnvoyStatsPort int
+}
+
+type Reconciler struct {
+	ctrlruntimeclient.Client
+	Options
+
+	Ctx context.Context
+	Log *zap.SugaredLogger
+
+	EnvoySnapshotCache envoycachev3.SnapshotCache
+}
+
+func (r *Reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	r.Log.Debugw("got reconcile request", "request", req)
+	err := r.sync()
+	if err != nil {
+		r.Log.Errorf("failed to reconcile", zap.Error(err))
+	}
+	return ctrl.Result{}, err
+}
+
+func (r *Reconciler) sync() error {
+	services := &corev1.ServiceList{}
+	if err := r.List(r.Ctx, services,
+		ctrlruntimeclient.InNamespace(r.Namespace),
+		client.MatchingFields{r.ExposeAnnotationKey: "true"},
+	); err != nil {
+		return errors.Wrap(err, "failed to list service's")
+	}
+
+	listeners, clusters := r.makeInitialResources()
+
+	for _, service := range services.Items {
+		serviceKey := ServiceKey(&service)
+		serviceLog := r.Log.With("service", serviceKey)
+
+		// This is redundant as we are using the field selector, but as this
+		// check makes the unit tests easier (FakeClient does not play nice
+		// with field selectors) and the performance penalty is negligible in
+		// this context we can keep it at the moment.
+		if !isExposed(&service, r.ExposeAnnotationKey) {
+			serviceLog.Debugf("skipping service: it does not have the annotation %s=true", r.ExposeAnnotationKey)
+			continue
+		}
+
+		// We only manage NodePort services so Kubernetes takes care of allocating a unique port
+		if service.Spec.Type != corev1.ServiceTypeNodePort {
+			serviceLog.Warn("skipping service: it is not of type NodePort", "service")
+			return nil
+		}
+
+		eps := corev1.Endpoints{}
+		if err := r.Get(context.Background(), types.NamespacedName{Namespace: service.Namespace, Name: service.Name}, &eps); err != nil {
+			if apierrors.IsNotFound(err) {
+				continue
+			}
+			return errors.Wrap(err, fmt.Sprintf("failed to get endpoints for service '%s'", serviceKey))
+		}
+		// If we have no pods, dont bother creating a cluster.
+		if len(eps.Subsets) == 0 {
+			serviceLog.Debug("skipping service: it has no running pods")
+			continue
+		}
+		l, c := r.makeListenersAndClustersForService(&service, &eps)
+		listeners = append(listeners, l...)
+		clusters = append(clusters, c...)
+
+	}
+
+	// Get current snapshot
+	currSnapshot, err := r.EnvoySnapshotCache.GetSnapshot(r.EnvoyNodeName)
+	if err != nil {
+		r.Log.Debugf("setting first snapshot: %v", err)
+		if err := r.EnvoySnapshotCache.SetSnapshot(r.EnvoyNodeName, newSnapshot("v0.0.0", clusters, listeners)); err != nil {
+			return errors.Wrap(err, "failed to set a new Envoy cache snapshot")
+		}
+		return nil
+	}
+
+	lastUsedVersion, err := semver.NewVersion(currSnapshot.GetVersion(envoyresourcev3.ClusterType))
+	if err != nil {
+		return errors.Wrap(err, "failed to parse version from last snapshot")
+	}
+
+	// Generate a new snapshot using the old version to be able to do a DeepEqual comparison
+	if reflect.DeepEqual(currSnapshot, newSnapshot(lastUsedVersion.String(), clusters, listeners)) {
+		r.Log.Debug("no changes detected")
+		return nil
+	}
+
+	r.Log.Info("detected a change. Updating the Envoy config cache...")
+	newVersion := lastUsedVersion.IncMajor()
+	newSnapshot := newSnapshot(newVersion.String(), clusters, listeners)
+
+	if err := newSnapshot.Consistent(); err != nil {
+		return errors.Wrap(err, "new Envoy config snapshot is not consistent")
+	}
+
+	if err := r.EnvoySnapshotCache.SetSnapshot(r.EnvoyNodeName, newSnapshot); err != nil {
+		return errors.Wrap(err, "failed to set a new Envoy cache snapshot")
+	}
+
+	return nil
+}
+
+func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if err := mgr.GetFieldIndexer().IndexField(r.Ctx, &corev1.Service{}, r.ExposeAnnotationKey, func(raw runtime.Object) []string {
+		var values []string
+		svc := raw.(*corev1.Service)
+		if isExposed(svc, r.ExposeAnnotationKey) {
+			values = append(values, "ok")
+		}
+		return values
+	}); err != nil {
+		return fmt.Errorf("error occurred while adding service index: %w", err)
+	}
+	return ctrl.NewControllerManagedBy(mgr).
+		// Ensures that only one new Snapshot is generated at a time
+		WithOptions(controller.Options{MaxConcurrentReconciles: 1}).
+		For(&corev1.Service{}, builder.WithPredicates(exposeAnnotationPredicate{annotation: r.ExposeAnnotationKey, log: r.Log})).
+		Watches(&source.Kind{Type: &corev1.Endpoints{}},
+			&handler.EnqueueRequestsFromMapFunc{ToRequests: handler.ToRequestsFunc(r.endpointsToService)}).
+		Complete(r)
+}
+
+func (r *Reconciler) endpointsToService(obj handler.MapObject) []ctrl.Request {
+	svcName := types.NamespacedName{
+		Name:      obj.Meta.GetName(),
+		Namespace: obj.Meta.GetNamespace(),
+	}
+	// Get the service associated to the Endpoints
+	svc := corev1.Service{}
+	if err := r.Client.Get(r.Ctx, svcName, &svc); err != nil {
+		// Avoid enqueuing events for endpoints that do not have an associated
+		// service (e.g. leader election).
+		if !apierrors.IsNotFound(err) {
+			r.Log.Errorw("error occurred while mapping endpoints to service", "endpoints", obj)
+		}
+		return nil
+	}
+
+	// Avoid enqueuing events for services that are not exposed.
+	if !isExposed(&svc, r.ExposeAnnotationKey) {
+		return nil
+	}
+	return []ctrl.Request{{NamespacedName: svcName}}
+}
+
+// exposeAnnotationPredicate is used to filter out events associated to
+// services that do not have the expose annotation and are thus not interesting
+// for this controller.
+type exposeAnnotationPredicate struct {
+	log        *zap.SugaredLogger
+	annotation string
+}
+
+// Create returns true if the Create event should be processed
+func (e exposeAnnotationPredicate) Create(event event.CreateEvent) bool {
+	return e.match(event.Meta)
+}
+
+// Delete returns true if the Delete event should be processed
+func (e exposeAnnotationPredicate) Delete(event event.DeleteEvent) bool {
+	return e.match(event.Meta)
+}
+
+// Update returns true if the Update event should be processed
+func (e exposeAnnotationPredicate) Update(event event.UpdateEvent) bool {
+	return e.match(event.MetaNew)
+}
+
+// Generic returns true if the Generic event should be processed
+func (e exposeAnnotationPredicate) Generic(event event.GenericEvent) bool {
+	return e.match(event.Meta)
+}
+
+func (e exposeAnnotationPredicate) match(obj metav1.Object) bool {
+	ie := isExposed(obj, e.annotation)
+	e.log.Debugw("processing event", "object", obj, "isExposed", ie)
+	return ie
+}
