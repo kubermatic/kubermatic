d165a3f9c12fbec0125c10d4647d88355db7ccab
Iacopo Rozzo <iacopo@kubermatic.com>
2020-11-24 16:33:05 +0100

    Add unit tests for test http client with retry transport

---
+++ pkg/test/e2e/api/utils/http_test.go
@@ -0,0 +1,189 @@
+/*
+Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestHttpClientWithRetries(t *testing.T) {
+	var testcases = []struct {
+		name              string
+		handlerFuncs      []http.HandlerFunc
+		retryInterval     time.Duration
+		retryTimeout      time.Duration
+		requestTimeout    time.Duration
+		allowedErrorCodes []int
+		expStatus         int
+		expErr            bool
+	}{
+		{
+			name: "success at first attempt",
+			handlerFuncs: []http.HandlerFunc{
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(200)
+					fmt.Fprintln(w, "success")
+				},
+			},
+			expStatus: 200,
+		},
+		{
+			name: "success after 2 allowed error codes",
+			handlerFuncs: []http.HandlerFunc{
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(404)
+					fmt.Fprintln(w, "failed")
+				},
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(404)
+					fmt.Fprintln(w, "failed")
+				},
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(200)
+					fmt.Fprintln(w, "success")
+				},
+			},
+			retryInterval:     1 * time.Millisecond,
+			retryTimeout:      10 * time.Millisecond,
+			allowedErrorCodes: []int{404},
+			expStatus:         200,
+		},
+		{
+			name: "success after 5xx",
+			handlerFuncs: []http.HandlerFunc{
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(500)
+					fmt.Fprintln(w, "temporary server error")
+				},
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(200)
+					fmt.Fprintln(w, "failed")
+				},
+			},
+			retryInterval: 1 * time.Millisecond,
+			retryTimeout:  10 * time.Millisecond,
+			expStatus:     200,
+		},
+		{
+			name: "do not retry after 501",
+			handlerFuncs: []http.HandlerFunc{
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(501)
+					fmt.Fprintln(w, "not implemented")
+				},
+			},
+			expStatus: 501,
+		},
+		{
+			name: "Error after retry timeout",
+			handlerFuncs: []http.HandlerFunc{
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(500)
+					fmt.Fprintln(w, "temporary server error")
+				},
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(503)
+					fmt.Fprintln(w, "temporary server error")
+				},
+			},
+			retryInterval: 1 * time.Millisecond,
+			retryTimeout:  10 * time.Millisecond,
+			expErr:        true,
+		},
+		{
+			name: "Success after initial request timeout",
+			handlerFuncs: []http.HandlerFunc{
+				func(w http.ResponseWriter, r *http.Request) {
+					time.AfterFunc(20*time.Millisecond, func() {
+						w.WriteHeader(200)
+						fmt.Fprintln(w, "success")
+					})
+				},
+				func(w http.ResponseWriter, r *http.Request) {
+					time.AfterFunc(20*time.Millisecond, func() {
+						w.WriteHeader(200)
+						fmt.Fprintln(w, "success")
+					})
+				},
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(200)
+					fmt.Fprintln(w, "success")
+				},
+			},
+			retryInterval:  1 * time.Millisecond,
+			retryTimeout:   10 * time.Millisecond,
+			requestTimeout: 10 * time.Millisecond,
+			expStatus:      200,
+		},
+		{
+			name: "Faile due to request timeout",
+			handlerFuncs: []http.HandlerFunc{
+				func(w http.ResponseWriter, r *http.Request) {
+					time.AfterFunc(20*time.Millisecond, func() {
+						w.WriteHeader(200)
+						fmt.Fprintln(w, "success")
+					})
+				},
+			},
+			retryInterval:  1 * time.Millisecond,
+			retryTimeout:   10 * time.Millisecond,
+			requestTimeout: 10 * time.Microsecond,
+			expErr:         true,
+		},
+	}
+	for _, tt := range testcases {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(iterateHandlerFuncs(tt.handlerFuncs...))
+			if tt.requestTimeout == 0 {
+				tt.requestTimeout = apiRequestTimeout
+			}
+			cli := NewHTTPClientWithRetries(t, tt.requestTimeout, tt.retryInterval, tt.retryTimeout, tt.allowedErrorCodes...)
+			req, err := http.NewRequest("GET", ts.URL, nil)
+			if err != nil {
+				t.Fatalf("Error occurred while creating request: %v", err)
+			}
+			res, err := cli.Do(req)
+			if err != nil {
+				if !tt.expErr {
+					t.Fatalf("Expected request success but get: %v", err)
+				}
+				return
+			}
+			defer res.Body.Close()
+			if tt.expErr {
+				t.Fatalf("Expected error but none get")
+			}
+			if res.StatusCode != tt.expStatus {
+				t.Errorf("Expected status %d but got %d", tt.expStatus, res.StatusCode)
+			}
+		})
+	}
+}
+
+func iterateHandlerFuncs(funcs ...http.HandlerFunc) http.HandlerFunc {
+	var i int = -1
+	return func(w http.ResponseWriter, r *http.Request) {
+		if i < len(funcs)-1 {
+			i++
+		}
+		funcs[i](w, r)
+	}
+}
