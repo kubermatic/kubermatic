48c19efafa42c4463d38e4923600c75abbc0f01d
Lovro Sviben <46844730+lsviben@users.noreply.github.com>
2020-12-10 16:20:22 +0100

    Opa config endpoints (#6286)
    * opa config get endpoint
    
    * opa-config-delete-endpoint
    
    * added create endpoint
    
    * gen swagger and api client for opa config
    
    * go mod tidy

---
+++ pkg/handler/v2/gatekeeperconfig/gatekeeperconfig.go
@@ -0,0 +1,209 @@
+/*
+Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gatekeeperconfig
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-kit/kit/endpoint"
+	configv1alpha1 "github.com/open-policy-agent/gatekeeper/apis/config/v1alpha1"
+
+	apiv2 "k8c.io/kubermatic/v2/pkg/api/v2"
+	handlercommon "k8c.io/kubermatic/v2/pkg/handler/common"
+	"k8c.io/kubermatic/v2/pkg/handler/middleware"
+	"k8c.io/kubermatic/v2/pkg/handler/v1/common"
+	"k8c.io/kubermatic/v2/pkg/handler/v2/cluster"
+	"k8c.io/kubermatic/v2/pkg/provider"
+	utilerrors "k8c.io/kubermatic/v2/pkg/util/errors"
+
+	"k8s.io/apimachinery/pkg/types"
+)
+
+const (
+	// Gatekeeper only uses the configs from the namespace which is set as "gatekeeper namespace" in the gatekeeper controller and audit.
+	// For our deployment, its always `gatekeeper-system`
+	ConfigNamespace = "gatekeeper-system"
+	// Gatekeeper audit also uses the hardcoded config name `config`
+	ConfigName = "config"
+)
+
+func GetEndpoint(userInfoGetter provider.UserInfoGetter, projectProvider provider.ProjectProvider,
+	privilegedProjectProvider provider.PrivilegedProjectProvider) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(gatekeeperConfigReq)
+		clusterProvider := ctx.Value(middleware.ClusterProviderContextKey).(provider.ClusterProvider)
+
+		clus, err := handlercommon.GetCluster(ctx, projectProvider, privilegedProjectProvider, userInfoGetter, req.ProjectID, req.ClusterID, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		clusterCli, err := common.GetClusterClient(ctx, userInfoGetter, clusterProvider, clus, req.ProjectID)
+		if err != nil {
+			return nil, common.KubernetesErrorToHTTPError(err)
+		}
+
+		conf := &configv1alpha1.Config{}
+		if err := clusterCli.Get(ctx, types.NamespacedName{Namespace: ConfigNamespace, Name: ConfigName}, conf); err != nil {
+			return nil, common.KubernetesErrorToHTTPError(err)
+		}
+
+		apiConf, err := convertExternalToAPI(conf)
+		if err != nil {
+			return nil, utilerrors.New(http.StatusInternalServerError, err.Error())
+		}
+
+		return apiConf, nil
+	}
+}
+
+// gatekeeperConfigReq defines HTTP request for the gatekeeper sync config
+// swagger:parameters getGatekeeperConfig deleteGatekeeperConfig
+type gatekeeperConfigReq struct {
+	cluster.GetClusterReq
+}
+
+func DecodeGatkeeperConfigReq(c context.Context, r *http.Request) (interface{}, error) {
+	var req gatekeeperConfigReq
+
+	cr, err := cluster.DecodeGetClusterReq(c, r)
+	if err != nil {
+		return nil, err
+	}
+
+	req.GetClusterReq = cr.(cluster.GetClusterReq)
+
+	return req, nil
+}
+
+func convertExternalToAPI(conf *configv1alpha1.Config) (*apiv2.GatekeeperConfig, error) {
+	apiConf := &apiv2.GatekeeperConfig{}
+
+	specRaw, err := json.Marshal(&conf.Spec)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling gatekeeper config spec: %v", err)
+	}
+
+	err = json.Unmarshal(specRaw, &apiConf.Spec)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling gatekeeper config spec: %v", err)
+	}
+
+	return apiConf, nil
+}
+
+func convertAPIToExternal(conf *apiv2.GatekeeperConfig) (*configv1alpha1.Config, error) {
+	externalConf := &configv1alpha1.Config{}
+
+	specRaw, err := json.Marshal(&conf.Spec)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling gatekeeper config spec: %v", err)
+	}
+
+	err = json.Unmarshal(specRaw, &externalConf.Spec)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling gatekeeper config spec: %v", err)
+	}
+
+	return externalConf, nil
+}
+
+func DeleteEndpoint(userInfoGetter provider.UserInfoGetter, projectProvider provider.ProjectProvider,
+	privilegedProjectProvider provider.PrivilegedProjectProvider) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(gatekeeperConfigReq)
+		clusterProvider := ctx.Value(middleware.ClusterProviderContextKey).(provider.ClusterProvider)
+
+		clus, err := handlercommon.GetCluster(ctx, projectProvider, privilegedProjectProvider, userInfoGetter, req.ProjectID, req.ClusterID, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		clusterCli, err := common.GetClusterClient(ctx, userInfoGetter, clusterProvider, clus, req.ProjectID)
+		if err != nil {
+			return nil, common.KubernetesErrorToHTTPError(err)
+		}
+
+		conf := &configv1alpha1.Config{}
+		conf.SetName(ConfigName)
+		conf.SetNamespace(ConfigNamespace)
+		if err := clusterCli.Delete(ctx, conf); err != nil {
+			return nil, common.KubernetesErrorToHTTPError(err)
+		}
+
+		return nil, nil
+	}
+}
+
+func CreateEndpoint(userInfoGetter provider.UserInfoGetter, projectProvider provider.ProjectProvider,
+	privilegedProjectProvider provider.PrivilegedProjectProvider) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(createGatekeeperConfigReq)
+		clusterProvider := ctx.Value(middleware.ClusterProviderContextKey).(provider.ClusterProvider)
+
+		clus, err := handlercommon.GetCluster(ctx, projectProvider, privilegedProjectProvider, userInfoGetter, req.ProjectID, req.ClusterID, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		clusterCli, err := common.GetClusterClient(ctx, userInfoGetter, clusterProvider, clus, req.ProjectID)
+		if err != nil {
+			return nil, common.KubernetesErrorToHTTPError(err)
+		}
+
+		conf, err := convertAPIToExternal(&req.Body)
+		if err != nil {
+			return nil, utilerrors.New(http.StatusInternalServerError, err.Error())
+		}
+		conf.SetName(ConfigName)
+		conf.SetNamespace(ConfigNamespace)
+
+		if err := clusterCli.Create(ctx, conf); err != nil {
+			return nil, common.KubernetesErrorToHTTPError(err)
+		}
+
+		return req.Body, nil
+	}
+}
+
+// createGatekeeperConfigReq defines HTTP request for the gatekeeper config create
+// swagger:parameters createGatekeeperConfig
+type createGatekeeperConfigReq struct {
+	cluster.GetClusterReq
+	// in: body
+	// required: true
+	Body apiv2.GatekeeperConfig
+}
+
+func DecodeCreateGatkeeperConfigReq(c context.Context, r *http.Request) (interface{}, error) {
+	var req createGatekeeperConfigReq
+
+	cr, err := cluster.DecodeGetClusterReq(c, r)
+	if err != nil {
+		return nil, err
+	}
+
+	req.GetClusterReq = cr.(cluster.GetClusterReq)
+
+	if err := json.NewDecoder(r.Body).Decode(&req.Body); err != nil {
+		return nil, err
+	}
+	return req, nil
+}
---
+++ pkg/handler/v2/gatekeeperconfig/gatekeeperconfig_test.go
@@ -0,0 +1,399 @@
+/*
+Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gatekeeperconfig_test
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	configv1alpha1 "github.com/open-policy-agent/gatekeeper/apis/config/v1alpha1"
+
+	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
+	apiv2 "k8c.io/kubermatic/v2/pkg/api/v2"
+	"k8c.io/kubermatic/v2/pkg/handler/test"
+	"k8c.io/kubermatic/v2/pkg/handler/test/hack"
+	"k8c.io/kubermatic/v2/pkg/handler/v2/gatekeeperconfig"
+
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func TestGetConfigEndpoint(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		Name                   string
+		ExpectedResponse       string
+		ProjectID              string
+		ClusterID              string
+		HTTPStatus             int
+		ExistingKubermaticObjs []runtime.Object
+		ExistingGatekeeperObjs []runtime.Object
+		ExistingAPIUser        *apiv1.User
+	}{
+		{
+			Name:                   "scenario 1: delete gatekeeper config",
+			ExpectedResponse:       `{"spec":{"sync":{"syncOnly":[{"version":"v1","kind":"Namespace"},{"version":"v1","kind":"Pod"}]},"validation":{"traces":[{"user":"bob","kind":{"version":"v1","kind":"Pod"}}]},"match":[{"excludedNamespaces":["default","kube-system"],"processes":["audit"]}],"readiness":{"statsEnabled":true}}}`,
+			ProjectID:              test.GenDefaultProject().Name,
+			ClusterID:              test.GenDefaultCluster().Name,
+			HTTPStatus:             http.StatusOK,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(test.GenDefaultCluster()),
+			ExistingGatekeeperObjs: []runtime.Object{genGatekeeperConfig()},
+			ExistingAPIUser:        test.GenDefaultAPIUser(),
+		},
+		{
+			Name:                   "scenario 2: fail getting non-existing gatekeeper config",
+			ExpectedResponse:       `{"error":{"code":404,"message":"configs.config.gatekeeper.sh \"config\" not found"}}`,
+			ProjectID:              test.GenDefaultProject().Name,
+			ClusterID:              test.GenDefaultCluster().Name,
+			HTTPStatus:             http.StatusNotFound,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(test.GenDefaultCluster()),
+			ExistingGatekeeperObjs: []runtime.Object{},
+			ExistingAPIUser:        test.GenDefaultAPIUser(),
+		},
+		{
+			Name:             "scenario 3: user john can not get bobs gatekeeper config",
+			ExpectedResponse: `{"error":{"code":403,"message":"forbidden: \"john@acme.com\" doesn't belong to the given project = my-first-project-ID"}}`,
+			ProjectID:        test.GenDefaultProject().Name,
+			ClusterID:        test.GenDefaultCluster().Name,
+			HTTPStatus:       http.StatusForbidden,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(
+				test.GenDefaultCluster(),
+				test.GenAdminUser("John", "john@acme.com", false),
+			),
+			ExistingGatekeeperObjs: []runtime.Object{genGatekeeperConfig()},
+			ExistingAPIUser:        test.GenAPIUser("John", "john@acme.com"),
+		},
+		{
+			Name:             "scenario 4: admin john can get bobs gatekeeper config",
+			ExpectedResponse: `{"spec":{"sync":{"syncOnly":[{"version":"v1","kind":"Namespace"},{"version":"v1","kind":"Pod"}]},"validation":{"traces":[{"user":"bob","kind":{"version":"v1","kind":"Pod"}}]},"match":[{"excludedNamespaces":["default","kube-system"],"processes":["audit"]}],"readiness":{"statsEnabled":true}}}`,
+			ProjectID:        test.GenDefaultProject().Name,
+			ClusterID:        test.GenDefaultCluster().Name,
+			HTTPStatus:       http.StatusOK,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(
+				test.GenDefaultCluster(),
+				test.GenAdminUser("John", "john@acme.com", true),
+			),
+			ExistingGatekeeperObjs: []runtime.Object{genGatekeeperConfig()},
+			ExistingAPIUser:        test.GenAPIUser("John", "john@acme.com"),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.Name, func(t *testing.T) {
+
+			req := httptest.NewRequest("GET", fmt.Sprintf("/api/v2/projects/%s/clusters/%s/gatekeeper/config", tc.ProjectID, tc.ClusterID), strings.NewReader(""))
+			res := httptest.NewRecorder()
+
+			ep, clientsSets, err := test.CreateTestEndpointAndGetClients(*tc.ExistingAPIUser, nil, nil, nil, tc.ExistingKubermaticObjs, nil, nil, hack.NewTestRouting)
+			if err != nil {
+				t.Fatalf("failed to create test endpoint due to %v", err)
+			}
+
+			for _, gkObject := range tc.ExistingGatekeeperObjs {
+				err = clientsSets.FakeClient.Create(context.Background(), gkObject)
+				if err != nil {
+					t.Fatalf("failed to create gk object %v due to %v", gkObject, err)
+				}
+			}
+
+			ep.ServeHTTP(res, req)
+
+			if res.Code != tc.HTTPStatus {
+				t.Fatalf("Expected HTTP status code %d, got %d: %s", tc.HTTPStatus, res.Code, res.Body.String())
+			}
+			test.CompareWithResult(t, res, tc.ExpectedResponse)
+		})
+	}
+}
+
+func TestDeleteConfigEndpoint(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		Name                   string
+		ExpectedResponse       string
+		ProjectID              string
+		ClusterID              string
+		HTTPStatus             int
+		ExistingKubermaticObjs []runtime.Object
+		ExistingGatekeeperObjs []runtime.Object
+		ExistingAPIUser        *apiv1.User
+	}{
+		{
+			Name:                   "scenario 1: delete gatekeeper config",
+			ExpectedResponse:       `{}`,
+			ProjectID:              test.GenDefaultProject().Name,
+			ClusterID:              test.GenDefaultCluster().Name,
+			HTTPStatus:             http.StatusOK,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(test.GenDefaultCluster()),
+			ExistingGatekeeperObjs: []runtime.Object{genGatekeeperConfig()},
+			ExistingAPIUser:        test.GenDefaultAPIUser(),
+		},
+		{
+			Name:                   "scenario 2: fail deleting non-existing gatekeeper config",
+			ExpectedResponse:       `{"error":{"code":404,"message":"configs.config.gatekeeper.sh \"config\" not found"}}`,
+			ProjectID:              test.GenDefaultProject().Name,
+			ClusterID:              test.GenDefaultCluster().Name,
+			HTTPStatus:             http.StatusNotFound,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(test.GenDefaultCluster()),
+			ExistingGatekeeperObjs: []runtime.Object{},
+			ExistingAPIUser:        test.GenDefaultAPIUser(),
+		},
+		{
+			Name:             "scenario 3: user john can not delete bobs gatekeeper config",
+			ExpectedResponse: `{"error":{"code":403,"message":"forbidden: \"john@acme.com\" doesn't belong to the given project = my-first-project-ID"}}`,
+			ProjectID:        test.GenDefaultProject().Name,
+			ClusterID:        test.GenDefaultCluster().Name,
+			HTTPStatus:       http.StatusForbidden,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(
+				test.GenDefaultCluster(),
+				test.GenAdminUser("John", "john@acme.com", false),
+			),
+			ExistingGatekeeperObjs: []runtime.Object{genGatekeeperConfig()},
+			ExistingAPIUser:        test.GenAPIUser("John", "john@acme.com"),
+		},
+		{
+			Name:             "scenario 4: admin john can delete bobs gatekeeper config",
+			ExpectedResponse: `{}`,
+			ProjectID:        test.GenDefaultProject().Name,
+			ClusterID:        test.GenDefaultCluster().Name,
+			HTTPStatus:       http.StatusOK,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(
+				test.GenDefaultCluster(),
+				test.GenAdminUser("John", "john@acme.com", true),
+			),
+			ExistingGatekeeperObjs: []runtime.Object{genGatekeeperConfig()},
+			ExistingAPIUser:        test.GenAPIUser("John", "john@acme.com"),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.Name, func(t *testing.T) {
+
+			req := httptest.NewRequest("DELETE", fmt.Sprintf("/api/v2/projects/%s/clusters/%s/gatekeeper/config", tc.ProjectID, tc.ClusterID), strings.NewReader(""))
+			res := httptest.NewRecorder()
+
+			ep, clientsSets, err := test.CreateTestEndpointAndGetClients(*tc.ExistingAPIUser, nil, nil, nil, tc.ExistingKubermaticObjs, nil, nil, hack.NewTestRouting)
+			if err != nil {
+				t.Fatalf("failed to create test endpoint due to %v", err)
+			}
+
+			for _, gkObject := range tc.ExistingGatekeeperObjs {
+				err = clientsSets.FakeClient.Create(context.Background(), gkObject)
+				if err != nil {
+					t.Fatalf("failed to create gk object %v due to %v", gkObject, err)
+				}
+			}
+
+			ep.ServeHTTP(res, req)
+
+			if res.Code != tc.HTTPStatus {
+				t.Fatalf("Expected HTTP status code %d, got %d: %s", tc.HTTPStatus, res.Code, res.Body.String())
+			}
+			test.CompareWithResult(t, res, tc.ExpectedResponse)
+		})
+	}
+}
+
+func TestCreateConfigEndpoint(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		Name                   string
+		ExpectedResponse       string
+		ProjectID              string
+		ClusterID              string
+		ToCreateConfig         *apiv2.GatekeeperConfig
+		HTTPStatus             int
+		ExistingKubermaticObjs []runtime.Object
+		ExistingGatekeeperObjs []runtime.Object
+		ExistingAPIUser        *apiv1.User
+	}{
+		{
+			Name:                   "scenario 1: create gatekeeper config",
+			ExpectedResponse:       `{"spec":{"sync":{"syncOnly":[{"version":"v1","kind":"Namespace"},{"version":"v1","kind":"Pod"}]},"validation":{"traces":[{"user":"bob","kind":{"version":"v1","kind":"Pod"}}]},"match":[{"excludedNamespaces":["default","kube-system"],"processes":["audit"]}],"readiness":{"statsEnabled":true}}}`,
+			ProjectID:              test.GenDefaultProject().Name,
+			ClusterID:              test.GenDefaultCluster().Name,
+			ToCreateConfig:         genAPIGatekeeperConfig(),
+			HTTPStatus:             http.StatusOK,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(test.GenDefaultCluster()),
+			ExistingAPIUser:        test.GenDefaultAPIUser(),
+		},
+		{
+			Name:                   "scenario 2: fail creating already existing gatekeeper config",
+			ExpectedResponse:       `{"error":{"code":409,"message":"configs.config.gatekeeper.sh \"config\" already exists"}}`,
+			ProjectID:              test.GenDefaultProject().Name,
+			ClusterID:              test.GenDefaultCluster().Name,
+			ToCreateConfig:         genAPIGatekeeperConfig(),
+			HTTPStatus:             http.StatusConflict,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(test.GenDefaultCluster()),
+			ExistingGatekeeperObjs: []runtime.Object{genGatekeeperConfig()},
+			ExistingAPIUser:        test.GenDefaultAPIUser(),
+		},
+		{
+			Name:             "scenario 3: user john can not create bob cluster gatekeeper config",
+			ExpectedResponse: `{"error":{"code":403,"message":"forbidden: \"john@acme.com\" doesn't belong to the given project = my-first-project-ID"}}`,
+			ProjectID:        test.GenDefaultProject().Name,
+			ClusterID:        test.GenDefaultCluster().Name,
+			ToCreateConfig:   genAPIGatekeeperConfig(),
+			HTTPStatus:       http.StatusForbidden,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(
+				test.GenDefaultCluster(),
+				test.GenAdminUser("John", "john@acme.com", false),
+			),
+			ExistingAPIUser: test.GenAPIUser("John", "john@acme.com"),
+		},
+		{
+			Name:             "scenario 4: admin john can create bobs gatekeeper config",
+			ExpectedResponse: `{"spec":{"sync":{"syncOnly":[{"version":"v1","kind":"Namespace"},{"version":"v1","kind":"Pod"}]},"validation":{"traces":[{"user":"bob","kind":{"version":"v1","kind":"Pod"}}]},"match":[{"excludedNamespaces":["default","kube-system"],"processes":["audit"]}],"readiness":{"statsEnabled":true}}}`,
+			ProjectID:        test.GenDefaultProject().Name,
+			ClusterID:        test.GenDefaultCluster().Name,
+			ToCreateConfig:   genAPIGatekeeperConfig(),
+			HTTPStatus:       http.StatusOK,
+			ExistingKubermaticObjs: test.GenDefaultKubermaticObjects(
+				test.GenDefaultCluster(),
+				test.GenAdminUser("John", "john@acme.com", true),
+			),
+			ExistingAPIUser: test.GenAPIUser("John", "john@acme.com"),
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.Name, func(t *testing.T) {
+
+			body, err := json.Marshal(tc.ToCreateConfig)
+			if err != nil {
+				t.Fatalf("error marshalling body into json: %v", err)
+			}
+
+			req := httptest.NewRequest("POST", fmt.Sprintf("/api/v2/projects/%s/clusters/%s/gatekeeper/config", tc.ProjectID, tc.ClusterID), bytes.NewBuffer(body))
+			res := httptest.NewRecorder()
+
+			ep, clientsSets, err := test.CreateTestEndpointAndGetClients(*tc.ExistingAPIUser, nil, nil, nil, tc.ExistingKubermaticObjs, nil, nil, hack.NewTestRouting)
+			if err != nil {
+				t.Fatalf("failed to create test endpoint due to %v", err)
+			}
+
+			for _, gkObject := range tc.ExistingGatekeeperObjs {
+				err = clientsSets.FakeClient.Create(context.Background(), gkObject)
+				if err != nil {
+					t.Fatalf("failed to create gk object %v due to %v", gkObject, err)
+				}
+			}
+
+			ep.ServeHTTP(res, req)
+
+			if res.Code != tc.HTTPStatus {
+				t.Fatalf("Expected HTTP status code %d, got %d: %s", tc.HTTPStatus, res.Code, res.Body.String())
+			}
+			test.CompareWithResult(t, res, tc.ExpectedResponse)
+		})
+	}
+}
+
+func genAPIGatekeeperConfig() *apiv2.GatekeeperConfig {
+	config := &apiv2.GatekeeperConfig{}
+
+	config.Spec = apiv2.GatekeeperConfigSpec{
+		Sync: apiv2.Sync{
+			SyncOnly: []apiv2.GVK{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Namespace",
+				},
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Pod",
+				},
+			},
+		},
+		Validation: apiv2.Validation{
+			Traces: []apiv2.Trace{
+				{
+					User: "bob",
+					Kind: apiv2.GVK{
+						Group:   "",
+						Version: "v1",
+						Kind:    "Pod",
+					},
+				},
+			},
+		},
+		Match: []apiv2.MatchEntry{
+			{
+				ExcludedNamespaces: []string{"default", "kube-system"},
+				Processes:          []string{"audit"},
+			},
+		},
+		Readiness: apiv2.ReadinessSpec{
+			StatsEnabled: true,
+		},
+	}
+
+	return config
+}
+
+func genGatekeeperConfig() *configv1alpha1.Config {
+	config := &configv1alpha1.Config{}
+	config.Name = gatekeeperconfig.ConfigName
+	config.Namespace = gatekeeperconfig.ConfigNamespace
+
+	config.Spec = configv1alpha1.ConfigSpec{
+		Sync: configv1alpha1.Sync{
+			SyncOnly: []configv1alpha1.SyncOnlyEntry{
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Namespace",
+				},
+				{
+					Group:   "",
+					Version: "v1",
+					Kind:    "Pod",
+				},
+			},
+		},
+		Validation: configv1alpha1.Validation{
+			Traces: []configv1alpha1.Trace{
+				{
+					User: "bob",
+					Kind: configv1alpha1.GVK{
+						Group:   "",
+						Version: "v1",
+						Kind:    "Pod",
+					},
+				},
+			},
+		},
+		Match: []configv1alpha1.MatchEntry{
+			{
+				ExcludedNamespaces: []string{"default", "kube-system"},
+				Processes:          []string{"audit"},
+			},
+		},
+		Readiness: configv1alpha1.ReadinessSpec{
+			StatsEnabled: true,
+		},
+	}
+
+	return config
+}
