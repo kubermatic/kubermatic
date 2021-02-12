715d3b923d152bd99b4049d52b5cff812bf9f496
Happy2C0de <46957159+Happy2C0de@users.noreply.github.com>
2021-02-12 09:13:52 +0100

    Fix go lint error
    Signed-off-by: Happy2C0de <46957159+Happy2C0de@users.noreply.github.com>

--- pkg/resources/cloudconfig/configmap.go
+++ pkg/resources/cloudconfig/configmap.go
@@ -144,7 +144,7 @@ func CloudConfig(
 			},
 			LoadBalancer: openstack.LoadBalancerOpts{
 				ManageSecurityGroups: manageSecurityGroups == nil || *manageSecurityGroups,
-				UseOctavia: dc.Spec.Openstack.UseOctavia,
+				UseOctavia:           dc.Spec.Openstack.UseOctavia,
 			},
 			Version: cluster.Spec.Version.String(),
 		}
