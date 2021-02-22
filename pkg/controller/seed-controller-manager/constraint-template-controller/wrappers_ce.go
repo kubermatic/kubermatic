e3218d4da6f723c2581035999a7ddd40716ccefa
lsviben <sviben.lovro@gmail.com>
2021-02-22 17:41:37 +0100

    fix 2020 -> 2021

--- pkg/controller/seed-controller-manager/constraint-template-controller/wrappers_ce.go
+++ pkg/controller/seed-controller-manager/constraint-template-controller/wrappers_ce.go
@@ -1,7 +1,7 @@
 // +build !ee
 
 /*
-Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+Copyright 2021 The Kubermatic Kubernetes Platform contributors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
