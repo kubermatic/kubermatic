55de487c69d5b761ddac2b5e5b5edff657ac929b
Lovro Sviben <46844730+lsviben@users.noreply.github.com>
2021-02-26 14:17:15 +0100

    [release/v2.16] Gatekeeper webhook timeout  (#6603)
    * Gatekeeper webhook timeout (#6590)
    
    * added configurable opa gatekeeper webhook timeout, default 10 sec
    
    * codegen and change webhook var to *int32
    
    * fix usercluster deployment
    
    * Renamed opaIntegationWebhookTimeout to simply opaWebhookTimeout
    
    * lint fix

--- pkg/test/e2e/utils/apiclient/models/o_p_a_integration_settings.go
+++ pkg/test/e2e/utils/apiclient/models/o_p_a_integration_settings.go
@@ -15,8 +15,12 @@ import (
 // swagger:model OPAIntegrationSettings
 type OPAIntegrationSettings struct {
 
-	// enabled
+	// Enabled is the flag for enabling OPA integration
 	Enabled bool `json:"enabled,omitempty"`
+
+	// WebhookTimeout is the timeout that is set for the gatekeeper validating webhook admission review calls.
+	// By default 10 seconds.
+	WebhookTimeoutSeconds int32 `json:"webhookTimeoutSeconds,omitempty"`
 }
 
 // Validate validates this o p a integration settings
