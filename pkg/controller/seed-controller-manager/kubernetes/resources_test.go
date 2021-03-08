695793533417ea9ac692771701aa9de11a70f11f
Iacopo Rozzo <iacopo@kubermatic.com>
2021-03-08 23:31:39 +0100

    Fix missing import

--- pkg/controller/seed-controller-manager/kubernetes/resources_test.go
+++ pkg/controller/seed-controller-manager/kubernetes/resources_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes/scheme"
