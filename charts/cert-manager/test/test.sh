ab68fc10ed6df0141e9bb4d351f62eda8ad34023
Christoph Mewes <christoph@loodse.com>
2021-03-26 13:51:12 +0100

    Update cert-manager to 1.2.0 (#6739)
    * update cert-manager to 1.1.1, replace with non-legacy CRDs
    
    * update API versions
    
    * begin work on basic helm chart tests
    
    * fix validating webhook path
    
    * bump to 1.2.0
    
    * do not spellcheck upstream CRDs
    
    * make lib.sh alpine compatible
    
    * fix old kubermatic chart

---
+++ charts/cert-manager/test/test.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The Kubermatic Kubernetes Platform contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd $(dirname $0)/../../..
+source hack/lib.sh
+
+# create kind cluster
+export KIND_CLUSTER_NAME=cert-manager-tester
+export DISABLE_CLUSTER_EXPOSER=yes
+source hack/ci/setup-kind-cluster.sh
+
+# try to install cert-manager
+echodate "Installing cert-manager..."
+kubectl apply -f charts/cert-manager/crd/
+
+helm3 upgrade \
+  --install \
+  --namespace cert-manager \
+  --create-namespace \
+  --atomic \
+  cert-manager charts/cert-manager/
+
+# make sure the webhook works, but before that, give the cainjector some
+# time to do its magic and make the webhook ready
+sleep 5
+
+echodate "Creating test certificate..."
+kubectl apply -f charts/cert-manager/test/certificate.yaml
+
+echodate "Deleting kind cluster..."
+kind delete cluster --name "$KIND_CLUSTER_NAME"
---
+++ hack/ci/test-helm-charts.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+### This script runs whenever one or more Helm charts were updated.
+### For every changed chart in a PR, it tries to find a `test/` directory
+### inside the chart and then execute it. The purpose is to run some
+### static tests for our Helm charts, as not all of them are tested
+### in the conformance tests.
+
+set -euo pipefail
+
+cd $(dirname $0)/../..
+source hack/lib.sh
+
+# find all changed charts (the pre-kubermatic-verify-charts job ensures
+# that all updated charts also change their Chart.yaml)
+changedCharts=$(git diff --name-status "${PULL_BASE_SHA}..${PULL_PULL_SHA}" 'charts/**/Chart.yaml' | awk '{ print $2 }' | xargs dirname | sort -u)
+
+for chartDirectory in $changedCharts; do
+  chartName="$(basename "$chartDirectory")"
+  if [ -x "$chartDirectory/test/test.sh" ]; then
+    echodate "Testing $chartName chart..."
+    $chartDirectory/test/test.sh
+    echodate "$chartName tests completed successfully."
+  else
+    echodate "$chartName chart was changed, but does not have any tests."
+  fi
+done
+
+echodate "All changed charts with tests have been tested successfully."
