3288a94fa8b3afbd6cf8a0b10901e4f217ef43c3
Aborilov Pavel <aborilov@gmail.com>
2021-04-02 11:08:21 +0300

    add docs

--- pkg/controller/seed-controller-manager/mla/mla.go
+++ pkg/controller/seed-controller-manager/mla/mla.go
@@ -52,7 +52,8 @@ var (
 
 // Add creates a new MLA controller that is responsible for
 // managing Monitoring, Logging and Alerting for user clusters.
-// * project controller - create/update Grafana organizations based on Kubermatic Projects
+// * project controller - create/update/delete Grafana organizations based on Kubermatic Projects
+// * userprojectbinding controller - create/update/delete Grafana Users to organizations based on Kubermatic UserProjectBindings
 func Add(
 	ctx context.Context,
 	mgr manager.Manager,
