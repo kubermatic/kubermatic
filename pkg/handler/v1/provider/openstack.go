b3fbd4376d723c318929ab563e532a6e857873d4
harshita <harshita.sharma1729@gmail.com>
2021-04-08 15:37:51 +0530

    fixing license spaces
    Signed-off-by: harshita <harshita.sharma6174@gmail.com>

--- pkg/handler/v1/provider/openstack.go
+++ pkg/handler/v1/provider/openstack.go
@@ -1,9 +1,12 @@
 /*
 Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,7 +35,6 @@ type Request struct {
 }
 
 func getCredentials(userInfo *provider.UserInfo, request Request, presetsProvider provider.PresetProvider) (Request, error) {
-	var err error
 	cred, err := getOpenstackCredentials(userInfo, request, presetsProvider)
 	return *cred, fmt.Errorf("error getting OpenStack credentials: %v", err)
 }
