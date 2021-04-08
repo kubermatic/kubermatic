43bcfa630f21c83a6854dfa4210d41796160b357
Moath Qasim <moad.qassem@gmail.com>
2021-04-08 19:52:47 +0200

    add cloudinit sa and roles (#6843)
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

---
+++ pkg/controller/user-cluster-controller-manager/resources/resources/cloudinitsettings/namespace.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2021 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cloudinitsettings
+
+import (
+	"k8c.io/kubermatic/v2/pkg/resources"
+	"k8c.io/kubermatic/v2/pkg/resources/reconciling"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+// NamespaceCreator creates the namespace for CloudInitSettingsNamespace
+func NamespaceCreator() (string, reconciling.NamespaceCreator) {
+	return resources.CloudInitSettingsNamespace, func(ns *corev1.Namespace) (*corev1.Namespace, error) {
+		return ns, nil
+	}
+}
