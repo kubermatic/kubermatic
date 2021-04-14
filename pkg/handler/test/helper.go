5b7a9dfde744b0d91e40e5547271595e6803559d
Lovro Sviben <46844730+lsviben@users.noreply.github.com>
2021-04-14 20:22:33 +0200

    Fix hetzner network being optional (#6878)
    * Make the network in hetzner cluster spec mandatory
    
    * gen swagger and api clients
    
    * add hetzner network to e2e test

--- pkg/handler/test/helper.go
+++ pkg/handler/test/helper.go
@@ -508,6 +508,13 @@ func GenTestSeed() *kubermaticv1.Seed {
 						HyperkubeImage:     "hyperkube-image",
 					},
 				},
+				"hetzner-dc": {
+					Location: "Frankfurt",
+					Country:  "Germany",
+					Spec: kubermaticv1.DatacenterSpec{
+						Hetzner: &kubermaticv1.DatacenterSpecHetzner{},
+					},
+				},
 			},
 		}}
 }
