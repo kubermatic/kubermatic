647e2b8494290b1936b3f0c4f11871979133b710
zreigz <zreigz@gmail.com>
2021-04-15 11:44:08 +0200

    Anexia endpoint to get vlan list

---
+++ pkg/handler/common/provider/anexia.go
@@ -0,0 +1,86 @@
+/*
+Copyright 2021 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package provider
+
+import (
+	"context"
+	"net/http"
+
+	handlercommon "k8c.io/kubermatic/v2/pkg/handler/common"
+	"k8c.io/kubermatic/v2/pkg/handler/middleware"
+	"k8c.io/kubermatic/v2/pkg/provider"
+	"k8c.io/kubermatic/v2/pkg/provider/cloud/anexia"
+	kubernetesprovider "k8c.io/kubermatic/v2/pkg/provider/kubernetes"
+
+	"github.com/anexia-it/go-anxcloud/pkg/client"
+	"github.com/anexia-it/go-anxcloud/pkg/vlan"
+
+	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
+	"k8c.io/kubermatic/v2/pkg/util/errors"
+)
+
+func ListAnexiaVlans(ctx context.Context, token string) (apiv1.AnexiaVlanList, error) {
+	response := apiv1.AnexiaVlanList{}
+
+	cli, err := getClient(token)
+	if err != nil {
+		return nil, errors.New(http.StatusInternalServerError, err.Error())
+	}
+	v := vlan.NewAPI(cli)
+	vlans, err := v.List(ctx, 1, 1000)
+	if err != nil {
+		return nil, errors.New(http.StatusInternalServerError, err.Error())
+	}
+
+	for _, vlan := range vlans {
+		apiVlan := apiv1.AnexiaVlan{
+			ID: vlan.Identifier,
+		}
+		response = append(response, apiVlan)
+	}
+
+	return response, nil
+}
+
+func AnexiaVlansWithClusterCredentialsEndpoint(ctx context.Context, userInfoGetter provider.UserInfoGetter, projectProvider provider.ProjectProvider, privilegedProjectProvider provider.PrivilegedProjectProvider, projectID, clusterID string) (interface{}, error) {
+	clusterProvider := ctx.Value(middleware.ClusterProviderContextKey).(provider.ClusterProvider)
+
+	cluster, err := handlercommon.GetCluster(ctx, projectProvider, privilegedProjectProvider, userInfoGetter, projectID, clusterID, &provider.ClusterGetOptions{CheckInitStatus: true})
+	if err != nil {
+		return nil, err
+	}
+	if cluster.Spec.Cloud.Anexia == nil {
+		return nil, errors.NewNotFound("cloud spec for %s", clusterID)
+	}
+	assertedClusterProvider, ok := clusterProvider.(*kubernetesprovider.ClusterProvider)
+	if !ok {
+		return nil, errors.New(http.StatusInternalServerError, "failed to assert clusterProvider")
+	}
+
+	secretKeySelector := provider.SecretKeySelectorValueFuncFactory(ctx, assertedClusterProvider.GetSeedClusterAdminRuntimeClient())
+	token, err := anexia.GetCredentialsForCluster(cluster.Spec.Cloud, secretKeySelector)
+	if err != nil {
+		return nil, err
+	}
+
+	return ListAnexiaVlans(ctx, token)
+}
+
+func getClient(token string) (client.Client, error) {
+	tokenOpt := client.TokenFromString(token)
+	return client.New(tokenOpt)
+}
