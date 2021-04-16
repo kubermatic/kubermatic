2d6647d111e021428e8dc04db606b6765a277fa0
Lukasz Zajaczkowski <zreigz@gmail.com>
2021-04-16 09:19:32 +0200

    Anexia endpoint to get vlan list (#6893)

---
+++ pkg/handler/v2/provider/anexia.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2021 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package provider
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-kit/kit/endpoint"
+
+	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
+	providercommon "k8c.io/kubermatic/v2/pkg/handler/common/provider"
+	"k8c.io/kubermatic/v2/pkg/handler/v1/common"
+	"k8c.io/kubermatic/v2/pkg/handler/v2/cluster"
+	"k8c.io/kubermatic/v2/pkg/provider"
+)
+
+func AnexiaVlansWithClusterCredentialsEndpoint(projectProvider provider.ProjectProvider, privilegedProjectProvider provider.PrivilegedProjectProvider, userInfoGetter provider.UserInfoGetter) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(anexiaNoCredentialReq)
+		return providercommon.AnexiaVlansWithClusterCredentialsEndpoint(ctx, userInfoGetter, projectProvider, privilegedProjectProvider, req.ProjectID, req.ClusterID)
+	}
+}
+
+// anexiaNoCredentialReq represent a request for Anexia resources
+// swagger:parameters listAnexiaVlansNoCredentialsV2
+type anexiaNoCredentialReq struct {
+	cluster.GetClusterReq
+}
+
+// GetSeedCluster returns the SeedCluster object
+func (req anexiaNoCredentialReq) GetSeedCluster() apiv1.SeedCluster {
+	return apiv1.SeedCluster{
+		ClusterID: req.ClusterID,
+	}
+}
+
+func DecodeAnexiaNoCredentialReq(c context.Context, r *http.Request) (interface{}, error) {
+	var req anexiaNoCredentialReq
+
+	clusterID, err := common.DecodeClusterID(c, r)
+	if err != nil {
+		return nil, err
+	}
+	req.ClusterID = clusterID
+
+	pr, err := common.DecodeProjectRequest(c, r)
+	if err != nil {
+		return nil, err
+	}
+	req.ProjectReq = pr.(common.ProjectReq)
+
+	return req, nil
+}
