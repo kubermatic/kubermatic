3dcc8f81765b9fea9276afe033c38537d5828599
zreigz <zreigz@gmail.com>
2021-04-16 11:10:22 +0200

    new endpoint to get anexia templates

--- pkg/handler/common/provider/anexia.go
+++ pkg/handler/common/provider/anexia.go
@@ -18,18 +18,20 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"net/http"
-
-	handlercommon "k8c.io/kubermatic/v2/pkg/handler/common"
-	"k8c.io/kubermatic/v2/pkg/handler/middleware"
-	"k8c.io/kubermatic/v2/pkg/provider"
-	"k8c.io/kubermatic/v2/pkg/provider/cloud/anexia"
-	kubernetesprovider "k8c.io/kubermatic/v2/pkg/provider/kubernetes"
 
 	"github.com/anexia-it/go-anxcloud/pkg/client"
 	"github.com/anexia-it/go-anxcloud/pkg/vlan"
+	"github.com/anexia-it/go-anxcloud/pkg/vsphere/provisioning/templates"
 
 	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
+	handlercommon "k8c.io/kubermatic/v2/pkg/handler/common"
+	"k8c.io/kubermatic/v2/pkg/handler/middleware"
+	"k8c.io/kubermatic/v2/pkg/handler/v1/common"
+	"k8c.io/kubermatic/v2/pkg/provider"
+	"k8c.io/kubermatic/v2/pkg/provider/cloud/anexia"
+	kubernetesprovider "k8c.io/kubermatic/v2/pkg/provider/kubernetes"
 	"k8c.io/kubermatic/v2/pkg/util/errors"
 )
 
@@ -51,6 +53,29 @@ func ListAnexiaVlans(ctx context.Context, token string) (apiv1.AnexiaVlanList, e
 			ID: vlan.Identifier,
 		}
 		response = append(response, apiVlan)
+	}
+
+	return response, nil
+}
+
+func ListAnexiaTemplates(ctx context.Context, token, locationID string) (apiv1.AnexiaTemplateList, error) {
+	response := apiv1.AnexiaTemplateList{}
+
+	cli, err := getClient(token)
+	if err != nil {
+		return nil, errors.New(http.StatusInternalServerError, err.Error())
+	}
+	t := templates.NewAPI(cli)
+	templates, err := t.List(ctx, locationID, "templates", 1, 1000)
+	if err != nil {
+		return nil, errors.New(http.StatusInternalServerError, err.Error())
+	}
+
+	for _, template := range templates {
+		apiTemplate := apiv1.AnexiaTemplate{
+			ID: template.ID,
+		}
+		response = append(response, apiTemplate)
 	}
 
 	return response, nil
@@ -78,6 +103,41 @@ func AnexiaVlansWithClusterCredentialsEndpoint(ctx context.Context, userInfoGett
 	}
 
 	return ListAnexiaVlans(ctx, token)
+}
+
+func AnexiaTemplatesWithClusterCredentialsEndpoint(ctx context.Context, userInfoGetter provider.UserInfoGetter, projectProvider provider.ProjectProvider, privilegedProjectProvider provider.PrivilegedProjectProvider, seedsGetter provider.SeedsGetter, projectID, clusterID string) (interface{}, error) {
+	clusterProvider := ctx.Value(middleware.ClusterProviderContextKey).(provider.ClusterProvider)
+
+	cluster, err := handlercommon.GetCluster(ctx, projectProvider, privilegedProjectProvider, userInfoGetter, projectID, clusterID, &provider.ClusterGetOptions{CheckInitStatus: true})
+	if err != nil {
+		return nil, err
+	}
+	if cluster.Spec.Cloud.Anexia == nil {
+		return nil, errors.NewNotFound("cloud spec for %s", clusterID)
+	}
+
+	datacenterName := cluster.Spec.Cloud.DatacenterName
+
+	assertedClusterProvider, ok := clusterProvider.(*kubernetesprovider.ClusterProvider)
+	if !ok {
+		return nil, errors.New(http.StatusInternalServerError, "failed to assert clusterProvider")
+	}
+
+	secretKeySelector := provider.SecretKeySelectorValueFuncFactory(ctx, assertedClusterProvider.GetSeedClusterAdminRuntimeClient())
+	token, err := anexia.GetCredentialsForCluster(cluster.Spec.Cloud, secretKeySelector)
+	if err != nil {
+		return nil, err
+	}
+	userInfo, err := userInfoGetter(ctx, "")
+	if err != nil {
+		return nil, common.KubernetesErrorToHTTPError(err)
+	}
+	_, datacenter, err := provider.DatacenterFromSeedMap(userInfo, seedsGetter, datacenterName)
+	if err != nil {
+		return nil, errors.New(http.StatusInternalServerError, fmt.Sprintf("failed to find Datacenter %q: %v", datacenterName, err))
+	}
+
+	return ListAnexiaTemplates(ctx, token, datacenter.Spec.Anexia.LocationID)
 }
 
 func getClient(token string) (client.Client, error) {
