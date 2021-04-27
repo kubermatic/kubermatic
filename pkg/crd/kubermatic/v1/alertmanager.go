5870f75d809191aff3f2d9ec721256049fa6140f
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2021-04-27 12:46:13 +0800

    Clean up

--- pkg/crd/kubermatic/v1/alertmanager.go
+++ pkg/crd/kubermatic/v1/alertmanager.go
@@ -21,10 +21,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const (
-	AlertmanagerKind = "Alertmanager"
-)
-
 //+genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
