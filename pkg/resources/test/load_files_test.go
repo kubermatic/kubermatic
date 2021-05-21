05fd9f6afc09b7aa6b66368bc940bbf5507f5956
Rastislav Szabo <rastislav@kubermatic.com>
2021-05-21 11:45:32 +0200

    Update resources test
    Signed-off-by: Rastislav Szabo <rastislav@kubermatic.com>

--- pkg/resources/test/load_files_test.go
+++ pkg/resources/test/load_files_test.go
@@ -54,6 +54,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/utils/pointer"
 	ctrlruntimefakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
@@ -292,8 +293,9 @@ func TestLoadFiles(t *testing.T) {
 								Pods: kubermaticv1.NetworkRanges{
 									CIDRBlocks: []string{"172.25.0.0/16"},
 								},
-								DNSDomain: "cluster.local",
-								ProxyMode: resources.IPVSProxyMode,
+								DNSDomain:                "cluster.local",
+								ProxyMode:                resources.IPVSProxyMode,
+								NodeLocalDNSCacheEnabled: pointer.BoolPtr(true),
 							},
 							MachineNetworks: []kubermaticv1.MachineNetworkingConfig{
 								{
