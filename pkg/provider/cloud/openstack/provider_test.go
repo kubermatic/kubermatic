447c6af49dceaf1978782fa5657f4a2a5435d491
Furkhat <vailodf@gmail.com>
2021-05-31 14:54:07 +0600

    rerun of cloud initialization deplicates finalizers (#7135)
    Signed-off-by: Furkhat Kasymovgeniiuulu <vailodf@gmail.com>

--- pkg/provider/cloud/openstack/provider_test.go
+++ pkg/provider/cloud/openstack/provider_test.go
@@ -24,11 +24,12 @@ import (
 	"github.com/go-test/deep"
 	"github.com/gophercloud/gophercloud"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	kubermaticv1 "k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1"
 	"k8c.io/kubermatic/v2/pkg/provider"
 	ostesting "k8c.io/kubermatic/v2/pkg/provider/cloud/openstack/internal/testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 func TestIgnoreRouterAlreadyHasPortInSubnetError(t *testing.T) {
@@ -349,6 +350,15 @@ func TestInitializeCloudProvider(t *testing.T) {
 			if tt.wantErr {
 				return
 			}
+			// We cannot guarantee order if finalizers serialized using sets and deep.Equal fails if order is different,
+			// thus we test finalizers equality separately.
+			if w, g := sets.NewString(tt.wantCluster.Finalizers...), sets.NewString(c.Finalizers...); !w.Equal(g) {
+				t.Errorf("Want finalizers: %v, got: %v", w, g)
+			} else {
+				tt.wantCluster.Finalizers = nil
+				c.Finalizers = nil
+			}
+
 			if diff := deep.Equal(tt.wantCluster, *c); len(diff) > 0 {
 				t.Errorf("Diff found between actual and wanted cluster: %v", diff)
 			}
