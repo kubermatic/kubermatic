e3801f6063fa821ac122fda5050105e19cfddb4b
lsviben <sviben.lovro@gmail.com>
2021-06-10 08:33:25 +0200

    added option for Azure Load Balancer SKU

--- pkg/validation/cluster.go
+++ pkg/validation/cluster.go
@@ -34,12 +34,14 @@ import (
 	"k8s.io/apimachinery/pkg/api/equality"
 	utilerror "k8s.io/apimachinery/pkg/util/errors"
 	kubenetutil "k8s.io/apimachinery/pkg/util/net"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 var (
 	// ErrCloudChangeNotAllowed describes that it is not allowed to change the cloud provider
-	ErrCloudChangeNotAllowed = errors.New("not allowed to change the cloud provider")
+	ErrCloudChangeNotAllowed  = errors.New("not allowed to change the cloud provider")
+	azureLoadBalancerSKUTypes = sets.NewString("", "basic", "standard")
 )
 
 // ValidateCreateClusterSpec validates the given cluster spec
@@ -482,6 +484,9 @@ func validateAnexiaCloudSpec(spec *kubermaticv1.AnexiaCloudSpec) error {
 		if err := kuberneteshelper.ValidateSecretKeySelector(spec.CredentialsReference, resources.AnexiaToken); err != nil {
 			return err
 		}
+	}
+	if !azureLoadBalancerSKUTypes.Has(spec.LoadBalancerSKU) {
+		return fmt.Errorf("azure LB SKU cannot be %q, allowed values are %v", spec.LoadBalancerSKU, azureLoadBalancerSKUTypes.List())
 	}
 
 	return nil
