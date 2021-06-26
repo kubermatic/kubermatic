03072a3a5da176a05782831b91458df2f48b7cf4
Lukasz Zajaczkowski <zreigz@gmail.com>
2021-06-26 09:04:10 +0200

    add aplication credentials for machine controller deployment (#7273)

--- pkg/provider/cloud/openstack/provider.go
+++ pkg/provider/cloud/openstack/provider.go
@@ -668,9 +668,20 @@ func GetCredentialsForCluster(cloud kubermaticv1.CloudSpec, secretKeySelector pr
 	token := cloud.Openstack.Token
 	var err error
 
+	if domain == "" {
+		if cloud.Openstack.CredentialsReference == nil {
+			return &resources.OpenstackCredentials{}, errors.New("no credentials provided")
+		}
+		domain, err = secretKeySelector(cloud.Openstack.CredentialsReference, resources.OpenstackDomain)
+		if err != nil {
+			return &resources.OpenstackCredentials{}, err
+		}
+	}
+
 	if useToken && token != "" {
 		return &resources.OpenstackCredentials{
-			Token: token,
+			Token:  token,
+			Domain: domain,
 		}, nil
 	}
 
@@ -678,7 +689,8 @@ func GetCredentialsForCluster(cloud kubermaticv1.CloudSpec, secretKeySelector pr
 		token, _ := secretKeySelector(cloud.Openstack.CredentialsReference, resources.OpenstackToken)
 		if token != "" {
 			return &resources.OpenstackCredentials{
-				Token: token,
+				Token:  token,
+				Domain: domain,
 			}, nil
 		}
 
@@ -688,6 +700,7 @@ func GetCredentialsForCluster(cloud kubermaticv1.CloudSpec, secretKeySelector pr
 		return &resources.OpenstackCredentials{
 			ApplicationCredentialSecret: applicationCredentialSecret,
 			ApplicationCredentialID:     applicationCredentialID,
+			Domain:                      domain,
 		}, nil
 	}
 
@@ -702,6 +715,7 @@ func GetCredentialsForCluster(cloud kubermaticv1.CloudSpec, secretKeySelector pr
 			return &resources.OpenstackCredentials{
 				ApplicationCredentialSecret: applicationCredentialSecret,
 				ApplicationCredentialID:     applicationCredentialID,
+				Domain:                      domain,
 			}, nil
 		}
 	}
@@ -735,16 +749,6 @@ func GetCredentialsForCluster(cloud kubermaticv1.CloudSpec, secretKeySelector pr
 
 	if tenantID == "" && cloud.Openstack.CredentialsReference != nil && cloud.Openstack.CredentialsReference.Name != "" {
 		tenantID, err = secretKeySelector(cloud.Openstack.CredentialsReference, resources.OpenstackTenantID)
-		if err != nil {
-			return &resources.OpenstackCredentials{}, err
-		}
-	}
-
-	if domain == "" {
-		if cloud.Openstack.CredentialsReference == nil {
-			return &resources.OpenstackCredentials{}, errors.New("no credentials provided")
-		}
-		domain, err = secretKeySelector(cloud.Openstack.CredentialsReference, resources.OpenstackDomain)
 		if err != nil {
 			return &resources.OpenstackCredentials{}, err
 		}
