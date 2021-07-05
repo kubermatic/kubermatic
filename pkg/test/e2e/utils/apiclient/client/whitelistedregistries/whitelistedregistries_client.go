4076822fd19a4fda9c47e3801e8619a77d95f2fc
lsviben <sviben.lovro@gmail.com>
2021-07-05 15:18:19 +0200

    gen swagger and api clients

--- pkg/test/e2e/utils/apiclient/client/whitelistedregistries/whitelistedregistries_client.go
+++ pkg/test/e2e/utils/apiclient/client/whitelistedregistries/whitelistedregistries_client.go
@@ -25,9 +25,47 @@ type Client struct {
 
 // ClientService is the interface for Client methods
 type ClientService interface {
+	DeleteWhitelistedRegistry(params *DeleteWhitelistedRegistryParams, authInfo runtime.ClientAuthInfoWriter) (*DeleteWhitelistedRegistryOK, error)
+
 	GetWhitelistedRegistry(params *GetWhitelistedRegistryParams, authInfo runtime.ClientAuthInfoWriter) (*GetWhitelistedRegistryOK, error)
 
+	PatchWhitelistedRegistry(params *PatchWhitelistedRegistryParams, authInfo runtime.ClientAuthInfoWriter) (*PatchWhitelistedRegistryOK, error)
+
 	SetTransport(transport runtime.ClientTransport)
+}
+
+/*
+  DeleteWhitelistedRegistry deletes the given whitelisted registry
+*/
+func (a *Client) DeleteWhitelistedRegistry(params *DeleteWhitelistedRegistryParams, authInfo runtime.ClientAuthInfoWriter) (*DeleteWhitelistedRegistryOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewDeleteWhitelistedRegistryParams()
+	}
+
+	result, err := a.transport.Submit(&runtime.ClientOperation{
+		ID:                 "deleteWhitelistedRegistry",
+		Method:             "DELETE",
+		PathPattern:        "/api/v2/whitelistedregistries/{whitelisted_registry}",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"https"},
+		Params:             params,
+		Reader:             &DeleteWhitelistedRegistryReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*DeleteWhitelistedRegistryOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*DeleteWhitelistedRegistryDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
 /*
@@ -61,6 +99,40 @@ func (a *Client) GetWhitelistedRegistry(params *GetWhitelistedRegistryParams, au
 	}
 	// unexpected success response
 	unexpectedSuccess := result.(*GetWhitelistedRegistryDefault)
+	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+}
+
+/*
+  PatchWhitelistedRegistry Patch a specified whitelisted registry
+*/
+func (a *Client) PatchWhitelistedRegistry(params *PatchWhitelistedRegistryParams, authInfo runtime.ClientAuthInfoWriter) (*PatchWhitelistedRegistryOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewPatchWhitelistedRegistryParams()
+	}
+
+	result, err := a.transport.Submit(&runtime.ClientOperation{
+		ID:                 "patchWhitelistedRegistry",
+		Method:             "PATCH",
+		PathPattern:        "/api/v2/whitelistedregistries/{whitelisted_registry}",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"https"},
+		Params:             params,
+		Reader:             &PatchWhitelistedRegistryReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*PatchWhitelistedRegistryOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	unexpectedSuccess := result.(*PatchWhitelistedRegistryDefault)
 	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
 }
 
