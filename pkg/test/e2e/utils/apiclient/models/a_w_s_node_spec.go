d997f340ad756e57e646c9ae1474b513c7a26279
Moath Qasim <moad.qassem@gmail.com>
2021-07-05 14:10:01 +0300

    regenerating api client Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

--- pkg/test/e2e/utils/apiclient/models/a_w_s_node_spec.go
+++ pkg/test/e2e/utils/apiclient/models/a_w_s_node_spec.go
@@ -34,17 +34,16 @@ type AWSNodeSpec struct {
 	// IsSpotInstance indicates whether the created machine is an aws ec2 spot instance or on-demand ec2 instance.
 	IsSpotInstance bool `json:"isSpotInstance,omitempty"`
 
-	// SpotInstanceInterruptionBehavior Sets the interruption behavior for the spot instance when capacity is no longer
+	// SpotInstanceInterruptionBehavior sets the interruption behavior for the spot instance when capacity is no longer
 	// available at the price you specified, if there is no capacity, or if a constraint cannot be met. Charges for EBS
 	// volume storage apply when an instance is stopped.
 	SpotInstanceInterruptionBehavior string `json:"spotInstanceInterruptionBehavior,omitempty"`
 
-	// SpotInstanceMaxPrice The maximum price you are willing to pay per instance hour. Your instance runs when
+	// SpotInstanceMaxPrice is the maximum price you are willing to pay per instance hour. Your instance runs when
 	// your maximum price is greater than the Spot Price.
 	SpotInstanceMaxPrice string `json:"spotInstanceMaxPrice,omitempty"`
 
-	// SpotInstancePersistentRequest Ensures that your request will be submitted every time your Spot Instance is terminated.
-	// For information about Persistent requests and other customization options
+	// SpotInstancePersistentRequest ensures that your request will be submitted every time your Spot Instance is terminated.
 	SpotInstancePersistentRequest bool `json:"spotInstancePersistentRequest,omitempty"`
 
 	// The VPC subnet to which the node shall be connected.
