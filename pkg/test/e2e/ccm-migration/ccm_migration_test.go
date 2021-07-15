93aa86ccfdb099a335ee07ef5f6f30ac9f2900c0
mlavacca <lavacca.mattia@gmail.com>
2021-07-15 14:52:19 +0200

    Test moved under different pkg
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

---
+++ pkg/test/e2e/ccm-migration/ccm_migration_test.go
@@ -0,0 +1,166 @@
+/*
+Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ccmmigration
+
+import (
+	"context"
+	"fmt"
+	"github.com/kubermatic/machine-controller/pkg/machines/v1alpha1"
+	types2 "github.com/kubermatic/machine-controller/pkg/providerconfig/types"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	clusterclient "k8c.io/kubermatic/v2/pkg/cluster/client"
+	kubermaticv1 "k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1"
+	"k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1/helper"
+	"k8c.io/kubermatic/v2/pkg/resources"
+	e2eutils "k8c.io/kubermatic/v2/pkg/test/e2e/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/apimachinery/pkg/util/wait"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
+	"time"
+)
+
+const (
+	userClusterPollInterval = 5 * time.Second
+	customTestTimeout = 10 * time.Minute
+)
+
+var _ = ginkgo.Describe("The Tunneling strategy", func() {
+	var (
+		userClient ctrlruntimeclient.Client
+		clusterJig  *ClusterJig
+	)
+
+	ginkgo.Context("supported provider", func() {
+		ginkgo.BeforeEach(func() {
+			seedClient, _, _ := e2eutils.GetClientsOrDie()
+			clusterClientProvider, err := clusterclient.NewInternal(seedClient)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+			clusterJig = &ClusterJig{
+				Log:            e2eutils.DefaultLogger,
+				SeedClient:     seedClient,
+				Name:           rand.String(10),
+				DatacenterName: options.datacenter,
+				Version:        options.kubernetesVersion,
+			}
+
+			gomega.Expect(clusterJig.SetUp(kubermaticv1.CloudSpec{
+				Openstack:   &kubermaticv1.OpenstackCloudSpec{
+					CredentialsReference: &types2.GlobalSecretKeySelector{
+						ObjectReference: corev1.ObjectReference{
+							Name:      fmt.Sprintf("credential-openstack-%s", clusterJig.Name),
+							Namespace: resources.KubermaticNamespace,
+						},
+					},
+				},
+				DatacenterName: options.datacenter,
+			}, options.osCredentials)).NotTo(gomega.HaveOccurred(), "user cluster should deploy successfully")
+
+			userClient, err = clusterClientProvider.GetClient(context.TODO(), clusterJig.Cluster)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("migrating cluster to external CCM", func() {
+			ginkgo.By("enabling externalCloudProvider feature")
+			newCluster := clusterJig.Cluster.DeepCopy()
+			newCluster.Spec.Features = map[string]bool {
+				kubermaticv1.ClusterFeatureExternalCloudProvider: true,
+			}
+			gomega.Expect(clusterJig.SeedClient.Patch(context.TODO(), newCluster, ctrlruntimeclient.MergeFrom(clusterJig.Cluster))).NotTo(gomega.HaveOccurred())
+
+			ginkgo.By("getting the patched cluster")
+			annotatedCluster := &kubermaticv1.Cluster{}
+			gomega.Expect(clusterJig.SeedClient.Get(context.TODO(), types.NamespacedName{Name: clusterJig.Cluster.Name}, annotatedCluster)).NotTo(gomega.HaveOccurred())
+
+			ginkgo.By("asserting the annotations not existence in the cluster")
+			_, ccmOk := annotatedCluster.Annotations[kubermaticv1.CCMMigrationNeededAnnotation]
+			_, csiOk := annotatedCluster.Annotations[kubermaticv1.CSIMigrationNeededAnnotation]
+			gomega.Expect(ccmOk && csiOk).To(gomega.BeTrue())
+
+			ginkgo.By("checking the -node-external-cloud-provider flag in the machineController webhook pod")
+			gomega.Expect(wait.Poll(userClusterPollInterval, customTestTimeout, func() (done bool, err error) {
+				machineControllerWebhookPods := &corev1.PodList{}
+				if err := clusterJig.SeedClient.List(context.TODO(), machineControllerWebhookPods, ctrlruntimeclient.InNamespace(clusterJig.Cluster.Status.NamespaceName), ctrlruntimeclient.MatchingLabels{
+					resources.AppLabelKey: resources.MachineControllerWebhookDeploymentName,
+				}); err != nil {
+					return false, err
+				}
+				if len(machineControllerWebhookPods.Items) != 1 {
+					return false, nil
+				}
+				for _, arg := range machineControllerWebhookPods.Items[0].Spec.Containers[0].Args {
+					if strings.Contains(arg, "-node-external-cloud-provider") {
+						return true, nil
+					}
+				}
+				return false, nil
+			})).NotTo(gomega.HaveOccurred())
+
+			ginkgo.By("rolling out all the machines")
+			gomega.Expect(userClient.DeleteAllOf(context.TODO(), &v1alpha1.Machine{})).NotTo(gomega.HaveOccurred())
+
+			ginkgo.By("waiting for the complete cluster migration")
+			gomega.Expect(wait.Poll(userClusterPollInterval, customTestTimeout, func() (done bool, err error) {
+				migratingCluster := &kubermaticv1.Cluster{}
+				if err := clusterJig.SeedClient.Get(context.TODO(), types.NamespacedName{Name: clusterJig.Cluster.Name}, migratingCluster); err != nil {
+					return false, err
+				}
+				if helper.ClusterConditionHasStatus(migratingCluster, kubermaticv1.ClusterConditionCSIKubeletMigrationCompleted, corev1.ConditionTrue) {
+					return true, nil
+				}
+				return false, nil
+			})).NotTo(gomega.HaveOccurred())
+		})
+	})
+
+	/*ginkgo.Context("unsupported provider", func() {
+		ginkgo.BeforeEach(func() {
+			k8scli, _, _ := e2eutils.GetClientsOrDie()
+			clusterJig = &ClusterJig{
+				Log:            e2eutils.DefaultLogger,
+				SeedClient:         k8scli,
+				Name:           rand.String(10),
+				DatacenterName: options.datacenter,
+				Version:        options.kubernetesVersion,
+			}
+			gomega.Expect(clusterJig.SetUp(kubermaticv1.CloudSpec{
+				Fake:   &kubermaticv1.FakeCloudSpec{},
+				DatacenterName: options.datacenter,
+			})).NotTo(gomega.HaveOccurred(), "user cluster should deploy successfully")
+		})
+
+		ginkgo.It("migrating cluster to external CCM", func() {
+			ginkgo.By("enabling externalCloudProvider feature")
+			newCluster := clusterJig.Cluster.DeepCopy()
+			newCluster.Spec.Features[kubermaticv1.ClusterFeatureExternalCloudProvider] = true
+			gomega.Expect(clusterJig.SeedClient.Patch(context.TODO(), newCluster, ctrlruntimeclient.MergeFrom(clusterJig.Cluster))).To(nil)
+
+			ginkgo.By("getting the patched cluster")
+			annotatedCluster := &kubermaticv1.Cluster{}
+			gomega.Expect(clusterJig.SeedClient.Get(context.TODO(), types.NamespacedName{Name: clusterJig.Cluster.Name}, annotatedCluster)).To(nil)
+
+			ginkgo.By("asserting the annotations not existence in the cluster")
+			_, ccmOk := annotatedCluster.Annotations[kubermaticv1.CCMMigrationNeededAnnotation]
+			_, csiOk := annotatedCluster.Annotations[kubermaticv1.CSIMigrationNeededAnnotation]
+			gomega.Expect(ccmOk && csiOk).To(gomega.BeFalse())
+		})
+	})*/
+})
---
+++ pkg/test/e2e/ccm-migration/cluster.go
@@ -0,0 +1,175 @@
+/*
+Copyright 2021 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ccmmigration
+
+import (
+	"context"
+	"fmt"
+	"k8c.io/kubermatic/v2/pkg/resources"
+	"time"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+
+	kubermaticv1 "k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1"
+	kubermaticv1helper "k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1/helper"
+	"k8c.io/kubermatic/v2/pkg/semver"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/utils/pointer"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	clusterReadinessCheckPeriod = 10 * time.Second
+	clusterReadinessTimeout     = 10 * time.Minute
+)
+
+// ClusterJig helps setting up a user cluster for testing.
+type ClusterJig struct {
+	Log            *zap.SugaredLogger
+	Name           string
+	DatacenterName string
+	Version        semver.Semver
+	SeedClient         ctrlruntimeclient.Client
+	userClient ctrlruntimeclient.Client
+
+	Cluster *kubermaticv1.Cluster
+}
+
+type credentials struct {
+	username string
+	password string
+	tenant string
+	domain string
+}
+
+func (c *ClusterJig) SetUp(cloudSpec kubermaticv1.CloudSpec, osCredentials credentials) error {
+	c.Log.Debugw("Creating cluster", "name", c.Name)
+
+	if err := c.createSecret(cloudSpec.Openstack.CredentialsReference.Name, osCredentials); err != nil {
+		return err
+	}
+	if err := c.createCluster(cloudSpec); err != nil {
+		return nil
+	}
+	return c.waitForClusterControlPlaneReady(c.Cluster)
+}
+
+func (c *ClusterJig) createSecret(secretName string, osCredentials credentials) error {
+	secretData := map[string][]byte{
+		resources.OpenstackUsername:                    []byte(osCredentials.username),
+		resources.OpenstackPassword:                    []byte(osCredentials.password),
+		resources.OpenstackTenant:                      []byte(osCredentials.tenant),
+		resources.OpenstackTenantID:                    []byte(""),
+		resources.OpenstackDomain:                      []byte(osCredentials.domain),
+		resources.OpenstackApplicationCredentialID:     []byte(""),
+		resources.OpenstackApplicationCredentialSecret: []byte(""),
+		resources.OpenstackToken:                       []byte(""),
+	}
+	credentialSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secretName,
+			Namespace: resources.KubermaticNamespace,
+			Labels: map[string]string{
+				"name":                         secretName,
+			},
+		},
+		Type: corev1.SecretTypeOpaque,
+		Data: secretData,
+	}
+
+	if err := c.SeedClient.Create(context.TODO(), credentialSecret); err != nil {
+		return errors.Wrap(err, "failed to create credential secret")
+	}
+
+	return nil
+}
+
+func (c *ClusterJig) createCluster(cloudSpec kubermaticv1.CloudSpec) error {
+	c.Cluster = &kubermaticv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: c.Name,
+		},
+		Spec: kubermaticv1.ClusterSpec{
+			Cloud: cloudSpec,
+			ClusterNetwork: kubermaticv1.ClusterNetworkingConfig{
+				Services: kubermaticv1.NetworkRanges{
+					CIDRBlocks: []string{"10.240.16.0/20"},
+				},
+				Pods: kubermaticv1.NetworkRanges{
+					CIDRBlocks: []string{"172.25.0.0/16"},
+				},
+				ProxyMode: "ipvs",
+			},
+			ComponentsOverride: kubermaticv1.ComponentSettings{
+				Apiserver: kubermaticv1.APIServerSettings{
+					EndpointReconcilingDisabled: pointer.BoolPtr(true),
+					DeploymentSettings: kubermaticv1.DeploymentSettings{
+						Replicas: pointer.Int32Ptr(1),
+					},
+				},
+				ControllerManager: kubermaticv1.ControllerSettings{
+					DeploymentSettings: kubermaticv1.DeploymentSettings{
+						Replicas: pointer.Int32Ptr(1),
+					},
+				},
+				Etcd: kubermaticv1.EtcdStatefulSetSettings{
+					ClusterSize: 1,
+				},
+				Scheduler: kubermaticv1.ControllerSettings{
+					DeploymentSettings: kubermaticv1.DeploymentSettings{
+						Replicas: pointer.Int32Ptr(1),
+					},
+				},
+			},
+			EnableUserSSHKeyAgent: pointer.BoolPtr(false),
+			ExposeStrategy:        kubermaticv1.ExposeStrategyTunneling,
+			HumanReadableName:     "test",
+			Version:               c.Version,
+		},
+		Status: kubermaticv1.ClusterStatus{
+			NamespaceName: fmt.Sprintf("cluster-%s", c.Name),
+			UserEmail:     "e2e@test.com",
+		},
+	}
+	if err := c.SeedClient.Create(context.TODO(), c.Cluster); err != nil {
+		return errors.Wrap(err, "failed to create cluster")
+	}
+
+	return nil
+}
+
+// CleanUp deletes the cluster.
+func (c *ClusterJig) CleanUp() error {
+	return c.SeedClient.Delete(context.TODO(), c.Cluster)
+}
+
+func (c *ClusterJig) waitForClusterControlPlaneReady(cl *kubermaticv1.Cluster) error {
+	return wait.PollImmediate(clusterReadinessCheckPeriod, clusterReadinessTimeout, func() (bool, error) {
+		if err := c.SeedClient.Get(context.Background(), ctrlruntimeclient.ObjectKey{Name: c.Name, Namespace: cl.Namespace}, cl); err != nil {
+			return false, err
+		}
+		_, cond := kubermaticv1helper.GetClusterCondition(cl, kubermaticv1.ClusterConditionSeedResourcesUpToDate)
+		if cond != nil && cond.Status == corev1.ConditionTrue {
+			return true, nil
+		}
+		return false, nil
+	})
+}
