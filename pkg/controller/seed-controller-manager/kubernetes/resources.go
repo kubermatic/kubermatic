954631035f78b368a19b223800115f856d0130cc
Iacopo Rozzo <iacopo@kubermatic.com>
2021-07-19 17:43:09 +0200

    Fix integration test

--- pkg/controller/seed-controller-manager/kubernetes/resources.go
+++ pkg/controller/seed-controller-manager/kubernetes/resources.go
@@ -410,7 +410,7 @@ func (r *Reconciler) ensureNetworkPolicies(ctx context.Context, c *kubermaticv1.
 		apiserver.EctdAllowCreator(c),
 		apiserver.OpenVPNServerAllowCreator(c),
 	}
-	if err := reconciling.ReconcileNetworkPolicies(ctx, namedNetworkPolicyCreatorGetters, "", r.Client); err != nil {
+	if err := reconciling.ReconcileNetworkPolicies(ctx, namedNetworkPolicyCreatorGetters, c.Status.NamespaceName, r.Client); err != nil {
 		return fmt.Errorf("failed to ensure Network Policies: %v", err)
 	}
 
