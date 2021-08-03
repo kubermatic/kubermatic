df43ae60478fa31b6ea2d7a1aa829a84509637fe
Marcin Maciaszczyk <marcin9yk@icloud.com>
2021-08-03 13:18:22 +0200

    Return seed data

--- pkg/provider/conversions.go
+++ pkg/provider/conversions.go
@@ -25,9 +25,11 @@ import (
 	"k8c.io/kubermatic/v2/pkg/util/errors"
 )
 
-// DatacenterFromSeedMap is needed because the cloud providers are initialized once during startup and get all DCs.
-// We need to change the cloud providers to by dynamically initialized when needed instead once we support datacenters
-// as CRDs.
+// DatacenterFromSeedMap returns datacenter from the seed:datacenter map.
+//
+// It is needed because the cloud providers are initialized once during startup and get all DCs. We need to change
+// the cloud providers to by dynamically initialized when needed instead once we support datacenters as CRDs.
+//
 // TODO: Find a way to lift the current requirement of unique datacenter names. It is needed only because we put
 // 	 the datacenter name in the cluster object but not the seed name.
 func DatacenterFromSeedMap(userInfo *UserInfo, seedsGetter SeedsGetter, datacenterName string) (*kubermaticv1.Seed, *kubermaticv1.Datacenter, error) {
@@ -36,22 +38,25 @@ func DatacenterFromSeedMap(userInfo *UserInfo, seedsGetter SeedsGetter, datacent
 		return nil, nil, errors.New(http.StatusInternalServerError, fmt.Sprintf("failed to list seeds: %v", err))
 	}
 
-	var datacenters []kubermaticv1.Datacenter
+	var matchingDatacenters []kubermaticv1.Datacenter
+	var matchingSeeds []*kubermaticv1.Seed
 	for _, seed := range seeds {
 		if dc, exists := seed.Spec.Datacenters[datacenterName]; exists {
-			datacenters = append(datacenters, dc)
+			matchingDatacenters = append(matchingDatacenters, dc)
+			matchingSeeds = append(matchingSeeds, seed)
 		}
 	}
 
-	if len(datacenters) == 0 {
+	if len(matchingDatacenters) == 0 {
 		return nil, nil, errors.New(http.StatusNotFound, fmt.Sprintf("datacenter %q not found", datacenterName))
 	}
 
-	if count := len(datacenters); count > 1 {
+	if count := len(matchingDatacenters); count > 1 {
 		return nil, nil, errors.New(http.StatusInternalServerError, fmt.Sprintf("expected to find exactly one datacenter with name %q, got %d", datacenterName, count))
 	}
 
-	matchingDatacenter := datacenters[0]
+	matchingDatacenter := matchingDatacenters[0]
+	matchingSeed := matchingSeeds[0]
 
 	if !userInfo.IsAdmin {
 		emailDomain, err := getUserEmailDomain(userInfo)
@@ -60,12 +65,11 @@ func DatacenterFromSeedMap(userInfo *UserInfo, seedsGetter SeedsGetter, datacent
 		}
 
 		if !canAccessDatacenter(matchingDatacenter, emailDomain) {
-			return nil, nil, errors.New(http.StatusUnauthorized, fmt.Sprintf("cannot access %s datacenter due to email domain requirements", datacenterName))
+			return nil, nil, errors.New(http.StatusForbidden, fmt.Sprintf("cannot access %s datacenter due to email domain requirements", datacenterName))
 		}
 	}
 
-	// TODO: Get seed.
-	return nil, &matchingDatacenter, nil
+	return matchingSeed, &matchingDatacenter, nil
 }
 
 // canAccessDatacenter returns information if user with provided email domain can access given datacenter
