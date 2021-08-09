282115c534c98166003a0258bbc8328a2972baa6
Matthias Osthues <osthues.matthias@gmail.com>
2021-08-09 10:35:46 +0200

    update api client

--- pkg/test/e2e/utils/apiclient/models/ip_v_s_configuration.go
+++ pkg/test/e2e/utils/apiclient/models/ip_v_s_configuration.go
@@ -6,6 +6,8 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"context"
+
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -22,6 +24,11 @@ type IPVSConfiguration struct {
 
 // Validate validates this IP v s configuration
 func (m *IPVSConfiguration) Validate(formats strfmt.Registry) error {
+	return nil
+}
+
+// ContextValidate validates this IP v s configuration based on context it is used
+func (m *IPVSConfiguration) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
 	return nil
 }
 
