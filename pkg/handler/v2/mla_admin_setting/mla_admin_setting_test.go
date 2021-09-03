7881ba7005b4657023e9d3d8ed337d8ca50b88c3
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2021-09-03 17:48:40 +0800

    Fix lint

--- pkg/handler/v2/mla_admin_setting/mla_admin_setting_test.go
+++ pkg/handler/v2/mla_admin_setting/mla_admin_setting_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package mla_admin_setting_test
+package mlaadminsetting_test
 
 import (
 	"bytes"
--- pkg/handler/v2/mla_admin_setting/request.go
+++ pkg/handler/v2/mla_admin_setting/request.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package mla_admin_setting
+package mlaadminsetting
 
 import (
 	"context"
