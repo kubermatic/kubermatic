2da0ad23c43135ba80a25ed429192a38632da6aa
Harshita <harshita.sharma6174@gmail.com>
2021-09-09 01:52:54 +0530

    Remove gatekeeper v3.5.2 crds on OPA disabled
    Signed-off-by: Harshita <harshita.sharma6174@gmail.com>

--- pkg/controller/user-cluster-controller-manager/resources/reconciler.go
+++ pkg/controller/user-cluster-controller-manager/resources/reconciler.go
@@ -51,6 +51,7 @@ import (
 	"k8c.io/kubermatic/v2/pkg/resources/certificates/triple"
 	"k8c.io/kubermatic/v2/pkg/resources/reconciling"
 
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -177,6 +178,12 @@ func (r *reconciler) reconcile(ctx context.Context) error {
 		}
 	} else {
 		if err := r.healthCheck(ctx); err != nil {
+			return err
+		}
+	}
+
+	if r.opaIntegration && !r.opaEnableMutation {
+		if err := r.ensureOPAExperimentalMutationWebhookIsRemoved(ctx); err != nil {
 			return err
 		}
 	}
@@ -515,7 +522,6 @@ func (r *reconciler) reconcileMutatingWebhookConfigurations(ctx context.Context,
 	if r.opaIntegration && r.opaEnableMutation {
 		creators = append(creators, gatekeeper.MutatingWebhookConfigurationCreator(r.opaWebhookTimeout))
 	}
-
 	if err := reconciling.ReconcileMutatingWebhookConfigurations(ctx, creators, "", r.Client); err != nil {
 		return fmt.Errorf("failed to reconcile MutatingWebhookConfigurations: %v", err)
 	}
@@ -830,6 +836,16 @@ func (r *reconciler) ensureOPAIntegrationIsRemoved(ctx context.Context) error {
 		}
 	}
 
+	return nil
+}
+
+func (r *reconciler) ensureOPAExperimentalMutationWebhookIsRemoved(ctx context.Context) error {
+	if err := r.Client.Delete(ctx, &admissionregistrationv1.MutatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: resources.GatekeeperMutatingWebhookConfigurationName,
+		}}); err != nil && !errors.IsNotFound(err) {
+		return fmt.Errorf("failed to remove Mutation Webhook: %v", err)
+	}
 	return nil
 }
 
