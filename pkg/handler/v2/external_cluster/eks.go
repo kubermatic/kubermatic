f2bb98c1473cd671a83a67b5977806391c68900c
harshita sharma <78943096+imharshita@users.noreply.github.com>
2021-11-12 17:16:01 +0530

    Get eks nodegroup endpoint (#8130)
    * get and resize eks nodegroup endpoint
    
    Signed-off-by: Harshita <harshita.sharma6174@gmail.com>
    
    * Get EKS nodegroup machinedeployments
    
    Signed-off-by: Harshita <harshita.sharma6174@gmail.com>

--- pkg/handler/v2/external_cluster/eks.go
+++ pkg/handler/v2/external_cluster/eks.go
@@ -22,16 +22,18 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/eks"
 
+	clusterv1alpha1 "github.com/kubermatic/machine-controller/pkg/apis/cluster/v1alpha1"
 	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
 	apiv2 "k8c.io/kubermatic/v2/pkg/api/v2"
 	kubermaticapiv1 "k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1"
 	"k8c.io/kubermatic/v2/pkg/handler/v1/common"
 	kuberneteshelper "k8c.io/kubermatic/v2/pkg/kubernetes"
 	"k8c.io/kubermatic/v2/pkg/provider"
-	"k8c.io/kubermatic/v2/pkg/provider/cloud/aws"
 	awsprovider "k8c.io/kubermatic/v2/pkg/provider/cloud/aws"
 	"k8c.io/kubermatic/v2/pkg/util/errors"
 )
+
+const EKSNodeGroupNameLabel = "eks.amazonaws.com/nodegroup"
 
 func createEKSCluster(ctx context.Context, name string, userInfoGetter provider.UserInfoGetter, project *kubermaticapiv1.Project, cloud *apiv2.ExternalClusterCloudSpec, clusterProvider provider.ExternalClusterProvider, privilegedClusterProvider provider.PrivilegedExternalClusterProvider) (*kubermaticapiv1.ExternalCluster, error) {
 	if cloud.EKS.Name == "" || cloud.EKS.Region == "" || cloud.EKS.AccessKeyID == "" || cloud.EKS.SecretAccessKey == "" {
@@ -57,7 +59,7 @@ func createEKSCluster(ctx context.Context, name string, userInfoGetter provider.
 
 func patchEKSCluster(ctx context.Context, old, new *apiv2.ExternalCluster, secretKeySelector provider.SecretKeySelectorValueFunc, cloudSpec *kubermaticapiv1.ExternalClusterCloudSpec) (*string, error) {
 
-	accessKeyID, secretAccessKey, err := aws.GetCredentialsForEKSCluster(*cloudSpec, secretKeySelector)
+	accessKeyID, secretAccessKey, err := awsprovider.GetCredentialsForEKSCluster(*cloudSpec, secretKeySelector)
 	if err != nil {
 		return nil, err
 	}
@@ -82,4 +84,78 @@ func patchEKSCluster(ctx context.Context, old, new *apiv2.ExternalCluster, secre
 	status := "Cluster Upgrade " + *updateOutput.Update.Status
 
 	return &status, nil
+}
+
+func getEKSNodePools(ctx context.Context, cluster *kubermaticapiv1.ExternalCluster, secretKeySelector provider.SecretKeySelectorValueFunc, cloudSpec *kubermaticapiv1.ExternalClusterCloudSpec, clusterProvider provider.ExternalClusterProvider) ([]apiv2.ExternalClusterMachineDeployment, error) {
+	accessKeyID, secretAccessKey, err := awsprovider.GetCredentialsForEKSCluster(*cloudSpec, secretKeySelector)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := awsprovider.GetClientSet(accessKeyID, secretAccessKey, cloudSpec.EKS.Region)
+	if err != nil {
+		return nil, err
+	}
+
+	clusterName := cloudSpec.EKS.Name
+	nodeInput := &eks.ListNodegroupsInput{
+		ClusterName: &clusterName,
+	}
+	nodeOutput, err := client.EKS.ListNodegroups(nodeInput)
+	if err != nil {
+		return nil, err
+	}
+	nodeGroups := nodeOutput.Nodegroups
+
+	machineDeployments := make([]apiv2.ExternalClusterMachineDeployment, 0, len(nodeGroups))
+
+	nodes, err := clusterProvider.ListNodes(cluster)
+	if err != nil {
+		return nil, common.KubernetesErrorToHTTPError(err)
+	}
+
+	for _, nodeGroupName := range nodeGroups {
+		var readyReplicas int32
+		for _, n := range nodes.Items {
+			if n.Labels != nil {
+				if n.Labels[EKSNodeGroupNameLabel] == *nodeGroupName {
+					readyReplicas++
+				}
+			}
+		}
+
+		nodeGroupInput := &eks.DescribeNodegroupInput{
+			ClusterName:   &clusterName,
+			NodegroupName: nodeGroupName,
+		}
+
+		nodeGroupOutput, err := client.EKS.DescribeNodegroup(nodeGroupInput)
+		if err != nil {
+			return nil, err
+		}
+		nodeGroup := nodeGroupOutput.Nodegroup
+		machineDeployments = append(machineDeployments, apiv2.ExternalClusterMachineDeployment{
+			NodeDeployment: apiv1.NodeDeployment{
+				ObjectMeta: apiv1.ObjectMeta{
+					ID:   *nodeGroup.NodegroupName,
+					Name: *nodeGroup.NodegroupName,
+				},
+				Spec: apiv1.NodeDeploymentSpec{
+					Replicas: int32(*nodeGroup.ScalingConfig.DesiredSize),
+					Template: apiv1.NodeSpec{
+						Versions: apiv1.NodeVersionInfo{
+							Kubelet: *nodeGroup.Version,
+						},
+					},
+				},
+				Status: clusterv1alpha1.MachineDeploymentStatus{
+					Replicas:      int32(*nodeGroup.ScalingConfig.DesiredSize),
+					ReadyReplicas: readyReplicas,
+				},
+			},
+		})
+
+	}
+
+	return machineDeployments, err
 }
