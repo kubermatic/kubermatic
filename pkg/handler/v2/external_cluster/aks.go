883977c4e073f81cf107e4110d23ea5e76849f8a
Harshita <imharshita@github.com>
2021-12-01 07:41:36 +0530

    upgrade-aks
    Signed-off-by: Harshita <imharshita@github.com>

--- pkg/handler/v2/external_cluster/aks.go
+++ pkg/handler/v2/external_cluster/aks.go
@@ -18,6 +18,10 @@ package externalcluster
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/containerservice/mgmt/containerservice"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
 
 	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
 	apiv2 "k8c.io/kubermatic/v2/pkg/api/v2"
@@ -25,6 +29,7 @@ import (
 	"k8c.io/kubermatic/v2/pkg/handler/v1/common"
 	kuberneteshelper "k8c.io/kubermatic/v2/pkg/kubernetes"
 	"k8c.io/kubermatic/v2/pkg/provider"
+	"k8c.io/kubermatic/v2/pkg/provider/cloud/azure"
 	"k8c.io/kubermatic/v2/pkg/util/errors"
 )
 
@@ -48,4 +53,41 @@ func createAKSCluster(ctx context.Context, name string, userInfoGetter provider.
 	newCluster.Spec.CloudSpec.AKS.CredentialsReference = keyRef
 
 	return createNewCluster(ctx, userInfoGetter, clusterProvider, privilegedClusterProvider, newCluster, project)
+}
+
+func patchAKSCluster(ctx context.Context, old, new *apiv2.ExternalCluster, secretKeySelector provider.SecretKeySelectorValueFunc, cloudSpec *kubermaticapiv1.ExternalClusterCloudSpec) (*apiv2.ExternalCluster, error) {
+	resourceGroupName := cloudSpec.AKS.ResourceGroup
+	resourceName := cloudSpec.AKS.Name
+	newVersion := new.Spec.Version.Semver().String()
+
+	cred, err := azure.GetCredentialsForAKSCluster(*cloudSpec, secretKeySelector)
+	if err != nil {
+		return nil, err
+	}
+
+	aksClient := containerservice.NewManagedClustersClient(cred.SubscriptionID)
+	aksClient.Authorizer, err = auth.NewClientCredentialsConfig(cred.ClientID, cred.ClientSecret, cred.TenantID).Authorizer()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create authorizer: %v", err.Error())
+	}
+
+	cluster, err := aksClient.Get(ctx, resourceGroupName, resourceName)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get AKS managed cluster %v from resource group %v: %v", cloudSpec.AKS.Name, cloudSpec.AKS.ResourceGroup, err)
+	}
+
+	location := *cluster.Location
+
+	updateCluster := containerservice.ManagedCluster{
+		Location: &location,
+		ManagedClusterProperties: &containerservice.ManagedClusterProperties{
+			KubernetesVersion: &newVersion,
+		},
+	}
+	_, err = aksClient.CreateOrUpdate(ctx, resourceGroupName, resourceName, updateCluster)
+	if err != nil {
+		return nil, err
+	}
+
+	return new, nil
 }
