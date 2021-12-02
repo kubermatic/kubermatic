fdf6c4223155fe7da8ab0ff49c89bdae0df83e02
mlavacca <lavacca.mattia@gmail.com>
2021-12-02 08:17:38 +0100

    Fix prow jobs
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

--- pkg/resources/cluster/cluster.go
+++ pkg/resources/cluster/cluster.go
@@ -63,10 +63,6 @@ func Spec(apiCluster apiv1.Cluster, dc *kubermaticv1.Datacenter, secretKeyGetter
 		spec.ClusterNetwork = *apiCluster.Spec.ClusterNetwork
 	}
 
-	// if apiCluster.Spec.AllowedIPRange != "" {
-	// 	spec.AllowedIPRange = apiCluster.Spec.AllowedIPRange
-	// }
-
 	providerName, err := provider.ClusterCloudProviderName(spec.Cloud)
 	if err != nil {
 		return nil, fmt.Errorf("invalid cloud spec: %v", err)
