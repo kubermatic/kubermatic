6f302712854b246fe0339a4b39680fd7be7a8d1b
lsviben <sviben.lovro@gmail.com>
2021-12-13 15:38:08 +0100

    fix import

--- pkg/api/v2/types.go
+++ pkg/api/v2/types.go
@@ -19,10 +19,9 @@ package v2
 import (
 	"github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1beta1"
 
-	ksemver "k8c.io/kubermatic/v2/pkg/semver"
-
 	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
 	crdapiv1 "k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1"
+	ksemver "k8c.io/kubermatic/v2/pkg/semver"
 
 	corev1 "k8s.io/api/core/v1"
 )
