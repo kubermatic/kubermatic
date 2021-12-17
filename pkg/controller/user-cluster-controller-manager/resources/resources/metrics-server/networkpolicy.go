35178bfd4e3178030ef6e6f4483704cf6ba440b8
Matthias Osthues <osthues.matthias@gmail.com>
2021-12-17 17:51:42 +0100

    Fix for Kubesystem network policiies (#8485)
    * fix wrong featuregate name
    
    * set proocol for metricsserver network policy
    
    * allow ingress to  metric-server from Konnectivity Agent

--- pkg/controller/user-cluster-controller-manager/resources/resources/metrics-server/networkpolicy.go
+++ pkg/controller/user-cluster-controller-manager/resources/resources/metrics-server/networkpolicy.go
@@ -17,6 +17,7 @@ limitations under the License.
 package metricsserver
 
 import (
+	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -31,6 +32,8 @@ func NetworkPolicyCreator() reconciling.NamedNetworkPolicyCreatorGetter {
 	return func() (string, reconciling.NetworkPolicyCreator) {
 		return "metrics-server", func(np *networkingv1.NetworkPolicy) (*networkingv1.NetworkPolicy, error) {
 			metricsPort := intstr.FromInt(9153)
+			httpsPort := intstr.FromInt(443)
+			protoTcp := v1.ProtocolTCP
 
 			np.Spec = networkingv1.NetworkPolicySpec{
 				PodSelector: metav1.LabelSelector{
@@ -54,7 +57,23 @@ func NetworkPolicyCreator() reconciling.NamedNetworkPolicyCreatorGetter {
 						},
 						Ports: []networkingv1.NetworkPolicyPort{
 							{
-								Port: &metricsPort,
+								Port:     &metricsPort,
+								Protocol: &protoTcp,
+							},
+						},
+					},
+					{
+						From: []networkingv1.NetworkPolicyPeer{
+							{
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{resources.AppLabelKey: resources.KonnectivityDeploymentName},
+								},
+							},
+						},
+						Ports: []networkingv1.NetworkPolicyPort{
+							{
+								Port:     &httpsPort,
+								Protocol: &protoTcp,
 							},
 						},
 					},
