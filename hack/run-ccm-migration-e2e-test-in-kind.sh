e7b37f8b2157eb89f1cd0117a82aa1ca5aac7ecb
mlavacca <lavacca.mattia@gmail.com>
2021-12-21 17:12:26 +0100

    shfmt fix
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

--- hack/run-ccm-migration-e2e-test-in-kind.sh
+++ hack/run-ccm-migration-e2e-test-in-kind.sh
@@ -264,7 +264,7 @@ EXTRA_ARGS="-openstack-domain=${OS_DOMAIN}
     -openstack-password=${OS_PASSWORD}
     -openstack-auth-url=${OS_AUTH_URL}
     -openstack-region=${OS_REGION}
-    -openstack-floating-ip-pool=${floatingIpPool}
+    -openstack-floating-ip-pool=ext-net
     -openstack-network=${OS_NETWORK_NAME}
     -openstack-seed-datacenter=syseleven-dbl1
     -vsphere-seed-datacenter=vsphere-hamburg
