18ac57303c452761ae51af34af5e4a558c05d4ba
Marcin Franczyk <marcin0franczyk@gmail.com>
2021-12-21 15:12:05 +0100

    test
    Signed-off-by: Marcin Franczyk <marcin0franczyk@gmail.com>

--- pkg/resources/cloudcontroller/kubevirt.go
+++ pkg/resources/cloudcontroller/kubevirt.go
@@ -92,7 +92,7 @@ func kubevirtDeploymentCreator(data *resources.TemplateData) reconciling.NamedDe
 			dep.Spec.Template.Spec.Containers = []corev1.Container{
 				{
 					Name:    ccmContainerName,
-					Image:   data.ImageRegistry(resources.RegistryQuay) + "/kubermatic/kubevirt-cloud-controller-manager:" + KubeVirtCCMTag,
+					Image:   data.ImageRegistry(resources.RegistryDocker) + "/mfranczy/kubevirt-cloud-controller-manager:" + KubeVirtCCMTag,
 					Command: []string{"/bin/kubevirt-cloud-controller-manager"},
 					Args:    getKVFlags(data),
 					VolumeMounts: append(getVolumeMounts(), corev1.VolumeMount{
