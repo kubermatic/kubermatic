6298b110e029a135f42ea2a3d8ad46836c22de3d
Marvin Beckers <marvin@kubermatic.com>
2022-01-04 11:24:34 +0100

    Update etcd to 3.5.1 for Kubernetes 1.22+ (#8563)
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/resources/etcd/statefulset.go
+++ pkg/resources/etcd/statefulset.go
@@ -351,7 +351,7 @@ func ImageTag(c *kubermaticv1.Cluster) string {
 		return "v3.4.3"
 	}
 
-	return "v3.5.0"
+	return "v3.5.1"
 }
 
 func computeReplicas(data etcdStatefulSetCreatorData, set *appsv1.StatefulSet) int32 {
