0832cc93ecd7bfea50b21fd11f5027f0c4e05b09
Waleed Malik <ahmedwaleedmalik@gmail.com>
2022-01-07 16:28:13 +0500

    Update generated api client
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/test/e2e/utils/apiclient/models/port.go
+++ pkg/test/e2e/utils/apiclient/models/port.go
@@ -12,7 +12,7 @@ import (
 	"github.com/go-openapi/swag"
 )
 
-// Port Port represents a port to expose from the virtual machine.
+// Port Port repesents a port to expose from the virtual machine.
 //
 // Default protocol TCP.
 // The port field is mandatory
