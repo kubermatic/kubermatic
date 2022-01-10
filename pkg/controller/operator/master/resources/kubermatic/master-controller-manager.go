57dccfaec870ae33bd5bcd4bcf560b3d56da7457
Christoph Mewes <christoph@loodse.com>
2022-01-11 00:37:20 +0100

    prevent KKP PDBs from blocking node rotations

--- pkg/controller/operator/master/resources/kubermatic/master-controller-manager.go
+++ pkg/controller/operator/master/resources/kubermatic/master-controller-manager.go
@@ -128,7 +128,13 @@ func MasterControllerManagerPDBCreator(cfg *operatorv1alpha1.KubermaticConfigura
 
 	return func() (string, reconciling.PodDisruptionBudgetCreator) {
 		return name, func(pdb *policyv1beta1.PodDisruptionBudget) (*policyv1beta1.PodDisruptionBudget, error) {
+			// To prevent the PDB from blocking node rotations, we accept
+			// 0 minAvailable if the replica count is only 1.
+			// NB: The cfg is defaulted, so Replicas==nil cannot happen.
 			min := intstr.FromInt(1)
+			if cfg.Spec.MasterController.Replicas != nil && *cfg.Spec.MasterController.Replicas < 2 {
+				min = intstr.FromInt(0)
+			}
 
 			pdb.Spec.MinAvailable = &min
 			pdb.Spec.Selector = &metav1.LabelSelector{
