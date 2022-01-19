bae39e17da4752d79a9d8ac606f284f3c2ffd7eb
Christoph Mewes <christoph@loodse.com>
2022-01-19 11:27:09 +0100

    update kube-state-metrics to 2.3.0

--- pkg/resources/kubestatemetrics/deployment.go
+++ pkg/resources/kubestatemetrics/deployment.go
@@ -48,7 +48,7 @@ var (
 
 const (
 	name    = "kube-state-metrics"
-	version = "v2.2.3"
+	version = "v2.3.0"
 )
 
 // DeploymentCreator returns the function to create and update the kube-state-metrics deployment
