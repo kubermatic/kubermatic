8e1bbc670f7457b33320e8eec4336f5ce2010fec
Christoph Mewes <christoph@loodse.com>
2022-01-26 11:17:47 +0100

    update docs

--- hack/update-docs.sh
+++ hack/update-docs.sh
@@ -29,13 +29,13 @@ dummy=kubermaticNoOmitPlease
 go mod vendor
 
 # remove omitempty tags from structs so that genyaml will not skip fields
-sed -i "s/,omitempty/,$dummy/g" pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go vendor/k8s.io/api/core/v1/*.go
+sed -i "s/,omitempty/,$dummy/g" pkg/apis/kubermatic/v1/*.go vendor/k8s.io/api/core/v1/*.go
 
 # there are some fields that we do actually want to ignore
-sed -i 's/omitgenyaml/omitempty/g' pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go
+sed -i 's/omitgenyaml/omitempty/g' pkg/apis/kubermatic/v1/*.go
 
 go run cmd/example-yaml-generator/main.go . docs
 
 # revert our changes
-sed -i 's/omitempty/omitgenyaml/g' pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go
-sed -i "s/,$dummy/,omitempty/g" pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go vendor/k8s.io/api/core/v1/*.go
+sed -i 's/omitempty/omitgenyaml/g' pkg/apis/kubermatic/v1/*.go
+sed -i "s/,$dummy/,omitempty/g" pkg/apis/kubermatic/v1/*.go vendor/k8s.io/api/core/v1/*.go
