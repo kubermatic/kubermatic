ae6946f55e49580e8221593eb6fb35450fe6713e
Marvin Beckers <marvin@kubermatic.com>
2022-02-01 18:05:37 +0100

    Make hack/update-docs.sh work on macOS (#8822)
    * Make hack/update-docs.sh work on macOS
    
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>
    
    * Make shfmt happy
    
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- hack/update-docs.sh
+++ hack/update-docs.sh
@@ -28,14 +28,17 @@ dummy=kubermaticNoOmitPlease
 # temporarily create a vendor folder
 go mod vendor
 
+sed="sed"
+[ "$(command -v gsed)" ] && sed="gsed"
+
 # remove omitempty tags from structs so that genyaml will not skip fields
-sed -i "s/,omitempty/,$dummy/g" pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go vendor/k8s.io/api/core/v1/*.go
+$sed -i "s/,omitempty/,$dummy/g" pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go vendor/k8s.io/api/core/v1/*.go
 
 # there are some fields that we do actually want to ignore
-sed -i 's/omitgenyaml/omitempty/g' pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go
+$sed -i 's/omitgenyaml/omitempty/g' pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go
 
 go run cmd/example-yaml-generator/main.go . docs
 
 # revert our changes
-sed -i 's/omitempty/omitgenyaml/g' pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go
-sed -i "s/,$dummy/,omitempty/g" pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go vendor/k8s.io/api/core/v1/*.go
+$sed -i 's/omitempty/omitgenyaml/g' pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go
+$sed -i "s/,$dummy/,omitempty/g" pkg/crd/kubermatic/v1/*.go pkg/crd/operator/v1alpha1/*.go vendor/k8s.io/api/core/v1/*.go
