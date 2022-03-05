b2dd6bd6c0ef9cf3d08540a520b807917c68f472
Christoph Mewes <christoph@loodse.com>
2022-03-05 22:05:46 +0100

    use reconciling framework in role-cloner controller

--- pkg/controller/user-cluster-controller-manager/role-cloner-controller/doc.go
+++ pkg/controller/user-cluster-controller-manager/role-cloner-controller/doc.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 /*
-Package rolecloner contains a controller that duplicates all roles with the `component=userClusterRole`
+Package roleclonercontroller contains a controller that duplicates all roles with the `component=userClusterRole`
 label that are in the kube-system namespace into all other namespaces.
 */
-package rolecloner
+package roleclonercontroller
