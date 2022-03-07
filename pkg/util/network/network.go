46cd6adabf26c5da1ee25151205f9df36e8faf2d
Rastislav Szabo <rastislav@kubermatic.com>
2022-03-07 13:20:59 +0100

    Add dual-stack support for AWS security groups (#9133)
    * Add dual-stack support for AWS security groups
    
    Signed-off-by: Rastislav Szabo <rastislav@kubermatic.com>
    
    * Fix aws cloud provider integration test
    
    Signed-off-by: Rastislav Szabo <rasto@macbook-pro.local>
    
    Co-authored-by: Rastislav Szabo <rasto@macbook-pro.local>

---
+++ pkg/util/network/network.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2022 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package network
+
+import (
+	kubermaticv1 "k8c.io/kubermatic/v2/pkg/apis/kubermatic/v1"
+
+	"k8s.io/utils/net"
+)
+
+// IsIPv4OnlyCluster returns true if the cluster networking is IPv4-only.
+func IsIPv4OnlyCluster(cluster *kubermaticv1.Cluster) bool {
+	return len(cluster.Spec.ClusterNetwork.Pods.CIDRBlocks) == 1 && net.IsIPv4CIDRString(cluster.Spec.ClusterNetwork.Pods.CIDRBlocks[0])
+}
+
+// IsIPv6OnlyCluster returns true if the cluster networking is IPv6-only.
+func IsIPv6OnlyCluster(cluster *kubermaticv1.Cluster) bool {
+	return len(cluster.Spec.ClusterNetwork.Pods.CIDRBlocks) == 1 && net.IsIPv6CIDRString(cluster.Spec.ClusterNetwork.Pods.CIDRBlocks[0])
+}
+
+// IsDualStackCluster returns true if the cluster networking is dual-stack (IPv4 + IPv6).
+func IsDualStackCluster(cluster *kubermaticv1.Cluster) bool {
+	res, err := net.IsDualStackCIDRStrings(cluster.Spec.ClusterNetwork.Pods.CIDRBlocks)
+	if err != nil {
+		return false
+	}
+	return res
+}
