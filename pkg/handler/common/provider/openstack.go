b6661b3fbffd4714ab70a80baabc29e660a9b108
Pratik Deoghare <pratik.deoghare@gmail.com>
2022-03-07 15:47:59 +0100

    validate openstack datacenter authURL and region (#9248)

--- pkg/handler/common/provider/openstack.go
+++ pkg/handler/common/provider/openstack.go
@@ -386,6 +386,15 @@ func getOpenstackAuthURLAndRegion(userInfo *provider.UserInfo, seedsGetter provi
 	if err != nil {
 		return "", "", fmt.Errorf("failed to find datacenter %q: %w", datacenterName, err)
 	}
+
+	if len(dc.Spec.Openstack.AuthURL) == 0 {
+		return "", "", fmt.Errorf("empty authURL in datacenter %q", datacenterName)
+	}
+
+	if len(dc.Spec.Openstack.Region) == 0 {
+		return "", "", fmt.Errorf("empty region in datacenter %q", datacenterName)
+	}
+
 	return dc.Spec.Openstack.AuthURL, dc.Spec.Openstack.Region, nil
 }
 
