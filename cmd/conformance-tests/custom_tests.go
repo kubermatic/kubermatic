0e2d60524775556f220a0b3ee534579ed27c02ad
Marvin Beckers <marvin@kubermatic.com>
2022-03-21 11:07:24 +0100

    Run a custom test to validate control plane components
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- cmd/conformance-tests/custom_tests.go
+++ cmd/conformance-tests/custom_tests.go
@@ -426,3 +426,69 @@ func (r *testRunner) testUserClusterPodAndNodeMetrics(ctx context.Context, log *
 	log.Info("Successfully validated user cluster pod and node metrics.")
 	return nil
 }
+
+func (r *testRunner) testUserClusterControlPlaneSecurityContext(ctx context.Context, log *zap.SugaredLogger, cluster *kubermaticv1.Cluster, seedClient ctrlruntimeclient.Client) error {
+	log.Infof("Testing security context of control plane components in Seed cluster...")
+
+	pods := &corev1.PodList{}
+
+	if err := seedClient.List(ctx, pods, &ctrlruntimeclient.ListOptions{Namespace: cluster.Status.NamespaceName}); err != nil {
+		return fmt.Errorf("failed to list control plane pods: %w", err)
+	}
+
+	if len(pods.Items) == 0 {
+		return fmt.Errorf("no control plane pods found")
+	}
+
+	errors := []string{}
+
+	for _, pod := range pods.Items {
+		var privilegedContainers int
+		for _, container := range pod.Spec.Containers {
+			if container.SecurityContext != nil && container.SecurityContext.Privileged != nil && *container.SecurityContext.Privileged {
+				privilegedContainers++
+			}
+		}
+
+		// all containers in the Pod are running as privileged, we can skip the Pod; privileged mode disables any seccomp profile
+		if len(pod.Spec.Containers) == privilegedContainers {
+			continue
+		}
+
+		// no security context means no seccomp profile
+		if pod.Spec.SecurityContext == nil {
+			errors = append(
+				errors,
+				fmt.Sprintf("expected security context on Pod %s/%s, got none", pod.Namespace, pod.Name),
+			)
+			continue
+		}
+
+		// no seccomp profile means no profile is applied to the containers
+		if pod.Spec.SecurityContext.SeccompProfile == nil {
+			errors = append(
+				errors,
+				fmt.Sprintf("expected seccomp profile on Pod %s/%s, got none", pod.Namespace, pod.Name),
+			)
+			continue
+		}
+
+		// the 'unconfined' profile disables any seccomp filtering
+		if pod.Spec.SecurityContext.SeccompProfile.Type == corev1.SeccompProfileTypeUnconfined {
+			errors = append(
+				errors,
+				fmt.Sprintf(
+					"seccomp profile of Pod %s/%s is '%s', should be '%s' or '%s'", pod.Namespace, pod.Name,
+					corev1.SeccompProfileTypeUnconfined, corev1.SeccompProfileTypeRuntimeDefault, corev1.SeccompProfileTypeLocalhost,
+				),
+			)
+			continue
+		}
+	}
+
+	if len(errors) == 0 {
+		return nil
+	}
+
+	return fmt.Errorf(strings.Join(errors, "\n"))
+}
