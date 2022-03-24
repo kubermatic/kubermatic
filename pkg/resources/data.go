df8382819cc6eb1c204fcca3b637adabc14733aa
Marko Mudrinić <mudrinic.mare@gmail.com>
2022-03-24 16:24:10 +0100

    Properly check for cluster version
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- pkg/resources/data.go
+++ pkg/resources/data.go
@@ -611,6 +611,12 @@ func GetCSIMigrationFeatureGates(cluster *kubermaticv1.Cluster) []string {
 	var featureFlags []string
 	gte23, _ := semver.NewConstraint(">= 1.23.0")
 	ccm := cluster.Spec.Features[kubermaticv1.ClusterFeatureExternalCloudProvider]
+
+	curVersion := cluster.Status.Versions.ControlPlane
+	if curVersion == "" {
+		curVersion = cluster.Spec.Version
+	}
+
 	if metav1.HasAnnotation(cluster.ObjectMeta, kubermaticv1.CSIMigrationNeededAnnotation) {
 		// The following feature gates are always enabled when the
 		// 'externalCloudProvider' feature is activated.
@@ -623,30 +629,27 @@ func GetCSIMigrationFeatureGates(cluster *kubermaticv1.Cluster) []string {
 		if cluster.Spec.Cloud.VSphere != nil {
 			featureFlags = append(featureFlags, "CSIMigrationvSphere=true")
 		}
-		v := cluster.Status.Versions.ControlPlane
-		if v == "" {
-			v = cluster.Spec.Version
-		}
+
 		// The CSIMigrationNeededAnnotation is removed when all kubelets have
 		// been migrated.
 		if cluster.Status.Conditions[kubermaticv1.ClusterConditionCSIKubeletMigrationCompleted].Status == corev1.ConditionTrue {
 			lessThan21, _ := semver.NewConstraint("< 1.21.0")
 			if cluster.Spec.Cloud.Openstack != nil {
-				if lessThan21.Check(v.Semver()) {
+				if lessThan21.Check(curVersion.Semver()) {
 					featureFlags = append(featureFlags, "CSIMigrationOpenStackComplete=true")
 				} else {
 					featureFlags = append(featureFlags, "InTreePluginOpenStackUnregister=true")
 				}
 			}
 			if cluster.Spec.Cloud.VSphere != nil {
-				if lessThan21.Check(v.Semver()) {
+				if lessThan21.Check(curVersion.Semver()) {
 					featureFlags = append(featureFlags, "CSIMigrationvSphereComplete=true")
 				} else {
 					featureFlags = append(featureFlags, "InTreePluginvSphereUnregister=true")
 				}
 			}
 		}
-	} else if !ccm && gte23.Check(cluster.Status.Versions.ControlPlane.Semver()) {
+	} else if !ccm && gte23.Check(curVersion.Semver()) {
 		// We disable CSIMigration only if Kubernetes version is >= 1.23 and
 		// there's no external CCM.
 		// If there's external CCM, in-tree volumes plugin is not enabled
