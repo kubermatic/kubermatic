95e44042d2956ff3dcd238b23b7451ccf7123559
lsviben <sviben.lovro@gmail.com>
2022-04-14 11:45:06 +0200

    gen fixtures

--- pkg/resources/webhook/deployment.go
+++ pkg/resources/webhook/deployment.go
@@ -60,6 +60,7 @@ func webhookPodLabels() map[string]string {
 func DeploymentCreator(data webhookData) reconciling.NamedDeploymentCreatorGetter {
 	return func() (string, reconciling.DeploymentCreator) {
 		return resources.UserClusterWebhookDeploymentName, func(d *appsv1.Deployment) (*appsv1.Deployment, error) {
+			d.Name = resources.UserClusterWebhookDeploymentName
 			d.Spec.Replicas = pointer.Int32Ptr(1)
 			d.Spec.Selector = &metav1.LabelSelector{
 				MatchLabels: webhookPodLabels(),
