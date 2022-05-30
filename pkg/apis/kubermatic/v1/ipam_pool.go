bb5125bed860e3f2914ba9705e4b091faeeffd2f
Hugo Bernardo <hugo@kubermatic.com>
2022-05-30 16:04:49 -0300

    Adding CRDs for IPAMPool and IPAMAllocation (#9910)
    * Adding CRD for IPAMPool
    
    * Adjustments after review
    
    * Adjusting comments
    
    * Changing the scope of the CRD to "Cluster"
    
    * Adding missing list
    
    * Adding CRD for IPAM allocation
    
    * Adjusting comment

---
+++ pkg/apis/kubermatic/v1/ipam_pool.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2022 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	// IPAMPoolResourceName represents "Resource" defined in Kubernetes.
+	IPAMPoolResourceName = "ipampool"
+
+	// IPAMPoolKindName represents "Kind" defined in Kubernetes.
+	IPAMPoolKindName = "IPAMPool"
+)
+
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:object:generate=true
+// +kubebuilder:object:root=true
+// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name="Age",type="date"
+
+// IPAMPool is the object representing Multi-Cluster IP Address Management (IPAM)
+// configuration for KKP user clusters.
+type IPAMPool struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec IPAMPoolSpec `json:"spec,omitempty"`
+}
+
+// IPAMPoolSpec specifies the  Multi-Cluster IP Address Management (IPAM)
+// configuration for KKP user clusters.
+type IPAMPoolSpec struct {
+	// Type is the allocation type to be used.
+	Type IPAMPoolAllocationType `json:"type"`
+	// Datacenters contains a map of datacenters (DCs) for the allocation.
+	Datacenters map[string]IPAMPoolDatacenterSettings `json:"datacenters"`
+}
+
+// IPAMPoolDatacenterSettings contains IPAM Pool configuration for a datacenter.
+type IPAMPoolDatacenterSettings struct {
+	// PoolCIDR is the pool CIDR to be used for the allocation.
+	PoolCIDR SubnetCIDR `json:"poolCidr"`
+
+	// +kubebuilder:validation:Minimum:=1
+	// +kubebuilder:validation:Maximum:=128
+	// AllocationPrefix is the prefix for the allocation.
+	// Used when "type=prefix".
+	AllocationPrefix uint8 `json:"allocationPrefix,omitempty"`
+
+	// +kubebuilder:validation:Minimum:=1
+	// AllocationRange is the range for the allocation.
+	// Used when "type=range".
+	AllocationRange uint32 `json:"allocationRange,omitempty"`
+}
+
+// +kubebuilder:validation:Pattern="((^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))/([0-9]|[1-2][0-9]|3[0-2])$)|(^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))/([0-9]|[0-9][0-9]|1[0-1][0-9]|12[0-8])$))"
+// SubnetCIDR is used to store IPv4/IPv6 CIDR.
+type SubnetCIDR string
+
+// +kubebuilder:validation:Enum=prefix;range
+// IPAMPoolAllocationType defines the type of allocation to be used.
+// Possible values are `prefix` and `range`.
+type IPAMPoolAllocationType string
+
+func (t IPAMPoolAllocationType) String() string {
+	return string(t)
+}
+
+const (
+	// IPAMPoolAllocationTypePrefix corresponds to prefix allocation type.
+	IPAMPoolAllocationTypePrefix IPAMPoolAllocationType = "prefix"
+	// IPAMPoolAllocationTypeRange corresponds to range allocation type.
+	IPAMPoolAllocationTypeRange IPAMPoolAllocationType = "range"
+)
+
+// +kubebuilder:object:generate=true
+// +kubebuilder:object:root=true
+
+type IPAMPoolList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []IPAMPool `json:"items"`
+}
