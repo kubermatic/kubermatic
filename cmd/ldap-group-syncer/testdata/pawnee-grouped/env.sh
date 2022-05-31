fae60831737ac23540e0b807a666f959e4fdabc7
Christoph Mewes <christoph@kubermatic.com>
2022-05-31 16:43:08 +0200

    add testdata

---
+++ cmd/ldap-group-syncer/testdata/pawnee-grouped/env.sh
@@ -0,0 +1,5 @@
+export LDAP_DOMAIN=pawnee.gov
+export LDAP_BASEDN="dc=pawnee,dc=gov"
+export LDAP_ORGANISATION="Pawnee, Indiana"
+export LDAP_BINDDN="cn=admin,dc=pawnee,dc=gov"
+export LDAP_SECRET=BreakfastFoodIsBestFood
