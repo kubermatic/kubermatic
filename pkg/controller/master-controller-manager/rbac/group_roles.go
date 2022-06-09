584ffa50296ce63dfa5334054689be81634c1b02
lsviben <sviben.lovro@gmail.com>
2022-06-09 13:50:30 +0200

    do not add gbp in deletion

--- pkg/controller/master-controller-manager/rbac/group_roles.go
+++ pkg/controller/master-controller-manager/rbac/group_roles.go
@@ -55,7 +55,9 @@ func getGroupRolesList(ctx context.Context, client ctrlruntimeclient.Client, pro
 
 	// add groupProjectBinding ones
 	for _, gbp := range gbpList.Items {
-		groupRoles = append(groupRoles, GroupRole{Group: gbp.Spec.Group, Role: gbp.Spec.Role})
+		if gbp.DeletionTimestamp == nil {
+			groupRoles = append(groupRoles, GroupRole{Group: gbp.Spec.Group, Role: gbp.Spec.Role})
+		}
 	}
 
 	return groupRoles, nil
