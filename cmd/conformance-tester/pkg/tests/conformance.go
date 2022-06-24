b6223693b1a29755d25bfd7b93830315dead8719
Christoph Mewes <christoph@kubermatic.com>
2022-06-24 22:58:43 +0200

    gimps

--- cmd/conformance-tester/pkg/tests/conformance.go
+++ cmd/conformance-tester/pkg/tests/conformance.go
@@ -30,13 +30,13 @@ import (
 	"github.com/onsi/ginkgo/reporters"
 	"github.com/prometheus/client_golang/prometheus"
 	"go.uber.org/zap"
-	"k8c.io/kubermatic/v2/pkg/util/wait"
 
 	"k8c.io/kubermatic/v2/cmd/conformance-tester/pkg/metrics"
 	"k8c.io/kubermatic/v2/cmd/conformance-tester/pkg/scenarios"
 	"k8c.io/kubermatic/v2/cmd/conformance-tester/pkg/types"
 	"k8c.io/kubermatic/v2/cmd/conformance-tester/pkg/util"
 	kubermaticv1 "k8c.io/kubermatic/v2/pkg/apis/kubermatic/v1"
+	"k8c.io/kubermatic/v2/pkg/util/wait"
 
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	corev1 "k8s.io/api/core/v1"
