5d08b422e961baf83387b8c1267a7d95d8b171a2
Christoph Mewes <christoph@kubermatic.com>
2022-06-28 21:23:06 +0200

    update Velero to 1.9.0 (#10192)

--- hack/update-velero-crds.sh
+++ hack/update-velero-crds.sh
@@ -23,7 +23,7 @@ containerize ./hack/update-velero-crds.sh
 
 velero=velero
 if ! [ -x "$(command -v $velero)" ]; then
-  version=v1.8.1
+  version=v1.9.0
   url="https://github.com/vmware-tanzu/velero/releases/download/$version/velero-$version-linux-amd64.tar.gz"
   velero=/tmp/velero
 
