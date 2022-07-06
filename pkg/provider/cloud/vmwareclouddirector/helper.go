1b2b8e1adabf693c31bd5af3c89715ef06ab09c0
Waleed Malik <ahmedwaleedmalik@gmail.com>
2022-07-06 20:20:51 +0500

    Fix cleanup for vApp
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/provider/cloud/vmwareclouddirector/helper.go
+++ pkg/provider/cloud/vmwareclouddirector/helper.go
@@ -23,12 +23,18 @@ import (
 )
 
 func deleteVApp(vdc *govcd.Vdc, vapp *govcd.VApp) error {
+	waitForUndeploy := true
 	task, err := vapp.Undeploy()
 	if err != nil {
-		return fmt.Errorf("failed to undeploy vApp before deletion: %w", err)
+		// Undeploy failed, it's still safe to delete vApp directly since it will take care of all the cleanup.
+		// Most common reason for failure is that the vApp is not in "running" state.
+		waitForUndeploy = false
 	}
-	if err = task.WaitTaskCompletion(); err != nil {
-		return fmt.Errorf("error waiting for vApp undeploy to complete: %w", err)
+
+	if waitForUndeploy {
+		if err = task.WaitTaskCompletion(); err != nil {
+			return fmt.Errorf("error waiting for vApp undeploy to complete: %w", err)
+		}
 	}
 
 	task, err = vapp.Delete()
