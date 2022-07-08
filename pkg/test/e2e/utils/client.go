ec82fbbc8ac3bdda15f61478bde6650e1a6ad190
Christoph Mewes <christoph@kubermatic.com>
2022-07-08 16:06:38 +0200

    remove now unused helpers

--- pkg/test/e2e/utils/client.go
+++ pkg/test/e2e/utils/client.go
@@ -44,9 +44,7 @@ import (
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/datacenter"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/digitalocean"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/gcp"
-	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/mlaadminsetting"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/project"
-	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/rulegroup"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/serviceaccounts"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/tokens"
 	"k8c.io/kubermatic/v2/pkg/test/e2e/utils/apiclient/client/users"
@@ -1844,81 +1842,6 @@ func (r *TestClient) ListAzureSizes(credential, location string) (models.AzureSi
 	}
 
 	return sizesResponse.Payload, nil
-}
-
-// UpdateAlertmanager updates alertmanager config for specific cluster.
-func (r *TestClient) UpdateAlertmanager(clusterID, projectID, config string) (*models.Alertmanager, error) {
-	params := &project.UpdateAlertmanagerParams{
-		Body: &models.Alertmanager{
-			Spec: &models.AlertmanagerSpec{
-				Config: []byte(config),
-			},
-		},
-		ClusterID: clusterID,
-		ProjectID: projectID,
-	}
-	SetupRetryParams(r.test, params, Backoff{
-		Duration: 1 * time.Second,
-		Steps:    4,
-		Factor:   1.5,
-	})
-	updateResponse, err := r.client.Project.UpdateAlertmanager(params, r.bearerToken)
-	if err != nil {
-		return nil, err
-	}
-	return updateResponse.Payload, nil
-}
-
-// CreateRuleGroup creates rule group with specific type.
-func (r *TestClient) CreateRuleGroup(clusterID, projectID string, ruleGroupType kubermaticv1.RuleGroupType, config []byte) (*models.RuleGroup, error) {
-	params := &rulegroup.CreateRuleGroupParams{
-		Body: &models.RuleGroup{
-			Data: config,
-			Type: models.RuleGroupType(ruleGroupType),
-		},
-		ClusterID: clusterID,
-		ProjectID: projectID,
-	}
-	SetupRetryParams(r.test, params, Backoff{
-		Duration: 1 * time.Second,
-		Steps:    4,
-		Factor:   1.5,
-	})
-	createResponse, err := r.client.Rulegroup.CreateRuleGroup(params, r.bearerToken)
-	if err != nil {
-		return nil, err
-	}
-	return createResponse.Payload, nil
-}
-
-// SetMonitoringMLARateLimits updates monitoring MLA rate limits.
-func (r *TestClient) SetMonitoringMLARateLimits(clusterID, projectID string, rateLimits kubermaticv1.MonitoringRateLimitSettings) (*models.MLAAdminSetting, error) {
-	params := &mlaadminsetting.CreateMLAAdminSettingParams{
-		Body: &models.MLAAdminSetting{
-			MonitoringRateLimits: &models.MonitoringRateLimitSettings{
-				IngestionBurstSize: rateLimits.IngestionBurstSize,
-				IngestionRate:      rateLimits.IngestionRate,
-				MaxSamplesPerQuery: rateLimits.MaxSamplesPerQuery,
-				MaxSeriesPerMetric: rateLimits.MaxSeriesPerMetric,
-				MaxSeriesPerQuery:  rateLimits.MaxSeriesPerQuery,
-				MaxSeriesTotal:     rateLimits.MaxSeriesTotal,
-				QueryBurstSize:     rateLimits.QueryBurstSize,
-				QueryRate:          rateLimits.QueryRate,
-			},
-		},
-		ClusterID: clusterID,
-		ProjectID: projectID,
-	}
-	SetupRetryParams(r.test, params, Backoff{
-		Duration: 1 * time.Second,
-		Steps:    4,
-		Factor:   1.5,
-	})
-	updateResponse, err := r.client.Mlaadminsetting.CreateMLAAdminSetting(params, r.bearerToken)
-	if err != nil {
-		return nil, err
-	}
-	return updateResponse.Payload, nil
 }
 
 func (r *TestClient) ConnectClusterTerminal(token, clusterID, projectID string) (*websocket.Conn, error) {
