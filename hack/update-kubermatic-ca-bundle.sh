27e566a3a968eb56e2ab419110274d3a47976483
Christoph Mewes <christoph@kubermatic.com>
2022-07-20 15:25:20 +0200

    update to Go 1.18.4

--- hack/update-kubermatic-ca-bundle.sh
+++ hack/update-kubermatic-ca-bundle.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 cd $(dirname $0)/..
 source hack/lib.sh
 
-CONTAINERIZE_IMAGE=golang:1.18.3 containerize ./hack/update-kubermatic-ca-bundle.sh
+CONTAINERIZE_IMAGE=golang:1.18.4 containerize ./hack/update-kubermatic-ca-bundle.sh
 
 echodate "Updating CA bundle..."
 curl -Lo charts/kubermatic-operator/static/ca-bundle.pem https://curl.se/ca/cacert.pem
