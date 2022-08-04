1d11e96601e1f0d6c72747b65ad9e798452d9061
Christoph Mewes <christoph@kubermatic.com>
2022-08-04 18:28:32 +0200

    implement support for packet

--- cmd/conformance-tester/pkg/scenarios/packet.go
+++ cmd/conformance-tester/pkg/scenarios/packet.go
@@ -18,7 +18,6 @@ package scenarios
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	clusterv1alpha1 "github.com/kubermatic/machine-controller/pkg/apis/cluster/v1alpha1"
@@ -142,12 +141,17 @@ func (s *packetScenario) NodeDeployments(_ context.Context, num int, _ types.Sec
 }
 
 func (s *packetScenario) MachineDeployments(_ context.Context, num int, secrets types.Secrets, cluster *kubermaticv1.Cluster) ([]clusterv1alpha1.MachineDeployment, error) {
-	// See alibaba provider for more info on this.
-	return nil, errors.New("not implemented for gitops yet")
+	facilities := []providerconfig.ConfigVarString{}
+	for _, facility := range s.datacenter.Spec.Packet.Facilities {
+		facilities = append(facilities, providerconfig.ConfigVarString{Value: facility})
+	}
 
-	//nolint:govet
 	md, err := createMachineDeployment(num, s.version, getOSNameFromSpec(s.osSpec), s.osSpec, providerconfig.CloudProviderPacket, equinixmetaltypes.RawConfig{
 		InstanceType: providerconfig.ConfigVarString{Value: packetInstanceType},
+		Token:        providerconfig.ConfigVarString{Value: secrets.Packet.APIKey},
+		ProjectID:    providerconfig.ConfigVarString{Value: secrets.Packet.ProjectID},
+		Metro:        providerconfig.ConfigVarString{Value: s.datacenter.Spec.Packet.Metro},
+		Facilities:   facilities,
 	})
 	if err != nil {
 		return nil, err
