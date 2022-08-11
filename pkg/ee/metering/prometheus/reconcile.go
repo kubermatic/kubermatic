73b5ae0f1bdbcd40247fcb789b3deb589b7d22ad
Osthues <osthues.matthias@gmail.com>
2022-08-11 15:33:33 +0200

    remove owner ref from clusterrole

--- pkg/ee/metering/prometheus/reconcile.go
+++ pkg/ee/metering/prometheus/reconcile.go
@@ -59,13 +59,13 @@ func ReconcilePrometheus(ctx context.Context, client ctrlruntimeclient.Client, s
 
 	if err := reconciling.ReconcileClusterRoles(ctx, []reconciling.NamedClusterRoleCreatorGetter{
 		prometheusClusterRole(),
-	}, "", client, seedOwner); err != nil {
+	}, "", client); err != nil {
 		return fmt.Errorf("failed to reconcile ClusterRole: %w", err)
 	}
 
 	if err := reconciling.ReconcileClusterRoleBindings(ctx, []reconciling.NamedClusterRoleBindingCreatorGetter{
 		prometheusClusterRoleBinding(Namespace),
-	}, "", client, seedOwner); err != nil {
+	}, "", client); err != nil {
 		return fmt.Errorf("failed to reconcile ClusterRoleBindings: %w", err)
 	}
 
