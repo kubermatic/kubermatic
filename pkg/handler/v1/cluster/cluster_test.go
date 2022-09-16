cf9e47fad9a7960f7c4cb318388f65563e9b61b3
Christoph Mewes <christoph@kubermatic.com>
2022-09-16 11:16:51 +0200

    fix package name

--- pkg/handler/v1/cluster/cluster_test.go
+++ pkg/handler/v1/cluster/cluster_test.go
@@ -820,7 +820,7 @@ func TestCreateClusterEndpoint(t *testing.T) {
 		},
 		Spec: kubermaticv1.KubermaticConfigurationSpec{
 			Versions: kubermaticv1.KubermaticVersioningConfiguration{
-				Versions: defaulting.DefaultKubernetesVersioning.Versions,
+				Versions: defaults.DefaultKubernetesVersioning.Versions,
 			},
 		},
 	}
