d52604eb37e00bd27af22b17bfa3b916a41065e2
Mara Sophie Grosch <mgrosch@anexia-it.com>
2022-09-22 16:50:07 +0200

    Fix Anexia disk config (#11030)
    Signed-off-by: Mara Sophie Grosch <mgrosch@anexia-it.com>
    
    Signed-off-by: Mara Sophie Grosch <mgrosch@anexia-it.com>

--- pkg/machine/convert.go
+++ pkg/machine/convert.go
@@ -44,6 +44,7 @@ import (
 	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
 
 	"k8s.io/apimachinery/pkg/util/json"
+	"k8s.io/utils/pointer"
 )
 
 // GetAPIV1OperatingSystemSpec returns the api compatible OperatingSystemSpec for the given machine.
@@ -348,11 +349,10 @@ func GetAPIV2NodeCloudSpec(machineSpec clusterv1alpha1.MachineSpec) (*apiv1.Node
 				TemplateID: config.TemplateID.Value,
 				CPUs:       config.CPUs,
 				Memory:     int64(config.Memory),
-				DiskSize:   &[]int64{int64(config.DiskSize)}[0],
 			}
 
 			if config.DiskSize > 0 {
-				cloudSpec.Anexia.DiskSize = &[]int64{int64(config.DiskSize)}[0]
+				cloudSpec.Anexia.DiskSize = pointer.Int64(int64(config.DiskSize))
 			}
 
 			if diskCount := len(config.Disks); diskCount > 0 {
@@ -362,7 +362,7 @@ func GetAPIV2NodeCloudSpec(machineSpec clusterv1alpha1.MachineSpec) (*apiv1.Node
 					cloudSpec.Anexia.Disks[diskIndex].Size = int64(diskConfig.Size)
 
 					if diskConfig.PerformanceType.Value != "" {
-						cloudSpec.Anexia.Disks[diskIndex].PerformanceType = &[]string{diskConfig.PerformanceType.Value}[0]
+						cloudSpec.Anexia.Disks[diskIndex].PerformanceType = pointer.String(diskConfig.PerformanceType.Value)
 					}
 				}
 			}
