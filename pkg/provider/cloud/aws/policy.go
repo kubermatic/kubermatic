d50c42ee6dde4a7b8028184aa480362d9c045aea
Christoph Mewes <christoph@kubermatic.com>
2022-09-27 18:24:55 +0200

    extend AWS control-plane policy

--- pkg/provider/cloud/aws/policy.go
+++ pkg/provider/cloud/aws/policy.go
@@ -18,6 +18,7 @@ package aws
 
 import (
 	"bytes"
+	"fmt"
 	"text/template"
 )
 
@@ -54,7 +55,16 @@ var (
 }
 `
 
-	// The role for control plane.
+	// The role for control plane. This combines the legacy rules for the in-tree
+	// provider and then the rules for the external CCM and CSI driver. The rules
+	// overlap in parts, but are kept as separate blocks for easier maintenance.
+	// All rules are deployed at all times, as cloud providers only reconcile after
+	// some time has passed and during that time a migration to the external CCM
+	// might get stuck.
+	// Once all AWS clusters have been migrated to the external CCM / CSI, the first
+	// chunk of rules can be removed.
+
+	// Legacy rules
 	// Based on https://github.com/kubernetes/kops/blob/master/docs/iam_roles.md
 	// We're using 2 filters:
 	// - RequestTag  = Makes sure the tag exists on the create request
@@ -65,9 +75,7 @@ var (
 	// - EC2 modify actions with the ResourceTag filter
 	// - ELB create actions with the RequestTag filter
 	// - ELB modify actions with the ResourceTag filter.
-	controlPlanePolicyTpl = template.Must(template.New("worker-policy").Parse(`{
-  "Version": "2012-10-17",
-  "Statement": [
+	legacyControlPlanePolicyStatements = `
     {
       "Effect": "Allow",
       "Action": [
@@ -179,19 +187,243 @@ var (
         }
       }
     }
-  ]
-}
-`))
+  `
+
+	// External CCM rules
+	// Based on https://github.com/kubernetes/cloud-provider-aws/blob/master/docs/prerequisites.md#iam-policies
+	externalCCMControlPlanePolicyStatements = `
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVolumes",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifyVolume",
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CreateRoute",
+        "ec2:DeleteRoute",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:DescribeVpcs",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:AttachLoadBalancerToSubnets",
+        "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancerPolicy",
+        "elasticloadbalancing:CreateLoadBalancerListeners",
+        "elasticloadbalancing:ConfigureHealthCheck",
+        "elasticloadbalancing:DeleteLoadBalancer",
+        "elasticloadbalancing:DeleteLoadBalancerListeners",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeLoadBalancerAttributes",
+        "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:ModifyLoadBalancerAttributes",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:CreateListener",
+        "elasticloadbalancing:CreateTargetGroup",
+        "elasticloadbalancing:DeleteListener",
+        "elasticloadbalancing:DeleteTargetGroup",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeLoadBalancerPolicies",
+        "elasticloadbalancing:DescribeTargetGroups",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticloadbalancing:ModifyListener",
+        "elasticloadbalancing:ModifyTargetGroup",
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:DeregisterTargets",
+        "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+        "iam:CreateServiceLinkedRole",
+        "kms:DescribeKey"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  `
+
+	// EBS CSI driver rules
+	// Based on https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/docs/example-iam-policy.json
+	csiDriverControlPlanePolicyStatements = `
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateSnapshot",
+        "ec2:AttachVolume",
+        "ec2:DetachVolume",
+        "ec2:ModifyVolume",
+        "ec2:DescribeAvailabilityZones",
+        "ec2:DescribeInstances",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeVolumesModifications"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateTags"
+      ],
+      "Resource": [
+        "arn:aws:ec2:*:*:volume/*",
+        "arn:aws:ec2:*:*:snapshot/*"
+      ],
+      "Condition": {
+        "StringEquals": {
+          "ec2:CreateAction": [
+            "CreateVolume",
+            "CreateSnapshot"
+          ]
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DeleteTags"
+      ],
+      "Resource": [
+        "arn:aws:ec2:*:*:volume/*",
+        "arn:aws:ec2:*:*:snapshot/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateVolume"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "aws:RequestTag/{{ .EBSCSIClusterTag }}": "true"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateVolume"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "aws:RequestTag/CSIVolumeName": "*"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateVolume"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "aws:RequestTag/{{ .ClusterTag }}": "owned"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DeleteVolume"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "ec2:ResourceTag/{{ .EBSCSIClusterTag }}": "true"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DeleteVolume"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "ec2:ResourceTag/CSIVolumeName": "*"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DeleteVolume"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "ec2:ResourceTag/{{ .ClusterTag }}": "owned"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DeleteSnapshot"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "ec2:ResourceTag/CSIVolumeSnapshotName": "*"
+        }
+      }
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DeleteSnapshot"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringLike": {
+          "ec2:ResourceTag/{{ .EBSCSIClusterTag }}": "true"
+        }
+      }
+    }
+  `
+
+	controlPlanePolicy = fmt.Sprintf(
+		`{"Version": "2012-10-17", "Statement": [%s, %s, %s]}`,
+		legacyControlPlanePolicyStatements,
+		externalCCMControlPlanePolicyStatements,
+		csiDriverControlPlanePolicyStatements,
+	)
+
+	controlPlanePolicyTpl = template.Must(template.New("control-plane-policy").Parse(controlPlanePolicy))
 )
 
 func getControlPlanePolicy(clusterName string) (string, error) {
 	tag := ec2ClusterTag(clusterName)
 
 	buf := &bytes.Buffer{}
-	err := controlPlanePolicyTpl.Execute(buf, policyTplData{ClusterTag: *tag.Key})
+	err := controlPlanePolicyTpl.Execute(buf, policyTplData{
+		ClusterTag:       *tag.Key,
+		EBSCSIClusterTag: fmt.Sprintf("ebs.csi.aws.com/%s", clusterName),
+	})
 	return buf.String(), err
 }
 
 type policyTplData struct {
-	ClusterTag string
+	ClusterTag       string
+	EBSCSIClusterTag string
 }
