55bd6f0b671e4b0b126d46045b6961f2dfde32a8
Pratik Deoghare <pratik.deoghare@gmail.com>
2022-10-04 14:52:08 +0200

    add EtcdLauncherNotEnabled phase to etcd restore (#11115)
    * add EtcdLauncherNotEnabled phase to etcd restore
    
    * update codegen
    
    * remove EtcdRestorePhase prefix
    
    Co-authored-by: Marvin Beckers <mail@embik.me>
    Signed-off-by: Pratik Deoghare <pratik.deoghare@gmail.com>
    
    * remove unnecessarily long, inconsistent prefix
    
    Signed-off-by: Pratik Deoghare <pratik.deoghare@gmail.com>
    Co-authored-by: Marvin Beckers <mail@embik.me>

--- pkg/controller/seed-controller-manager/etcdrestore/etcd_restore_controller.go
+++ pkg/controller/seed-controller-manager/etcdrestore/etcd_restore_controller.go
@@ -172,6 +172,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request reconcile.Request) (
 func (r *Reconciler) reconcile(ctx context.Context, log *zap.SugaredLogger, restore *kubermaticv1.EtcdRestore, cluster *kubermaticv1.Cluster,
 	seed *kubermaticv1.Seed) (*reconcile.Result, error) {
 	if !cluster.Spec.Features[kubermaticv1.ClusterFeatureEtcdLauncher] {
+		restore.Status.Phase = kubermaticv1.EtcdRestorePhaseEtcdLauncherNotEnabled
 		return nil, fmt.Errorf("etcdLauncher not enabled on cluster: %q", cluster.Name)
 	}
 
