3c7f168282b2bd7204370f3733f37effbe53dd95
Christoph Mewes <christoph@kubermatic.com>
2022-10-12 20:19:21 +0200

    [release/v2.20] Fix Seed-Proxy ServiceAccount token not being generated (#11190) (#11196)

--- pkg/controller/master-controller-manager/seed-proxy/resources.go
+++ pkg/controller/master-controller-manager/seed-proxy/resources.go
@@ -88,6 +88,25 @@ func seedServiceAccountCreator(seed *kubermaticv1.Seed) reconciling.NamedService
 	}
 }
 
+func seedSecretCreator(seed *kubermaticv1.Seed) reconciling.NamedSecretCreatorGetter {
+	return func() (string, reconciling.SecretCreator) {
+		return SeedSecretName, func(sa *corev1.Secret) (*corev1.Secret, error) {
+			sa.Labels = defaultLabels(SeedSecretName, seed.Name)
+
+			// ensure Kubernetes has enough info to fill in the SA token
+			sa.Type = corev1.SecretTypeServiceAccountToken
+
+			if sa.Annotations == nil {
+				sa.Annotations = map[string]string{}
+			}
+
+			sa.Annotations[corev1.ServiceAccountNameKey] = SeedServiceAccountName
+
+			return sa, nil
+		}
+	}
+}
+
 func seedMonitoringRoleCreator(seed *kubermaticv1.Seed) reconciling.NamedRoleCreatorGetter {
 	name := seedMonitoringRoleName(seed)
 
