ff8f916d307cc76a581b19c4a048f218932257cc
Marvin Beckers <marvin@kubermatic.com>
2022-11-09 08:31:53 +0100

    [release/v2.20] use optimistic locking when adding finalizer 'kubermatic.k8c.io/cleanup-usersshkeys-cluster-ids'
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/controller/master-controller-manager/usersshkeyssynchronizer/controller.go
+++ pkg/controller/master-controller-manager/usersshkeyssynchronizer/controller.go
@@ -213,7 +213,7 @@ func (r *Reconciler) reconcile(ctx context.Context, log *zap.SugaredLogger, requ
 	oldCluster := cluster.DeepCopy()
 	if !kubernetes.HasFinalizer(cluster, UserSSHKeysClusterIDsCleanupFinalizer) {
 		kubernetes.AddFinalizer(cluster, UserSSHKeysClusterIDsCleanupFinalizer)
-		if err := seedClient.Patch(ctx, cluster, ctrlruntimeclient.MergeFrom(oldCluster)); err != nil {
+		if err := seedClient.Patch(ctx, cluster, ctrlruntimeclient.MergeFromWithOptions(oldCluster, ctrlruntimeclient.MergeFromWithOptimisticLock{})); err != nil {
 			return fmt.Errorf("failed adding %s finalizer: %w", UserSSHKeysClusterIDsCleanupFinalizer, err)
 		}
 	}
