7533ed278039637111dcdef78f2f2dfea5b0d3ba
Vincent Gramer <vincent@kubermatic.com>
2022-11-21 09:29:58 +0100

    fix typo
    Signed-off-by: Vincent Gramer <vincent@kubermatic.com>

--- pkg/applications/test/doc.go
+++ pkg/applications/test/doc.go
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package test constains helper functions for tests related to helm.
+// Package test contains helper functions for tests related to helm.
 package test
