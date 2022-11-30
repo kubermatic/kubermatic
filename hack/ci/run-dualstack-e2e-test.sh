c7b40d9f6230637f4138d84d193905a784403caf
Christoph Mewes <christoph@kubermatic.com>
2022-11-30 13:47:45 +0100

    skip vsphere tests if $VSPHERE_E2E_DISABLED is not empty

--- hack/ci/run-dualstack-e2e-test.sh
+++ hack/ci/run-dualstack-e2e-test.sh
@@ -73,6 +73,11 @@ export METAL_PROJECT_ID="${METAL_PROJECT_ID:-$(vault kv get -field=METAL_PROJECT
 export VSPHERE_E2E_USERNAME="${VSPHERE_E2E_USERNAME:-$(vault kv get -field=username dev/e2e-vsphere)}"
 export VSPHERE_E2E_PASSWORD="${VSPHERE_E2E_PASSWORD:-$(vault kv get -field=password dev/e2e-vsphere)}"
 
+if [ "$PROVIDER" == "vsphere" ] && [ -n "${VSPHERE_E2E_DISABLED:-}" ]; then
+  echodate "\$VSPHERE_E2E_DISABLED is not empty, skipping tests."
+  exit 0
+fi
+
 echodate "Successfully got secrets from Vault"
 echodate "Running dualstack tests..."
 
