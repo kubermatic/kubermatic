59fc931b43747368be3a7bf15e7c74aef69a989d
Marvin Beckers <marvin@kubermatic.com>
2022-12-07 10:09:19 +0100

    [release/v2.21] Update to Go 1.18.9
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 cd $(dirname $0)/..
 source hack/lib.sh
 
-CONTAINERIZE_IMAGE=golang:1.18.4 containerize ./hack/update-codegen.sh
+CONTAINERIZE_IMAGE=golang:1.18.9 containerize ./hack/update-codegen.sh
 
 echodate "Running go generate"
 go generate ./pkg/...
--- hack/update-fixtures.sh
+++ hack/update-fixtures.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 cd $(dirname $0)/..
 source hack/lib.sh
 
-CONTAINERIZE_IMAGE=golang:1.18.4 containerize ./hack/update-fixtures.sh
+CONTAINERIZE_IMAGE=golang:1.18.9 containerize ./hack/update-fixtures.sh
 
 echodate "Updating fixtures..."
 make test-update &> /dev/null
