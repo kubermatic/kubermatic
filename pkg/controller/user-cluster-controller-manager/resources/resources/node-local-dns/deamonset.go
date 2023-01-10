5c0ddd517c442bc45cb163e2bb99a0c6d4a01949
Matthias Osthues <osthues.matthias@gmail.com>
2023-01-10 22:22:21 +0100

    Add support for kube-dns configmap for node-local-dns

--- pkg/controller/user-cluster-controller-manager/resources/resources/node-local-dns/deamonset.go
+++ pkg/controller/user-cluster-controller-manager/resources/resources/node-local-dns/deamonset.go
@@ -105,6 +105,10 @@ func DaemonSetReconciler(imageRewriter registry.ImageRewriter) reconciling.Named
 							Name:      "config-volume",
 							MountPath: "/etc/coredns",
 						},
+						{
+							Name:      "kube-dns-config",
+							MountPath: "/etc/kube-dns",
+						},
 					},
 
 					Ports: []corev1.ContainerPort{
@@ -173,6 +177,17 @@ func DaemonSetReconciler(imageRewriter registry.ImageRewriter) reconciling.Named
 									Path: "Corefile.base",
 								},
 							},
+						},
+					},
+				},
+				{
+					Name: "kube-dns-config",
+					VolumeSource: corev1.VolumeSource{
+						ConfigMap: &corev1.ConfigMapVolumeSource{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "kube-dns",
+							},
+							Optional: pointer.Bool(true),
 						},
 					},
 				},
