464f5f915dfba0d911e5ece600adc29acb6d0a8a
Matthias Osthues <osthues.matthias@gmail.com>
2023-01-10 22:20:54 +0100

    Fix Corefile mounting error

--- pkg/controller/user-cluster-controller-manager/resources/resources/node-local-dns/deamonset.go
+++ pkg/controller/user-cluster-controller-manager/resources/resources/node-local-dns/deamonset.go
@@ -93,7 +93,7 @@ func DaemonSetReconciler(imageRewriter registry.ImageRewriter) reconciling.Named
 						"-localip",
 						kubesystem.NodeLocalDNSCacheAddress,
 						"-conf",
-						"/etc/coredns/Corefile",
+						"/etc/Corefile",
 					},
 
 					VolumeMounts: []corev1.VolumeMount{
@@ -170,7 +170,7 @@ func DaemonSetReconciler(imageRewriter registry.ImageRewriter) reconciling.Named
 							Items: []corev1.KeyToPath{
 								{
 									Key:  "Corefile",
-									Path: "Corefile",
+									Path: "Corefile.base",
 								},
 							},
 						},
