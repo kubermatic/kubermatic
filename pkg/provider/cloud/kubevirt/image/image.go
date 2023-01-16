cb694a0016ccfe99601fb2c54e8f5c45c1217fea
Marcin Franczyk <marcin0franczyk@gmail.com>
2023-01-16 17:16:43 +0100

    fix imports order
    Signed-off-by: Marcin Franczyk <marcin0franczyk@gmail.com>

--- pkg/provider/cloud/kubevirt/image/image.go
+++ pkg/provider/cloud/kubevirt/image/image.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package image
 
 import (
@@ -6,16 +22,17 @@ import (
 
 	"github.com/imdario/mergo"
 	"go.uber.org/zap"
+	cdiv1beta1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
+
+	kubermaticv1 "k8c.io/kubermatic/v2/pkg/apis/kubermatic/v1"
+	kkpreconciling "k8c.io/kubermatic/v2/pkg/resources/reconciling"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilpointer "k8s.io/utils/pointer"
-	cdiv1beta1 "kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1"
 	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
-
-	kubermaticv1 "k8c.io/kubermatic/v2/pkg/apis/kubermatic/v1"
-	kkpreconciling "k8c.io/kubermatic/v2/pkg/resources/reconciling"
 )
 
 const (
--- pkg/provider/cloud/kubevirt/image/image_custom.go
+++ pkg/provider/cloud/kubevirt/image/image_custom.go
@@ -1,10 +1,28 @@
+/*
+Copyright 2023 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package image
 
 import (
 	"context"
 
 	"go.uber.org/zap"
+
 	kubermaticv1 "k8c.io/kubermatic/v2/pkg/apis/kubermatic/v1"
+
 	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -16,6 +34,12 @@ func ReconcileCustomImages(ctx context.Context, cluster *kubermaticv1.Cluster, c
 	return dvr.reconcile()
 }
 
+func customImagesAnnotations() map[string]string {
+	return map[string]string{
+		dataVolumeDeleteAfterCompletionAnnotationKey: "false",
+	}
+}
+
 // customImages returns a list of PreAllocatedDataVolumes based on the list of standard images contained in the datacenter,
 func listCustomImages(cluster *kubermaticv1.Cluster) []kubermaticv1.PreAllocatedDataVolume {
 	return cluster.Spec.Cloud.Kubevirt.PreAllocatedDataVolumes
@@ -23,10 +47,4 @@ func listCustomImages(cluster *kubermaticv1.Cluster) []kubermaticv1.PreAllocated
 
 func customImagesFilter(annotations map[string]string) bool {
 	return annotations != nil && annotations[customImageTypeAnnotationKey] != ""
-}
-
-func customImagesAnnotations() map[string]string {
-	return map[string]string{
-		dataVolumeDeleteAfterCompletionAnnotationKey: "false",
-	}
 }
