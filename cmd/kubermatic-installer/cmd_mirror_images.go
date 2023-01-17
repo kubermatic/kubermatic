43262d02e41d2ba11b1547b8c48bfa936e20dc26
Marvin Beckers <marvin@kubermatic.com>
2023-01-17 09:31:43 +0100

    Ignore repository overrides in KubermaticConfiguration by default when mirroring images
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- cmd/kubermatic-installer/cmd_mirror_images.go
+++ cmd/kubermatic-installer/cmd_mirror_images.go
@@ -41,9 +41,10 @@ type MirrorImagesOptions struct {
 
 	Registry string
 
-	Config        string
-	VersionFilter string
-	DryRun        bool
+	Config                    string
+	VersionFilter             string
+	DryRun                    bool
+	IgnoreRepositoryOverrides bool
 
 	AddonsPath  string
 	AddonsImage string
@@ -93,6 +94,7 @@ func MirrorImagesCommand(logger *logrus.Logger, versions kubermaticversion.Versi
 	cmd.PersistentFlags().StringVar(&opt.Config, "config", "", "Path to the KubermaticConfiguration YAML file")
 	cmd.PersistentFlags().StringVar(&opt.VersionFilter, "version-filter", "", "Version constraint which can be used to filter for specific versions")
 	cmd.PersistentFlags().BoolVar(&opt.DryRun, "dry-run", false, "Only print the names of source and destination images")
+	cmd.PersistentFlags().BoolVar(&opt.IgnoreRepositoryOverrides, "ignore-repository-overrides", true, "Ignore any configured registry overrides in the referenced KubermaticConfiguration to re-use a configuration that already specifies overrides (note that custom tags will still be observed and that this does not affect Helm charts configured via values.yaml; defaults to true)")
 
 	cmd.PersistentFlags().StringVar(&opt.AddonsPath, "addons-path", "", "Path to a local directory containing KKP addons. Takes precedence over --addons-image")
 	cmd.PersistentFlags().StringVar(&opt.AddonsImage, "addons-image", "", "Docker image containing KKP addons, if not given, falls back to the Docker image configured in the KubermaticConfiguration")
@@ -148,6 +150,24 @@ func MirrorImagesFunc(logger *logrus.Logger, versions kubermaticversion.Versions
 
 		if config == nil {
 			return errors.New("please specify your KubermaticConfiguration via --config")
+		}
+
+		// if we pass the option to ignore repository overrides in the KubermaticConfiguration,
+		// we make sure we omit any repository configured in the loaded config so they get
+		// properly defaulted.
+		if options.IgnoreRepositoryOverrides {
+			config.Spec.API.DockerRepository = ""
+			config.Spec.UI.DockerRepository = ""
+			config.Spec.MasterController.DockerRepository = ""
+			config.Spec.SeedController.DockerRepository = ""
+			config.Spec.Webhook.DockerRepository = ""
+			config.Spec.UserCluster.KubermaticDockerRepository = ""
+			config.Spec.UserCluster.DNATControllerDockerRepository = ""
+			config.Spec.UserCluster.EtcdLauncherDockerRepository = ""
+			config.Spec.UserCluster.Addons.DockerRepository = ""
+			config.Spec.VerticalPodAutoscaler.Recommender.DockerRepository = ""
+			config.Spec.VerticalPodAutoscaler.Updater.DockerRepository = ""
+			config.Spec.VerticalPodAutoscaler.AdmissionController.DockerRepository = ""
 		}
 
 		kubermaticConfig, err := defaulting.DefaultConfiguration(config, zap.NewNop().Sugar())
