a81d251795028765c432fc955f4b0f524655f938
Matthias Osthues <osthues.matthias@gmail.com>
2023-01-24 09:41:22 +0100

    Bump Metering to v1.0.2

--- pkg/ee/metering/reconcile.go
+++ pkg/ee/metering/reconcile.go
@@ -56,7 +56,7 @@ const (
 )
 
 func getMeteringImage(overwriter registry.ImageRewriter) string {
-	return registry.Must(overwriter(resources.RegistryQuay + "/kubermatic/metering:v1.0.1"))
+	return registry.Must(overwriter(resources.RegistryQuay + "/kubermatic/metering:v1.0.2"))
 }
 
 // ReconcileMeteringResources reconciles the metering related resources.
