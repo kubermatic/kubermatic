58ee79ea1f3d1810bf3293901105ef63d89bf057
Waleed Malik <ahmedwaleedmalik@gmail.com>
2023-03-30 11:40:20 +0500

    Support for configuring additional volumes for the UI (#12107)
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/controller/operator/master/resources/kubermatic/ui.go
+++ pkg/controller/operator/master/resources/kubermatic/ui.go
@@ -56,6 +56,18 @@ func UIDeploymentReconciler(cfg *kubermaticv1.KubermaticConfiguration, versions 
 				tag = cfg.Spec.UI.DockerTag
 			}
 
+			volumeMounts := []corev1.VolumeMount{
+				{
+					MountPath: "/dist/config/",
+					Name:      "config",
+					ReadOnly:  true,
+				},
+			}
+
+			if cfg.Spec.UI.ExtraVolumeMounts != nil {
+				volumeMounts = append(volumeMounts, cfg.Spec.UI.ExtraVolumeMounts...)
+			}
+
 			d.Spec.Template.Spec.Containers = []corev1.Container{
 				{
 					Name:    "webserver",
@@ -69,14 +81,8 @@ func UIDeploymentReconciler(cfg *kubermaticv1.KubermaticConfiguration, versions 
 							Protocol:      corev1.ProtocolTCP,
 						},
 					},
-					VolumeMounts: []corev1.VolumeMount{
-						{
-							MountPath: "/dist/config/",
-							Name:      "config",
-							ReadOnly:  true,
-						},
-					},
-					Resources: cfg.Spec.UI.Resources,
+					VolumeMounts: volumeMounts,
+					Resources:    cfg.Spec.UI.Resources,
 				},
 			}
 
@@ -89,6 +95,10 @@ func UIDeploymentReconciler(cfg *kubermaticv1.KubermaticConfiguration, versions 
 						},
 					},
 				},
+			}
+
+			if cfg.Spec.UI.ExtraVolumes != nil {
+				d.Spec.Template.Spec.Volumes = append(d.Spec.Template.Spec.Volumes, cfg.Spec.UI.ExtraVolumes...)
 			}
 
 			return d, nil
