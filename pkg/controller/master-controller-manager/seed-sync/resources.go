96295e4f27e6611f18005a25e07b563befc92b91
Matthias Osthues <osthues.matthias@gmail.com>
2023-04-04 18:04:33 +0200

    explicitly remove finalizer on kubermatic configuration reconciler (#12137)

--- pkg/controller/master-controller-manager/seed-sync/resources.go
+++ pkg/controller/master-controller-manager/seed-sync/resources.go
@@ -76,6 +76,8 @@ func configReconciler(config *kubermaticv1.KubermaticConfiguration) kkpreconcili
 			c.Labels[ManagedByLabel] = ControllerName
 
 			c.Annotations = config.Annotations
+			c.Finalizers = nil
+
 			c.Spec = config.Spec
 
 			return c, nil
