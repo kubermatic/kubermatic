eacb4bb74c15090eb1dbdae501084b3dbf660630
Matthias Osthues <osthues.matthias@gmail.com>
2023-06-06 22:40:13 +0200

    deprecate canal 3.22 and alter auto upgrade rule if k8s verion is greater or equal 1.26

--- pkg/cni/version.go
+++ pkg/cni/version.go
@@ -47,7 +47,7 @@ var (
 	// supportedCNIPluginVersions contains a list of all currently supported CNI versions for each CNI type.
 	// Only supported versions are available for selection in KKP UI.
 	supportedCNIPluginVersions = map[kubermaticv1.CNIPluginType]sets.Set[string]{
-		kubermaticv1.CNIPluginTypeCanal: sets.New("v3.22", "v3.23", "v3.24", "v3.25"),
+		kubermaticv1.CNIPluginTypeCanal: sets.New("v3.23", "v3.24", "v3.25"),
 		kubermaticv1.CNIPluginTypeCilium: sets.New(
 			"v1.11",
 			"v1.12",
@@ -62,7 +62,7 @@ var (
 	// Deprecated versions are not available for selection in KKP UI, but are still accepted
 	// by the validation webhook for backward compatibility.
 	deprecatedCNIPluginVersions = map[kubermaticv1.CNIPluginType]sets.Set[string]{
-		kubermaticv1.CNIPluginTypeCanal: sets.New("v3.8", "v3.19", "v3.20", "v3.21"),
+		kubermaticv1.CNIPluginTypeCanal: sets.New("v3.8", "v3.19", "v3.20", "v3.21", "v3.22"),
 	}
 )
 
