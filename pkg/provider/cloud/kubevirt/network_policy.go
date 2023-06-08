6b9d5ee757e04a522934cf9532602ddcca574465
Matthias Osthues <osthues.matthias@gmail.com>
2023-06-08 12:09:05 +0200

    switch to egress based network policies for kubervirt provider (#12329)
    * switch to egress based network policies for kubervirt provider
    
    * fix spelling
    
    * remove custom netpol for kubevirt test seed
    
    * increase e2e timeout duration

--- pkg/provider/cloud/kubevirt/network_policy.go
+++ pkg/provider/cloud/kubevirt/network_policy.go
@@ -23,29 +23,123 @@ import (
 	kubermaticv1 "k8c.io/kubermatic/v2/pkg/apis/kubermatic/v1"
 	"k8c.io/reconciler/pkg/reconciling"
 
+	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func clusterIsolationNetworkPolicyReconciler() reconciling.NamedNetworkPolicyReconcilerFactory {
+// clusterIsolationNetworkPolicyReconciler creates a network policy that restrict Egress traffic. By default it allows access to
+// each other pod in the same namespace, Internet, Kubermatic nodeport proxy and DNS servers.
+func clusterIsolationNetworkPolicyReconciler(clusterIp string, nameservers []string) reconciling.NamedNetworkPolicyReconcilerFactory {
+	apiServerCIDR := clusterIp + "/32"
+	dnsPort := intstr.FromInt(53)
+	tcp := corev1.ProtocolTCP
+	udp := corev1.ProtocolUDP
+
+	var dnsRules []networkingv1.NetworkPolicyEgressRule
+	for _, ns := range nameservers {
+		dnsRules = append(dnsRules, networkingv1.NetworkPolicyEgressRule{
+			To: []networkingv1.NetworkPolicyPeer{
+				{
+					IPBlock: &networkingv1.IPBlock{
+						CIDR: ns + "/32",
+					},
+				},
+			},
+			Ports: []networkingv1.NetworkPolicyPort{
+				{
+					Protocol: &tcp,
+					Port:     &dnsPort,
+				},
+				{
+					Protocol: &udp,
+					Port:     &dnsPort,
+				},
+			},
+		})
+	}
+
 	return func() (string, reconciling.NetworkPolicyReconciler) {
 		return "cluster-isolation", func(np *networkingv1.NetworkPolicy) (*networkingv1.NetworkPolicy, error) {
 			np.Spec = networkingv1.NetworkPolicySpec{
 				PodSelector: metav1.LabelSelector{},
-				Ingress: []networkingv1.NetworkPolicyIngressRule{
+				Egress: []networkingv1.NetworkPolicyEgressRule{
+					// Allow access to pod's in same namespace
 					{
-						From: []networkingv1.NetworkPolicyPeer{
+						To: []networkingv1.NetworkPolicyPeer{
 							{
 								PodSelector: &metav1.LabelSelector{},
 							},
 						},
 					},
+					// Allow public ip addresses
+					{
+						To: []networkingv1.NetworkPolicyPeer{
+							{
+								IPBlock: &networkingv1.IPBlock{
+									CIDR:   "0.0.0.0/0",
+									Except: []string{"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"},
+								},
+							},
+						},
+					},
+					// Allow kubermatic nodeport-proxy ip address
+					{
+						To: []networkingv1.NetworkPolicyPeer{
+							{
+								IPBlock: &networkingv1.IPBlock{
+									CIDR: apiServerCIDR,
+								},
+							},
+						},
+					},
 				},
 				PolicyTypes: []networkingv1.PolicyType{
-					networkingv1.PolicyTypeIngress,
+					networkingv1.PolicyTypeEgress,
 				},
 			}
+
+			// Allow dns servers
+			np.Spec.Egress = append(np.Spec.Egress, dnsRules...)
+			return np, nil
+		}
+	}
+}
+
+// clusterImporterNetworkPolicyReconciler creates a special network policy that allows the importer pod to access
+// the image-repo in the cluster.
+func clusterImporterNetworkPolicyReconciler() reconciling.NamedNetworkPolicyReconcilerFactory {
+	return func() (string, reconciling.NetworkPolicyReconciler) {
+		return "allow-kubevirt-importer", func(np *networkingv1.NetworkPolicy) (*networkingv1.NetworkPolicy, error) {
+			np.Spec = networkingv1.NetworkPolicySpec{
+				PodSelector: metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app":             "containerized-data-importer",
+						"cdi.kubevirt.io": "importer",
+					},
+				},
+				Egress: []networkingv1.NetworkPolicyEgressRule{
+					// Allow access to pod in any namespace with specific label
+					{
+						To: []networkingv1.NetworkPolicyPeer{
+							{
+								NamespaceSelector: &metav1.LabelSelector{},
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"app": "image-repo",
+									},
+								},
+							},
+						},
+					},
+				},
+				PolicyTypes: []networkingv1.PolicyType{
+					networkingv1.PolicyTypeEgress,
+				},
+			}
+
 			return np, nil
 		}
 	}
@@ -61,9 +155,10 @@ func customNetworkPolicyReconciler(existing *kubermaticv1.CustomNetworkPolicy) r
 	}
 }
 
-func reconcileClusterIsolationNetworkPolicy(ctx context.Context, cluster *kubermaticv1.Cluster, client ctrlruntimeclient.Client) error {
+func reconcileClusterIsolationNetworkPolicy(ctx context.Context, cluster *kubermaticv1.Cluster, dc *kubermaticv1.DatacenterSpecKubevirt, client ctrlruntimeclient.Client) error {
 	namedNetworkPolicyReconcilerFactorys := []reconciling.NamedNetworkPolicyReconcilerFactory{
-		clusterIsolationNetworkPolicyReconciler(),
+		clusterIsolationNetworkPolicyReconciler(cluster.Status.Address.IP, dc.DNSConfig.Nameservers),
+		clusterImporterNetworkPolicyReconciler(),
 	}
 	if err := reconciling.ReconcileNetworkPolicies(ctx, namedNetworkPolicyReconcilerFactorys, cluster.Status.NamespaceName, client); err != nil {
 		return fmt.Errorf("failed to ensure Network Policies: %w", err)
