5a1c4c0042ccf3edd375b2c2717434eac31a8a40
Marvin Beckers <marvin@kubermatic.com>
2023-07-06 14:57:13 +0200

    add support for creating snapshots via etcd-launcher
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

---
+++ cmd/etcd-launcher/cmd_snapshot.go
@@ -0,0 +1,102 @@
+/*
+Copyright 2023 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"go.etcd.io/etcd/etcdutl/v3/snapshot"
+	"go.uber.org/zap"
+
+	"k8c.io/kubermatic/v2/cmd/etcd-launcher/pkg/etcd"
+)
+
+type snapshotOptions struct {
+	options
+
+	file string
+}
+
+func SnapshotCommand(log *zap.SugaredLogger) *cobra.Command {
+	opt := snapshotOptions{}
+
+	cmd := &cobra.Command{
+		Use:          "snapshot",
+		Short:        "Create etcd database snapshot and save it to file",
+		RunE:         SnapshotFunc(log, &opt),
+		SilenceUsage: true,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			opts.CopyInto(&opt.options)
+
+			return nil
+		},
+	}
+
+	cmd.PersistentFlags().StringVar(&opt.file, "file", "/backup/snapshot.db", "file to save database snapshot to")
+
+	return cmd
+}
+
+func SnapshotFunc(log *zap.SugaredLogger, opt *snapshotOptions) cobraFuncE {
+	return handleErrors(log, func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		log := log.With("cluster", opt.cluster)
+
+		e := &etcd.Cluster{
+			Cluster:           opt.cluster,
+			EtcdctlAPIVersion: opt.etcdctlAPIVersion,
+
+			CaCertFile:     opt.etcdCAFile,
+			ClientCertFile: opt.etcdCertFile,
+			ClientKeyFile:  opt.etcdKeyFile,
+		}
+
+		if _, err := e.Init(ctx); err != nil {
+			return fmt.Errorf("failed to initialize etcd cluster configuration: %w", err)
+		}
+
+		if err := e.SetClusterSize(ctx); err != nil {
+			return fmt.Errorf("failed to set expected cluster size: %w", err)
+		}
+
+		configs, err := e.GetEtcdEndpointConfigs(ctx, log)
+		if err != nil {
+			return fmt.Errorf("failed to get etcd cluster clients: %w", err)
+		}
+
+		for _, config := range configs {
+			if len(config.Endpoints) != 1 {
+				log.Warnw("unexpected number of endpoints, skipping this configuration", "endpoints", strings.Join(config.Endpoints, ","))
+				continue
+			}
+
+			snapv3 := snapshot.NewV3(log.Desugar())
+			err := snapv3.Save(ctx, config, opt.file)
+
+			if err == nil {
+				log.Infow("saved snapshot from endpoint", "endpoint", strings.Join(config.Endpoints, ","), "file", opt.file)
+				return nil
+			}
+
+			log.Errorw("failed to save snapshot from endpoint, trying next endpoint", "endpoint", strings.Join(config.Endpoints, ","), zap.Error(err))
+		}
+
+		return fmt.Errorf("exhausted all endpoints, no snapshot was successful")
+	})
+}
