9c8dbca71ed4d9ac7c9f7f4bf3788a31dd44087d
Karol Szwaj <karol@kubermatic.com>
2023-08-30 16:25:55 +0200

    Fix pkg sort

--- pkg/controller/seed-controller-manager/ipam/controller.go
+++ pkg/controller/seed-controller-manager/ipam/controller.go
@@ -28,6 +28,7 @@ import (
 	"k8c.io/kubermatic/v2/pkg/provider"
 	"k8c.io/kubermatic/v2/pkg/resources/reconciling"
 	"k8c.io/kubermatic/v2/pkg/version/kubermatic"
+
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
