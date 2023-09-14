b1c07c204191a65d02b2db9cedc3c1ed2914e8a9
Marvin Beckers <marvin@kubermatic.com>
2023-09-14 11:47:26 +0200

    Add Kubernetes patch releases released on 2023-09-13
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/defaulting/configuration.go
+++ pkg/defaulting/configuration.go
@@ -217,7 +217,7 @@ var (
 	}
 
 	DefaultKubernetesVersioning = kubermaticv1.KubermaticVersioningConfiguration{
-		Default: semver.NewSemverOrDie("v1.27.3"),
+		Default: semver.NewSemverOrDie("v1.27.6"),
 		// NB: We keep all patch releases that we supported, even if there's
 		// an auto-upgrade rule in place. That's because removing a patch
 		// release from this slice can break reconciliation loop for clusters
@@ -233,14 +233,17 @@ var (
 			newSemver("v1.25.6"),
 			newSemver("v1.25.9"),
 			newSemver("v1.25.11"),
+			newSemver("v1.25.14"),
 			// Kubernetes 1.26
 			newSemver("v1.26.1"),
 			newSemver("v1.26.4"),
 			newSemver("v1.26.6"),
+			newSemver("v1.26.9"),
 			// Kubernetes 1.27
 			newSemver("v1.27.3"),
+			newSemver("v1.27.6"),
 			// Kubernetes 1.28
-			newSemver("v1.28.1"),
+			newSemver("v1.28.2"),
 		},
 		Updates: []kubermaticv1.Update{
 			// ======= 1.24 =======
