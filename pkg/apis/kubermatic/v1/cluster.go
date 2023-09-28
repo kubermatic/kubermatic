fad8af795ad7cb700611cde80e864c137967faee
Marvin Beckers <marvin@kubermatic.com>
2023-09-28 10:28:12 +0200

    Improve field description
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/apis/kubermatic/v1/cluster.go
+++ pkg/apis/kubermatic/v1/cluster.go
@@ -1060,7 +1060,7 @@ type VSphereCloudSpec struct {
 	// machines.
 	// +optional
 	Folder string `json:"folder"`
-	// Optional: BasePath configures a vCenter folder path that KKP will create an individual "cluster-CLUSTER-ID" folder in.
+	// Optional: BasePath configures a vCenter folder path that KKP will create an individual cluster folder in.
 	// If it's an absolute path, the RootPath configured in the datacenter will be ignored. If it is a relative path,
 	// the BasePath part will be appended to the RootPath (if set) to construct the full path.
 	// +optional
