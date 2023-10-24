89a060297ed40ba5eb45a014083fc6bd9d1adfb1
Karol Szwaj <karol@kubermatic.com>
2023-10-24 12:21:16 +0200

    Bump Cillium version to 1.13.8 and 1.14.3 (#12761)
    Signed-off-by: Karol Szwaj <karol@kubermatic.com>

--- pkg/cni/cilium/cilium.go
+++ pkg/cni/cilium/cilium.go
@@ -129,6 +129,19 @@ func ApplicationDefinitionReconciler(config *kubermaticv1.KubermaticConfiguratio
 					},
 				},
 				{
+					Version: "1.13.8",
+					Template: appskubermaticv1.ApplicationTemplate{
+						Source: appskubermaticv1.ApplicationSource{
+							Helm: &appskubermaticv1.HelmSource{
+								ChartName:    ciliumHelmChartName,
+								ChartVersion: "1.13.8",
+								URL:          "oci://" + config.Spec.UserCluster.SystemApplications.HelmRepository,
+								Credentials:  credentials,
+							},
+						},
+					},
+				},
+				{
 					Version: "1.14.1",
 					Template: appskubermaticv1.ApplicationTemplate{
 						Source: appskubermaticv1.ApplicationSource{
@@ -148,6 +161,19 @@ func ApplicationDefinitionReconciler(config *kubermaticv1.KubermaticConfiguratio
 							Helm: &appskubermaticv1.HelmSource{
 								ChartName:    ciliumHelmChartName,
 								ChartVersion: "1.14.2",
+								URL:          "oci://" + config.Spec.UserCluster.SystemApplications.HelmRepository,
+								Credentials:  credentials,
+							},
+						},
+					},
+				},
+				{
+					Version: "1.14.3",
+					Template: appskubermaticv1.ApplicationTemplate{
+						Source: appskubermaticv1.ApplicationSource{
+							Helm: &appskubermaticv1.HelmSource{
+								ChartName:    ciliumHelmChartName,
+								ChartVersion: "1.14.3",
 								URL:          "oci://" + config.Spec.UserCluster.SystemApplications.HelmRepository,
 								Credentials:  credentials,
 							},
