ce771fd8b6f9ae0cf55d8616aaceb8ea7ff833a7
Marvin Beckers <marvin@kubermatic.com>
2023-12-06 10:49:43 +0100

    Create CiliumNetworkPolicies if feature gate is enabled and seed has API type
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- pkg/resources/resources.go
+++ pkg/resources/resources.go
@@ -1001,6 +1001,7 @@ const (
 	NetworkPolicyClusterExternalAddrAllow           = "cluster-external-addr-allow"
 	NetworkPolicyOIDCIssuerAllow                    = "oidc-issuer-allow"
 	NetworkPolicySeedApiserverAllow                 = "seed-apiserver-allow"
+	CiliumNetworkPolicySeedApiserverAllow           = "cilium-seed-apiserver-allow"
 	NetworkPolicyApiserverInternalAllow             = "apiserver-internal-allow"
 )
 
