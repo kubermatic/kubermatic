c2b7e2f4787d25a240759b7740eb28abb5847bd0
Karol Szwaj <karol@kubermatic.com>
2024-02-01 15:59:58 +0100

    improve getCNIApplicationinstallation func
    Signed-off-by: Karol Szwaj <karol@kubermatic.com>

--- pkg/controller/seed-controller-manager/initial-application-installation-controller/controller.go
+++ pkg/controller/seed-controller-manager/initial-application-installation-controller/controller.go
@@ -249,7 +249,8 @@ func getCNIApplicationInstallation(ctx context.Context, userClusterClient ctrlru
 		if err := userClusterClient.Get(ctx, types.NamespacedName{Namespace: metav1.NamespaceSystem, Name: name}, app); err != nil {
 			return nil, fmt.Errorf("failed to get Cilium ApplicationInstallation in user cluster: %w", err)
 		}
+		return app, nil
 	}
 
-	return app, nil
+	return nil, fmt.Errorf("unsupported CNI type: %s", cniType)
 }
