fad78737a25da63ddd33a9f019355d0332375555
Waleed Malik <ahmedwaleedmalik@gmail.com>
2024-02-09 14:43:40 +0500

    Bump k8s API to v1.29.1
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- cmd/conformance-tester/pkg/tests/storage.go
+++ cmd/conformance-tester/pkg/tests/storage.go
@@ -131,7 +131,7 @@ func TestStorage(ctx context.Context, log *zap.SugaredLogger, opts *ctypes.Optio
 					},
 					Spec: corev1.PersistentVolumeClaimSpec{
 						AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-						Resources: corev1.ResourceRequirements{
+						Resources: corev1.VolumeResourceRequirements{
 							Requests: corev1.ResourceList{corev1.ResourceStorage: resource.MustParse("1Gi")},
 						},
 					},
