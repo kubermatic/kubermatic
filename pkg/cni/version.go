20e551f878c844060ff44dbaed9aedec3447c385
Karol Szwaj <karol@kubermatic.com>
2024-04-04 21:24:28 +0200

    Introduce Cilium 1.15 and bump older to 1.13.14 and 1.14.9 (#13241)
    * Introduce Cilium 1.15 and bump older to 1.13.14 and 1.14.9
    
    * update mirror-chart
    
    * fix deprecated versions

--- pkg/cni/version.go
+++ pkg/cni/version.go
@@ -33,7 +33,7 @@ const CanalCNILastUnspecifiedVersion = "v3.8"
 var (
 	defaultCNIPluginVersion = map[kubermaticv1.CNIPluginType]string{
 		kubermaticv1.CNIPluginTypeCanal:  "v3.27",
-		kubermaticv1.CNIPluginTypeCilium: "1.14.3",
+		kubermaticv1.CNIPluginTypeCilium: "1.15.3",
 	}
 )
 
@@ -49,10 +49,11 @@ var (
 	supportedCNIPluginVersions = map[kubermaticv1.CNIPluginType]sets.Set[string]{
 		kubermaticv1.CNIPluginTypeCanal: sets.New("v3.25", "v3.26", "v3.27"),
 		kubermaticv1.CNIPluginTypeCilium: sets.New(
-			// NOTE: as of 1.13.0, we moved to Application infra for Cilium CNI management and started using real smever
+			// NOTE: as of 1.13.0, we moved to Application infra for Cilium CNI management and started using real semver
 			// See pkg/cni/cilium docs for details on introducing a new version.
-			"1.13.8",
-			"1.14.3",
+			"1.13.14",
+			"1.14.9",
+			"1.15.3",
 		),
 		kubermaticv1.CNIPluginTypeNone: sets.New(""),
 	}
@@ -69,8 +70,10 @@ var (
 			"1.13.4",
 			"1.13.6", // CVE-2023-39347, CVE-2023-41333 (Moderate Severity), CVE-2023-41332 (Low Severity)
 			"1.13.7", // CVE-2023-44487 (High Severity)
+			"1.13.8", // CVE-2024-28860, CVE-2024-28248 (High Severity)
 			"1.14.1", // CVE-2023-39347, CVE-2023-41333 (Moderate Severity), CVE-2023-41332 (Low Severity)
 			"1.14.2", // CVE-2023-44487 (High Severity)
+			"1.14.3", // CVE-2024-28860, CVE-2024-28248 (High Severity)
 		),
 	}
 )
