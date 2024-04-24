568716a31849882ae55fa1ed9d785ab72476cd96
Waleed Malik <ahmedwaleedmalik@gmail.com>
2024-04-24 21:42:17 +0500

    Enable local command for Enterprise Edition
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- cmd/kubermatic-installer/main_ee.go
+++ cmd/kubermatic-installer/main_ee.go
@@ -40,6 +40,7 @@ func addCommands(cmd *cobra.Command, logger *logrus.Logger, versions kubermaticv
 		PrintCommand(),
 		VersionCommand(logger, versions),
 		MirrorImagesCommand(logger, versions),
+		LocalCommand(logger),
 	)
 }
 
