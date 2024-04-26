7aae4b17a386bfd5bbcb9c64dc8a102e2f09d630
Christoph Mewes <christoph@kubermatic.com>
2024-04-26 17:21:49 +0200

    k8s.io/apiserver/pkg/apis/config/v1 became k8s.io/apiserver/pkg/apis/apiserver/v1

--- pkg/resources/apiserver/encryption.go
+++ pkg/resources/apiserver/encryption.go
@@ -29,7 +29,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	apiserverconfigv1 "k8s.io/apiserver/pkg/apis/config/v1"
+	apiserverconfigv1 "k8s.io/apiserver/pkg/apis/apiserver/v1"
 	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/yaml"
 )
