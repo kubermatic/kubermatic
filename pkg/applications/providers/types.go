a9c833d3c6c4eb76ff896aa893749f70e8ce8d80
Simon Bein <simontheleg@gmail.com>
2024-05-27 14:22:06 +0200

    [release/v2.25] Applications: discover if a release is stuck and rollback if necessary (#13332)
    * Discover if a release is stuck and rollback if necessary
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * linter + noop stubs
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * add additional logging
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * rename to isStuck
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * remove timeout from message, as we do not explicitly check for it
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * remove printing directive for AppInstall, as we already print it centrally via the logger
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * upgrade to helm v3.14.3
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * update go.sum
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    
    * manually implement GetReleases to avoid bumping Helm so far
    
    ---------
    
    Signed-off-by: Simon Bein <simontheleg@gmail.com>
    Co-authored-by: Christoph Mewes <christoph@kubermatic.com>

--- pkg/applications/providers/types.go
+++ pkg/applications/providers/types.go
@@ -59,6 +59,12 @@ type TemplateProvider interface {
 
 	// Uninstall the application.
 	Uninstall(applicationInstallation *appskubermaticv1.ApplicationInstallation) (util.StatusUpdater, error)
+
+	// Check if a release is stuck
+	IsStuck(applicationInstallation *appskubermaticv1.ApplicationInstallation) (bool, error)
+
+	// Rollback the Application to the previous release
+	Rollback(applicationInstallation *appskubermaticv1.ApplicationInstallation) error
 }
 
 // NewTemplateProvider return the concrete implementation of TemplateProvider according to the templateMethod.
