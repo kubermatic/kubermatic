de9281abf4c5cea4e81b204ca398772486634f12
Christoph Mewes <christoph@kubermatic.com>
2024-06-26 15:25:31 +0200

    create ConfigMap for Velero to use custom restore helper image

--- pkg/ee/cluster-backup/resources/user-cluster/cluster_backup.go
+++ pkg/ee/cluster-backup/resources/user-cluster/cluster_backup.go
@@ -25,7 +25,6 @@
 package userclusterresources
 
 import (
-	"context"
 	"fmt"
 
 	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
@@ -34,6 +33,7 @@ import (
 	"k8c.io/kubermatic/v2/pkg/kubernetes"
 	"k8c.io/kubermatic/v2/pkg/resources"
 	kkpreconciling "k8c.io/kubermatic/v2/pkg/resources/reconciling"
+	"k8c.io/kubermatic/v2/pkg/resources/registry"
 	"k8c.io/reconciler/pkg/reconciling"
 
 	corev1 "k8s.io/api/core/v1"
@@ -41,9 +41,10 @@ import (
 )
 
 const (
-	ClusterRoleBindingName = "velero"
-	clusterBackupAppName   = "velero"
-	DefaultBSLName         = "default-cluster-backup-bsl"
+	ClusterRoleBindingName     = "velero"
+	clusterBackupAppName       = "velero"
+	DefaultBSLName             = "default-cluster-backup-bsl"
+	customizationConfigMapName = "fs-restore-action-config"
 
 	CloudCredentialsSecretName           = "velero-cloud-credentials"
 	defaultCloudCredentialsSecretKeyName = "cloud"
@@ -105,7 +106,7 @@ func ClusterRoleBindingReconciler() reconciling.NamedClusterRoleBindingReconcile
 }
 
 // BSLReconciler creates the default BackupStorage location is created for velero.
-func BSLReconciler(ctx context.Context, cluster *kubermaticv1.Cluster, cbsl *kubermaticv1.ClusterBackupStorageLocation) kkpreconciling.NamedBackupStorageLocationReconcilerFactory {
+func BSLReconciler(cluster *kubermaticv1.Cluster, cbsl *kubermaticv1.ClusterBackupStorageLocation) kkpreconciling.NamedBackupStorageLocationReconcilerFactory {
 	return func() (string, kkpreconciling.BackupStorageLocationReconciler) {
 		return DefaultBSLName, func(bsl *velerov1.BackupStorageLocation) (*velerov1.BackupStorageLocation, error) {
 			kubernetes.EnsureLabels(bsl, resources.BaseAppLabels(clusterBackupAppName, nil))
@@ -121,6 +122,34 @@ func BSLReconciler(ctx context.Context, cluster *kubermaticv1.Cluster, cbsl *kub
 			// add bucket prefix using projectID/clusterID to avoid collision.
 			bsl.Spec.ObjectStorage.Prefix = fmt.Sprintf("%s/%s", projectID, cluster.Name)
 			return bsl, nil
+		}
+	}
+}
+
+// CustomizationConfigMapReconciler reconciles a ConfigMap to configure the velero restore helper,
+// see https://velero.io/docs/v1.12/file-system-backup/#customize-restore-helper-container.
+func CustomizationConfigMapReconciler(rewriter registry.ImageRewriter) reconciling.NamedConfigMapReconcilerFactory {
+	return func() (string, reconciling.ConfigMapReconciler) {
+		return customizationConfigMapName, func(cm *corev1.ConfigMap) (*corev1.ConfigMap, error) {
+			// ensure Velero can find this ConfigMap
+			kubernetes.EnsureLabels(cm, map[string]string{
+				"velero.io/plugin-config":      "",
+				"velero.io/pod-volume-restore": "RestoreItemAction",
+			})
+
+			if cm.Data == nil {
+				cm.Data = map[string]string{}
+			}
+
+			// all we want to set is the custom restore helper image
+			rewritten, err := rewriter("docker.io/velero/velero-restore-helper")
+			if err != nil {
+				return nil, fmt.Errorf("failed to rewrite image: %w", err)
+			}
+
+			cm.Data["image"] = rewritten
+
+			return cm, nil
 		}
 	}
 }
