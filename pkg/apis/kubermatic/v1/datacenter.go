a5df4ab472dcb781f2c4d04aca9e383173f1a4d4
Waleed Malik <ahmedwaleedmalik@gmail.com>
2024-08-27 16:09:19 +0500

    Fix doc generation for DNSPolicy (#13630)
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/apis/kubermatic/v1/datacenter.go
+++ pkg/apis/kubermatic/v1/datacenter.go
@@ -796,8 +796,10 @@ type DatacenterSpecFake struct {
 type DatacenterSpecKubevirt struct {
 	// NamespacedMode represents the configuration for enabling the single namespace mode for all user-clusters in the KubeVirt datacenter.
 	NamespacedMode *NamespacedMode `json:"namespacedMode,omitempty"`
+
 	// +kubebuilder:validation:Enum=ClusterFirstWithHostNet;ClusterFirst;Default;None
 	// +kubebuilder:default=ClusterFirst
+
 	// DNSPolicy represents the dns policy for the pod. Valid values are 'ClusterFirstWithHostNet', 'ClusterFirst',
 	// 'Default' or 'None'. Defaults to "ClusterFirst". DNS parameters given in DNSConfig will be merged with the
 	// policy selected with DNSPolicy.
