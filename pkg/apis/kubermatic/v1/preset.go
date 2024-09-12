5feff85a99c495d070103845c38a6ecfb0c6e8b3
Ahmad Hamzh <85109141+ahmadhamzh@users.noreply.github.com>
2024-09-12 22:49:34 +0300

    fix a typo (#13707)

--- pkg/apis/kubermatic/v1/preset.go
+++ pkg/apis/kubermatic/v1/preset.go
@@ -116,7 +116,7 @@ type ProviderPreset struct {
 	Enabled *bool `json:"enabled,omitempty"`
 	// IsCustomizable marks a preset as editable on the KKP UI; Customizable presets still have the credentials obscured on the UI, but other fields that are not considered private are displayed during cluster creation. Users can then update those fields, if required.
 	// NOTE: This is only supported for OpenStack Cloud Provider in KKP 2.26. Support for other providers will be added later on.
-	IsCustomizable bool `json:"isCutomizable,omitempty"`
+	IsCustomizable bool `json:"isCustomizable,omitempty"`
 	// If datacenter is set, this preset is only applicable to the
 	// configured datacenter.
 	Datacenter string `json:"datacenter,omitempty"`
