4697184dc54075fd2dffd89d8e4a5e544bfff5d1
Moath Qasim <moad.qassem@gmail.com>
2024-11-07 18:03:09 +0100

    support provider networks in KKP KubeVirt Datacneter
    Signed-off-by: moadqassem <moad.qassem@gmail.com>

--- pkg/apis/kubermatic/v1/datacenter.go
+++ pkg/apis/kubermatic/v1/datacenter.go
@@ -829,6 +829,27 @@ type DatacenterSpecKubevirt struct {
 	// In the tenant cluster, the created StorageClass name will have as name:
 	// kubevirt-<infra-storageClass-name>
 	InfraStorageClasses []KubeVirtInfraStorageClass `json:"infraStorageClasses,omitempty"`
+
+	// Optional: ProviderNetwork describes the infra cluster network fabric that is being used
+	ProviderNetwork ProviderNetwork `json:"providerNetwork,omitempty"`
+}
+
+// ProviderNetwork describes the infra cluster network fabric that is being used.
+type ProviderNetwork struct {
+	Name string `json:"name"`
+	VPC  []VPC  `json:"vpc"`
+}
+
+// VPC  is a virtual network dedicated to a single tenant within a KubeVirt, where the resources in the VPC
+// is isolated from any other resources within the KubeVirt infra cluster.
+type VPC struct {
+	Name   string   `json:"name"`
+	Subnet []Subnet `json:"subnet,omitempty"`
+}
+
+// Subnet a smaller, segmented portion of a larger network, like a Virtual Private Cloud (VPC).
+type Subnet struct {
+	Name string `json:"name"`
 }
 
 type NamespacedMode struct {
@@ -843,7 +864,7 @@ type KubeVirtInfraStorageClass struct {
 	Name string `json:"name"`
 	// Optional: IsDefaultClass. If true, the created StorageClass in the tenant cluster will be annotated with:
 	// storageclass.kubernetes.io/is-default-class : true
-	// If missing or false, annotation will be:
+	// If missing or false,  annotation will be:
 	// storageclass.kubernetes.io/is-default-class : false
 	IsDefaultClass *bool `json:"isDefaultClass,omitempty"`
 	// VolumeBindingMode indicates how PersistentVolumeClaims should be provisioned and bound. When unset,
