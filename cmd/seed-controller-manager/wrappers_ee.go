2f13acc4064a928609cfbee4f9cb3d48a817b509
Karol Szwaj <karol.szwaj@gmail.com>
2024-11-20 12:04:06 +0100

    [release/v2.26] Setup Kubevirt provider network controller in the seed-controller-manager (#13878)
    * Setup Kubevirt provider network controller in the seed-controller-manager (#13858)
    
    * Add cluster-isolation network policy for KubeVirt namespaced mode
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * Init kubevirt-network-controller to reconcile clusters and watch for subnet events
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * setup controller
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * add scheme
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * add kube-ovn dependencies
    
    Signed-off-by: Navid <yaa110@gmail.com>
    
    * list subnets of a cluster and reconcile cluster based on workload subnet label
    
    Signed-off-by: Navid <yaa110@gmail.com>
    
    * reconcile netpols
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * datacenter api: add NetworkPolicyEnabled field
    
    Signed-off-by: Navid <yaa110@gmail.com>
    
    * provide gateways and cidrs to create network policy
    
    Signed-off-by: Navid <yaa110@gmail.com>
    
    * check for dc kubevirt settings
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * setup kv infra client
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * process kubeovn networks
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * setup client for subnet processing
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * setup client for subnet processing
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * lint
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * set kv-network-controller in pkg/ee
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * update boilerplate
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    * fix lint
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    ---------
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    Signed-off-by: Navid <yaa110@gmail.com>
    Co-authored-by: Navid <yaa110@gmail.com>
    
    * go mod tidy
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    ---------
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    Signed-off-by: Navid <yaa110@gmail.com>
    Co-authored-by: Navid <yaa110@gmail.com>

--- cmd/seed-controller-manager/wrappers_ee.go
+++ cmd/seed-controller-manager/wrappers_ee.go
@@ -26,6 +26,7 @@ import (
 	eeseedctrlmgr "k8c.io/kubermatic/v2/pkg/ee/cmd/seed-controller-manager"
 	groupprojectbindingcontroller "k8c.io/kubermatic/v2/pkg/ee/group-project-binding/controller"
 	kubelbcontroller "k8c.io/kubermatic/v2/pkg/ee/kubelb"
+	kubevirtnetworkcontroller "k8c.io/kubermatic/v2/pkg/ee/kubevirt-network-controller"
 	resourcequotaseedcontroller "k8c.io/kubermatic/v2/pkg/ee/resource-quota/seed-controller"
 	"k8c.io/kubermatic/v2/pkg/provider"
 
@@ -51,6 +52,10 @@ func setupControllers(ctrlCtx *controllerContext) error {
 
 	if err := kubelbcontroller.Add(ctrlCtx.mgr, ctrlCtx.runOptions.workerCount, ctrlCtx.runOptions.workerName, ctrlCtx.runOptions.overwriteRegistry, ctrlCtx.seedGetter, ctrlCtx.clientProvider, ctrlCtx.log, ctrlCtx.versions); err != nil {
 		return fmt.Errorf("failed to create KubeLB controller: %w", err)
+	}
+
+	if err := kubevirtnetworkcontroller.Add(ctrlCtx.mgr, ctrlCtx.log, ctrlCtx.runOptions.workerCount, ctrlCtx.runOptions.workerName, ctrlCtx.seedGetter, ctrlCtx.configGetter, ctrlCtx.versions); err != nil {
+		return fmt.Errorf("failed to create KubeVirt network controller: %w", err)
 	}
 
 	return nil
