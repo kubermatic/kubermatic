e8d1aa0d3e3f703345ea41804347a8673aba9d66
soer3n <43064202+soer3n@users.noreply.github.com>
2024-12-04 17:15:59 +0100

    add default namespace spec to appdef crd (#13916)
    * add default namespace spec to appdef crd
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>
    
    * change field description for defaultNamespace
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>
    
    * change defaultNamespace field to pointer because it is optional
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>
    
    * regenerate crds after changing field to pointer
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>
    
    ---------
    
    Signed-off-by: soer3n <srenhenning@googlemail.com>

--- pkg/apis/apps.kubermatic/v1/application_definition.go
+++ pkg/apis/apps.kubermatic/v1/application_definition.go
@@ -220,6 +220,10 @@ type ApplicationDefinitionSpec struct {
 	// DefaultValuesBlock specifies default values for the UI which are passed to helm templating when creating an application. Comments are preserved.
 	DefaultValuesBlock string `json:"defaultValuesBlock,omitempty"`
 
+	// DefaultNamespace specifies the default namespace which is used if a referencing ApplicationInstallation has no target namespace defined.
+	// If unset, the name of the ApplicationDefinition is being used instead.
+	DefaultNamespace *AppNamespaceSpec `json:"defaultNamespace,omitempty"`
+
 	// DefaultDeployOptions holds the settings specific to the templating method used to deploy the application.
 	// These settings can be overridden in applicationInstallation.
 	DefaultDeployOptions *DeployOptions `json:"defaultDeployOptions,omitempty"`
