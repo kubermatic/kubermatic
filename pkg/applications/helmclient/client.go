eabd27869079735e865b5d943333351f642be51a
soer3n <srenhenning@googlemail.com>
2025-01-02 15:58:21 +0100

    set rollback revision explicit to last deployed
    Signed-off-by: soer3n <srenhenning@googlemail.com>

--- pkg/applications/helmclient/client.go
+++ pkg/applications/helmclient/client.go
@@ -473,7 +473,12 @@ func (h HelmClient) GetMetadata(releaseName string) (*action.Metadata, error) {
 // Rollback wraps helms Rollback command to be used with our ActionConfig.
 func (h HelmClient) Rollback(releaseName string) error {
 	client := action.NewRollback(h.actionConfig)
-	err := client.Run(releaseName)
+	latestDeployedRelease, err := h.actionConfig.Releases.Deployed(releaseName)
+	if err != nil {
+		return fmt.Errorf("Could not fetch last successful release %q: %w", releaseName, err)
+	}
+	client.Version = latestDeployedRelease.Version
+	err = client.Run(releaseName)
 	if err != nil {
 		return fmt.Errorf("Could not rollback release %q: %w", releaseName, err)
 	}
