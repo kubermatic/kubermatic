c4ab0594b6e5fa70641f87a3c91c3d7f2e97d13a
Iskren <iskren@kubermatic.com>
2025-01-09 23:51:53 +0200

    add more applications
    Signed-off-by: Iskren <iskren@kubermatic.com>

---
+++ pkg/test/e2e/default_app_catalog_tests/nvidia_gpu_operator/nvidia_gpu_operator.go
@@ -0,0 +1,52 @@
+package cert_manager
+
+import (
+	"encoding/json"
+	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
+)
+
+type NvidiaGpuOperator struct {
+	Namespace string
+	Name      string
+}
+
+var DefaultNvidiaGpuOperator = NvidiaGpuOperator{
+	Namespace: "nvidia-gpu-operator",
+	Name:      "nvidia-gpu-operator",
+}
+
+func (ngo *NvidiaGpuOperator) GetApplication() ([]byte, error) {
+	app := apiv1.Application{
+		ObjectMeta: apiv1.ObjectMeta{
+			Name:      ngo.Name,
+			Namespace: ngo.Namespace,
+		},
+		Spec: apiv1.ApplicationSpec{
+			Namespace: apiv1.NamespaceSpec{
+				Name:   ngo.Namespace,
+				Create: true,
+			},
+			ApplicationRef: apiv1.ApplicationRef{
+				Name:    ngo.Name,
+				Version: "v23.9.1",
+			},
+		},
+	}
+	applications := []apiv1.Application{app}
+	data, err := json.Marshal(applications)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (ngo *NvidiaGpuOperator) FetchData() (name, namespace, key string, names []string) {
+	names = []string{
+		"gpu-operator",
+		"node-feature-discovery",
+	}
+
+	key = "app.kubernetes.io/name"
+	return ngo.Name, ngo.Namespace, key, names
+}
