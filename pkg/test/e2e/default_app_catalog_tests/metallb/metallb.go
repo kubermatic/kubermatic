2f40e0a39952433a71a3ecff9653f325fa5b0b6b
Iskren <iskren@kubermatic.com>
2025-01-09 23:51:53 +0200

    add more applications
    Signed-off-by: Iskren <iskren@kubermatic.com>

---
+++ pkg/test/e2e/default_app_catalog_tests/metallb/metallb.go
@@ -0,0 +1,51 @@
+package cert_manager
+
+import (
+	"encoding/json"
+	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
+)
+
+type MetalLB struct {
+	Namespace string
+	Name      string
+}
+
+var DefaultMetalLB = MetalLB{
+	Namespace: "metallb",
+	Name:      "metallb",
+}
+
+func (mlb *MetalLB) GetApplication() ([]byte, error) {
+	app := apiv1.Application{
+		ObjectMeta: apiv1.ObjectMeta{
+			Name:      mlb.Name,
+			Namespace: mlb.Namespace,
+		},
+		Spec: apiv1.ApplicationSpec{
+			Namespace: apiv1.NamespaceSpec{
+				Name:   mlb.Namespace,
+				Create: true,
+			},
+			ApplicationRef: apiv1.ApplicationRef{
+				Name:    mlb.Name,
+				Version: "v0.14.3",
+			},
+		},
+	}
+	applications := []apiv1.Application{app}
+	data, err := json.Marshal(applications)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (mlb *MetalLB) FetchData() (name, namespace, key string, names []string) {
+	names = []string{
+		"metallb",
+	}
+
+	key = "app.kubernetes.io/name"
+	return mlb.Name, mlb.Namespace, key, names
+}
---
+++ pkg/test/e2e/default_app_catalog_tests/trivy_operator/trivy_operator.go
@@ -0,0 +1,51 @@
+package cert_manager
+
+import (
+	"encoding/json"
+	apiv1 "k8c.io/kubermatic/v2/pkg/api/v1"
+)
+
+type TrivyOperator struct {
+	Namespace string
+	Name      string
+}
+
+var DefaultTrivyOperator = TrivyOperator{
+	Namespace: "trivy-operator",
+	Name:      "trivy-operator",
+}
+
+func (to *TrivyOperator) GetApplication() ([]byte, error) {
+	app := apiv1.Application{
+		ObjectMeta: apiv1.ObjectMeta{
+			Name:      to.Name,
+			Namespace: to.Namespace,
+		},
+		Spec: apiv1.ApplicationSpec{
+			Namespace: apiv1.NamespaceSpec{
+				Name:   to.Namespace,
+				Create: true,
+			},
+			ApplicationRef: apiv1.ApplicationRef{
+				Name:    to.Name,
+				Version: "0.18.4",
+			},
+		},
+	}
+	applications := []apiv1.Application{app}
+	data, err := json.Marshal(applications)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (to *TrivyOperator) FetchData() (name, namespace, key string, names []string) {
+	names = []string{
+		"trivy-operator",
+	}
+
+	key = "app.kubernetes.io/name"
+	return to.Name, to.Namespace, key, names
+}
