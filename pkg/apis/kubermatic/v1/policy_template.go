e8bdd0130863321218d50d4da8222310d3dda0e1
Adonis Murati <adonismurati@gmail.com>
2025-01-15 09:19:32 +0100

    update codegen
    Signed-off-by: Adonis Murati <adonismurati@gmail.com>

--- pkg/apis/kubermatic/v1/policy_template.go
+++ pkg/apis/kubermatic/v1/policy_template.go
@@ -16,9 +16,9 @@ limitations under the License.
 package v1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
