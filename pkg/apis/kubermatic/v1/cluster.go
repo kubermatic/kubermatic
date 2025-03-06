e2ec872ae4bbc00bccc50eef94464b90ff213122
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2025-03-06 18:19:19 +0300

    (feat): Add Support for Configuring HTTP Proxy at User Cluster Level (#14209)
    * Add new configurations on Cluster API to allow users to configure proxy
    configurations on the nodes through OSM for each specific user-clusters.
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>
    
    * fix potential nil ptr reference
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>
    
    * add boilterplates to new test files
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>
    
    * update fixtures
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- pkg/apis/kubermatic/v1/cluster.go
+++ pkg/apis/kubermatic/v1/cluster.go
@@ -758,7 +758,7 @@ type ComponentSettings struct {
 	// UserClusterController configures the KKP usercluster-controller deployed as part of the cluster control plane.
 	UserClusterController *ControllerSettings `json:"userClusterController,omitempty"`
 	// OperatingSystemManager configures operating-system-manager (the component generating node bootstrap scripts for machine-controller).
-	OperatingSystemManager *ControllerSettings `json:"operatingSystemManager,omitempty"`
+	OperatingSystemManager *OSMControllerSettings `json:"operatingSystemManager,omitempty"`
 	// CoreDNS configures CoreDNS deployed as part of the cluster control plane.
 	CoreDNS *DeploymentSettings `json:"coreDNS,omitempty"`
 }
@@ -777,6 +777,13 @@ type KonnectivityProxySettings struct {
 	// The option is propagated to agents and server.
 	// Defaults to 1m.
 	KeepaliveTime string `json:"keepaliveTime,omitempty"`
+}
+
+type OSMControllerSettings struct {
+	ControllerSettings `json:",inline"`
+	// ProxySettings defines optional flags for OperatingSystemManager deployment to allow
+	// setting specific proxy configurations for specific user clusters.
+	Proxy ProxySettings `json:"proxy,omitempty"`
 }
 
 type ControllerSettings struct {
