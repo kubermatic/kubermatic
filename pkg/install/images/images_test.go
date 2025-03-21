63c6d632f63c4df02ca0faa3f2ae2c99dc697231
Mohamed Rafraf <mohamedrafraf@insat.u-carthage.tn>
2025-03-21 16:22:48 +0100

    Improve mirror-images experience

--- pkg/install/images/images_test.go
+++ pkg/install/images/images_test.go
@@ -86,7 +86,7 @@ func TestRetagImageForAllVersions(t *testing.T) {
 		}
 	}
 
-	if _, _, err := CopyImages(context.Background(), log, true, sets.List(imageSet), "test-registry:5000", "kubermatic-installer/test"); err != nil {
+	if _, _, _, err := CopyImages(context.Background(), log, true, sets.List(imageSet), "test-registry:5000", "kubermatic-installer/test"); err != nil {
 		t.Errorf("Error calling processImages: %v", err)
 	}
 }
