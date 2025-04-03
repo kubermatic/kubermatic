9c82af61dca9c387cbc1a717e9ea585758a80428
Iskren <iskren@kubermatic.com>
2025-04-04 01:16:24 +0300

    fixed application test
    Signed-off-by: Iskren <iskren@kubermatic.com>

--- pkg/test/e2e/default_app_catalog_tests/application_test.go
+++ pkg/test/e2e/default_app_catalog_tests/application_test.go
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package default_app_catalog_applications_tests
+package default_app_catalog_tests
 
 import (
 	"context"
