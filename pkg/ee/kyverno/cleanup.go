1cb7414cedb68221a222d88e9ba7b7e202ec3d32
Adonis Murati <adonismurati@gmail.com>
2025-05-05 18:59:10 +0200

    fix lint build issues
    Signed-off-by: Adonis Murati <adonismurati@gmail.com>

--- pkg/ee/kyverno/cleanup.go
+++ pkg/ee/kyverno/cleanup.go
@@ -2,7 +2,7 @@
 
 /*
                   Kubermatic Enterprise Read-Only License
-                         Version 1.0 ("KERO-1.0")
+                         Version 1.0 ("KERO-1.0”)
                      Copyright © 2025 Kubermatic GmbH
 
    1.	You may only view, read and display for studying purposes the source
@@ -11,7 +11,7 @@
    2.	Any use of the software which exceeds the foregoing right, including,
       without limitation, its execution, compilation, copying, modification
       and distribution, is expressly prohibited.
-   3.	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   3.	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
       EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
       MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
       IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
