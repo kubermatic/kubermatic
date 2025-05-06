a5e006e9cf22fe3b5e676ca6e1581acc409cfeb9
Christoph Mewes <christoph@kubermatic.com>
2025-05-06 18:08:39 +0200

    include SDK

--- hack/ci/verify-go-version.sh
+++ hack/ci/verify-go-version.sh
@@ -38,20 +38,34 @@ go_minor_version() {
   grep -E '^go 1' "$1" | sed -E 's/^go (1\.[0-9]+).*$/\1/'
 }
 
-releaseURL="https://github.com/$REPO_OWNER/$REPO_NAME/raw/refs/heads/$TARGET_BRANCH/go.mod"
-wget -O release.go.mod --no-verbose "$releaseURL"
+go_remote_minor_version() {
+  local branch="$1"
+  local modulePath="$2"
 
-currentVersion="$(go_minor_version go.mod)"
-releaseVersion="$(go_minor_version release.go.mod)"
-rm release.go.mod
+  local url="https://github.com/$REPO_OWNER/$REPO_NAME/raw/refs/heads/$branch/${modulePath}go.mod"
+  wget -O tmp.go.mod --quiet "$url"
 
-echo "$TARGET_BRANCH's Go release: $releaseVersion"
-echo "This PR's Go release......: $currentVersion"
+  go_minor_version tmp.go.mod
+  rm tmp.go.mod
+}
+
+newVersions="main module: $(go_minor_version go.mod)"
+branchVersions="main module: $(go_remote_minor_version "$TARGET_BRANCH" "")"
+
+if [ -d sdk ]; then
+  newVersions="$newVersions, SDK module: $(go_minor_version sdk/go.mod)"
+  branchVersions="$branchVersions, SDK module: $(go_remote_minor_version "$TARGET_BRANCH" "sdk/")"
+fi
+
+echo "Go releases:"
+frmt="  %-15s : %s\n"
+printf "$frmt" "$TARGET_BRANCH" "$branchVersions"
+printf "$frmt" "This PR" "$newVersions"
 echo
 
-if [[ "$currentVersion" != "$releaseVersion" ]]; then
-  echo "Go upgrades are not allowed in release branches."
+if [[ "$branchVersions" != "$newVersions" ]]; then
+  echo "Error: Go upgrades are not allowed in release branches."
   exit 1
 fi
 
-echo "go.mod is valid."
+echo "Go modules are valid."
