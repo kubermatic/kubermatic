a2fcde7f1987152785721960c69cc7c665f15f61
Christoph Mewes <christoph@kubermatic.com>
2025-05-06 17:46:38 +0200

    verify that no accidental Go upgrade happens in release branches

--- hack/ci/verify.sh
+++ hack/ci/verify.sh
@@ -24,13 +24,14 @@ cd $(dirname $0)/../..
 source hack/lib.sh
 
 EXIT_CODE=0
+SUMMARY=
 
 try() {
   local title="$1"
   shift
 
   heading "$title"
-  echo -e "$@\n"
+  echo
 
   start_time=$(date +%s)
 
@@ -42,12 +43,17 @@ try() {
   elapsed_time=$(($(date +%s) - $start_time))
   TEST_NAME="$title" write_junit $exitCode "$elapsed_time"
 
+  local status
   if [[ $exitCode -eq 0 ]]; then
     echo -e "\n[${elapsed_time}s] SUCCESS :)"
+    status=OK
   else
     echo -e "\n[${elapsed_time}s] FAILED."
+    status=FAIL
     EXIT_CODE=1
   fi
+
+  SUMMARY="$SUMMARY\n$(printf "%-35s %s" "$title" "$status")"
 
   git reset --hard --quiet
   git clean --force
@@ -65,10 +71,19 @@ try "Verify Grafana dashboards" ./hack/verify-grafana-dashboards.sh
 try "Verify Prometheus rules" ./hack/verify-prometheus-rules.sh
 try "Verify User Cluster Prometheus rules" ./hack/ci/verify-user-cluster-prometheus-configs.sh
 try "Display Versioning Information" ./hack/versions-gen.sh
+try "Verify Go Version" ./hack/ci/verify-go-version.sh
 
 # -l        list files whose formatting differs from shfmt's
 # -d        error with a diff when the formatting differs
 # -i uint   indent: 0 for tabs (default), >0 for number of spaces
 try "shfmt" shfmt -l -sr -i 2 -d hack
+
+echo
+echo "SUMMARY"
+echo "======="
+echo
+echo "Check                               Result"
+echo -n "------------------------------------------"
+echo -e "$SUMMARY"
 
 exit $EXIT_CODE
