babff61f415950aae4e535f5255656dc5e88e593
Christoph Mewes <christoph@kubermatic.com>
2025-05-06 17:48:27 +0200

    update readmes.sh

--- hack/ci/verify-go-version.sh
+++ hack/ci/verify-go-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+# Copyright 2025 The Kubermatic Kubernetes Platform contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
