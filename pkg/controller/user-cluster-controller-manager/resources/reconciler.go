094271be197bc73429765b36cd9469945ca3c5b9
soer3n <srenhenning@googlemail.com>
2025-05-12 13:44:21 +0200

    add condition to skip tls snapshot cert secret creation it it is disabled
    Signed-off-by: soer3n <srenhenning@googlemail.com>

--- pkg/controller/user-cluster-controller-manager/resources/reconciler.go
+++ pkg/controller/user-cluster-controller-manager/resources/reconciler.go
@@ -863,7 +863,7 @@ func (r *reconciler) reconcileSecrets(ctx context.Context, data reconcileData) e
 		)
 	}
 
-	if data.csiCloudConfig != nil {
+	if !data.cluster.Spec.DisableCSIDriver && data.csiCloudConfig != nil {
 		if r.cloudProvider == kubermaticv1.AzureCloudProvider || r.cloudProvider == kubermaticv1.OpenstackCloudProvider || r.cloudProvider == kubermaticv1.VSphereCloudProvider {
 			creators = append(creators, cloudcontroller.CloudConfig(data.csiCloudConfig, resources.CSICloudConfigSecretName))
 		}
@@ -881,8 +881,10 @@ func (r *reconciler) reconcileSecrets(ctx context.Context, data reconcileData) e
 		}
 	}
 
-	if r.cloudProvider == kubermaticv1.OpenstackCloudProvider || r.cloudProvider == kubermaticv1.DigitaloceanCloudProvider {
-		creators = append(creators, csisnapshotter.TLSServingCertificateReconciler(resources.CSISnapshotValidationWebhookName, data.caCert))
+	if !data.cluster.Spec.DisableCSIDriver {
+		if r.cloudProvider == kubermaticv1.OpenstackCloudProvider || r.cloudProvider == kubermaticv1.DigitaloceanCloudProvider {
+			creators = append(creators, csisnapshotter.TLSServingCertificateReconciler(resources.CSISnapshotValidationWebhookName, data.caCert))
+		}
 	}
 
 	if r.userSSHKeyAgent {
