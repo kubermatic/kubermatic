7ba02da80e57efb485c9a9a919ca45c041888332
Christoph Mewes <christoph@kubermatic.com>
2025-05-21 13:27:01 +0200

    [release/v2.25] fix skip seed validation in installer (#14585)

--- cmd/kubermatic-installer/cmd_deploy.go
+++ cmd/kubermatic-installer/cmd_deploy.go
@@ -242,6 +242,7 @@ func DeployFunc(logger *logrus.Logger, versions kubermaticversion.Versions, opt 
 			Versions:                           versions,
 			SkipCharts:                         opt.SkipCharts,
 			DeployDefaultAppCatalog:            opt.DeployDefaultAppCatalog,
+			SkipSeedValidation:                 opt.SkipSeedValidation,
 		}
 
 		// prepapre Kubernetes and Helm clients
