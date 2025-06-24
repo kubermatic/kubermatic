c974e5915d0cff2bdaa9a78bf03c1d2c8c3a650d
rajaSahil <sahilraja242@gmail.com>
2025-06-24 15:24:39 +0530

    fix(14713): add check for not found
    Signed-off-by: rajaSahil <sahilraja242@gmail.com>

--- pkg/ee/resource-quota/seed-controller/controller.go
+++ pkg/ee/resource-quota/seed-controller/controller.go
@@ -30,7 +30,6 @@ import (
 	"reflect"
 
 	"go.uber.org/zap"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	k8cequality "k8c.io/kubermatic/sdk/v2/apis/equality"
 	kubermaticv1 "k8c.io/kubermatic/sdk/v2/apis/kubermatic/v1"
@@ -38,6 +37,7 @@ import (
 	"k8c.io/kubermatic/v2/pkg/util/workerlabel"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
