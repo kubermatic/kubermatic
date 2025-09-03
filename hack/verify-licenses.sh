5a439ec29e5fcb6e9055c0ca342b5d568cc72e06
archups <archupsawant@gmail.com>
2025-09-04 00:06:33 +0530

    [release/v2.26] Bump Go build images tag in prow jobs
    Signed-off-by: archups <archupsawant@gmail.com>

--- hack/verify-licenses.sh
+++ hack/verify-licenses.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 cd $(dirname $0)/..
 source hack/lib.sh
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.23-node-20-12 containerize ./hack/verify-licenses.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.23-node-20-13 containerize ./hack/verify-licenses.sh
 
 go mod vendor
 
