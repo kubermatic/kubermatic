dd680b78299212e8af046557b091339362fd8dce
Archana Sawant <archupsawant@gmail.com>
2025-09-08 17:09:19 +0530

    [release/v2.26] Bump Go version to 1.23.12 (#14958)
    * [release/v2.26] Bump Go build images tag in prow jobs
    
    Signed-off-by: archups <archupsawant@gmail.com>
    
    * Bump Go build to 1.23.12
    
    Signed-off-by: archups <archupsawant@gmail.com>
    
    * Update the Go Toolchain to 1.23.12
    
    Signed-off-by: archups <archupsawant@gmail.com>
    
    * Bump more files with Go version 1.23.12
    
    Signed-off-by: archups <archupsawant@gmail.com>
    
    ---------
    
    Signed-off-by: archups <archupsawant@gmail.com>

--- hack/update-fixtures.sh
+++ hack/update-fixtures.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 cd $(dirname $0)/..
 source hack/lib.sh
 
-CONTAINERIZE_IMAGE=golang:1.23.10 containerize ./hack/update-fixtures.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.23-node-20-14 containerize ./hack/update-fixtures.sh
 
 echodate "Updating fixtures..."
 make test-update &> /dev/null
