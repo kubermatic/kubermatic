c1d52af832d5a35c9cae203c33bdbe087334bbe7
Waleed Malik <ahmedwaleedmalik@gmail.com>
2025-10-14 17:15:39 +0500

    Remove deprecation notice for defaultComponentSettings in Seed
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- sdk/apis/kubermatic/v1/datacenter.go
+++ sdk/apis/kubermatic/v1/datacenter.go
@@ -294,7 +294,6 @@ type SeedSpec struct {
 	// Optional: MLA allows configuring seed level MLA (Monitoring, Logging & Alerting) stack settings.
 	MLA *SeedMLASettings `json:"mla,omitempty"`
 	// DefaultComponentSettings are default values to set for newly created clusters.
-	// Deprecated: Use DefaultClusterTemplate instead.
 	DefaultComponentSettings ComponentSettings `json:"defaultComponentSettings,omitempty"`
 	// DefaultClusterTemplate is the name of a cluster template of scope "seed" that is used
 	// to default all new created clusters
