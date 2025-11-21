0f5363ba11051b64f9a9b1134175b2a0424728ce
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2025-11-21 13:43:49 +0300

    Use  for  to ensure backward compatibility
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- pkg/resources/machinecontroller/deployment.go
+++ pkg/resources/machinecontroller/deployment.go
@@ -53,7 +53,9 @@ var (
 )
 
 const (
-	Tag = "v1.64.0"
+	// Name is the alias for `resources.MachineControllerContainerName` for backward compatibility.
+	Name = resources.MachineControllerContainerName
+	Tag  = "v1.64.0"
 )
 
 type machinecontrollerData interface {
