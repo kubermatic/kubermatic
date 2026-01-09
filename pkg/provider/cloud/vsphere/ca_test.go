0cf465d18b9b44bc488a651f725ec5e6432042fa
Adonis Murati <adonismurati@gmail.com>
2026-01-09 18:12:56 +0100

    Convert vSphere integration tests to use vcsim simulator instead of real infra
    Signed-off-by: Adonis Murati <adonismurati@gmail.com>

--- pkg/provider/cloud/vsphere/ca_test.go
+++ pkg/provider/cloud/vsphere/ca_test.go
@@ -20,46 +20,97 @@ package vsphere
 
 import (
 	"context"
+	"crypto/tls"
 	"crypto/x509"
 	"strings"
 	"testing"
 
+	"github.com/vmware/govmomi/simulator"
+
+	kubermaticv1 "k8c.io/kubermatic/sdk/v2/apis/kubermatic/v1"
 	"k8c.io/kubermatic/v2/pkg/resources/certificates"
 )
 
 func TestVSphereCA(t *testing.T) {
-	t.Skip()
+	// Set up vcsim simulator
+	model := simulator.VPX()
+
+	err := model.Create()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer model.Remove()
+
+	// Create a TLS server by configuring TLS on the service before creating the server
+	model.Service.TLS = new(tls.Config)
+	server := model.Service.NewServer()
+	defer server.Close()
+
+	username := simulator.DefaultLogin.Username()
+	password, _ := simulator.DefaultLogin.Password()
+
+	serverURL := server.URL.String()
+	serverHost := server.URL.Host
+
+	conn, err := tls.Dial("tcp", serverHost, &tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		t.Fatalf("failed to connect to get server cert: %v", err)
+	}
+	serverCert := conn.ConnectionState().PeerCertificates[0]
+	conn.Close()
+
+	validCAPool := x509.NewCertPool()
+	validCAPool.AddCert(serverCert)
+
 	tests := []struct {
 		name           string
-		expectedError  bool
+		allowInsecure  bool
 		caBundle       *x509.CertPool
+		expectedError  bool
 		errMsgContains string
 	}{
 		{
-			name:           "fail accessing vSphere with fake certificate",
-			expectedError:  true,
+			name:          "succeed with AllowInsecure=true (skip cert validation)",
+			allowInsecure: true,
+			caBundle:      nil,
+			expectedError: false,
+		},
+		{
+			name:           "fail with fake certificate and AllowInsecure=false",
+			allowInsecure:  false,
 			caBundle:       certificates.NewFakeCABundle().CertPool(),
+			expectedError:  true,
 			errMsgContains: "certificate",
 		},
 		{
-			name:          "succeed accessing vSphere with root/host certificate",
+			name:          "succeed with valid server certificate",
+			allowInsecure: false,
+			caBundle:      validCAPool,
 			expectedError: false,
-			caBundle:      nil,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			_, err := GetNetworks(context.Background(), getTestDC(), vSphereUsername, vSpherePassword, test.caBundle)
+			dc := &kubermaticv1.DatacenterSpecVSphere{
+				Datacenter:    "DC0",
+				Endpoint:      strings.TrimSuffix(serverURL, "/sdk"),
+				AllowInsecure: test.allowInsecure,
+			}
+
+			_, err := GetNetworks(context.Background(), dc, username, password, test.caBundle)
+
 			if test.expectedError {
 				if err == nil {
-					t.Fatal("expected err, got nil")
+					t.Fatal("expected error, got nil")
 				}
 				if !strings.Contains(err.Error(), test.errMsgContains) {
-					t.Fatalf("expected err msg %q to contain %q", err.Error(), test.errMsgContains)
+					t.Fatalf("expected error message to contain %q, got: %v", test.errMsgContains, err)
 				}
-			} else if err != nil {
-				t.Fatal(err)
+			} else {
+				if err != nil {
+					t.Fatalf("expected no error, got: %v", err)
+				}
 			}
 		})
 	}
