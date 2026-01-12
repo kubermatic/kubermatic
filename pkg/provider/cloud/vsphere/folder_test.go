db868dae8952c9e15709080663fa2953c59e96d9
Adonis Murati <adonismurati@gmail.com>
2026-01-12 17:42:27 +0100

    refactor some of the code
    Signed-off-by: Adonis Murati <adonismurati@gmail.com>

--- pkg/provider/cloud/vsphere/folder_test.go
+++ pkg/provider/cloud/vsphere/folder_test.go
@@ -20,11 +20,9 @@ package vsphere
 
 import (
 	"context"
-	"strings"
 	"testing"
 
 	"github.com/vmware/govmomi/find"
-	"github.com/vmware/govmomi/simulator"
 
 	kubermaticv1 "k8c.io/kubermatic/sdk/v2/apis/kubermatic/v1"
 
@@ -32,93 +30,76 @@ import (
 )
 
 func TestProvider_GetVMFolders(t *testing.T) {
-	// Set up vcsim simulator
-	model := simulator.VPX()
+	sim := vSphereSimulator{t: t}
+	sim.setUp()
+	defer sim.tearDown()
 
-	err := model.Create()
+	dc := &kubermaticv1.DatacenterSpecVSphere{
+		RootPath: "/DC0/vm",
+	}
+	sim.fillClientInfo(dc)
+
+	ctx := context.Background()
+	session, err := newSession(ctx, dc, sim.username(), sim.password(), nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer model.Remove()
 
-	server := model.Service.NewServer()
-	defer server.Close()
+	finder := find.NewFinder(session.Client.Client, true)
+	datacenter, err := finder.Datacenter(ctx, dc.Datacenter)
+	if err != nil {
+		t.Fatalf("failed to find datacenter: %v", err)
+	}
+	finder.SetDatacenter(datacenter)
 
-	username := simulator.DefaultLogin.Username()
-	password, _ := simulator.DefaultLogin.Password()
-
-	dc := &kubermaticv1.DatacenterSpecVSphere{
-		Datacenter:    "DC0",
-		Endpoint:      strings.TrimSuffix(server.URL.String(), "/sdk"),
-		AllowInsecure: true,
-		RootPath:      "/DC0/vm",
+	vmFolder, err := finder.Folder(ctx, dc.RootPath)
+	if err != nil {
+		t.Fatalf("failed to find vm folder: %v", err)
 	}
 
-	t.Run("create and list folders", func(t *testing.T) {
-		ctx := context.Background()
-		session, err := newSession(ctx, dc, username, password, nil)
-		if err != nil {
-			t.Fatal(err)
+	testFolderName := "kubermatic-e2e-" + rand.String(8)
+	expectedFolderPath := dc.RootPath + "/" + testFolderName
+
+	if _, err = vmFolder.CreateFolder(ctx, testFolderName); err != nil {
+		t.Fatalf("failed to create folder: %v", err)
+	}
+	defer cleanupFolder(t, finder, ctx, expectedFolderPath)
+
+	folders, err := GetVMFolders(ctx, dc, sim.username(), sim.password(), nil)
+	if err != nil {
+		t.Fatalf("GetVMFolders failed: %v", err)
+	}
+
+	if len(folders) == 0 {
+		t.Fatal("expected at least one folder, got none")
+	}
+
+	folderFound := false
+	for _, folder := range folders {
+		if folder.Path == "" {
+			t.Error("folder Path should not be empty")
 		}
-
-		finder := find.NewFinder(session.Client.Client, true)
-		datacenter, err := finder.Datacenter(ctx, dc.Datacenter)
-		if err != nil {
-			t.Fatalf("failed to find datacenter: %v", err)
+		if folder.Path == expectedFolderPath {
+			folderFound = true
 		}
-		finder.SetDatacenter(datacenter)
+	}
 
-		vmFolder, err := finder.Folder(ctx, dc.RootPath)
-		if err != nil {
-			t.Fatalf("failed to find vm folder: %v", err)
-		}
+	if !folderFound {
+		t.Errorf("created folder %s not found in GetVMFolders results", expectedFolderPath)
+	}
+}
 
-		testFolderName := "kubermatic-e2e-" + rand.String(8)
-		expectedFolderPath := dc.RootPath + "/" + testFolderName
+func cleanupFolder(t *testing.T, finder *find.Finder, ctx context.Context, path string) {
+	folder, err := finder.Folder(ctx, path)
+	if err != nil {
+		t.Logf("warning: failed to find test folder for cleanup: %v", err)
+		return
+	}
 
-		_, err = vmFolder.CreateFolder(ctx, testFolderName)
-		if err != nil {
-			t.Fatalf("failed to create folder: %v", err)
-		}
-
-		// Clean up
-		defer func() {
-			testFolder, err := finder.Folder(ctx, expectedFolderPath)
-			if err != nil {
-				t.Logf("warning: failed to find test folder for cleanup: %v", err)
-				return
-			}
-			task, err := testFolder.Destroy(ctx)
-			if err != nil {
-				t.Logf("warning: failed to destroy test folder: %v", err)
-				return
-			}
-			_ = task.Wait(ctx)
-		}()
-
-		folders, err := GetVMFolders(ctx, dc, username, password, nil)
-		if err != nil {
-			t.Fatalf("GetVMFolders failed: %v", err)
-		}
-
-		if len(folders) == 0 {
-			t.Fatal("expected at least one folder, got none")
-		}
-
-		// Verify that we have the folder in the list
-		folderFound := false
-		for _, folder := range folders {
-			if folder.Path == "" {
-				t.Error("folder Path should not be empty")
-			}
-
-			if folder.Path == expectedFolderPath {
-				folderFound = true
-			}
-		}
-
-		if !folderFound {
-			t.Errorf("created folder %s not found in GetVMFolders results", expectedFolderPath)
-		}
-	})
+	task, err := folder.Destroy(ctx)
+	if err != nil {
+		t.Logf("warning: failed to destroy test folder: %v", err)
+		return
+	}
+	_ = task.Wait(ctx)
 }
