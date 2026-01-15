428a1c0b94c07d50892078e43829da6c4a0500b7
Archana Sawant <archupsawant@gmail.com>
2026-01-15 18:00:02 +0530

    Bump Go build image version to 1.25.5 (#15308)
    Signed-off-by: Archana Sawant <archupsawant@gmail.com>

--- hack/update-docs.sh
+++ hack/update-docs.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 cd $(dirname $0)/..
 source hack/lib.sh
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.25-node-22-4 containerize ./hack/update-docs.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.25-node-22-5 containerize ./hack/update-docs.sh
 
 (
   cd docs
