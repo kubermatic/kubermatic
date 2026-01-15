b6619e82e518f21c9146ba083844d73531dae1cb
Mohamed Rafraf <mohamedrafraf99@gmail.com>
2026-01-15 16:37:37 +0100

    Configure helm registry connection options of sys and default apps in Kubermatic Configuration  (TLS skip and plain HTTP)

--- pkg/applicationdefinitions/application_catalog.go
+++ pkg/applicationdefinitions/application_catalog.go
@@ -163,5 +163,9 @@ func updateApplicationDefinition(appDef *appskubermaticv1.ApplicationDefinition,
 		if credentials != nil {
 			appDef.Spec.Versions[i].Template.Source.Helm.Credentials = credentials
 		}
+
+		// Set Insecure and PlainHTTP flags based on the configuration
+		appDef.Spec.Versions[i].Template.Source.Helm.Insecure = &config.Spec.UserCluster.SystemApplications.InsecureSkipTLSVerify
+		appDef.Spec.Versions[i].Template.Source.Helm.PlainHTTP = &config.Spec.UserCluster.SystemApplications.PlainHTTP
 	}
 }
