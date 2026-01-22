4572c52b2c1428624ece780887870d7849c0962b
Adonis Murati <adonismurati@gmail.com>
2026-01-22 11:30:06 +0100

    update RBAC for OSM and machine controller and add EndpointSlices support
    Signed-off-by: Adonis Murati <adonismurati@gmail.com>

--- pkg/resources/machinecontroller/deployment.go
+++ pkg/resources/machinecontroller/deployment.go
@@ -53,7 +53,7 @@ var controllerResourceRequirements = map[string]*corev1.ResourceRequirements{
 const (
 	// Name is the alias for `resources.MachineControllerContainerName` for backward compatibility.
 	Name = resources.MachineControllerContainerName
-	Tag  = "v1.64.1"
+	Tag  = "01ad12aa435b2644bb76081ea7ebc00c451cf0dc"
 )
 
 type machinecontrollerData interface {
