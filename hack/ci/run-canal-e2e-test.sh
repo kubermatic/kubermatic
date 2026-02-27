b166f51e2ec4f58ff929daa84d4ba9a15b6d9f9f
rajaSahil <sahilraja242@gmail.com>
2026-02-27 23:12:15 +0530

    add pre kubermatic canal e2e tests
    Signed-off-by: rajaSahil <sahilraja242@gmail.com>

---
+++ hack/ci/run-canal-e2e-test.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Copyright 2026 The Kubermatic Kubernetes Platform contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+### This script runs Canal CNI e2e tests, verifying Canal networking
+### and kube-proxy proxy mode handling.
+
+set -euo pipefail
+
+cd $(dirname $0)/../..
+source hack/lib.sh
+
+TEST_NAME="Pre-warm Go build cache"
+echodate "Attempting to pre-warm Go build cache"
+
+beforeGocache=$(nowms)
+make download-gocache
+pushElapsed gocache_download_duration_milliseconds $beforeGocache
+
+export KIND_CLUSTER_NAME="${SEED_NAME:-kubermatic}"
+export KUBERMATIC_YAML=hack/ci/testdata/kubermatic.yaml
+source hack/ci/setup-kind-cluster.sh
+
+# gather the logs of all things in the cluster control plane and in the Kubermatic namespace
+protokol --kubeconfig "$KUBECONFIG" --flat --output "$ARTIFACTS/logs/cluster-control-plane" --namespace 'cluster-*' > /dev/null 2>&1 &
+protokol --kubeconfig "$KUBECONFIG" --flat --output "$ARTIFACTS/logs/kubermatic" --namespace kubermatic > /dev/null 2>&1 &
+
+source hack/ci/setup-kubermatic-in-kind.sh
+
+export GIT_HEAD_HASH="$(git rev-parse HEAD | tr -d '\n')"
+
+if [ -z "${E2E_SSH_PUBKEY:-}" ]; then
+  echodate "Getting default SSH pubkey for machines from Vault"
+  retry 5 vault_ci_login
+  E2E_SSH_PUBKEY="$(mktemp)"
+  vault kv get -field=pubkey dev/e2e-machine-controller-ssh-key > "${E2E_SSH_PUBKEY}"
+else
+  E2E_SSH_PUBKEY_CONTENT="${E2E_SSH_PUBKEY}"
+  E2E_SSH_PUBKEY="$(mktemp)"
+  echo "${E2E_SSH_PUBKEY_CONTENT}" > "${E2E_SSH_PUBKEY}"
+fi
+
+echodate "SSH public key will be $(head -c 25 ${E2E_SSH_PUBKEY})...$(tail -c 25 ${E2E_SSH_PUBKEY})"
+
+echodate "Running Canal tests..."
+
+go_test canal_e2e -timeout 1h -tags e2e -v ./pkg/test/e2e/canal \
+  -aws-kkp-datacenter "$AWS_E2E_TESTS_DATACENTER" \
+  -ssh-pub-key "$(cat "$E2E_SSH_PUBKEY")"
+
+echodate "Canal tests done."
+
---
+++ pkg/test/e2e/canal/canal_test.go
@@ -0,0 +1,442 @@
+//go:build e2e
+
+/*
+Copyright 2026 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package canal
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap"
+
+	apiv1 "k8c.io/kubermatic/sdk/v2/api/v1"
+	appskubermaticv1 "k8c.io/kubermatic/sdk/v2/apis/apps.kubermatic/v1"
+	kubermaticv1 "k8c.io/kubermatic/sdk/v2/apis/kubermatic/v1"
+	"k8c.io/kubermatic/v2/pkg/cni"
+	"k8c.io/kubermatic/v2/pkg/log"
+	"k8c.io/kubermatic/v2/pkg/resources"
+	"k8c.io/kubermatic/v2/pkg/test/e2e/jig"
+	"k8c.io/kubermatic/v2/pkg/test/e2e/utils"
+	"k8c.io/kubermatic/v2/pkg/util/wait"
+	yamlutil "k8c.io/kubermatic/v2/pkg/util/yaml"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
+	kyaml "k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/tools/clientcmd"
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlruntimelog "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var (
+	userconfig  string
+	credentials jig.AWSCredentials
+	logOptions  = utils.DefaultLogOptions
+)
+
+const (
+	projectName  = "canal-test-project"
+	canalTestNs  = "canal-test"
+	nginxAppName = "nginx-canal-test"
+)
+
+func init() {
+	flag.StringVar(&userconfig, "userconfig", "", "path to kubeconfig of usercluster")
+	credentials.AddFlags(flag.CommandLine)
+	jig.AddFlags(flag.CommandLine)
+	logOptions.AddFlags(flag.CommandLine)
+}
+
+func TestInExistingCluster(t *testing.T) {
+	if userconfig == "" {
+		t.Logf("kubeconfig for usercluster not provided, test passes vacuously.")
+		t.Logf("to run against an existing usercluster use following command:")
+		t.Logf("go test ./pkg/test/e2e/canal -v -tags e2e -timeout 30m -run TestInExistingCluster -userconfig <USERCLUSTER KUBECONFIG>")
+		return
+	}
+
+	rawLog := log.NewFromOptions(logOptions)
+	logger := rawLog.Sugar()
+
+	config, err := clientcmd.BuildConfigFromFlags("", userconfig)
+	if err != nil {
+		t.Fatalf("failed to build config: %v", err)
+	}
+
+	client, err := ctrlruntimeclient.New(config, ctrlruntimeclient.Options{})
+	if err != nil {
+		t.Fatalf("failed to build ctrlruntime client: %v", err)
+	}
+
+	// set the logger used by sigs.k8s.io/controller-runtime
+	ctrlruntimelog.SetLogger(zapr.NewLogger(rawLog.WithOptions(zap.AddCallerSkip(1))))
+
+	testUserCluster(context.Background(), t, logger, client)
+}
+
+func TestCanalClusters(t *testing.T) {
+	rawLog := log.NewFromOptions(logOptions)
+	logger := rawLog.Sugar()
+	ctx := context.Background()
+
+	if err := credentials.Parse(); err != nil {
+		t.Fatalf("Failed to get credentials: %v", err)
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+	if err != nil {
+		t.Fatalf("failed to build config: %v", err)
+	}
+
+	seedClient, err := ctrlruntimeclient.New(config, ctrlruntimeclient.Options{})
+	if err != nil {
+		t.Fatalf("failed to build ctrlruntime client: %v", err)
+	}
+
+	testAppDefs, err := resourcesFromYaml("./testdata/test-app-def.yaml")
+	if err != nil {
+		t.Fatalf("failed to read objects from yaml: %v", err)
+	}
+	for _, testAppDef := range testAppDefs {
+		if err := seedClient.Create(ctx, testAppDef); err != nil {
+			t.Fatalf("failed to apply resource: %v", err)
+		}
+
+		logger.Infow("Created object", "kind", testAppDef.GetObjectKind(), "name", testAppDef.GetName())
+	}
+
+	// set the logger used by sigs.k8s.io/controller-runtime
+	ctrlruntimelog.SetLogger(zapr.NewLogger(rawLog.WithOptions(zap.AddCallerSkip(1))))
+
+	tests := []struct {
+		name      string
+		proxyMode string
+	}{
+		{
+			name:      "ipvs proxy mode test",
+			proxyMode: resources.IPVSProxyMode,
+		},
+		{
+			name:      "iptables proxy mode test",
+			proxyMode: resources.IPTablesProxyMode,
+		},
+		{
+			name:      "nftables proxy mode test",
+			proxyMode: resources.NFTablesProxyMode,
+		},
+	}
+
+	for _, test := range tests {
+		proxyMode := test.proxyMode
+		t.Run(test.name, func(t *testing.T) {
+			client, cleanup, tLogger, err := createUserCluster(ctx, t, logger.With("proxymode", proxyMode), seedClient, proxyMode)
+			if cleanup != nil {
+				defer cleanup()
+			}
+
+			if err != nil {
+				t.Fatalf("failed to create user cluster: %v", err)
+			}
+
+			testUserCluster(ctx, t, tLogger, client)
+		})
+	}
+}
+
+func testUserCluster(ctx context.Context, t *testing.T, log *zap.SugaredLogger, client ctrlruntimeclient.Client) {
+	log.Info("Waiting for Canal and kube-proxy pods to get ready...")
+	err := waitForPods(ctx, t, log, client, "kube-system", "k8s-app", []string{
+		"calico-kube-controllers",
+		"canal",
+		"kube-proxy",
+	})
+	if err != nil {
+		t.Fatalf("Canal/kube-proxy pods never became ready: %v", err)
+	}
+
+	log.Info("Verifying kube-proxy DaemonSet has config hash annotation...")
+	err = verifyKubeProxyConfigHash(ctx, log, client)
+	if err != nil {
+		t.Fatalf("kube-proxy config hash verification failed: %v", err)
+	}
+
+	log.Info("Verifying kube-proxy ConfigMap exists...")
+	cm := &corev1.ConfigMap{}
+	if err := client.Get(ctx, types.NamespacedName{Name: "kube-proxy", Namespace: "kube-system"}, cm); err != nil {
+		t.Fatalf("failed to get kube-proxy ConfigMap: %v", err)
+	}
+
+	if _, ok := cm.Data["config.conf"]; !ok {
+		t.Fatal("kube-proxy ConfigMap is missing config.conf key")
+	}
+
+	// --- Connectivity checks ---
+	log.Info("Running Canal connectivity tests...")
+	ns := corev1.Namespace{}
+	ns.Name = canalTestNs
+	if err := client.Create(ctx, &ns); err != nil {
+		t.Fatalf("failed to create %q namespace: %v", canalTestNs, err)
+	}
+	defer func() {
+		if err := client.Delete(ctx, &ns); err != nil {
+			log.Errorw("failed to delete connectivity-test namespace", zap.Error(err))
+		}
+	}()
+
+	log = log.With("namespace", canalTestNs)
+	log.Debug("Namespace created")
+
+	installConnectivityCheck(ctx, t, log, client)
+
+	log.Info("Waiting for connectivity-check pods to get ready...")
+	err = waitForPods(ctx, t, log, client, canalTestNs, "name", []string{
+		"echo-a",
+		"echo-b",
+		"echo-b-host",
+		"host-to-b-multi-node-clusterip",
+		"host-to-b-multi-node-headless",
+		"pod-to-a",
+		"pod-to-a-allowed-knp",
+		"pod-to-a-denied-knp",
+		"pod-to-b-multi-node-clusterip",
+		"pod-to-b-multi-node-headless",
+		"pod-to-b-multi-node-nodeport",
+		"pod-to-b-intra-node-nodeport",
+		"pod-to-external-1111",
+		"pod-to-external-fqdn-google",
+	})
+	if err != nil {
+		t.Fatalf("connectivity-check pods never became ready: %v", err)
+	}
+
+	log.Info("Checking for the test nginx ApplicationInstallation...")
+	err = wait.PollLog(ctx, log, 2*time.Second, 5*time.Minute, func(ctx context.Context) (error, error) {
+		app := &appskubermaticv1.ApplicationInstallation{}
+		if err := client.Get(context.Background(), types.NamespacedName{Namespace: metav1.NamespaceSystem, Name: nginxAppName}, app); err != nil {
+			return fmt.Errorf("failed to get nginx ApplicationInstallation in user cluster: %w", err), nil
+		}
+		if app.Status.ApplicationVersion == nil {
+			return fmt.Errorf("application not yet installed: %v", app.Status), nil
+		}
+		return nil, nil
+	})
+	if err != nil {
+		t.Fatalf("Application observe test failed: %v", err)
+	}
+
+	log.Info("Canal cluster tests passed.")
+}
+
+// installConnectivityCheck deploys echo servers, client pods, and services
+// from the testdata manifests into the canal-test namespace.
+func installConnectivityCheck(ctx context.Context, t *testing.T, log *zap.SugaredLogger, client ctrlruntimeclient.Client) {
+	objs, err := resourcesFromYaml("./testdata/connectivity-check.yaml")
+	if err != nil {
+		t.Fatalf("failed to read connectivity-check manifests: %v", err)
+	}
+
+	for _, obj := range objs {
+		obj.SetNamespace(canalTestNs)
+		if err := client.Create(ctx, obj); err != nil {
+			t.Fatalf("failed to apply resource: %v", err)
+		}
+
+		log.Debugw("Created object", "kind", obj.GetObjectKind(), "name", obj.GetName())
+	}
+}
+
+// verifyKubeProxyConfigHash checks that the kube-proxy DaemonSet pod template
+// has the checksum/config annotation set (non-empty), which ensures pods will
+// be restarted when the kube-proxy ConfigMap changes.
+func verifyKubeProxyConfigHash(ctx context.Context, log *zap.SugaredLogger, client ctrlruntimeclient.Client) error {
+	return wait.PollLog(ctx, log, 5*time.Second, 3*time.Minute, func(ctx context.Context) (error, error) {
+		ds := &appsv1.DaemonSet{}
+		if err := client.Get(ctx, types.NamespacedName{Name: "kube-proxy", Namespace: "kube-system"}, ds); err != nil {
+			return fmt.Errorf("failed to get kube-proxy DaemonSet: %w", err), nil
+		}
+
+		annotations := ds.Spec.Template.Annotations
+		if annotations == nil {
+			return errors.New("kube-proxy DaemonSet pod template has no annotations"), nil
+		}
+
+		hash, ok := annotations["checksum/config"]
+		if !ok {
+			return errors.New("kube-proxy DaemonSet pod template is missing checksum/config annotation"), nil
+		}
+
+		if hash == "" {
+			return errors.New("kube-proxy DaemonSet checksum/config annotation is empty"), nil
+		}
+
+		log.Infow("kube-proxy config hash verified", "hash", hash)
+		return nil, nil
+	})
+}
+
+func waitForPods(ctx context.Context, t *testing.T, log *zap.SugaredLogger, client ctrlruntimeclient.Client, namespace string, key string, names []string) error {
+	log = log.With("namespace", namespace)
+
+	r, err := labels.NewRequirement(key, selection.In, names)
+	if err != nil {
+		return fmt.Errorf("failed to build requirement: %w", err)
+	}
+	l := labels.NewSelector().Add(*r)
+
+	return wait.PollLog(ctx, log, 5*time.Second, 5*time.Minute, func(ctx context.Context) (error, error) {
+		pods := corev1.PodList{}
+		err = client.List(ctx, &pods, ctrlruntimeclient.InNamespace(namespace), ctrlruntimeclient.MatchingLabelsSelector{Selector: l})
+		if err != nil {
+			return fmt.Errorf("failed to list Pods: %w", err), nil
+		}
+
+		if len(pods.Items) == 0 {
+			return errors.New("no Pods found"), nil
+		}
+
+		unready := sets.New[string]()
+		for _, pod := range pods.Items {
+			ready := false
+			for _, c := range pod.Status.Conditions {
+				if c.Type == corev1.ContainersReady {
+					ready = c.Status == corev1.ConditionTrue
+				}
+			}
+
+			if !ready {
+				unready.Insert(pod.Name)
+			}
+		}
+
+		if unready.Len() > 0 {
+			return fmt.Errorf("not all Pods are ready: %v", sets.List(unready)), nil
+		}
+
+		return nil, nil
+	})
+}
+
+func resourcesFromYaml(filename string) ([]ctrlruntimeclient.Object, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	manifests, err := yamlutil.ParseMultipleDocuments(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+
+	var objs []ctrlruntimeclient.Object
+	for _, m := range manifests {
+		obj := &unstructured.Unstructured{}
+		if err := kyaml.NewYAMLOrJSONDecoder(bytes.NewReader(m.Raw), 1024).Decode(obj); err != nil {
+			return nil, err
+		}
+
+		objs = append(objs, obj)
+	}
+
+	return objs, nil
+}
+
+func getTestApplicationAnnotation(appName string) ([]byte, error) {
+	var values json.RawMessage
+	err := json.Unmarshal([]byte(`{"controller":{"ingressClass":"test-nginx"}}`), &values)
+	if err != nil {
+		return nil, err
+	}
+
+	app := apiv1.Application{
+		ObjectMeta: apiv1.ObjectMeta{
+			Name: appName,
+		},
+		Spec: apiv1.ApplicationSpec{
+			Namespace: apiv1.NamespaceSpec{
+				Name: metav1.NamespaceSystem,
+			},
+			ApplicationRef: apiv1.ApplicationRef{
+				Name:    appName,
+				Version: "1.8.1",
+			},
+			Values: values,
+		},
+	}
+	applications := []apiv1.Application{app}
+	data, err := json.Marshal(applications)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// createUserCluster creates a user cluster on AWS with Canal CNI.
+func createUserCluster(
+	ctx context.Context,
+	t *testing.T,
+	log *zap.SugaredLogger,
+	masterClient ctrlruntimeclient.Client,
+	proxyMode string,
+) (ctrlruntimeclient.Client, func(), *zap.SugaredLogger, error) {
+	testAppAnnotation, err := getTestApplicationAnnotation(nginxAppName)
+	if err != nil {
+		return nil, nil, log, fmt.Errorf("failed to prepare test application: %w", err)
+	}
+
+	testJig := jig.NewAWSCluster(masterClient, log, credentials, 2, nil)
+	testJig.ProjectJig.WithHumanReadableName(projectName)
+	testJig.ClusterJig.
+		WithTestName("canal").
+		WithProxyMode(proxyMode).
+		WithKonnectivity(true).
+		WithCNIPlugin(&kubermaticv1.CNIPluginSettings{
+			Type:    kubermaticv1.CNIPluginTypeCanal,
+			Version: cni.GetDefaultCNIPluginVersion(kubermaticv1.CNIPluginTypeCanal),
+		}).
+		WithAnnotations(map[string]string{
+			kubermaticv1.InitialApplicationInstallationsRequestAnnotation: string(testAppAnnotation),
+		})
+
+	cleanup := func() {
+		testJig.Cleanup(ctx, t, true)
+	}
+
+	// let the magic happen
+	if _, _, err := testJig.Setup(ctx, jig.WaitForReadyPods); err != nil {
+		return nil, cleanup, log, fmt.Errorf("failed to setup test environment: %w", err)
+	}
+
+	clusterClient, err := testJig.ClusterClient(ctx)
+
+	return clusterClient, cleanup, log, err
+}
