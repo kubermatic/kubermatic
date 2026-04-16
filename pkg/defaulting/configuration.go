ac35bc6769c6044a45086e5f6acaa80a22035045
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2026-04-16 17:50:28 +0300

    add support for k8s patch releases v1.34.7 and v1.33.11
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- pkg/defaulting/configuration.go
+++ pkg/defaulting/configuration.go
@@ -226,7 +226,7 @@ var (
 	}
 
 	DefaultKubernetesVersioning = kubermaticv1.KubermaticVersioningConfiguration{
-		Default: semver.NewSemverOrDie("v1.33.10"),
+		Default: semver.NewSemverOrDie("v1.33.11"),
 		// NB: We keep all patch releases that we supported, even if there's
 		// an auto-upgrade rule in place. That's because removing a patch
 		// release from this slice can break reconciliation loop for clusters
@@ -264,6 +264,7 @@ var (
 			newSemver("v1.33.8"),
 			newSemver("v1.33.9"),
 			newSemver("v1.33.10"),
+			newSemver("v1.33.11"),
 			// Kubernetes 1.34
 			newSemver("v1.34.1"),
 			newSemver("v1.34.2"),
@@ -271,6 +272,7 @@ var (
 			newSemver("v1.34.4"),
 			newSemver("v1.34.5"),
 			newSemver("v1.34.6"),
+			newSemver("v1.34.7"),
 		},
 		Updates: []kubermaticv1.Update{
 			// ======= 1.31 =======
