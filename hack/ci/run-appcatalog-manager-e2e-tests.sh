7253a24158312b97220efd50d047a48ad3a8d533
Adonis Murati <adonismurati@gmail.com>
2026-05-14 10:37:39 +0200

    Fix app catalog e2e
    Signed-off-by: Adonis Murati <adonismurati@gmail.com>

--- hack/ci/run-appcatalog-manager-e2e-tests.sh
+++ hack/ci/run-appcatalog-manager-e2e-tests.sh
@@ -51,7 +51,7 @@ source hack/ci/setup-kind-cluster.sh
 protokol --kubeconfig "$KUBECONFIG" --flat --output "$ARTIFACTS/logs/cluster-control-plane" --namespace 'cluster-*' > /dev/null 2>&1 &
 protokol --kubeconfig "$KUBECONFIG" --flat --output "$ARTIFACTS/logs/kubermatic" --namespace kubermatic > /dev/null 2>&1 &
 
-kubectl apply -f pkg/crd/k8c.io
+create_crds_for_fresh_cluster pkg/crd/k8c.io
 source hack/ci/setup-kubermatic-in-kind.sh
 
 export GIT_HEAD_HASH="$(git rev-parse HEAD | tr -d '\n')"
