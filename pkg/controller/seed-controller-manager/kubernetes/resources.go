7c5483f2eaded9007ef9505921a8eed90c7b020b
Adonis Murati <adonismurati@gmail.com>
2026-06-19 11:11:04 +0200

    Use helper for better readibility to perform port validation for OIDC issuer URLs
    Signed-off-by: Adonis Murati <adonismurati@gmail.com>
    (cherry picked from commit 5d28b85d723a4b38eb8edd29493edf3dc8e0f7ff)

--- pkg/controller/seed-controller-manager/kubernetes/resources.go
+++ pkg/controller/seed-controller-manager/kubernetes/resources.go
@@ -72,6 +72,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/clientcmd"
 	certutil "k8s.io/client-go/util/cert"
@@ -1488,8 +1489,8 @@ func urlsToOIDCIssuerDestinationsWithResolver(ctx context.Context, urls []string
 
 func oidcIssuerURLPort(u *url.URL) (int32, error) {
 	if port := u.Port(); port != "" {
-		parsedPort, err := strconv.ParseInt(port, 10, 32)
-		if err != nil || parsedPort < 1 || parsedPort > 65535 {
+		parsedPort, err := strconv.Atoi(port)
+		if err != nil || len(validation.IsValidPortNum(parsedPort)) > 0 {
 			return 0, fmt.Errorf("invalid port %q", port)
 		}
 		return int32(parsedPort), nil
