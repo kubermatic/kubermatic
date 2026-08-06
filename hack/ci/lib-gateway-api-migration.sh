13f4e8b8380905b82f6cb0f47ea5e3cfb4334cc3
Archana Sawant <archupsawant@gmail.com>
2026-08-06 16:04:11 +0530

    Pin helm v3 binary in v2.30 installer calls
    Signed-off-by: Archana Sawant <archupsawant@gmail.com>

--- hack/ci/lib-gateway-api-migration.sh
+++ hack/ci/lib-gateway-api-migration.sh
@@ -340,6 +340,7 @@ deploy_kkp_v230_with_gateway_api_flag() {
     --storageclass copy-default \
     --config "${KUBERMATIC_CONFIG}" \
     --helm-values "${HELM_VALUES_FILE_V230_GATEWAY_API}" \
+    --helm-binary "${KKP_LEGACY_HELM_BINARY}" \
     --migrate-gateway-api
 
   retry 10 check_all_deployments_ready kubermatic
@@ -354,7 +355,8 @@ deploy_kkp_v230_without_gateway_api_flag() {
     --charts-directory "${INSTALL_DIR_V230}/charts" \
     --storageclass copy-default \
     --config "${KUBERMATIC_CONFIG}" \
-    --helm-values "${HELM_VALUES_FILE_V230_NGINX}"
+    --helm-values "${HELM_VALUES_FILE_V230_NGINX}" \
+    --helm-binary "${KKP_LEGACY_HELM_BINARY}"
 
   retry 10 check_all_deployments_ready kubermatic
   retry 10 check_all_deployments_ready nginx-ingress-controller
