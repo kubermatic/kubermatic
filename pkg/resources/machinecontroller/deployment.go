9462312617afcbea120cf8571f39ccafb6b391af
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2026-08-20 00:45:27 +0300

    Bump machine-controller to v1.66.2 (#16279)
    * Bump machine-controller to v1.66.2
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>
    
    * Update test fixtures for machine-controller v1.66.2
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- pkg/resources/machinecontroller/deployment.go
+++ pkg/resources/machinecontroller/deployment.go
@@ -53,7 +53,7 @@ var controllerResourceRequirements = map[string]*corev1.ResourceRequirements{
 const (
 	// Name is the alias for `resources.MachineControllerContainerName` for backward compatibility.
 	Name = resources.MachineControllerContainerName
-	Tag  = "v1.66.1"
+	Tag  = "v1.66.2"
 )
 
 type machinecontrollerData interface {
