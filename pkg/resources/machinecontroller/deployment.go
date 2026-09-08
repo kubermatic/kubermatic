1a84f57976a832dd3c2f88eded140521cba91a7a
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2026-09-09 00:03:33 +0300

    [release/v2.30] Bump machine-controller to v1.65.7 and operating-system-manager to v1.10.9 (#16348)
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- pkg/resources/machinecontroller/deployment.go
+++ pkg/resources/machinecontroller/deployment.go
@@ -53,7 +53,7 @@ var controllerResourceRequirements = map[string]*corev1.ResourceRequirements{
 const (
 	// Name is the alias for `resources.MachineControllerContainerName` for backward compatibility.
 	Name = resources.MachineControllerContainerName
-	Tag  = "v1.65.6"
+	Tag  = "v1.65.7"
 )
 
 type machinecontrollerData interface {
