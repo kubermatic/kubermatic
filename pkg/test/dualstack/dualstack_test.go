9310b78566102c2744db0be0241ef4e14b0211e1
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2026-09-08 15:48:31 +0300

    Update the Kubermatic util image to 2.10.0 (#16337)
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- pkg/test/dualstack/dualstack_test.go
+++ pkg/test/dualstack/dualstack_test.go
@@ -340,7 +340,7 @@ func egressValidatorPod(ipVersion int) *corev1.Pod {
 			Containers: []corev1.Container{
 				{
 					Name:  fmt.Sprintf("egress-validator-%d-container", ipVersion),
-					Image: "quay.io/kubermatic/util:2.8.1",
+					Image: "quay.io/kubermatic/util:2.10.0",
 					Command: []string{
 						"/bin/sh",
 						"-c",
