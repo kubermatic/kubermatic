3d8e2302de612ec9edff5cee13d8c0c7f6bebc9f
Burak Sekili <32663655+buraksekili@users.noreply.github.com>
2026-09-19 16:31:23 +0300

    tolerate informer cache lag when labeling freshly created namespaces
    Signed-off-by: Burak Sekili <32663655+buraksekili@users.noreply.github.com>

--- pkg/install/util/kubernetes.go
+++ pkg/install/util/kubernetes.go
@@ -18,11 +18,14 @@ package util
 
 import (
 	"context"
+	"time"
 
 	"github.com/sirupsen/logrus"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -40,7 +43,16 @@ func EnsureNamespace(ctx context.Context, log logrus.FieldLogger, kubeClient ctr
 
 func EnsureNamespaceLabel(ctx context.Context, kubeClient ctrlruntimeclient.Client, namespace, key, value string) error {
 	ns := &corev1.Namespace{}
-	if err := kubeClient.Get(ctx, ctrlruntimeclient.ObjectKey{Name: namespace}, ns); err != nil {
+
+	if err := wait.PollUntilContextTimeout(ctx, 250*time.Millisecond, 15*time.Second, true, func(ctx context.Context) (bool, error) {
+		if err := kubeClient.Get(ctx, ctrlruntimeclient.ObjectKey{Name: namespace}, ns); err != nil {
+			if apierrors.IsNotFound(err) {
+				return false, nil
+			}
+			return false, err
+		}
+		return true, nil
+	}); err != nil {
 		return err
 	}
 
